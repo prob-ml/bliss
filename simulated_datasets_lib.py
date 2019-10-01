fa315829e3831f8777fc3dd541f5ddb01d4713b4
Runjing <runjing_liu@berkeley.edu>
2019-10-01 15:04:07 -0700

    forgot to change interpolation back

--- simulated_datasets_lib.py
+++ simulated_datasets_lib.py
@@ -73,10 +73,10 @@ def plot_one_star(slen, locs, psf, cached_grid = None):
     locs = (locs - 0.5) * 2
     grid_loc = grid.view(1, slen, slen, 2) - locs.view(batchsize, 1, 1, 2)
 
-    star = F.grid_sample(psf.expand(batchsize, 1, -1, -1), grid_loc, mode = 'nearest')
+    star = F.grid_sample(psf.expand(batchsize, 1, -1, -1), grid_loc)
 
     # normalize so one star still sums to 1
-    return star # / star.sum(3, keepdim=True).sum(2, keepdim=True)
+    return star # star.sum(3, keepdim=True).sum(2, keepdim=True)
 
 def plot_multiple_stars(slen, locs, n_stars, fluxes, psf, cached_grid = None):
     # locs is batchsize x max_stars x x_loc x y_loc
