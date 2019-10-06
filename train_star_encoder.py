c4bfc7601e3260b4a531c053e72ff51b8266b370
Runjing <runjing_liu@berkeley.edu>
2019-10-06 11:07:24 -0700

    try on even smaller patches

--- train_star_encoder.py
+++ train_star_encoder.py
@@ -68,7 +68,7 @@ loader = torch.utils.data.DataLoader(
 star_encoder = starnet_vae_lib.StarEncoder(full_slen = data_params['slen'],
                                            stamp_slen = 9,
                                            step = 4,
-                                           edge_padding = 2,
+                                           edge_padding = 3,
                                            n_bands = 1,
                                            max_detections = 8)
 
@@ -126,12 +126,12 @@ for epoch in range(n_epochs):
         print('**** test loss: {:.3f}; counter loss: {:.3f}; locs loss: {:.3f}; fluxes loss: {:.3f} ****'.format(\
             test_loss, test_counter_loss, test_locs_loss, test_fluxes_loss))
 
-        outfile = './fits/starnet_invKL_encoder_batched_images_2000stars_smallpatch4'
+        outfile = './fits/starnet_invKL_encoder_batched_images_2000stars_smallpatch5'
         print("writing the encoder parameters to " + outfile)
         torch.save(star_encoder.state_dict(), outfile)
 
         test_losses[:, epoch // print_every] = np.array([test_loss, test_counter_loss, test_locs_loss, test_fluxes_loss])
-        np.savetxt('./fits/test_losses_2000stars_smallpatch4', test_losses)
+        np.savetxt('./fits/test_losses_2000stars_smallpatch5', test_losses)
 
 
 print('done')
