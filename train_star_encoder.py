184f43952a90d6e700636c92504b886421d48ac0
Runjing <runjing_liu@berkeley.edu>
2019-10-16 10:53:56 -0700

    train with new implementation

--- train_star_encoder.py
+++ train_star_encoder.py
@@ -66,11 +66,11 @@ loader = torch.utils.data.DataLoader(
 
 # define VAE
 star_encoder = starnet_vae_lib.StarEncoder(full_slen = data_params['slen'],
-                                           stamp_slen = 7,
+                                           stamp_slen = 9,
                                            step = 2,
-                                           edge_padding = 2.5,
+                                           edge_padding = 3,
                                            n_bands = 1,
-                                           max_detections = 2)
+                                           max_detections = 4)
 
 star_encoder.to(device)
 
@@ -126,12 +126,12 @@ for epoch in range(n_epochs):
         print('**** test loss: {:.3f}; counter loss: {:.3f}; locs loss: {:.3f}; fluxes loss: {:.3f} ****'.format(\
             test_loss, test_counter_loss, test_locs_loss, test_fluxes_loss))
 
-        outfile = './fits/starnet_invKL_encoder-10152019-one_pixel'
+        outfile = './fits/testing'
         print("writing the encoder parameters to " + outfile)
         torch.save(star_encoder.state_dict(), outfile)
 
         test_losses[:, epoch // print_every] = np.array([test_loss, test_counter_loss, test_locs_loss, test_fluxes_loss])
-        np.savetxt('./fits/test_losses-10152019-one_pixel', test_losses)
+        np.savetxt('./fits/test_losses-testing', test_losses)
 
 
 print('done')
