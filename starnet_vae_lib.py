049259c4bded996eef68731e62c036ccf13b3014
Ubuntu <ubuntu@ip-172-31-37-166.us-west-2.compute.internal>
2019-10-17 00:16:38 +0000

    .float()

--- starnet_vae_lib.py
+++ starnet_vae_lib.py
@@ -41,7 +41,7 @@ class StarEncoder(nn.Module):
         # TODO: make this variable for mean stars
         mean_stars_per_patch = 0.4
         poisson_dstr = poisson.Poisson(rate = mean_stars_per_patch)
-        inv_weights = torch.exp(poisson_dstr.log_prob(torch.arange(max_detections + 1)))
+        inv_weights = torch.exp(poisson_dstr.log_prob(torch.arange(max_detections + 1).float()))
 
         self.weights = (inv_weights.max() / inv_weights).to(device)
 
