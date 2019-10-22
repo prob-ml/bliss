7a56fd495f8398cbe4933659114ce3d9e4b6c2e5
Runjing <runjing_liu@berkeley.edu>
2019-10-22 14:25:45 -0700

    experimenting again with psf transform

--- train_psf_transform.py
+++ train_psf_transform.py
@@ -48,19 +48,20 @@ star_encoder = starnet_vae_lib.StarEncoder(full_slen = full_image.shape[-1],
                                            edge_padding = 3,
                                            n_bands = 1,
                                            max_detections = 4)
-
-# star_encoder.load_state_dict(torch.load('./fits/starnet_invKL_encoder-10092019-reweighted_samples',
-#                                map_location=lambda storage, loc: storage))
-# star_encoder.to(device)
+encoder_file = './fits/starnet-10172019-no_reweighting'
+star_encoder.load_state_dict(torch.load(encoder_file,
+							   map_location=lambda storage, loc: storage));
+star_encoder.to(device);
+star_encoder.eval();
 
 # We want to set batchnorm to eval
 # https://discuss.pytorch.org/t/freeze-batchnorm-layer-lead-to-nan/8385
-def set_bn_eval(m):
-    classname = m.__class__.__name__
-    if classname.find('BatchNorm') != -1:
-      m.eval()
-
-star_encoder.apply(set_bn_eval)
+# def set_bn_eval(m):
+#     classname = m.__class__.__name__
+#     if classname.find('BatchNorm') != -1:
+#       m.eval()
+#
+# star_encoder.apply(set_bn_eval)
 
 # define transform
 psf_transform = PsfLocalTransform(torch.Tensor(simulator.psf_og).to(device),
@@ -91,9 +92,12 @@ for epoch in range(n_epochs):
 
 	# get params: these normally would be the variational parameters.
 	# using true parameters atm
-	locs = true_full_locs
-	fluxes = true_full_fluxes
-	n_stars = torch.sum(true_full_fluxes > 0, dim = 1)
+	# locs = true_full_locs
+	# fluxes = true_full_fluxes
+	# n_stars = torch.sum(true_full_fluxes > 0, dim = 1)
+	locs, fluxes, n_stars = \
+		sample_star_encoder(star_encoder, full_image, full_background,
+								n_samples = 100, return_map = False)
 
 	psf_trained = psf_transform.forward()
 
