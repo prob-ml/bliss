b98d23e9faa231629f43238c74ec76f968a4d723
Ubuntu <ubuntu@ip-172-31-37-166.us-west-2.compute.internal>
2019-10-24 01:01:07 +0000

    experimenting

--- train_wake_sleep.py
+++ train_wake_sleep.py
@@ -110,7 +110,7 @@ for iteration in range(0, 6):
 
     # get optimizer
     psf_lr = 0.1 / (1 + 40 * iteration)
-    psf_optimizer = optim.SGD([
+    psf_optimizer = optim.Adam([
                         {'params': psf_transform.parameters(),
                         'lr': psf_lr}], weight_decay = 1e-5)
 
