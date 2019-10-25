fd1abb44c05641c1354314c823907582916f6d87
Runjing <runjing_liu@berkeley.edu>
2019-10-25 16:16:53 -0700

    maybe train psf for longer

--- sync_results.sh
+++ sync_results.sh
@@ -1,4 +1,4 @@
 rsync -avL --progress -e 'ssh -i ../../bryans_key_oregon.pem' \
-   ubuntu@ec2-35-161-65-132.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/DeblendingStarfields/fits/.\
+   ubuntu@ec2-34-218-59-115.us-west-2.compute.amazonaws.com:/home/ubuntu/astronomy/DeblendingStarfields/fits/.\
    /home/runjing_liu/Documents/astronomy/DeblendingStarfields/fits/.
 
--- train_wake_sleep.py
+++ train_wake_sleep.py
@@ -114,7 +114,7 @@ for iteration in range(0, 6):
 
     run_wake(full_image, full_background, star_encoder, psf_transform,
                     optimizer = psf_optimizer,
-                    n_epochs = 81,
+                    n_epochs = 161,
                     n_samples = 50,
                     out_filename = filename + '-psf_transform',
                     iteration = iteration,
