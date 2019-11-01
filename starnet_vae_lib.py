73e45eb92ba6f3d5e1ad38e27073676343ba9c85
Runjing <runjing_liu@berkeley.edu>
2019-11-01 16:10:35 -0700

    debugging issues with new prior

--- starnet_vae_lib.py
+++ starnet_vae_lib.py
@@ -143,8 +143,9 @@ class StarEncoder(nn.Module):
     ############################
     def _forward_to_pooled_hidden(self, image, background):
         # forward to the layer that is shared by all n_stars
-        log_img = image - background
-        # log_img = torch.log(image - background + 1000)
+        assert torch.all((image - background) > 0.)
+        log_img = torch.log(image - background + 1000)
+
 
         # means = log_img.view(image.shape[0], self.n_bands, -1).mean(-1)
         # stds = log_img.view(image.shape[0], self.n_bands, -1).std(-1)
