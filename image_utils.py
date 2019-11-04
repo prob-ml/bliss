6d073fc95069c02e9225da36ef2da233ba1b9d7d
Runjing <runjing_liu@berkeley.edu>
2019-11-04 13:56:10 -0800

    this is much faster?

--- image_utils.py
+++ image_utils.py
@@ -151,7 +151,7 @@ def get_params_in_patches(tile_coords, locs, fluxes, slen, subimage_slen,
     is_on_array = which_locs_array.view(subimage_batchsize, max_stars).type(torch.bool).to(device)
     n_stars = is_on_array.float().sum(dim = 1).type(torch.LongTensor).to(device)
 
-    is_on_array_sorted = utils.get_is_on_from_n_stars(n_stars, max(n_stars))
+    is_on_array_sorted = utils.get_is_on_from_n_stars(n_stars, n_stars.max())
 
     indx = is_on_array_sorted.clone()
     indx[indx == 1] = torch.nonzero(is_on_array)[:, 1]
@@ -190,7 +190,7 @@ def get_full_params_from_patch_params(patch_locs, patch_fluxes,
 
     n_stars = torch.sum(fluxes_full_image > 0, dim = 1)
 
-    is_on_array_full = utils.get_is_on_from_n_stars(n_stars, max(n_stars))
+    is_on_array_full = utils.get_is_on_from_n_stars(n_stars, n_stars.max())
     indx = is_on_array_full.clone()
     indx[indx == 1] = torch.nonzero(fluxes_full_image)[:, 1]
 
