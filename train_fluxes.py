38700e99aaaa20608cc0e1f04754fa9ef3cef938
Runjing <runjing_liu@berkeley.edu>
2019-11-05 14:09:39 -0800

    foo

--- train_fluxes.py
+++ train_fluxes.py
@@ -126,23 +126,23 @@ for epoch in range(n_epochs):
 
 	test_losses[epoch] = avg_loss
 
-	if (epoch % print_every) == 0:
+    if (epoch % print_every) == 0:
         outfile = './fits/results_11042019/starnet_testing-11042019';
         print("writing the encoder parameters to " + outfile);
         torch.save(star_encoder.state_dict(), outfile);
 
         locs, fluxes, n_stars = \
-            star_encoder.sample_star_encoder(full_image, full_background,
-                                                n_samples = 1,
-                                                return_map = True,
-                                                return_log_q = False,
-                                                training = True)[0:3]
+        star_encoder.sample_star_encoder(full_image, full_background,
+                                            n_samples = 1,
+                                            return_map = True,
+                                            return_log_q = False,
+                                            training = True)[0:3]
 
         loss = get_psf_loss(full_image, full_background,
-                                locs.detach(), fluxes.detach(), n_stars.detach(),
-                                psf,
-                                pad = 5,
-                                grid = cached_grid)[1]
+                            locs.detach(), fluxes.detach(), n_stars.detach(),
+                            psf,
+                            pad = 5,
+                            grid = cached_grid)[1]
 
         print(locs.mean())
         print(fluxes.mean())
