e9bc7724078ca970f4aecb916d572b6b9cc07b96
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2019-11-17 22:27:54 -0500

    The galsim dataset and the original ds now agree. More changes will come to the galbasic dataset to make it more realistic.

--- datasets.py
+++ datasets.py
@@ -7,22 +7,21 @@ import scipy.stats as stats
 from torch.utils.data import Dataset
 from WeakLensingDeblending import descwl
 
-#sky level and exposure, etc. for LSST 
+
+# sky level and exposure, etc. for LSST
 
 class GalBasic(Dataset):
 
     def __init__(self, slen, mean_galaxies=1, min_galaxies=1, max_galaxies=1,
-                 num_images=1600, num_bands=1, padding=3, centered=True,
-                 brightness=30000, survey_name='lsst', 
-                 snr=50, sky=700):
+                 num_images=1000, num_bands=1, padding=3, centered=True,
+                 flux=50000, survey_name='lsst',
+                 snr=200, sky=700):
         """
         This class uses galsim.
         """
-        super(GalBasic, self).__init__() #runs init of the super class. 
+        super(GalBasic, self).__init__()  # runs init of the super class.
 
-
-
-        self.slen = slen #number of pixel dimensions. 
+        self.slen = slen  # number of pixel dimensions.
         self.mean_galaxies = mean_galaxies
         self.min_galaxies = min_galaxies
         self.max_galaxies = max_galaxies
@@ -30,82 +29,78 @@ class GalBasic(Dataset):
         self.num_bands = num_bands
         self.padding = padding
         self.centered = centered
-        self.brightness = brightness
-        self.snr=snr
+        self.snr = snr
 
-        #survey specific parameters. 
-        # self.survey_name = survey_name 
+        # survey specific parameters.
+        # self.survey_name = survey_name
         # self.survey = descwl.survey.Survey(no_analysis=True, survey_name=self.survey_name, filter_band='r')
         # self.sky = self.survey.mean_sky_level
-        self.sky=sky
-        self.pixel_scale=0.2
+        self.sky = sky
+        self.pixel_scale = 0.2
+        self.flux = flux
 
-        if self.num_bands > 1 or not self.centered or survey_name != 'lsst': 
+        if self.num_bands > 1 or not self.centered or survey_name != 'lsst':
             raise NotImplementedError("Not yet implemented multiple bands, uncentering")
 
     def __len__(self):
         return self.num_images
 
     def __getitem__(self, idx):
-        #right now this completely ignores the index and returns some random Gaussian galaxy using galsim.
-        #scale is LSST scale.
-
+        # right now this completely ignores the index and returns some random Gaussian galaxy using galsim.
+        # scale is LSST scale.
 
         poisson_galaxies = np.random.poisson(self.mean_galaxies)
         num_galaxies = np.maximum(np.minimum(poisson_galaxies, self.max_galaxies), self.min_galaxies)
 
-        #get random galaxy parameters. 
+        # get random galaxy parameters.
 
-        #do not want too small of sigma to avoid pixel galaxies. 
-        sigma = max(np.random.sample() * self.pixel_scale*2, self.pixel_scale*.75) 
+        # do not want too small of sigma to avoid pixel galaxies.
+        # we can use the same size for everything for now.
+        sigma = self.slen*self.pixel_scale / 8
 
-        r = min(np.random.sample(), 0.99) #magnitude needs to be < 1 . 
-        theta = np.random.sample()* np.pi * 2
-        e1,e2 = r*np.cos(theta), r*np.sin(theta) 
+        r = min(np.random.sample(), 0.99)  # magnitude needs to be < 1 .
+        theta = np.random.sample() * np.pi * 2
+        e1, e2 = min(r * np.cos(theta),0.5), min(r * np.sin(theta),0.5)
 
-
-
-        gal = galsim.Gaussian(flux=1000, sigma=sigma)
+        gal = galsim.Gaussian(flux=self.flux, sigma=sigma)
         gal = gal.shear(e1=e1, e2=e2)
-        img = gal.drawImage(nx=self.slen, ny=self.slen, scale=self.pixel_scale, 
-                            method='phot', poisson_flux=True)
+        img = gal.drawImage(nx=self.slen, ny=self.slen, scale=self.pixel_scale,
+                            method='auto')
+                            # , poisson_flux=True)
 
         noisy_img = img.copy()
 
-        #add noise. 
+        # add noise.
         rng = galsim.BaseDeviate(0)
         noise = galsim.GaussianNoise(rng=rng)
-        noise = galsim.PoissonNoise(rng = rng, sky_level = self.sky)
+        noise = galsim.PoissonNoise(rng=rng, sky_level=self.sky)
         _ = noisy_img.addNoiseSNR(noise, self.snr, preserve_flux=True)
 
-
-        #obtain background 
-        nimage = noisy_img.array
+        # obtain background
+        noisy_arr = noisy_img.array
         image = np.zeros((self.num_bands, self.slen, self.slen), dtype=np.float32)
-        image[0, :, :] = nimage
-        background=np.full_like(image, self.sky)
-        image+= background
+        image[0, :, :] = noisy_arr
+        background = np.full_like(image, self.sky)
+        image += background
 
         return {'image': image,
                 'background': background,
                 'num_galaxies': num_galaxies}
 
 
-
-
 class Synthetic(Dataset):
 
     def __init__(self, slen, mean_galaxies=2, min_galaxies=0, max_galaxies=3,
                  num_images=1600, num_bands=5, padding=3, centered=False,
-                 brightness=30000):
+                 flux=30000):
         """
-        Questions: 
+        Questions:
         - What is s_density?
-        - What  is A**B where both are matrices? 
+        - What  is A**B where both are matrices?
         """
         super(Synthetic, self).__init__()
 
-        self.slen = slen #number of pixel dimensions. 
+        self.slen = slen  # number of pixel dimensions.
         self.mean_galaxies = mean_galaxies
         self.min_galaxies = min_galaxies
         self.max_galaxies = max_galaxies
@@ -113,17 +108,20 @@ class Synthetic(Dataset):
         self.num_bands = num_bands
         self.padding = padding
         self.centered = centered
-        self.brightness = brightness
+        self.flux = flux
+        self.sky=700
+        self.snr=-1
+
 
     def __len__(self):
         return self.num_images
 
     def __getitem__(self, idx):
-        #right now this completely ignors the index. 
+        # right now this completely ignores the index.
 
-        sky = 700.0
-        background_sample = np.random.randn(self.num_bands, self.slen, self.slen) * np.sqrt(sky) + sky
-        background = np.full_like(background_sample, sky, dtype=np.float32)
+
+        background_sample = np.random.randn(self.num_bands, self.slen, self.slen) * np.sqrt(self.sky) + self.sky
+        background = np.full_like(background_sample, self.sky, dtype=np.float32)
         image = np.asarray(background_sample, dtype=np.float32)
 
         axis_lengths = np.array([[3.0, 0.0], [0.0, 7.0]])
@@ -134,22 +132,23 @@ class Synthetic(Dataset):
         for j in range(num_galaxies):
             angle = np.pi * np.random.rand()
             rotation = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-            covar = np.matmul(np.matmul(rotation.transpose(), axis_lengths), rotation) #rotate the initial axis lenghts
-            loc = np.random.rand(2) - 0.5 #centered by default. 
+            covar = np.matmul(np.matmul(rotation.transpose(), axis_lengths),
+                              rotation)  # rotate the initial axis lengths
+            loc = np.random.rand(2) - 0.5  # centered by default.
             if not self.centered:
                 loc *= self.slen - 2 * self.padding
             bvn = stats.multivariate_normal(loc, covar)
 
             offset = (self.slen - 1) / 2
             y, x = np.mgrid[-offset:(offset + 1), -offset:(offset + 1)]
-            pos = np.dstack((y, x)) #all the positions of the different pixels in the considered grid. 
+            pos = np.dstack((y, x))  # all the positions of the different pixels in the considered grid.
             s_density = bvn.pdf(pos)
 
             temperature = 1.0 + np.random.rand()
-            brightness = self.brightness * temperature ** np.mgrid[1:(self.num_bands + 1)] 
-            brightness = brightness.reshape(self.num_bands, 1, 1)
+            flux = self.flux * temperature ** np.mgrid[1:(self.num_bands + 1)]
+            flux = flux.reshape(self.num_bands, 1, 1)
             s_density = s_density.reshape(1, self.slen, self.slen)
-            s_intensity = s_density * brightness
+            s_intensity = s_density * flux
             s_noise = np.random.randn(self.num_bands, self.slen, self.slen)
             s_contrib = s_intensity + np.sqrt(s_intensity) * s_noise
             image += s_contrib
--- train_galaxy_model.py
+++ train_galaxy_model.py
@@ -10,65 +10,65 @@ import matplotlib.pyplot as plt
 
 import torch
 from torch.utils.data import DataLoader, sampler
-import torch.optim as optim
+from torch.optim import Adam
 
 import datasets
 import galaxy_net
 
-
 torch.backends.cudnn.benchmark = True
 plt.switch_backend("Agg")
 
-
-parser = argparse.ArgumentParser(description='GalaxyNet',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--batch-size', type=int, default=10, metavar='N',
+parser = argparse.ArgumentParser(description='GalaxyNet', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--batch-size', type=int, default=50, metavar='N',
                     help='input batch size for training.')
-parser.add_argument('--mode', type=str, default="rnn", metavar='N',
-                    help='specifies the dataset and the vae type: one, rnn, or grid')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--dataset', type=str, default="galbasic", metavar='N',
+                    help='specifies the dataset.')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='number of epochs to train.')
 parser.add_argument('--seed', type=int, default=64, metavar='S',
                     help='random seed.')
 parser.add_argument('--nocuda', action='store_true',
                     help="whether to using a discrete graphics card")
+parser.add_argument('--slen', type=int, default=15, help='Number of pixels in images.')
 parser.add_argument('--device', type=int, default=0, metavar='N',
                     help='GPU device ID')
-parser.add_argument('--dir', type=str, default="data/test", metavar='N',
+parser.add_argument('--dir', type=str, default="test", metavar='N',
                     help='run-specific directory to read from / write to')
 parser.add_argument('--overwrite', action='store_true',
                     help='Whether to overwrite if directory already exists.')
 args = parser.parse_args()
 
-args.dir= "data/"+args.dir
+args.dir = "data/" + args.dir
 
 torch.cuda.manual_seed(args.seed)
 np.random.seed(args.seed)
+
 
 def plot_reconstruction(data_loader, vae, epoch, sleep=False):
     num_examples = min(10, args.batch_size)
 
     plt.ioff()
-    plt.figure(figsize=(5 * (3), 2 + 4 * num_examples))
+    plt.figure(figsize=(5 * 3, 2 + 4 * num_examples))
     plt.tight_layout()
     plt.suptitle("Epoch {:d}".format(epoch))
 
-    num_cols = 3 #also look at recon_var 
+    num_cols = 3  # also look at recon_var
 
     with torch.no_grad():
         for batch_idx, data in enumerate(data_loader):
-            image = data["image"].cuda() #copies from cpu to gpu memory. 
-            background = data["background"].cuda() #maybe not having background will be a problem. 
+            image = data["image"].cuda()  # copies from cpu to gpu memory.
+            background = data["background"].cuda()  # maybe not having background will be a problem.
             num_galaxies = data["num_galaxies"]
             vae.eval()
 
-            if sleep: #what does this part do? 
+            if sleep:  # what does this part do?
                 really_is_on, _, _, image = vae.synthetic_image(background)
                 num_galaxies = really_is_on.sum(dim=0).cpu().int().detach().numpy()
 
             recon_mean, recon_var, _ = vae(image, background)
 
             for i in range(num_examples):
-                vmax1 = image[i, 0].max() #we are looking at the ith sample in the first band. 
+                vmax1 = image[i, 0].max()  # we are looking at the ith sample in the first band.
                 plt.subplot(num_examples, num_cols, num_cols * i + 1)
                 plt.title("image [{} galaxies]".format(num_galaxies[i]))
                 plt.imshow(image[i, 0].data.cpu().numpy(), vmax=vmax1)
@@ -94,7 +94,7 @@ def plot_reconstruction(data_loader, vae, epoch, sleep=False):
 
 
 def train_epoch(vae, train_loader, optimizer):
-    vae.train() #set in training mode. 
+    vae.train()  # set in training mode.
     avg_loss = 0.0
 
     for batch_idx, data in enumerate(train_loader):
@@ -104,7 +104,7 @@ def train_epoch(vae, train_loader, optimizer):
         loss = vae.loss(image, background)
         avg_loss += loss.item()
 
-        optimizer.zero_grad() #clears the gradients of all optimized torch.Tensors
+        optimizer.zero_grad()  # clears the gradients of all optimized torch.Tensors
         loss.backward()
         optimizer.step()
 
@@ -113,12 +113,12 @@ def train_epoch(vae, train_loader, optimizer):
 
 
 def eval_epoch(vae, data_loader):
-    vae.eval() #set in evaluation mode. 
+    vae.eval()  # set in evaluation mode.
     avg_loss = 0.0
 
-    with torch.no_grad(): #no need to compute gradients outside training. 
+    with torch.no_grad():  # no need to compute gradients outside training.
         for batch_idx, data in enumerate(data_loader):
-            image = data["image"].cuda() #shape: [nsamples, num_bands, slen, slen]
+            image = data["image"].cuda()  # shape: [nsamples, num_bands, slen, slen]
             background = data["background"].cuda()
             loss = vae.loss(image, background)
             avg_loss += loss.item()
@@ -128,10 +128,10 @@ def eval_epoch(vae, data_loader):
 
 
 def train_module(vae, ds, epochs=10000, lr=1e-4):
-    optimizer = optim.Adam(vae.parameters(), lr=lr, weight_decay=1e-6)
+    optimizer = Adam(vae.parameters(), lr=lr, weight_decay=1e-6)
 
-    tt_split = int(0.1 * len(ds)) 
-    test_indices = np.mgrid[:tt_split] #10% of data only is for test. 
+    tt_split = int(0.1 * len(ds))
+    test_indices = np.mgrid[:tt_split]  # 10% of data only is for test.
     train_indices = np.mgrid[tt_split:len(ds)]
 
     test_loader = DataLoader(ds, batch_size=args.batch_size,
@@ -140,6 +140,20 @@ def train_module(vae, ds, epochs=10000, lr=1e-4):
     train_loader = DataLoader(ds, batch_size=args.batch_size,
                               num_workers=2, pin_memory=True,
                               sampler=sampler.SubsetRandomSampler(train_indices))
+
+    dir_path = Path(args.dir)
+    dir_path.mkdir()
+    prop_file = open(f"{args.dir}/props.txt", 'w')
+    print(f"dataset: {args.dataset} \n"
+          f"epochs: {args.epochs} \n"
+          f"batch_size: {args.batch_size}\n"
+          f"learning rate: {lr}\n"
+          f"sky level: {ds.sky}\n"
+          f"slen: {ds.slen}\n"
+          f"snr: {ds.snr}\n"
+          f"flux: {ds.flux}\n"
+          f"num bands: {ds.num_bands}", file=prop_file)
+    prop_file.close()
 
     for epoch in range(0, epochs):
         np.random.seed(args.seed + epoch)
@@ -164,17 +178,27 @@ def train_module(vae, ds, epochs=10000, lr=1e-4):
             test_loss = eval_epoch(vae, test_loader)
             print("  * test loss: {:.0f}\n".format(test_loss))
             loss_file = Path(args.dir, "loss.dat")
-            with open(loss_file.as_posix(), 'a') as f: 
+            with open(loss_file.as_posix(), 'a') as f:
                 f.write(f"epoch {epoch}, test loss: {test_loss}\n")
 
 
 def run():
-    """ 
-    Questions: 
-    * What does vae.cuda() do? 
     """
-    ds = datasets.GalBasic(15, min_galaxies=1, max_galaxies=1, mean_galaxies=1, num_images=1000, centered=True,sky=0)
-    vae = galaxy_net.OneCenteredGalaxy(15, num_bands=1)
+    Questions:
+    * What does vae.cuda() do?
+    """
+    if args.dataset == 'galbasic':
+        ds = datasets.GalBasic(args.slen, min_galaxies=1, max_galaxies=1, mean_galaxies=1, num_images=1000,
+                               centered=True, sky=700)
+
+    elif args.dataset == 'synthetic':  # default one.
+        ds = datasets.Synthetic(args.slen, min_galaxies=1, max_galaxies=1, mean_galaxies=1, num_images=1000,
+                                centered=True, num_bands=1)
+
+    else:
+        raise NotImplementedError("That dataset has not yet been implemented.")
+
+    vae = galaxy_net.OneCenteredGalaxy(args.slen, num_bands=1)
     vae.cuda()
 
     print("training...")
@@ -182,13 +206,12 @@ def run():
 
 
 if __name__ == "__main__":
-    #check if directory exists
+    # check if directory exists
     if Path(args.dir).is_dir() and not args.overwrite:
         raise IOError("Directory already exists.")
 
-    elif Path(args.dir).is_dir(): 
+    elif Path(args.dir).is_dir():
         subprocess.run(f"rm -r {args.dir}", shell=True)
-
 
     with torch.cuda.device(args.device):
         run()
