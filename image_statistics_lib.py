fixed units for magnitude comparison

image_statistics_lib.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+from sdss_dataset_lib import convert_nmgy_to_mag
+
 def filter_params(locs, fluxes, slen, pad = 5):
     assert len(locs.shape) == 2
 
@@ -28,37 +30,50 @@ def get_fluxes_error(fluxes, true_fluxes):
     return torch.abs(torch.log10(fluxes).unsqueeze(0) - \
                      torch.log10(true_fluxes).unsqueeze(1))
 
-def get_summary_stats(est_locs, true_locs, slen, est_fluxes, true_fluxes, pad = 5,
-                        slack = 0.5):
+def get_mag_error(mags, true_mags):
+    # get matrix of l1 error in magnitude
+    # truth x estimated
+    return torch.abs(mags.unsqueeze(0) - \
+                     true_mags.unsqueeze(1))
 
+def get_summary_stats(est_locs, true_locs, slen, est_fluxes, true_fluxes,
+                        nelec_per_nmgy,
+                        pad = 5, slack = 0.5):
+
+    # remove border
     est_locs, est_fluxes = filter_params(est_locs, est_fluxes, slen, pad)
     true_locs, true_fluxes = filter_params(true_locs, true_fluxes, slen, pad)
 
     if (est_fluxes is None) or (true_fluxes is None):
-        fluxes_error = 0.
+        mag_error = 0.
     else:
-        fluxes_error = get_fluxes_error(est_fluxes, true_fluxes)
+        # convert to magnitude
+        est_mags = convert_nmgy_to_mag(est_fluxes / nelec_per_nmgy)
+        true_mags = convert_nmgy_to_mag(true_fluxes / nelec_per_nmgy)
+        mag_error = get_mag_error(est_mags, true_mags)
 
     locs_error = get_locs_error(est_locs * (slen - 1), true_locs * (slen - 1))
 
-    completeness_bool = torch.any((locs_error < slack) * (fluxes_error < slack), dim = 1).float()
+    completeness_bool = torch.any((locs_error < slack) * (mag_error < slack), dim = 1).float()
 
-    tpr_bool = torch.any((locs_error < slack) * (fluxes_error < slack), dim = 0).float()
+    tpr_bool = torch.any((locs_error < slack) * (mag_error < slack), dim = 0).float()
 
     return completeness_bool.mean(), tpr_bool.mean(), completeness_bool, tpr_bool
 
 def get_completeness_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes,
+                            nelec_per_nmgy,
                             pad = 5, mag_vec = None):
 
     est_locs, est_fluxes = filter_params(est_locs, est_fluxes, slen, pad)
     true_locs, true_fluxes = filter_params(true_locs, true_fluxes, slen, pad)
 
-    true_mag = torch.log10(true_fluxes)
+    # convert to magnitude
+    true_mags = convert_nmgy_to_mag(true_fluxes / nelec_per_nmgy)
 
     if mag_vec is None:
         # round to nearest half integer
-        max_mag = torch.ceil(true_mag.max() * 2) / 2
-        min_mag = torch.floor(true_mag.min() * 2) / 2
+        max_mag = torch.ceil(true_mags.max() * 2) / 2
+        min_mag = torch.floor(true_mags.min() * 2) / 2
 
         mag_vec = np.arange(min_mag, max_mag + 0.1, 0.5)
 
@@ -67,25 +82,28 @@ def get_completeness_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes,
     counts_vec = np.zeros(len(mag_vec) - 1)
 
     for i in range(len(mag_vec) - 1):
-        which_true = (true_mag > mag_vec[i]) & (true_mag < mag_vec[i + 1])
+        which_true = (true_mags > mag_vec[i]) & (true_mags < mag_vec[i + 1])
         counts_vec[i] = torch.sum(which_true)
 
         completeness_vec[i] = \
             get_summary_stats(est_locs, true_locs[which_true], slen,
-                            est_fluxes, true_fluxes[which_true])[0]
+                            est_fluxes, true_fluxes[which_true],
+                            nelec_per_nmgy, pad = pad)[0]
 
     return completeness_vec, mag_vec, counts_vec
 
-def get_tpr_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes, pad = 5, mag_vec = None):
+def get_tpr_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes,
+                nelec_per_nmgy,
+                pad = 5, mag_vec = None):
 
     est_locs, est_fluxes = filter_params(est_locs, est_fluxes, slen, pad)
     true_locs, true_fluxes = filter_params(true_locs, true_fluxes, slen, pad)
 
-    est_mag = torch.log10(est_fluxes)
+    est_mags = convert_nmgy_to_mag(est_fluxes / nelec_per_nmgy)
 
     if mag_vec is None:
-        max_mag = torch.ceil(est_mag.max() * 2) / 2
-        min_mag = torch.floor(est_mag.min() * 2) / 2
+        max_mag = torch.ceil(est_mags.max() * 2) / 2
+        min_mag = torch.floor(est_mags.min() * 2) / 2
 
         mag_vec = np.arange(min_mag, max_mag + 0.1, 0.5)
 
@@ -93,7 +111,7 @@ def get_tpr_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes, pad = 5, mag
     counts_vec = np.zeros(len(mag_vec) - 1)
 
     for i in range(len(mag_vec) - 1):
-        which_est = (est_mag > mag_vec[i]) & (est_mag < mag_vec[i + 1])
+        which_est = (est_mags > mag_vec[i]) & (est_mags < mag_vec[i + 1])
 
         counts_vec[i] = torch.sum(which_est)
 
@@ -102,7 +120,8 @@ def get_tpr_vec(est_locs, true_locs, slen, est_fluxes, true_fluxes, pad = 5, mag
 
         tpr_vec[i] = \
             get_summary_stats(est_locs[which_est], true_locs, slen,
-                            est_fluxes[which_est], true_fluxes)[1]
+                            est_fluxes[which_est], true_fluxes,
+                            nelec_per_nmgy, pad = pad)[1]
 
     return tpr_vec, mag_vec, counts_vec
 