740f1b025835dc491f9a33f44e0c5d7ac115a7c6
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-03-12 15:12:01 -0400

    Added an __init__ file to execute which will make it easier to run more files the future.

--- execute/run-generate-images.py
+++ execute/run-generate-images.py
@@ -3,12 +3,8 @@
 Runs one of the other python files in execute in a loop or so. Nothing major should be in here.
 Please run from outside the execute folder. In the root 'galaxy-net' directory.
 """
+from __init__ import PYPATH
 import subprocess
-from os.path import dirname
-from pathlib import Path
-
-project_path = Path(dirname(dirname(__file__)))  # path to galaxy-net
-path = f"PYTHONPATH={project_path.as_posix()} "
 
 
 def main():
@@ -16,16 +12,16 @@ def main():
     ps = []
     for i in range(15):
         ps.append(
-            subprocess.Popen(f"{path}"
+            subprocess.Popen(f"{PYPATH}"
                              f"./src/generate-images.py --dir test2 --filename images{i}"
                              f"--num-images 2000 --overwrite",
                              shell=True))
 
     # wait for all processes to complete.
-    exit_codes = [p.communicate() for p in ps]
+    _ = [p.communicate() for p in ps]
 
     # then we merge the results.
-    subprocess.run(f"{path}"
+    subprocess.run(f"{PYPATH}"
                    f"./src/generate-images.py --dir test2 --merge",
                    shell=True)
 
--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from os.path import dirname
 import sys
 
