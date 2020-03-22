6305bc98787b852d319247d0774571331cff4e9b
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-03-22 07:49:05 -0400

    Changed structure a bit, need to make sure changes propagated. Goal is to be able to load classes in one package from another

--- execute/__init__.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname
-from pathlib import Path
-
-project_path = Path(dirname(dirname(__file__)))  # path to galaxy-net
-PYPATH = f"PYTHONPATH={project_path.as_posix()}"  # constant.
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname
-import sys
-
-sys.path.insert(0, dirname(__file__))  # src
-sys.path.insert(0, dirname(dirname(__file__)))  # galaxy-net
