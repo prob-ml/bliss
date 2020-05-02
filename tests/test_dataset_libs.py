c1a22b9a47d9a5d4ff4eb5bb01ee00ed90b2ffb7
Zhe Zhao <zhezhao@Zhes-MacBook-Pro.local>
2020-05-02 09:26:22 -0400

    modify test_dataset_libs

--- tests/test_dataset_libs.py
+++ tests/test_dataset_libs.py
@@ -1,15 +1,16 @@
+import unittest
 import numpy as np
 import torch
-import pytorch_lightning as pl
 import json
 import fitsio
 
 from celeste.data import simulated_datasets_lib
+from celeste.utils import const
 
 psf_dir = "./data/"
 psf_r = fitsio.FITS(psf_dir + "sdss-002583-2-0136-psf-r.fits")[0].read()
 psf_i = fitsio.FITS(psf_dir + "sdss-002583-2-0136-psf-i.fits")[0].read()
-psf_og = torch.Tensor(np.array([psf_r, psf_i]))
+psf_og = torch.Tensor(np.array([psf_r, psf_i])).to(const.device)  # waiting for new push
 
 with open("./data/default_star_parameters.json", "r") as fp:
     data_params = json.load(fp)
@@ -18,7 +19,7 @@ data_params["slen"] = 31
 data_params["mean_stars"] = 10
 
 
-class TestSDSSDataset(pl.LightningModule):
+class TestSDSSDataset(unittest.TestCase):
     def test_fresh_data(self):
         # this checks that we are actually drawing fresh data
         # at each epoch (or not)
@@ -29,88 +30,57 @@ class TestSDSSDataset(pl.LightningModule):
             torch.ones(psf_og.shape[0], data_params["slen"], data_params["slen"])
             * 686.0
         )
-        star_dataset = simulated_datasets_lib.load_dataset_from_params(
-            psf_og,
-            data_params,
+        star_dataset = simulated_datasets_lib.StarsDataset.load_dataset_from_params(
             n_images=n_images,
+            data_params=data_params,
+            psf=psf_og,
             background=background,
-            add_noise=True,
             transpose_psf=False,
+            add_noise=True,
         )
 
-        # get loader
+        # get batch
         batchsize = 8
 
-        loader = torch.utils.data.DataLoader(dataset=star_dataset, batch_size=batchsize)
+        # loader = torch.utils.data.DataLoader(dataset=star_dataset, batch_size=batchsize)
 
         #############################################
-        # First check: all data should be the same
+        # Check: by creating new batches fresh data
+        # should be drawn
         #############################################
-        images_vec = torch.zeros(5)
-        locs_vec = torch.zeros(5)
-        fluxes_vec = torch.zeros(5)
-        n_stars_vec = torch.zeros(5)
-        for i in range(5):
+        num_epoch = 5
+        images_vec = torch.zeros(num_epoch)
+        locs_vec = torch.zeros(num_epoch)
+        fluxes_vec = torch.zeros(num_epoch)
+        n_sources_vec = torch.zeros(num_epoch)
+
+        for i in range(num_epoch):
             images_mean = 0
             true_locs_mean = 0
             true_fluxes_mean = 0
-            true_n_stars_mean = 0
+            true_n_sources_mean = 0
 
-            for _, data in enumerate(loader):
-                true_fluxes = data["fluxes"]
-                true_locs = data["locs"]
-                true_n_stars = data["n_stars"]
-                images = data["image"]
+            num_baches = int(len(star_dataset) / batchsize)
+
+            for j in range(num_baches):
+                batch = star_dataset.get_batch(batchsize)
+
+                true_fluxes = batch["fluxes"]
+                true_locs = batch["locs"]
+                true_n_sources = batch["n_sources"]
+                images = batch["image"]
 
                 images_mean += images.mean()
                 true_locs_mean += true_locs.mean()
                 true_fluxes_mean += true_fluxes.mean()
-                true_n_stars_mean += true_n_stars.float().mean()
+                true_n_sources_mean += true_n_sources.float().mean()
 
             images_vec[i] = images_mean
             locs_vec[i] = true_locs_mean
             fluxes_vec[i] = true_fluxes_mean
-            n_stars_vec[i] = true_fluxes_mean
+            n_sources_vec[i] = true_n_sources_mean
 
-        assert len(images_vec.unique()) == 1
-        assert len(locs_vec.unique()) == 1
-        assert len(fluxes_vec.unique()) == 1
-        assert len(n_stars_vec.unique()) == 1
-
-        #############################################
-        # Second check: by adding the "loader.dataset.set_params_and_images()"
-        # command, fresh data should be drawn
-        #############################################
-        images_vec = torch.zeros(5)
-        locs_vec = torch.zeros(5)
-        fluxes_vec = torch.zeros(5)
-        n_stars_vec = torch.zeros(5)
-        for i in range(5):
-            images_mean = 0
-            true_locs_mean = 0
-            true_fluxes_mean = 0
-            true_n_stars_mean = 0
-
-            for _, data in enumerate(loader):
-                true_fluxes = data["fluxes"]
-                true_locs = data["locs"]
-                true_n_stars = data["n_stars"]
-                images = data["image"]
-
-                images_mean += images.mean()
-                true_locs_mean += true_locs.mean()
-                true_fluxes_mean += true_fluxes.mean()
-                true_n_stars_mean += true_n_stars.float().mean()
-
-            images_vec[i] = images_mean
-            locs_vec[i] = true_locs_mean
-            fluxes_vec[i] = true_fluxes_mean
-            n_stars_vec[i] = true_fluxes_mean
-
-            # reset
-            loader.dataset.set_params_and_images()
-
-        assert len(images_vec.unique()) == 5
-        assert len(locs_vec.unique()) == 5
-        assert len(fluxes_vec.unique()) == 5
-        assert len(n_stars_vec.unique()) == 5
+        assert len(images_vec.unique()) == num_epoch
+        assert len(locs_vec.unique()) == num_epoch
+        assert len(fluxes_vec.unique()) == num_epoch
+        assert len(n_sources_vec.unique()) == num_epoch
