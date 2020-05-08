acc5e53b21a876cbcf5268b4e1d5ba4a8bd2f106
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-05-08 15:43:41 -0400

    save before testing, run_save_single_galaxy_images.py

--- bin/run_save_single_galaxy_images.py
+++ bin/run_save_single_galaxy_images.py
@@ -3,7 +3,6 @@
 import argparse
 import h5py
 import numpy as np
-import subprocess
 import multiprocessing
 
 from celeste.utils import const
@@ -14,18 +13,30 @@ all_datasets = {
     cls.__name__: cls for cls in galaxy_datasets.GalaxyDataset.__subclasses__()
 }
 
-
-def save_background(output_path):
-    new_file_path = output_path.joinpath("images.hdf5")
-    background_path = output_path.joinpath("background.npy")
-    with h5py.File(new_file_path, "r") as curr_file:
-        ds = curr_file[const.image_h5_name]
-        background = ds.attrs[const.background_h5_name]
-        np.save(background_path, background)
+final_image_name = "images.hd5f"
+background_name = "background.npy"
 
 
-def merge_files():
-    new_file_path = output_path.joinpath("images.hdf5")
+def generate(
+    n_images, file_path, output_path, dataset, overwrite=False,
+):
+    """
+    Generate a single image .hdf5 file with the dataset and dataset parameters specified.
+    """
+
+    prop_file_path = output_path.joinpath(f"prop_{file_path.name}.txt")
+
+    assert (
+        not file_path.exists() or overwrite
+    ), "Trying to overwrite file without --overwrite"
+
+    generate_images(
+        dataset, file_path, prop_file_path=prop_file_path, n_images=n_images
+    )
+
+
+def merge_files(output_path):
+    new_file_path = output_path.joinpath(final_image_name)
     assert not new_file_path.exists(), "The merged file is already there."
 
     h5_files = [pth for pth in output_path.iterdir() if pth.suffix == ".hdf5"]
@@ -40,14 +51,14 @@ def merge_files():
             dtype = ds.dtype
 
     # then we copy them.
-    with h5py.File(new_file_path, "w") as nfile:
-        fds = nfile.create_dataset(
+    with h5py.File(new_file_path, "w") as new_file:
+        fds = new_file.create_dataset(
             const.image_h5_name, shape=(total_images, *shape[1:]), dtype=dtype
         )
         images_copied = 0
         for pth in h5_files:
-            with h5py.File(pth, "r") as currfile:
-                ds = currfile[const.image_h5_name]
+            with h5py.File(pth, "r") as curr_file:
+                ds = curr_file[const.image_h5_name]
                 num_images = ds.shape[0]
                 fds[images_copied : images_copied + num_images, :, :, :] = ds[
                     :, :, :, :
@@ -56,28 +67,13 @@ def merge_files():
                 images_copied += num_images
 
 
-def generate(
-    n_images,
-    file_path,
-    output_path,
-    dataset_name,
-    dset_param_file=None,
-    overwrite=False,
-):
-    """
-    Generate a single image .hdf5 file with the dataset and dataset parameters specified.
-    """
-
-    dataset = all_datasets[dataset_name].load_dataset_from_params(dset_param_file)
-    prop_file_path = output_path.joinpath(f"prop_{file_path.name}.txt")
-
-    assert (
-        not file_path.exists() or overwrite
-    ), "Trying to overwrite file without --overwrite"
-
-    generate_images(
-        dataset, file_path, prop_file_path=prop_file_path, n_images=n_images
-    )
+def save_background(output_path):
+    new_file_path = output_path.joinpath(final_image_name)
+    background_path = output_path.joinpath(background_name)
+    with h5py.File(new_file_path, "r") as curr_file:
+        ds = curr_file[const.image_h5_name]
+        background = ds.attrs[const.background_h5_name]
+        np.save(background_path, background)
 
 
 def main(args):
@@ -88,8 +84,21 @@ def main(args):
     output_path = const.data_path.joinpath(f"{args.out_dir}")
     output_path.mkdir(exist_ok=True)
 
-    pool = multiprocessing.Pool(processes=args.n_processes)
-    generate_args = []
+    # load dataset only once.
+    dataset = all_datasets[args.dataset_name].load_dataset_from_params(
+        args.dset_param_file
+    )
+
+    file_paths = [output_path.joinpath(f"image{i}") for i in range(args.n_processes)]
+    generate_args = [
+        (args.n_images_per_process, fp, output_path, dataset, args.overwrite)
+        for fp in file_paths
+    ]
+    with multiprocessing.Pool(processes=args.n_processes) as pool:
+        pool.starmap(generate_images, generate_args)
+
+    merge_files(output_path)
+    save_background(output_path)
 
 
 if __name__ == "__main__":
@@ -124,7 +133,7 @@ if __name__ == "__main__":
         help="Specify params file relative to data folder, "
         "from which to load params for dataset.",
     )
-    parser.add_argument("--n-images", type=int, required=True)
+    parser.add_argument("--images-per-process", type=int, required=True)
     parser.add_argument("--slen", type=int, default=51)
     parser.add_argument("--n-bands", type=int)
 
@@ -133,6 +142,7 @@ if __name__ == "__main__":
         "--n-processes",
         type=str,
         help="Number of parallel processes " "to run to write images in disk.",
+        required=True,
     )
 
     pargs = parser.parse_args()
@@ -141,4 +151,4 @@ if __name__ == "__main__":
         pargs.num_bands in [1, 6] or not pargs.generate
     ), "Only 1 or 6 bands supported."
 
-main(pargs)
+    main(pargs)
