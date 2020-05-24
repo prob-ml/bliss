1f5e68e8bd386a9ab0f0efda1da552cf5c535726
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-05-24 08:43:18 -0400

    change scope of fixtures from package to session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,16 @@ import pytest
 import pathlib
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="session")
 def root_path():
     return pathlib.Path(__file__).parent.parent.absolute()
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="session")
 def data_path(root_path):
     return root_path.joinpath("data")
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="session")
 def config_path(root_path):
     return root_path.joinpath("config")
