put back temporary fix minus signs, now try to understand it better

celeste/sleep.py
@@ -290,8 +290,7 @@ def _get_min_perm_loss(
         galaxy_params_log_probs_all_perm, 1, indx.unsqueeze(1)
     ).squeeze()
 
-    # TODO: Put back minus sign once we fix galaxy bool loss
-    galaxy_bool_loss = torch.gather(
+    galaxy_bool_loss = -torch.gather(
         galaxy_bool_log_probs_all_perm, 1, indx.unsqueeze(1)
     ).squeeze()
 

tests/test_sleep.py
@@ -260,7 +260,6 @@ class TestStarEncoderObjective:
                 torch.abs(galaxy_params_loss[i] - min_galaxy_params_loss) < 1e-5
             ), torch.abs(galaxy_params_loss[i] - min_galaxy_params_loss)
 
-            # TODO: Revive once we fix the bool loss in sleep.py.
-            # assert (
-            #     torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss) < 1e-5
-            # ), torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss)
+            assert (
+                torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss) < 1e-5
+            ), torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss)