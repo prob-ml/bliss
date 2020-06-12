77fe821548af975e35132b0edd1ee18fd0d340cd
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-06-12 14:58:47 -0400

    is there a better way to run tests with a different device?

--- celeste/__init__.py
+++ celeste/__init__.py
@@ -2,3 +2,10 @@ import torch
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def update_device(new_device):
+    global device
+    device = new_device
+    if use_cuda:
+        torch.cuda.set_device(device)
