4f8814f6383185394027e6666c75680b353ac1ea
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-06-15 18:55:24 -0400

    changed names, corrected torch.rand to torch.randn when adding noise

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -1,265 +1,119 @@
-from itertools import permutations
-from copy import deepcopy
-
-import numpy as np
 import torch
-from torch.distributions import Normal
+import pytest
 
-from celeste import device
-from celeste import sleep
+from celeste import use_cuda
+from celeste import train
 from celeste.datasets import simulated_datasets
+from celeste.models import sourcenet
 
 
-class TestStarEncoderObjective:
-    def test_get_params_logprob_all_combs(self):
-        # this checks that our function to return all combination of losses
-        # is correct
+@pytest.fixture(scope="module")
+def trained_encoder(
+    data_path, single_band_galaxy_decoder, single_band_fitted_powerlaw_psf, device,
+):
 
-        n_ptiles = 10
-        max_detections = 4
-        n_source_params = 8
+    # create training dataset
+    n_bands = 1
+    max_stars = 20
+    mean_stars = 15
+    min_stars = 5
+    f_min = 1e4
+    slen = 50
 
-        # true parameters
-        true_params = torch.rand(
-            n_ptiles, max_detections, n_source_params, device=device
-        )
+    # set background
+    background = torch.zeros(n_bands, slen, slen, device=device)
+    background[0] = 686.0
 
-        # estimated parameters
-        param_mean = torch.randn(
-            n_ptiles, max_detections, n_source_params, device=device
-        )
-        param_logvar = torch.randn(
-            n_ptiles, max_detections, n_source_params, device=device
-        )
+    # simulate dataset
+    n_images = 128
+    simulator_args = (
+        single_band_galaxy_decoder,
+        single_band_fitted_powerlaw_psf,
+        background,
+    )
 
-        # get all losses
-        param_log_probs_all = sleep._get_params_logprob_all_combs(
-            true_params, param_mean, param_logvar
-        )
+    simulator_kwargs = dict(
+        slen=slen,
+        n_bands=n_bands,
+        max_sources=max_stars,
+        mean_sources=mean_stars,
+        min_sources=min_stars,
+        f_min=f_min,
+        prob_galaxy=0.0,  # enforce only stars are created in the training images.
+    )
 
-        # just for my sanity
-        assert list(param_log_probs_all.shape) == [
-            n_ptiles,
-            max_detections,
-            max_detections,
-        ]
+    dataset = simulated_datasets.SourceDataset(
+        n_images, simulator_args, simulator_kwargs
+    )
 
-        # for each batch, check that the ith estimated parameter is correctly
-        # matched with the jth true parameter
-        for n in range(n_ptiles):
-            for i in range(max_detections):
-                for j in range(max_detections):
+    # setup Star Encoder
+    encoder = sourcenet.SourceEncoder(
+        slen=slen,
+        ptile_slen=8,
+        step=2,
+        edge_padding=3,
+        n_bands=n_bands,
+        max_detections=2,
+        n_galaxy_params=single_band_galaxy_decoder.latent_dim,
+        enc_conv_c=5,
+        enc_kern=3,
+        enc_hidden=64,
+    ).to(device)
 
-                    param_loglik_ij = (
-                        Normal(
-                            param_mean[n, i],
-                            (torch.exp(param_logvar[n, i]) + 1e-5).sqrt(),
-                        )
-                        .log_prob(true_params[n, j])
-                        .sum()
-                    )
+    # train encoder
+    # training wrapper
+    SleepTraining = train.SleepTraining(
+        model=encoder,
+        dataset=dataset,
+        slen=slen,
+        n_bands=n_bands,
+        verbose=False,
+        batchsize=32,
+    )
 
-                    assert param_loglik_ij == param_log_probs_all[n, i, j]
+    n_epochs = 100 if use_cuda else 1
+    SleepTraining.run(n_epochs=n_epochs)
 
-    def test_get_min_perm_loss(self):
-        """
-        Same as previous function but checks that we can get the permutation with the minimum loss.
-        """
+    return encoder
 
-        # data parameters
-        n_ptiles = 100
-        n_bands = 2
-        n_galaxy_params = 8
-        max_detections = 4
 
-        # true parameters
-        true_n_sources = torch.from_numpy(
-            np.random.choice(max_detections + 1, n_ptiles)
-        ).to(device)
-        true_is_on_array = simulated_datasets.get_is_on_from_n_sources(
-            true_n_sources, max_detections
-        ).float()
+class TestStarSleepEncoder:
+    def test_star_sleep(self, trained_encoder, test_star, device):
 
-        # locations, fluxes and galaxy parameters
-        true_locs = torch.rand(
-            n_ptiles, max_detections, 2, device=device
-        ) * true_is_on_array.unsqueeze(2)
-        true_log_fluxes = torch.randn(
-            n_ptiles, max_detections, n_bands, device=device
-        ) * true_is_on_array.unsqueeze(2)
-        true_galaxy_params = torch.randn(
-            n_ptiles, max_detections, n_galaxy_params, device=device
-        ) * true_is_on_array.unsqueeze(2)
+        # load test image
+        test_image = test_star["images"]
 
-        # boolean indicating whether source is galaxy
-        true_galaxy_bool = (
-            (torch.rand(n_ptiles, max_detections) > 0.5).float().to(device)
-        )
-
-        # estimated parameters
-        loc_mean = torch.randn(
-            n_ptiles, max_detections, 2, device=device
-        ) * true_is_on_array.unsqueeze(2)
-        loc_mean = loc_mean + (true_is_on_array == 0).float().unsqueeze(-1) * 1e16
-        loc_logvar = torch.randn(
-            n_ptiles, max_detections, 2, device=device
-        ) * true_is_on_array.unsqueeze(2)
-
-        log_flux_mean = torch.randn(
-            n_ptiles, max_detections, n_bands, device=device
-        ) * true_is_on_array.unsqueeze(2)
-        log_flux_logvar = torch.randn(
-            n_ptiles, max_detections, n_bands, device=device
-        ) * true_is_on_array.unsqueeze(2)
-
-        galaxy_params_mean = torch.randn(
-            n_ptiles, max_detections, n_galaxy_params, device=device
-        ) * true_is_on_array.unsqueeze(2)
-        galaxy_params_logvar = torch.randn(
-            n_ptiles, max_detections, n_galaxy_params, device=device
-        ) * true_is_on_array.unsqueeze(2)
-
-        # for each detection, prob that it is a galaxy
-        prob_galaxy = torch.rand(n_ptiles, max_detections, device=device)
-
-        # get loss for locations
-        locs_log_probs_all = sleep._get_params_logprob_all_combs(
-            true_locs, loc_mean, loc_logvar
-        )
-
-        # get loss for fluxes
-        star_params_log_probs_all = sleep._get_params_logprob_all_combs(
-            true_log_fluxes, log_flux_mean, log_flux_logvar
-        )
-
-        # loss for galaxy parameters
-        galaxy_params_log_probs_all = sleep._get_params_logprob_all_combs(
-            true_galaxy_params, galaxy_params_mean, galaxy_params_logvar
-        )
-
-        (
-            locs_loss,
-            galaxy_params_loss,
-            star_params_loss,
-            galaxy_bool_loss,
-        ) = sleep._get_min_perm_loss(
-            locs_log_probs_all,
-            galaxy_params_log_probs_all,
-            star_params_log_probs_all,
-            prob_galaxy,
-            true_galaxy_bool,
-            true_is_on_array,
-        )
-
-        # when no sources, all losses should be zero
-        assert (locs_loss[true_n_sources == 0] == 0).all()
-        assert (star_params_loss[true_n_sources == 0] == 0).all()
-        assert (galaxy_params_loss[true_n_sources == 0] == 0).all()
-        assert (galaxy_bool_loss[true_n_sources == 0] == 0).all()
-        assert (
-            locs_loss[true_n_sources == 1]
-            == -locs_log_probs_all[true_n_sources == 1][:, 0, 0]
-        ).all()
-
-        # when there are no stars: stars loss should be zero
-        which_no_stars = ((1 - true_galaxy_bool) * true_is_on_array).sum(1) == 0
-        assert (star_params_loss[which_no_stars] == 0).all()
-
-        # when there are no galaxies: galaxy loss should be zero
-        which_no_gal = (true_galaxy_bool * true_is_on_array).sum(1) == 0
-        assert (galaxy_params_loss[which_no_gal] == 0).all()
-
-        # when there is only one source, and that source is a star
-        which_one_star = (true_n_sources == 1) & (true_galaxy_bool[:, 0] == 0)
-        assert (
-            star_params_loss[which_one_star]
-            == -star_params_log_probs_all[which_one_star][:, 0, 0]
-        ).all()
-        # when there is only one source, and that source is a galaxy
-        which_one_gal = (true_n_sources == 1) & (true_galaxy_bool[:, 0] == 1)
-
-        assert (
-            galaxy_params_loss[which_one_gal]
-            == -galaxy_params_log_probs_all[which_one_gal][:, 0, 0]
-        ).all()
-
-        # a more thorough check for all possible true_n_sources
-        for i in range(n_ptiles):
-            _true_n_sources = int(true_n_sources[i])
-            _true_galaxy_bool = true_galaxy_bool[i, 0:_true_n_sources]
-
-            if true_n_sources[i] == 0:
-                assert locs_loss[i] == 0
-                assert star_params_loss[i] == 0
-                assert galaxy_bool_loss[i] == 0
-                continue
-
-            # get parameters for ith observation
-            _true_locs = true_locs[i, 0:_true_n_sources, :]
-            _loc_mean = loc_mean[i, 0:_true_n_sources, :]
-            _loc_logvar = loc_logvar[i, 0:_true_n_sources, :]
-
-            _true_log_fluxes = true_log_fluxes[i, 0:_true_n_sources, :]
-            _log_flux_mean = log_flux_mean[i, 0:_true_n_sources, :]
-            _log_flux_logvar = log_flux_logvar[i, 0:_true_n_sources, :]
-
-            _true_galaxy_params = true_galaxy_params[i, 0:_true_n_sources, :]
-            _galaxy_params_mean = galaxy_params_mean[i, 0:_true_n_sources, :]
-            _galaxy_params_logvar = galaxy_params_logvar[i, 0:_true_n_sources, :]
-
-            _prob_galaxy = prob_galaxy[i, 0:_true_n_sources]
-
-            min_locs_loss = 1e16
-            for perm in permutations(range(_true_n_sources)):
-                locs_loss_perm = -Normal(
-                    _loc_mean[perm, :], (torch.exp(_loc_logvar[perm, :]) + 1e-5).sqrt()
-                ).log_prob(_true_locs)
-
-                star_params_loss_perm = (
-                    -Normal(
-                        _log_flux_mean[perm, :],
-                        (torch.exp(_log_flux_logvar[perm, :]) + 1e-5).sqrt(),
-                    )
-                    .log_prob(_true_log_fluxes)
-                    .sum(-1)
-                )
-
-                galaxy_params_loss_perm = (
-                    -Normal(
-                        _galaxy_params_mean[perm, :],
-                        (torch.exp(_galaxy_params_logvar[perm, :]) + 1e-5).sqrt(),
-                    )
-                    .log_prob(_true_galaxy_params)
-                    .sum(-1)
-                )
-
-                p = _prob_galaxy.unsqueeze(0)[:, perm].squeeze()
-                galaxy_bool_loss_perm = -_true_galaxy_bool * torch.log(p)
-                galaxy_bool_loss_perm -= (1 - _true_galaxy_bool) * torch.log(1 - p)
-
-                if locs_loss_perm.sum() < min_locs_loss:
-                    min_perm = deepcopy(perm)
-                    min_locs_loss = locs_loss_perm.sum()
-                    min_star_params_loss = (
-                        star_params_loss_perm * (1 - _true_galaxy_bool)
-                    ).sum()
-                    min_galaxy_params_loss = (
-                        galaxy_params_loss_perm * _true_galaxy_bool
-                    ).sum()
-                    min_galaxy_bool_loss = galaxy_bool_loss_perm.sum()
-
-            assert torch.abs(locs_loss[i] - min_locs_loss) < 1e-5, torch.abs(
-                locs_loss[i] - min_locs_loss
+        with torch.no_grad():
+            # get the estimated params
+            trained_encoder.eval()
+            (
+                n_sources,
+                locs,
+                galaxy_params,
+                log_fluxes,
+                galaxy_bool,
+            ) = trained_encoder.sample_encoder(
+                test_image.to(device),
+                n_samples=1,
+                return_map_n_sources=True,
+                return_map_source_params=True,
             )
 
-            assert (
-                torch.abs(star_params_loss[i] - min_star_params_loss) < 1e-5
-            ), torch.abs(star_params_loss[i] - min_star_params_loss)
+        # we only expect our assert statements to be true
+        # when the model is trained in full, which requires cuda
+        if not use_cuda:
+            return
 
-            assert (
-                torch.abs(galaxy_params_loss[i] - min_galaxy_params_loss) < 1e-5
-            ), torch.abs(galaxy_params_loss[i] - min_galaxy_params_loss)
+        # test that parameters match.
+        assert n_sources == test_star["n_sources"].to(device)
 
-            assert (
-                torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss) < 1e-5
-            ), torch.abs(galaxy_bool_loss[i] - min_galaxy_bool_loss)
+        diff_locs = test_star["locs"].sort(1)[0].to(device) - locs.sort(1)[0]
+        diff_locs *= test_image.size(-1)
+        assert diff_locs.abs().max() <= 0.5
+
+        # fluxes
+        diff = test_star["log_fluxes"].sort(1)[0].to(device) - log_fluxes.sort(1)[0]
+        assert torch.all(diff.abs() <= log_fluxes.sort(1)[0].abs() * 0.10)
+        assert torch.all(
+            diff.abs() <= test_star["log_fluxes"].sort(1)[0].abs().to(device) * 0.10
+        )
