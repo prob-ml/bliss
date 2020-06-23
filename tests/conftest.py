d7db9f1edbcdb92104cfc24a5a2e7a75954293ce
Ismael Mendoza <11745764+ismael2395@users.noreply.github.com>
2020-06-23 16:10:55 -0400

    first steps towards binary encoder working (#30)
    * put back temporary fix minus signs, now try to understand it better
    
    * added assertion to avoid cases where max_detections > max_sources
    
    * some refactoring to make shapes more explicit
    
    * added some questions in sleep.py for my understanding
    
    * fix up more names (is_on_array alwasy at the end)
    
    * crucial error, log1p != log(1-p); this fixes problem fo distinguishing stars and galaxies
    
    * add option to switch device used in tests
    
    * added option to select cuda to run in test
    
    * is there a better way to run tests with a different device?
    
    * able to specify cuda device in tests, will change the one in the package
    
    * didn't need the update_device
    
    * removing config directory
    
    * working on getting single-centered galaxy images again
    
    * fixed test_star_encoder_sleep.py and working towards getting save_single_galaxy_images.py to work
    
    * encoder works for single galaxies, now testing multiple galaxies. Seems to do better with smaller ptiles
    
    * typo
    
    * changed names, corrected torch.rand to torch.randn when adding noise
    
    * updated names
    
    * updated more names
    
    * made plot -> render
    
    * changed star image to have Gaussian noise, test_sleep.py still passing
    
    * tried layer norm without much success (really slow and if increases learning rate then it doesn't converge)
    
    * added changing momentum, will try weight_norm too
    
    * switched to LayerNorm, try to see if we can make it work
    
    * removed BatchNorm -> now LayerNorm, test_sleep.py passes; Dropout made it detect 4 instead of 3 stars sometimes
    
    * refactored galaxy decoder and datasets to make it easy to rerun VAE, will do that soon
    
    * batchsize -> batch_size
    removed some docstrings
    renamed Dataset class to be correct
    minor refactoring
    moved sleep training to sleep.py, no more train.py
    consistent use of image_encoder
    fixing unit tests, test_kl_pq.py and test_vaes.py pass
    
    * fixed conftest.py, added dropout but not sure if stable still
    
    * removed utils, conftest.py has get_trained_encoder now
    
    * bug in sample_locs in decoder.py
    
    * wake phase test passes
    
    * just removed use_val until new pytorch lightning is released.
    
    * fix test_vaes.py in circle.ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,47 @@ import pytest
 import pathlib
 import torch
 
-from celeste.datasets.simulated_datasets import get_fitted_powerlaw_psf
-from celeste.datasets.galaxy_datasets import DecoderSamples
+from celeste.models.decoder import get_fitted_powerlaw_psf, get_galaxy_decoder
+from celeste import use_cuda, sleep
+
+import pytorch_lightning as pl
+from pytorch_lightning.profiler import AdvancedProfiler
+from celeste.models import decoder, encoder
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--device-id",
+        action="store",
+        default=0,
+        type=int,
+        help="ID of cuda device to use.",
+    )
+    parser.addoption(
+        "--profile",
+        action="store",
+        default=None,
+        type=str,
+        help="None or file path to store profiler",
+    )
+
+
+@pytest.fixture(scope="session")
+def device_id(pytestconfig):
+    return pytestconfig.getoption("device_id")
+
+
+@pytest.fixture(scope="session")
+def profile(pytestconfig):
+    return pytestconfig.getoption("profile")
+
+
+@pytest.fixture(scope="session")
+def device(device_id):
+    new_device = torch.device(f"cuda:{device_id}" if use_cuda else "cpu")
+    if use_cuda:
+        torch.cuda.set_device(new_device)
+    return new_device
 
 
 @pytest.fixture(scope="session")
@@ -17,11 +56,6 @@ def data_path(root_path):
 
 
 @pytest.fixture(scope="session")
-def config_path(root_path):
-    return root_path.joinpath("config")
-
-
-@pytest.fixture(scope="session")
 def single_band_fitted_powerlaw_psf(data_path):
     psf_file = data_path.joinpath("fitted_powerlaw_psf_params.npy")
     return get_fitted_powerlaw_psf(psf_file)[None, 0, ...]
@@ -29,10 +63,12 @@ def single_band_fitted_powerlaw_psf(data_path):
 
 @pytest.fixture(scope="session")
 def single_band_galaxy_decoder(data_path):
-    galaxy_slen = 51
-    n_bands = 1
-    galaxy_decoder_file = data_path.joinpath("decoder_params_100_single_band_i.dat")
-    return DecoderSamples(galaxy_slen, galaxy_decoder_file, n_bands=n_bands)
+
+    decoder_state_file = data_path.joinpath("decoder_params_100_single_band_i.dat")
+    galaxy_decoder = get_galaxy_decoder(
+        decoder_state_file, slen=51, n_bands=1, latent_dim=8
+    )
+    return galaxy_decoder
 
 
 @pytest.fixture(scope="session")
@@ -41,15 +77,84 @@ def test_star(data_path):
     return test_star
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--profile",
-        action="store",
-        default=None,
-        help="None or file path to store profiler",
-    )
-
-
 @pytest.fixture(scope="session")
-def profile(pytestconfig):
-    return pytestconfig.getoption("--profile")
+def get_trained_encoder():
+    def func(
+        galaxy_decoder,
+        psf,
+        device,
+        device_id,
+        profile=None,
+        n_bands=1,
+        max_stars=20,
+        mean_stars=15,
+        min_stars=5,
+        f_min=1e4,
+        slen=50,
+        n_images=128,
+        batch_size=32,
+        n_epochs=150,
+    ):
+        assert galaxy_decoder.n_bands == psf.size(0) == n_bands
+
+        n_epochs = n_epochs if use_cuda else 1
+
+        background = torch.zeros(n_bands, slen, slen, device=device)
+        background.fill_(686.0)
+
+        simulator_args = (
+            galaxy_decoder,
+            psf,
+            background,
+        )
+
+        simulator_kwargs = dict(
+            slen=slen,
+            n_bands=n_bands,
+            max_sources=max_stars,
+            mean_sources=mean_stars,
+            min_sources=min_stars,
+            f_min=f_min,
+            prob_galaxy=0.0,  # only stars will be drawn.
+        )
+
+        n_batches = int(n_images / batch_size)
+        dataset = decoder.SimulatedDataset(
+            n_batches, batch_size, simulator_args, simulator_kwargs
+        )
+
+        # setup Star Encoder
+        image_encoder = encoder.ImageEncoder(
+            slen=slen,
+            ptile_slen=8,
+            step=2,
+            edge_padding=3,
+            n_bands=n_bands,
+            max_detections=2,
+            n_galaxy_params=galaxy_decoder.latent_dim,
+            enc_conv_c=5,
+            enc_kern=3,
+            enc_hidden=64,
+        ).to(device)
+
+        # training wrapper
+        sleep_net = sleep.SleepPhase(dataset=dataset, image_encoder=image_encoder)
+
+        profiler = AdvancedProfiler(output_filename=profile) if profile else None
+
+        # runs on gpu or cpu?
+        n_device = [device_id] if use_cuda else 0  # 0 means no gpu
+
+        sleep_trainer = pl.Trainer(
+            gpus=n_device,
+            profiler=profiler,
+            min_epochs=n_epochs,
+            max_epochs=n_epochs,
+            reload_dataloaders_every_epoch=True,
+        )
+
+        sleep_trainer.fit(sleep_net)
+
+        return sleep_net.image_encoder
+
+    return func
