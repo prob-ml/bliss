77b63137c33c9aeb283d3a44190c89d3f56a482e
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-07-03 17:39:38 -0400

    removed unnecessary stuff

--- celeste/models/galaxy_net.py
+++ celeste/models/galaxy_net.py
@@ -1,11 +1,9 @@
 import inspect
 
-# import io
 import numpy as np
 import pytorch_lightning as pl
 import matplotlib.pyplot as plt
 
-# import tensorflow as tf
 
 import torch
 from torch.utils.data import DataLoader, SubsetRandomSampler
@@ -17,23 +15,6 @@ from torch.optim import Adam
 from .. import device
 
 plt.switch_backend("Agg")
-
-
-# def plot_to_image(figure):
-#     """Converts the matplotlib plot specified by 'figure' to a PNG image and
-#   returns it. The supplied figure is closed and inaccessible after this call."""
-#     # Save the plot to a PNG in memory.
-#     buf = io.BytesIO()
-#     plt.savefig(buf, format="png")
-#     # Closing the figure prevents it from being displayed directly inside
-#     # the notebook.
-#     plt.close(figure)
-#     buf.seek(0)
-#     # Convert PNG buffer to TF image
-#     image = tf.image.decode_png(buf.getvalue(), channels=4)
-#     # Add the batch dimension
-#     image = tf.expand_dims(image, 0)
-#     return image
 
 
 class Flatten(nn.Module):
