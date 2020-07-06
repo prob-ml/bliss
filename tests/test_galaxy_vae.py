41683ba6248d19715fb7000eefd64a6a5adbbf20
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-07-06 14:02:38 -0400

    test passes in gpu

--- tests/test_galaxy_vae.py
+++ tests/test_galaxy_vae.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 import pytest
 import torch
+import numpy as np
 
 from celeste import use_cuda
 from celeste.datasets import galaxy_datasets
@@ -10,8 +11,7 @@ from celeste.models import galaxy_net
 class TestGalaxyVAE:
     @pytest.fixture(scope="module")
     def trained_galaxy_vae(self, gpus, data_path):
-
-        h5_file = data_path.joinpath("catsim_single_galaxies.hd5f")
+        h5_file = data_path.joinpath("catsim_single_galaxies.hdf5")
         dataset = galaxy_datasets.H5Catalog(h5_file, slen=51, n_bands=1)
         n_epochs = 100 if use_cuda else 1
         trainer = pl.Trainer(
@@ -22,8 +22,10 @@ class TestGalaxyVAE:
             profiler=None,
             logger=False,
             checkpoint_callback=False,
-            limit_val_batches=0,  # no validation.
+            limit_val_batches=0.0,  # no validation.
         )
+
+        # TODO: Allow tt_split ==0 if no validation.
         galaxy_vae = galaxy_net.OneCenteredGalaxy(
             dataset,
             slen=51,
@@ -31,18 +33,31 @@ class TestGalaxyVAE:
             latent_dim=8,
             num_workers=0,
             batch_size=64,
-            tt_split=0.0,
+            tt_split=0.1,
         )
 
         trainer.fit(galaxy_vae)
         galaxy_vae.freeze()
+        galaxy_vae.eval()
         return galaxy_vae
 
-    def test_galaxy_vae(self, data_path, trained_galaxy_vae):
-
+    def test_galaxy_vae(self, data_path, trained_galaxy_vae, device):
         galaxy_image = torch.load(data_path.joinpath("1_catsim_galaxy.pt"))
-        pred_image = trained_galaxy_vae(galaxy_image)
+        background = torch.from_numpy(
+            np.load(data_path.joinpath("background_galaxy_single_band_i.npy"))
+        )
+
+        galaxy_image = galaxy_image.to(device)
+        galaxy_image = galaxy_image.reshape(1, 1, galaxy_image.size(-1), -1)
+
+        with torch.no_grad():
+            pred_image, _, _ = trained_galaxy_vae(galaxy_image, background)
+
         residual = (galaxy_image - pred_image) / torch.sqrt(galaxy_image)
+
+        # only expect tests to pass in cuda:
+        if not use_cuda:
+            return
 
         # check residuals follow gaussian noise, most pixels are between 68%
         n_pixels = residual.size(-1) ** 2
