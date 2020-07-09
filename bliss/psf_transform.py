d992ae5fc16780717f46c8e062cd7129893bd5fc
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-07-09 19:03:45 -0400

    refactoring psf_transform.py bc confused

--- bliss/psf_transform.py
+++ bliss/psf_transform.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
-from torch.nn.functional import unfold, pad
+from torch.nn.functional import pad
 from astropy.io import fits
 
 from .models import decoder
 from . import device
 
-########################
-# function for Power law PSF
-########################
+
 def get_psf_params(psfield_fit_file, bands):
     psfield = fits.open(psfield_fit_file)
 
@@ -40,37 +38,14 @@ def psf_fun(r, sigma1, sigma2, sigmap, beta, b, p0):
     return (term1 + term2 + term3) / (1 + b + p0)
 
 
-def get_psf(slen, psf_params, cached_radii_grid=None):
-    assert (slen % 2) == 1
-
-    if cached_radii_grid is None:
-        grid = decoder.get_mgrid(slen) * (slen - 1) / 2
-        radii_grid = (grid ** 2).sum(2).sqrt()
-    else:
-        radii_grid = cached_radii_grid
-
-    _psf_params = torch.exp(psf_params)
-    return psf_fun(
-        radii_grid,
-        _psf_params[0],
-        _psf_params[1],
-        _psf_params[2],
-        _psf_params[3],
-        _psf_params[4],
-        _psf_params[5],
-    )
-
-
 class PowerLawPSF(nn.Module):
     def __init__(self, init_psf_params, psf_slen=25, image_slen=101):
 
         super(PowerLawPSF, self).__init__()
-
         assert len(init_psf_params.shape) == 2
         assert image_slen % 2 == 1, "image_slen must be odd"
 
         self.n_bands = init_psf_params.shape[0]
-
         self.init_psf_params = init_psf_params.clone()
 
         self.psf_slen = psf_slen
@@ -85,24 +60,33 @@ class PowerLawPSF(nn.Module):
         # get normalization_constant
         self.normalization_constant = torch.zeros(self.n_bands)
         for i in range(self.n_bands):
-            self.normalization_constant[i] = (
-                1
-                / get_psf(
-                    self.psf_slen, self.init_psf_params[i], self.cached_radii_grid
-                ).sum()
+            psf_i = get_psf(
+                self.psf_slen, self.init_psf_params[i], self.cached_radii_grid
             )
+            self.normalization_constant[i] = 1 / psf_i.sum()
 
         # initial psf
         self.init_psf = self.get_psf()
         self.init_psf_sum = self.init_psf.sum(-1).sum(-1).detach()
 
+    def get_psf_single_band(self, psf_params):
+        assert (self.psf_slen % 2) == 1
+        _psf_params = torch.exp(psf_params)
+        return psf_fun(
+            self.cached_radii_grid,
+            _psf_params[0],
+            _psf_params[1],
+            _psf_params[2],
+            _psf_params[3],
+            _psf_params[4],
+            _psf_params[5],
+        )
+
     def get_psf(self):
         # TODO make the psf function vectorized ...
         for i in range(self.n_bands):
-            _psf = (
-                get_psf(self.psf_slen, self.params[i], self.cached_radii_grid)
-                * self.normalization_constant[i]
-            )
+            _psf = self.get_psf_single_band(self.params[i])
+            _psf *= self.normalization_constant[i]
 
             if i == 0:
                 psf = _psf.unsqueeze(0)
