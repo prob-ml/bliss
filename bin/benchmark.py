4a9b04f152eca945a9b045c0f532eb00c4686f1a
Zhe Zhao <zzhaozhe@umich.edu>
2020-07-27 11:19:56 -0400

    implement wake-phase profiler

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -4,8 +4,9 @@ import pathlib
 import torch
 from line_profiler import LineProfiler
 
-from bliss import sleep
+from bliss import sleep, wake
 from bliss.datasets.simulated import SimulatedDataset
+from bliss.models.decoder import get_mgrid
 
 
 # set up path
@@ -47,17 +48,50 @@ encoder_kwargs = dict(
 sleep_net = sleep.SleepPhase(dataset, encoder_kwargs)
 
 # profile the dataloader
-profile = LineProfiler(sleep_net.train_dataloader)
-profile.runcall(sleep_net.train_dataloader)
-profile.print_stats()
+sleep_profile = LineProfiler(sleep_net.train_dataloader)
+sleep_profile.runcall(sleep_net.train_dataloader)
 
 
 # profile the forward step
 with torch.no_grad():
     for batch_idx, batch in enumerate(sleep_net.train_dataloader()):
-        profile.add_function(sleep_net.training_step)
-        profile.runcall(sleep_net.training_step, batch, batch_idx)
-        profile.print_stats()
+        sleep_profile.add_function(sleep_net.training_step)
+        sleep_profile.runcall(sleep_net.training_step, batch, batch_idx)
+        sleep_profile.print_stats()
 
 # save the profile
-profile.dump_stats("sleep_dataloader_and_forward")
+sleep_profile.dump_stats("sleep_dataloader_and_forward")
+
+
+# Profile the wake face
+## set up
+test_star = torch.load(data_path.joinpath("3_star_test.pt"))
+test_image = test_star["images"]
+test_slen = test_image.size(-1)
+
+init_background_params = torch.zeros(1, 3, device=device)
+init_background_params[0, 0] = 686.0
+
+n_samples = 100
+hparams = {"n_samples": n_samples, "lr": 0.001}
+
+image_decoder = dataset.image_decoder
+image_decoder.slen = test_slen
+image_decoder.cached_grid = get_mgrid(test_slen)
+wake_phase_model = wake.WakeNet(
+    sleep_net.image_encoder, image_decoder, test_image, init_background_params, hparams,
+)
+
+# Profile wake phase dataloader
+wake_profile = LineProfiler(wake_phase_model.train_dataloader)
+wake_profile.runcall(wake_phase_model.train_dataloader)
+
+# Profile wake phase forward
+with torch.no_grad():
+    for batch_idx, batch in enumerate(wake_phase_model.train_dataloader()):
+        wake_profile.add_function(wake_phase_model.training_step)
+        wake_profile.runcall(wake_phase_model.training_step, batch, batch_idx)
+        wake_profile.print_stats()
+
+# save the profile
+wake_profile.dump_stats("wake_dataloader_and_forward")
