352e405d33c4a8add94bc248437137f83e9cd2e3
Zhe Zhao <zzhaozhe@umich.edu>
2020-07-27 12:48:42 -0400

    make user can specify sleep or wake to benchmark

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3
+import os
+import sys
 
-import pathlib
+path = os.path.abspath("..")
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+import argparse
 import torch
 from line_profiler import LineProfiler
 
-from bliss import sleep, wake
+from bliss.sleep import SleepPhase
+from bliss.wake import WakeNet
 from bliss.datasets.simulated import SimulatedDataset
 from bliss.models.decoder import get_mgrid
 
 
-# set up path
-root_path = pathlib.Path(__file__).parent.parent.absolute()
-data_path = root_path.joinpath("data")
+# sleep phase set up
+## set up path
+data_path = path + "/data"
 
-# set up Training class
-psf_file = data_path.joinpath("fitted_powerlaw_psf_params.npy")
+## set up Training class
+psf_file = data_path + "/fitted_powerlaw_psf_params.npy"
 psf_params = SimulatedDataset.get_psf_params_from_file(psf_file)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-background = torch.zeros(1, 50, 50, device=device)
+background = torch.zeros(1, 50, 50)
 background[0] = 686.0
 
 dec_args = (None, psf_params[range(1)], background)
@@ -45,53 +50,78 @@ encoder_kwargs = dict(
     n_galaxy_params=latent_dim,
 )
 
-sleep_net = sleep.SleepPhase(dataset, encoder_kwargs)
 
-# profile the dataloader
-sleep_profile = LineProfiler(sleep_net.train_dataloader)
-sleep_profile.runcall(sleep_net.train_dataloader)
-
-
-# profile the forward step
-with torch.no_grad():
-    for batch_idx, batch in enumerate(sleep_net.train_dataloader()):
-        sleep_profile.add_function(sleep_net.training_step)
-        sleep_profile.runcall(sleep_net.training_step, batch, batch_idx)
-        sleep_profile.print_stats()
-
-# save the profile
-sleep_profile.dump_stats("sleep_dataloader_and_forward")
+def benchamark_sleep():
+    sleep_net = SleepPhase(dataset, encoder_kwargs)
+    return sleep_net
 
 
-# Profile the wake face
-## set up
-test_star = torch.load(data_path.joinpath("3_star_test.pt"))
+# wake phase set up
+test_star = torch.load(data_path + "/3_star_test.pt")
 test_image = test_star["images"]
 test_slen = test_image.size(-1)
 
-init_background_params = torch.zeros(1, 3, device=device)
+init_background_params = torch.zeros(1, 3)
 init_background_params[0, 0] = 686.0
 
 n_samples = 100
 hparams = {"n_samples": n_samples, "lr": 0.001}
 
-image_decoder = dataset.image_decoder
-image_decoder.slen = test_slen
-image_decoder.cached_grid = get_mgrid(test_slen)
-wake_phase_model = wake.WakeNet(
-    sleep_net.image_encoder, image_decoder, test_image, init_background_params, hparams,
-)
 
-# Profile wake phase dataloader
-wake_profile = LineProfiler(wake_phase_model.train_dataloader)
-wake_profile.runcall(wake_phase_model.train_dataloader)
+def benchmark_wake():
+    image_decoder = dataset.image_decoder
+    image_decoder.slen = test_slen
+    image_decoder.cached_grid = get_mgrid(test_slen)
+    sleep_net = benchamark_sleep()
+    wake_phase_model = WakeNet(
+        sleep_net.image_encoder,
+        image_decoder,
+        test_image,
+        init_background_params,
+        hparams,
+    )
+    return wake_phase_model
 
-# Profile wake phase forward
-with torch.no_grad():
-    for batch_idx, batch in enumerate(wake_phase_model.train_dataloader()):
-        wake_profile.add_function(wake_phase_model.training_step)
-        wake_profile.runcall(wake_phase_model.training_step, batch, batch_idx)
-        wake_profile.print_stats()
 
-# save the profile
-wake_profile.dump_stats("wake_dataloader_and_forward")
+# add --wake and --sleep argument for users to benchmark different phase
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Benchmark Sleep Phase training.")
+    parser.add_argument(
+        "--sleep",
+        "-s",
+        action="store_true",
+        help="Activate the Bencharmark for the sleep phase training.",
+    )
+
+    parser.add_argument(
+        "--wake",
+        "-w",
+        action="store_true",
+        help="Activate the Bencharmark for the sleep phase training.",
+    )
+
+    args = parser.parse_args()
+
+    if args.sleep:
+        sleep_net = benchamark_sleep()
+        profile = LineProfiler(sleep_net.train_dataloader)
+        profile.runcall(sleep_net.train_dataloader)
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(sleep_net.train_dataloader()):
+                profile.add_function(sleep_net.training_step)
+                profile.runcall(sleep_net.training_step, batch, batch_idx)
+
+        print("Benchmark for the sleep phase training dataloader and forward")
+        profile.print_stats()
+
+    if args.wake:
+        wake_phase_model = benchmark_wake()
+        profile = LineProfiler(wake_phase_model.train_dataloader)
+        profile.runcall(wake_phase_model.train_dataloader)
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(wake_phase_model.train_dataloader()):
+                profile.add_function(wake_phase_model.training_step)
+                profile.runcall(wake_phase_model.training_step, batch, batch_idx)
+
+        print("Benchmark for the wake phase training dataloader and forward")
+        profile.print_stats()
