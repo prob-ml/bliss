588d6b3a05e42a5507956e1fce824754c67bf545
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-09-18 12:29:52 -0400

    merging after adding note

--- tests/test_sleep_wake.py
+++ tests/test_sleep_wake.py
@@ -44,6 +44,7 @@ def trained_encoder(star_dataset, encoder_setup, device_setup):
     return trained_encoder.to(device_setup.device)
 
 
+# TODO: Test unstable in GPU
 class TestStarSleepEncoder:
     @pytest.mark.parametrize("n_stars", ["1", "3"])
     def test_star_sleep(self, trained_encoder, n_stars, paths, device_setup):
