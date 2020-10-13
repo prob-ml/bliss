cee0dc090958a7d8f8216cd055e21cea754e1ab6
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-13 09:47:32 -0400

    update test configs

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,18 +1,10 @@
-from hydra.experimental import initialize, compose
-from bliss.datasets import simulated
-from bliss.sleep import SleepPhase
-
-
-def test_basic_configs():
+def test_basic_configs(get_dataset, get_trained_encoder):
     # test creating star model using config files.
-    with initialize(config_path="../config"):
-        cfg = compose("config", overrides=["model=basic_sleep_star"])
-        dataset = simulated.SimulatedDataset(cfg)
-        sleep = SleepPhase(cfg, dataset)
-        image_decoder = dataset.image_decoder
-        image_encoder = sleep.image_encoder
-
-        assert image_decoder.slen == image_encoder.slen
-        assert image_encoder.n_bands == image_decoder.n_bands == 1
-        assert image_encoder.n_galaxy_params == image_decoder.n_galaxy_params == 8
-        assert image_decoder.tile_slen == image_encoder.tile_slen
+    overrides = ["model=basic_sleep_star"]
+    dataset = get_dataset(overrides=overrides)
+    image_decoder = dataset.image_decoder
+    image_encoder = get_trained_encoder(1, dataset, overrides=overrides)
+    assert image_decoder.slen == image_encoder.slen
+    assert image_encoder.n_bands == image_decoder.n_bands == 1
+    assert image_encoder.n_galaxy_params == image_decoder.n_galaxy_params == 8
+    assert image_decoder.tile_slen == image_encoder.tile_slen
