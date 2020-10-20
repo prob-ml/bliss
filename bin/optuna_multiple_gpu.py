c9147f37d8a617e4fb8d4d5783dcfd32e58473e6
Zhe Zhao <zzhaozhe@umich.edu>
2020-10-19 21:20:21 -0400

    revise to fit in hydra

--- bin/optuna_multiple_gpu.py
+++ bin/optuna_multiple_gpu.py
@@ -1,12 +1,8 @@
-import os, sys
-
-path = os.path.abspath("..")
-if path not in sys.path:
-    sys.path.insert(0, path)
-print(path)
+import os
 import optuna
-import torch
-import numpy as np
+import hydra
+import warnings
+from omegaconf import DictConfig
 
 import multiprocessing as mp
 from joblib import parallel_backend
@@ -27,62 +23,20 @@ class MetricsCallback(Callback):
         self.metrics.append(trainer.callback_metrics)
 
 
-metrics_callback = MetricsCallback()
+@hydra.main(config_path="../config", config_name="config")
+def main(cfg: DictConfig):
 
-model_dir = os.getcwd()
-data_path = path + "/data"
+    model_dir = os.getcwd()
 
-# get psf
-psf_file = path + "/data/fitted_powerlaw_psf_params.npy"
-psf_params = torch.from_numpy(np.load(psf_file))
-psf_params = psf_params[range(1)]
+    monitor = "val_loss"
 
-# background
-background = torch.zeros(1, 50, 50)
-background[0] = 686.0
-
-# decoder arguments
-dec_args = (None, psf_params, background)
-dec_kwargs = {}
-dec_kwargs.update({"prob_galaxy": 0.0, "n_bands": 1, "slen": 50})
-
-# set up encoder
-encoder_kwargs = dict(
-    enc_conv_c=(5, 25, 5),
-    enc_kern=3,
-    enc_hidden=(64, 128, 64),
-    ptile_slen=8,
-    max_detections=2,
-    slen=50,
-    n_bands=1,
-    n_galaxy_params=8,
-)
-
-monitor = "val_loss"
-
-sleepobjectiveargs = {
-    "encoder_kwargs": encoder_kwargs,
-    "max_epochs": 100,
-    "lr": (1e-4, 1e-2),
-    "weight_decay": (1e-6, 1e-4),
-    "model_dir": model_dir,
-    "metrics_callback": metrics_callback,
-    "monitor": monitor,
-    "n_batches": 4,
-    "batch_size": 32,
-    "dec_args": dec_args,
-    "dec_kwargs": dec_kwargs,
-}
-
-if __name__ == "__main__":
     pruner = optuna.pruners.MedianPruner(n_startup_trials=30)
     study = optuna.create_study(
         storage="sqlite:///zz.db",
         direction="minimize",
         pruner=pruner,
     )
-    processes = []
-    n_gpu = (1, 3, 4, 5, 6, 7)
+    n_gpu = (1, 3, 5)
 
     # set up queue of devices
     mp.set_start_method("spawn")
@@ -90,12 +44,37 @@ if __name__ == "__main__":
     for i in n_gpu:
         gpu_queue.put(i)
 
-    sleepobjectiveargs["gpu_queue"] = gpu_queue
+    cfg.model.encoder.params.update(
+        {
+            "enc_conv_c": (5, 25, 5),
+            "enc_kern": 3,
+            "enc_hidden": (64, 128, 64),
+            "ptile_slen": 8,
+            "max_detections": 2,
+        }
+    )
+    print(cfg.model.encoder.params.enc_conv_c)
+    cfg.optimizer.params.update({"lr": (1e-4, 1e-2), "weight_decay": (1e-6, 1e-4)})
+    objects = SleepObjective(
+        cfg,
+        max_epochs=100,
+        model_dir=model_dir,
+        metrics_callback=MetricsCallback(),
+        monitor=monitor,
+        n_batches=4,
+        batch_size=32,
+        gpu_queue=gpu_queue,
+    )
 
     with parallel_backend("multiprocessing", n_jobs=len(n_gpu)):
+        warnings.simplefilter("error")
         study.optimize(
-            SleepObjective(**sleepobjectiveargs),
+            objects,
             n_trials=100,
             n_jobs=len(n_gpu),
             timeout=600,
         )
+
+
+if __name__ == "__main__":
+    main()
--- bliss/hyperparameter.py
+++ bliss/hyperparameter.py
@@ -1,6 +1,8 @@
 import os
 import torch
 import pytorch_lightning as pl
+from omegaconf import DictConfig
+import warnings
 from optuna.integration import PyTorchLightningPruningCallback
 
 from bliss.sleep import SleepPhase
@@ -10,46 +12,31 @@ from bliss.datasets.simulated import SimulatedDataset
 class SleepObjective(object):
     def __init__(
         self,
-        encoder_kwargs: dict,
+        cfg: DictConfig,
         max_epochs: int,
-        lr: tuple,
-        weight_decay: tuple,
         model_dir,
         metrics_callback,
         monitor,
         n_batches,
         batch_size,
-        dec_args,
-        dec_kwargs,
         single_gpu_id=1,
         gpu_queue=None,
     ):
-        # dataset set up
+
+        self.cfg = cfg
         self.n_batches = n_batches
         self.batch_size = batch_size
-        self.dec_args = dec_args
-        self.dec_kwargs = dec_kwargs
 
-        assert type(encoder_kwargs["enc_conv_c"]) is tuple
-        assert type(encoder_kwargs["enc_hidden"]) is tuple
-        assert (
-            len(encoder_kwargs["enc_conv_c"]) == 3
-            and len(encoder_kwargs["enc_hidden"]) == 3
-        )
-        self.encoder_kwargs = encoder_kwargs
-        self.enc_conv_c_min = self.encoder_kwargs["enc_conv_c"][0]
-        self.enc_conv_c_max = self.encoder_kwargs["enc_conv_c"][1]
-        self.enc_conv_c_int = self.encoder_kwargs["enc_conv_c"][2]
+        self.enc_conv_c_min = cfg.model.encoder.params.enc_conv_c[0]
+        self.enc_conv_c_max = cfg.model.encoder.params.enc_conv_c[1]
+        self.enc_conv_c_int = cfg.model.encoder.params.enc_conv_c[2]
 
-        self.enc_hidden_min = self.encoder_kwargs["enc_hidden"][0]
-        self.enc_hidden_max = self.encoder_kwargs["enc_hidden"][1]
-        self.enc_hidden_int = self.encoder_kwargs["enc_hidden"][2]
+        self.enc_hidden_min = cfg.model.encoder.params.enc_hidden[0]
+        self.enc_hidden_max = cfg.model.encoder.params.enc_hidden[1]
+        self.enc_hidden_int = cfg.model.encoder.params.enc_hidden[2]
 
-        assert type(lr) is tuple
-        assert type(weight_decay) is tuple
-        assert len(lr) == 2 and len(weight_decay) == 2
-        self.lr = lr
-        self.weight_decay = weight_decay
+        self.lr = cfg.optimizer.params.lr
+        self.weight_decay = cfg.optimizer.params.weight_decay
 
         self.max_epochs = max_epochs
         self.model_dir = model_dir
@@ -76,21 +63,20 @@ class SleepObjective(object):
         else:
             device = torch.device("cpu")
 
-        dec_args = list(self.dec_args)
-        dec_args[1] = dec_args[1].to(device)
-        dec_args = tuple(dec_args)
-        star_dataset = SimulatedDataset(
-            self.n_batches, self.batch_size, dec_args, self.dec_kwargs
+        self.cfg.dataset.params.update(
+            {"n_batches": self.n_batches, "batch_size": self.batch_size}
         )
 
-        self.encoder_kwargs["enc_conv_c"] = trial.suggest_int(
+        star_dataset = SimulatedDataset(cfg=self.cfg)
+
+        self.cfg.model.encoder.params["enc_conv_c"] = trial.suggest_int(
             "enc_conv_c",
             self.enc_conv_c_min,
             self.enc_conv_c_max,
             self.enc_conv_c_int,
         )
 
-        self.encoder_kwargs["enc_hidden"] = trial.suggest_int(
+        self.cfg.model.encoder.params["enc_hidden"] = trial.suggest_int(
             "enc_hidden",
             self.enc_hidden_min,
             self.enc_hidden_max,
@@ -101,30 +87,34 @@ class SleepObjective(object):
         weight_decay = trial.suggest_loguniform(
             "weight_decay", self.weight_decay[0], self.weight_decay[1]
         )
+        self.cfg.optimizer.params.update(dict(lr=lr, weight_decay=weight_decay))
 
         checkpoint_callback = pl.callbacks.ModelCheckpoint(
             os.path.join(self.model_dir, "trial_{}".format(trial.number), "{epoch}"),
             monitor="val_loss",
         )
 
-        model = SleepPhase(star_dataset, self.encoder_kwargs, lr, weight_decay)
+        model = SleepPhase(self.cfg, star_dataset)
 
         # put correct device to model
         use_gpu = [gpu_id] if self.gpu_queue is not None else [self.single_gpu]
         use_cpu = 0
 
-        trainer = pl.Trainer(
-            logger=False,
-            gpus=use_gpu if torch.cuda.is_available() else use_cpu,
-            checkpoint_callback=checkpoint_callback,
-            max_epochs=self.max_epochs,
-            callbacks=[self.metrics_callback],
-            early_stop_callback=PyTorchLightningPruningCallback(
-                trial, monitor=self.monitor
-            ),
-        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            # code that produces a warning
+            trainer = pl.Trainer(
+                logger=False,
+                gpus=use_gpu if torch.cuda.is_available() else use_cpu,
+                checkpoint_callback=checkpoint_callback,
+                max_epochs=self.max_epochs,
+                callbacks=[self.metrics_callback],
+                early_stop_callback=PyTorchLightningPruningCallback(
+                    trial, monitor=self.monitor
+                ),
+            )
 
-        trainer.fit(model)
+            trainer.fit(model)
 
         if self.gpu_queue is not None:
             self.gpu_queue.put(gpu_id)
