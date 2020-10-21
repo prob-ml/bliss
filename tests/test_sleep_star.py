bcde016e9f5387d873f49b8d8219569f864ea89f
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-10-21 13:24:57 -0400

    test_sleep_star.py passing basically just not sure how to sort

--- tests/test_sleep_star.py
+++ tests/test_sleep_star.py
@@ -14,7 +14,7 @@ class TestSleepStar:
         dataset = get_dataset(overrides)
         return get_trained_encoder(dataset, overrides)
 
-    @pytest.mark.parametrize("n_stars", ["1", "3"])
+    @pytest.mark.parametrize("n_stars", ["1", "2", "3"])
     def test_star_sleep(self, n_stars, trained_encoder, paths, devices):
         device = devices.device
         test_star = torch.load(paths["data"].joinpath(f"{n_stars}_star_test.pt"))
@@ -36,16 +36,26 @@ class TestSleepStar:
         if not devices.use_cuda:
             return
 
-        # test n_sources and locs
-        assert n_sources == test_star["n_sources"].to(device)
+        # test n_sources
+        true_n_sources = test_star["n_sources"].to(device).int().item()
+        n_sources = n_sources.int().item()
+        assert n_sources == true_n_sources
 
-        diff_locs = test_star["locs"].sort(1)[0].to(device) - locs.sort(1)[0]
+        # test locs.
+        true_locs = test_star["locs"].reshape(-1, 2).to(device).sort(1)[0]
+        true_locs = true_locs[:true_n_sources]
+        locs = locs.reshape(-1, 2).to(device).sort(1)[0]
+        locs = locs[:n_sources]
+
+        diff_locs = true_locs - locs
         diff_locs *= test_image.size(-1)
         assert diff_locs.abs().max() <= 0.5
 
         # test fluxes
-        diff = test_star["log_fluxes"].sort(1)[0].to(device) - log_fluxes.sort(1)[0]
-        assert torch.all(diff.abs() <= log_fluxes.sort(1)[0].abs() * 0.10)
-        assert torch.all(
-            diff.abs() <= test_star["log_fluxes"].sort(1)[0].abs().to(device) * 0.10
-        )
+        n_bands = log_fluxes.shape[-1]
+        true_log_fluxes = test_star["log_fluxes"].reshape(-1, n_bands)
+        true_log_fluxes = true_log_fluxes.to(device).sort(1)[0][:true_n_sources]
+        log_fluxes = log_fluxes.reshape(-1, n_bands).to(device).sort(1)[0][:n_sources]
+        diff = true_log_fluxes - log_fluxes
+        assert torch.all(diff.abs() <= log_fluxes * 0.1)
+        assert torch.all(diff.abs() <= true_log_fluxes * 0.1)
