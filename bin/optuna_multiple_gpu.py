2b1f5eac95f142b4c605d566ca26d83da61a81c1
Zhe Zhao <zzhaozhe@umich.edu>
2020-11-01 22:55:27 -0500

    fix the circle.ci and edit the bin.optuna_multiple_gpu

--- bin/optuna_multiple_gpu.py
+++ bin/optuna_multiple_gpu.py
@@ -3,81 +3,53 @@ import optuna
 import hydra
 from omegaconf import DictConfig
 
-import multiprocessing as mp
-from joblib import parallel_backend
-
-from pytorch_lightning import Callback
-
-from bliss.hyperparameter import SleepObjective
-
-
-class MetricsCallback(Callback):
-    """PyTorch Lightning metric callback."""
-
-    def __init__(self):
-        super().__init__()
-        self.metrics = []
-
-    def on_validation_end(self, trainer, pl_module):
-        self.metrics.append(trainer.callback_metrics)
+from bliss.hyperparameter import SleepTune
 
 
 @hydra.main(config_path="../config", config_name="config")
 def main(cfg: DictConfig):
-    # grid search
-    # search_space = {
-    #    "enc_conv_c": [5, 10],
-    #    "enc_hidden": [64, 128],
-    #    "learning rate": [0.001, 0.000492],
-    #    "weight_decay": [1e-6, 0.000003],
-    # }
 
+    # where to save the checkpoint
     model_dir = os.getcwd()
 
+    # which metric to monitor
     monitor = "val_loss"
 
     # set up pruner
-    pruner = optuna.pruners.MedianPruner(n_warmup_steps=20)
+    pruner = optuna.pruners.MedianPruner(n_warmup_steps=10)
 
-    # initialize study object
-    study = optuna.create_study(
-        storage="sqlite:///zz.db",
-        direction="minimize",
-        # sampler=optuna.samplers.GridSampler(search_space),
-        pruner=pruner,
-    )
-    n_gpu = (1, 4, 5)
+    # how many gpus do we need
+    n_gpu = 3
 
-    # set up queue of devices
-    # mp.set_start_method("spawn")
-    gpu_queue = mp.Manager().Queue()
-    for i in n_gpu:
-        gpu_queue.put(i)
-
+    # set up the config for the training
     cfg.model.encoder.params.update(
         {
             "enc_conv_c": (5, 25, 5),
             "enc_hidden": (64, 128, 64),
         }
     )
-    print(cfg.model.encoder.params.enc_conv_c)
+
     cfg.optimizer.params.update({"lr": (1e-4, 1e-2), "weight_decay": (1e-6, 1e-4)})
-    objects = SleepObjective(
+
+    SleepTune(
         cfg,
         max_epochs=100,
         model_dir=model_dir,
-        metrics_callback=MetricsCallback(),
         monitor=monitor,
         n_batches=4,
         batch_size=32,
-        gpu_queue=gpu_queue,
+        direction="minimize",
+        data_seed=10,
+        n_trials=100,
+        time_out=600,
+        gc_after_trial=True,
+        sampler=None,  # use the default sampler of the optuna
+        pruner=pruner,
+        num_gpu=n_gpu,
+        storage="sqlite:///zz.db",
     )
 
-    with parallel_backend("loky", n_jobs=len(n_gpu)):
-        # warnings.simplefilter("error")
-        study.optimize(
-            objects, n_trials=100, n_jobs=len(n_gpu), timeout=600, gc_after_trial=True
-        )
+    print("Tunning is finished")
 
 
 if __name__ == "__main__":
