e7ef598d6dd345ce7a6108460b8fceb382a62db7
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-11-13 08:15:45 -0500

    combined dataset+datamodule into one

--- bliss/datasets/simulated.py
+++ bliss/datasets/simulated.py
@@ -1,47 +1,27 @@
 from omegaconf import DictConfig
+import pytorch_lightning as pl
+
 import torch
 from torch.utils.data import IterableDataset, Dataset
-import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 from bliss.models.decoder import ImageDecoder
 
 
-class SimulatedModule(pl.LightningDataModule):
-    def __init__(self, cfg: DictConfig):
-        super(SimulatedModule, self).__init__()
-        self.cfg = cfg
-        self.dataset = SimulatedDataset(cfg)
-
-        # check datamodule is sensible for sleep training.
-        n_tiles_per_image = self.dataset.image_decoder.n_tiles_per_image
-        total_ptiles = n_tiles_per_image * self.dataset.batch_size
-        assert total_ptiles > 1, "Need at least 2 tiles in each batch."
-
-    def train_dataloader(self):
-        return DataLoader(self.dataset, batch_size=None)
-
-    def val_dataloader(self):
-        return DataLoader(self.dataset, batch_size=None)
-
-    def test_dataloader(self):
-        if self.cfg.testing.file is None:
-            return DataLoader(self.dataset, batch_size=None)
-
-        else:
-            test_dataset = SavedSimulated(self.cfg.testing.file)
-            batch_size = self.cfg.testing.batch_size
-            return DataLoader(test_dataset, batch_size=batch_size, num_workers=0)
-
-
-class SimulatedDataset(IterableDataset):
+class SimulatedDataset(pl.LightningDataModule, IterableDataset):
     def __init__(self, cfg: DictConfig):
         super(SimulatedDataset, self).__init__()
+        self.cfg = cfg
 
         self.n_batches = cfg.dataset.params.n_batches
         self.batch_size = cfg.dataset.params.batch_size
         self.image_decoder = ImageDecoder(**cfg.model.decoder.params)
-        self.image_decoder.requires_grad_(False)
+        self.image_decoder.requires_grad_(False)  # freeze decoder weights.
+
+        # check sleep training will work.
+        n_tiles_per_image = self.image_decoder.n_tiles_per_image
+        total_ptiles = n_tiles_per_image * self.batch_size
+        assert total_ptiles > 1, "Need at least 2 tiles in each batch."
 
     def __iter__(self):
         return self.batch_generator()
@@ -66,17 +46,34 @@ class SimulatedDataset(IterableDataset):
 
         return batch
 
+    def train_dataloader(self):
+        return DataLoader(self, batch_size=None)
 
-class SavedSimulated(Dataset):
+    def val_dataloader(self):
+        return DataLoader(self, batch_size=None)
+
+    def test_dataloader(self):
+        if self.cfg.testing.file is None:
+            return DataLoader(self, batch_size=None)
+
+        else:
+            test_dataset = SavedDataset(self.cfg.testing.file)
+            batch_size = self.cfg.testing.batch_size
+            return DataLoader(test_dataset, batch_size=batch_size, num_workers=0)
+
+
+class SavedDataset(Dataset):
     def __init__(self, pt_file="example.pt"):
         """A dataset created from simulated batches saved as a single dict created from
-        SimulatedDataset. Hint: Create a single big batch."""
-        super().__init__()
+        bin/generate.py"""
+        super(SavedDataset, self).__init__()
 
-        self.data = torch.load(pt_file)
-        assert isinstance(self.data, dict)
-        self.background = self.data.pop("background")
+        data = torch.load(pt_file)
+        assert isinstance(data, dict)
+
+        self.data = data
         self.size = self.data["images"].shape[0]
+        self.background = self.data.pop("background")
 
     def __len__(self):
         """Number of batches saved in the file."""
