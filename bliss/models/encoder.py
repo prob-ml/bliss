237ff8cdccbb809f8d9012479667e435a449feba
Ismael Salvador Mendoza Serrano <imendoza@umich.edu>
2020-11-19 17:26:34 -0500

    slight change of order

--- bliss/models/encoder.py
+++ bliss/models/encoder.py
@@ -156,11 +156,11 @@ class ImageEncoder(nn.Module):
         ptile_slen=8,
         max_detections=2,
         n_galaxy_params=8,
-        background_pad_value=686.0,
         enc_conv_c=20,
         enc_kern=3,
         enc_hidden=256,
         momentum=0.5,
+        background_pad_value=686.0,
         pad_border_w_constant=True,
     ):
         """
