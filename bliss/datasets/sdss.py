87097dafc3bc620beafd571fa77ade2af042d66b
Derek Hansen <dereklh@umich.edu>
2021-01-20 17:28:03 -0500

    Reformat the sdss.py file with black

--- bliss/datasets/sdss.py
+++ bliss/datasets/sdss.py
@@ -12,16 +12,18 @@ from astropy.io import fits
 from astropy.wcs import WCS, FITSFixedWarning
 from math import floor, ceil
 
+
 def construct_subpixel_grid_base(size_x, size_y):
     grid_x = np.linspace(-1.0, 1.0, num=size_x)
     grid_y = np.linspace(-1.0, 1.0, num=size_y)
 
-    G_x    = torch.stack([torch.from_numpy(grid_x)]*size_y, dim=0)
-    G_y    = torch.stack([torch.from_numpy(grid_y)]*size_x, dim=1)
+    G_x = torch.stack([torch.from_numpy(grid_x)] * size_y, dim=0)
+    G_y = torch.stack([torch.from_numpy(grid_y)] * size_x, dim=1)
 
-    G      = torch.stack([G_x, G_y], dim=2)
+    G = torch.stack([G_x, G_y], dim=2)
 
     return G
+
 
 def construct_subpixel_grid(G, shift_x, shift_y):
     G_shift = G.clone()
@@ -29,29 +31,34 @@ def construct_subpixel_grid(G, shift_x, shift_y):
     G_shift[:, :, 1] += shift_y
     return G_shift
 
+
 def center_stamp_subpixel(stamp, pt, pr, G):
     stamp_torch = torch.from_numpy(stamp)
     size_y, size_x = stamp.shape
-    shift_x     = 2*(pt - int(pt + 0.5)) / size_x
-    shift_y     = 2*(pr - int(pr + 0.5)) / size_y
-    G_shift     = construct_subpixel_grid(G, shift_x, shift_y).float()
-    stamp_shifted = F.grid_sample(stamp_torch.unsqueeze(0).unsqueeze(0), G_shift.unsqueeze(0), align_corners=False)
+    shift_x = 2 * (pt - int(pt + 0.5)) / size_x
+    shift_y = 2 * (pr - int(pr + 0.5)) / size_y
+    G_shift = construct_subpixel_grid(G, shift_x, shift_y).float()
+    stamp_shifted = F.grid_sample(
+        stamp_torch.unsqueeze(0).unsqueeze(0), G_shift.unsqueeze(0), align_corners=False
+    )
     return stamp_shifted.squeeze(0).squeeze(0).numpy()
+
 
 def read_psf(psf_fit_file, band=2):
     psfield = fitsio.FITS(psf_fit_file)
-    hdu     = psfield[band+1]
-    psf     = hdu.read()
-    return {'hdu': hdu, 'psf': psf}
+    hdu = psfield[band + 1]
+    psf = hdu.read()
+    return {"hdu": hdu, "psf": psf}
+
 
 class SdssPSF:
     def __init__(self, psf_fit_file, bands):
         self.psf_fit_file = psf_fit_file
-        self.bands        = bands
+        self.bands = bands
         self.init_cache()
 
     def init_cache(self):
-        self.cache        = [None] * len(self.bands)
+        self.cache = [None] * len(self.bands)
 
     def __getitem__(self, idx):
         if self.cache[idx] is None:
@@ -59,9 +66,10 @@ class SdssPSF:
             self.cache[idx] = x
         return self.cache[idx]
 
+
 def psf_at_points(x, y, psf_data):
-    hdu = psf_data['hdu']
-    psf = psf_data['psf']
+    hdu = psf_data["hdu"]
+    psf = psf_data["psf"]
     # psfield = fitsio.FITS(psf_fit_file)
     # hdu = psfield[3]
 
@@ -88,7 +96,9 @@ def psf_at_points(x, y, psf_data):
         (gridi, gridj) = np.meshgrid(range(nrb), range(ncb))
 
         if psfimgs is None:
-            psfimgs = [np.zeros_like(hdu["rrows"].read()[k]) for xy in np.broadcast(x, y)]
+            psfimgs = [
+                np.zeros_like(hdu["rrows"].read()[k]) for xy in np.broadcast(x, y)
+            ]
             (outh, outw) = (hdu["rnrow"].read()[k], hdu["rncol"].read()[k])
 
         for i, (xi, yi) in enumerate(np.broadcast(x, y)):
@@ -100,14 +110,28 @@ def psf_at_points(x, y, psf_data):
     if rtnscalar:
         return psfimgs[0]
     return psfimgs
+
+
 class SloanDigitalSkySurvey(Dataset):
-    def __init__(self, sdss_dir, run=3900, camcol=6, fields=None, bands=[2], stampsize=5, overwrite_fits_cache=False, overwrite_psf_cache=True, overwrite_cache=False, center_subpixel=True):
+    def __init__(
+        self,
+        sdss_dir,
+        run=3900,
+        camcol=6,
+        fields=None,
+        bands=[2],
+        stampsize=5,
+        overwrite_fits_cache=False,
+        overwrite_psf_cache=True,
+        overwrite_cache=False,
+        center_subpixel=True,
+    ):
         super(SloanDigitalSkySurvey, self).__init__()
 
         self.sdss_path = pathlib.Path(sdss_dir)
         self.rcfgcs = []
         self.bands = bands
-        self.stampsize=stampsize
+        self.stampsize = stampsize
         self.overwrite_cache = overwrite_cache
         self.center_subpixel = center_subpixel
 
@@ -126,42 +150,58 @@ class SloanDigitalSkySurvey(Dataset):
             gain = fieldgains[i]
             if (not fields) or _field in fields:
                 # load the catalog distributed with SDSS
-                po_file  = "photoObj-{:06d}-{:d}-{:04d}.fits".format(run, camcol, _field)
+                po_file = "photoObj-{:06d}-{:d}-{:04d}.fits".format(run, camcol, _field)
                 po_cache = "photoObj-{:06d}-{:d}-{:04d}.pkl".format(run, camcol, _field)
-                po_path  = camcol_path.joinpath(str(_field), po_file)
+                po_path = camcol_path.joinpath(str(_field), po_file)
                 po_cache_path = camcol_path.joinpath(str(_field), po_cache)
                 if (not po_cache_path.exists()) or (overwrite_fits_cache):
                     try:
                         po_fits = fits.getdata(po_path)
                     except IndexError as e:
-                            print("Warning: IndexError while accessing field: {}. This field will not be included.".format(_field))
-                            print(e)
-                            po_fits = None
+                        print(
+                            "Warning: IndexError while accessing field: {}. This field will not be included.".format(
+                                _field
+                            )
+                        )
+                        print(e)
+                        po_fits = None
                     pickle.dump(po_fits, po_cache_path.open("wb+"))
                 else:
                     po_fits = pickle.load(po_cache_path.open("rb+"))
                     if po_fits is None:
-                        print("Warning: cached data for field {} is None. This field will not be included".format(_field))
+                        print(
+                            "Warning: cached data for field {} is None. This field will not be included".format(
+                                _field
+                            )
+                        )
 
                 # Load the PSF produced by SDSS
-                psf_file  = "psField-{:06d}-{:d}-{:04d}.fits".format(run, camcol, _field)
+                psf_file = "psField-{:06d}-{:d}-{:04d}.fits".format(run, camcol, _field)
                 psf_cache = "psField-{:06d}-{:d}-{:04d}.pkl".format(run, camcol, _field)
 
-                psf_path       = camcol_path.joinpath(str(_field), psf_file)
+                psf_path = camcol_path.joinpath(str(_field), psf_file)
                 psf_cache_path = camcol_path.joinpath(str(_field), psf_cache)
                 if (not psf_cache_path.exists()) or (overwrite_psf_cache):
                     try:
                         psf = SdssPSF(psf_path.as_posix(), bands)
                     except IndexError as e:
-                            print("Warning: IndexError while accessing field: {}. This field will not be included.".format(_field))
-                            print(e)
-                            psf = None
+                        print(
+                            "Warning: IndexError while accessing field: {}. This field will not be included.".format(
+                                _field
+                            )
+                        )
+                        print(e)
+                        psf = None
                     pickle.dump(psf, psf_cache_path.open("wb+"))
                 else:
-                    raise(Exception("cannot currently use cache with PSF"))
+                    raise (Exception("cannot currently use cache with PSF"))
                     psf = pickle.load(psf_cache_path.open("rb+"))
                     if psf is None:
-                        print("Warning: cached psf data for field {} is None.".format(_field))
+                        print(
+                            "Warning: cached psf data for field {} is None.".format(
+                                _field
+                            )
+                        )
 
                 if po_fits is not None:
                     self.rcfgcs.append((run, camcol, _field, gain, po_fits, psf))
@@ -192,32 +232,34 @@ class SloanDigitalSkySurvey(Dataset):
         flxs = []
 
         G = construct_subpixel_grid_base(self.stampsize, self.stampsize)
-        for (ra, dec, f, flux) in zip(ras, decs, po_fits["thing_id"][is_target], fluxes):
+        for (ra, dec, f, flux) in zip(
+            ras, decs, po_fits["thing_id"][is_target], fluxes
+        ):
             # pt = "time" in pixel coordinates
             pt, pr = wcs.wcs_world2pix(ra, dec, 0)
             pti, pri = int(pt + 0.5), int(pr + 0.5)
 
-            row_lower = pri - self.stampsize//2
-            row_upper = pri + self.stampsize//2 + 1
-            col_lower = pti - self.stampsize//2
-            col_upper = pti + self.stampsize//2 + 1
-            edge_stamp=False
+            row_lower = pri - self.stampsize // 2
+            row_upper = pri + self.stampsize // 2 + 1
+            col_lower = pti - self.stampsize // 2
+            col_upper = pti + self.stampsize // 2 + 1
+            edge_stamp = False
             if row_lower < 0:
                 row_lower = 0
                 row_upper = self.stampsize
-                edge_stamp=True
+                edge_stamp = True
             if row_upper > img.shape[0]:
                 row_lower = img.shape[0] - self.stampsize
                 row_upper = img.shape[0]
-                edge_stamp=True
+                edge_stamp = True
             if col_lower < 0:
                 col_lower = 0
                 col_upper = self.stampsize
-                edge_stamp=True
+                edge_stamp = True
             if col_upper > img.shape[1]:
                 col_lower = img.shape[1] - self.stampsize
                 col_upper = img.shape[1]
-                edge_stamp=True
+                edge_stamp = True
             if (not edge_stamp) or allow_edge:
                 stamp = img[row_lower:row_upper, col_lower:col_upper]
                 if self.center_subpixel:
@@ -229,7 +271,13 @@ class SloanDigitalSkySurvey(Dataset):
                 bgs.append(stamp_bg)
                 flxs.append(flux)
 
-        return np.asarray(stamps), np.asarray(pts), np.asarray(prs), np.asarray(flxs), np.asarray(bgs)
+        return (
+            np.asarray(stamps),
+            np.asarray(pts),
+            np.asarray(prs),
+            np.asarray(flxs),
+            np.asarray(bgs),
+        )
 
     def get_from_disk(self, idx, verbose=False):
         if self.rcfgcs[idx] is None:
@@ -300,12 +348,14 @@ class SloanDigitalSkySurvey(Dataset):
 
             frame.close()
 
-        stamps, pts, prs, fluxes, stamp_bgs = self.fetch_bright_stars(po_fits, image_list[2], wcs_list[2], background_list[2], psf[2])
+        stamps, pts, prs, fluxes, stamp_bgs = self.fetch_bright_stars(
+            po_fits, image_list[2], wcs_list[2], background_list[2], psf[2]
+        )
         stamp_psfs = np.asarray(psf_at_points(pts, prs, psf[2]))
         if len(stamp_bgs) > 0:
             psf_center = stamp_psfs.shape[-1] // 2
-            psf_lower  = psf_center - floor(self.stampsize/2)
-            psf_upper  = psf_center + ceil(self.stampsize/2)
+            psf_lower = psf_center - floor(self.stampsize / 2)
+            psf_upper = psf_center + ceil(self.stampsize / 2)
             stamp_psfs = stamp_psfs[:, psf_lower:psf_upper, psf_lower:psf_upper]
 
         ret = {
@@ -321,7 +371,7 @@ class SloanDigitalSkySurvey(Dataset):
             "prs": prs,
             "fluxes": fluxes,
             "bright_star_bgs": stamp_bgs,
-            "sdss_psfs" : stamp_psfs
+            "sdss_psfs": stamp_psfs,
         }
         pickle.dump(ret, cache_path.open("wb+"))
 
