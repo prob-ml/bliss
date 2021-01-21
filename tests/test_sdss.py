19f91baffb37a38f3f6c81a5151217714bb75ff2
Derek Hansen <dereklh@umich.edu>
2021-01-21 11:27:59 -0500

    Updated test for better coverage of SDSS:

--- tests/test_sdss.py
+++ tests/test_sdss.py
@@ -8,14 +8,40 @@ class TestSDSS:
     def test_sdss(self, paths):
         sdss_dir = paths["data"].joinpath("sdss")
         sdss_obj = sdss.SloanDigitalSkySurvey(
-            sdss_dir, run=3900, camcol=6, fields=[269], bands=range(5)
+            sdss_dir,
+            run=3900,
+            camcol=6,
+            fields=[269],
+            bands=range(5),
+            stampsize=5,
+            overwrite_cache=True,
+            overwrite_fits_cache=True,
+            overwrite_psf_cache=True,
         )
         assert sdss_obj[0]["gain"][3] == pytest.approx(4.76)
 
         assert len(sdss_obj[0]["bright_stars"]) == 43
+        assert sdss_obj[0]["bright_stars"].shape[1] == 5
+        assert sdss_obj[0]["bright_stars"].shape[2] == 5
         super_star = sdss_obj[0]["bright_stars"].sum(axis=0)
         assert np.all(super_star[2, 2] + 1e-4 >= super_star)
 
-        cache_file = sdss_dir.joinpath("3900/6/269/cache.pkl")
-        if os.path.exists(cache_file):
-            os.remove(cache_file)
+        assert len(sdss_obj[0]["sdss_psfs"]) == 43
+
+        sdss_obj9 = sdss.SloanDigitalSkySurvey(
+            sdss_dir, run=3900, camcol=6, fields=[269, 745], bands=range(5), stampsize=9
+        )
+
+        assert sdss_obj9[0]["bright_stars"].shape[1] == 9
+        assert sdss_obj9[0]["bright_stars"].shape[2] == 9
+
+        sdss_obj9_cached = sdss.SloanDigitalSkySurvey(
+            sdss_dir, run=3900, camcol=6, fields=[269, 745], bands=range(5), stampsize=9
+        )
+
+        assert sdss_obj9_cached[0]["bright_stars"].shape[1] == 9
+        assert sdss_obj9_cached[0]["bright_stars"].shape[2] == 9
+
+        sdss_obj.clear_cache()
+        sdss_obj9.clear_cache()
+        sdss_obj9_cached.clear_cache()
