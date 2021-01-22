bf457391e47550ffd63b7b1bf57dc4099e4ab05e
Derek Hansen <dereklh@umich.edu>
2021-01-22 16:07:27 -0500

    Made onedim fnp example a test
    This commit moves a lot of the code that was in the
    onedim.py file into the main fnp.py file. This lets us
    write tests using the one dimensional FNP example
    
    Squashed commit of the following:
    
    commit 0db2d3e032d9e8fd4481a8311379234b57c6384f
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 16:06:36 2021 -0500
    
        Incorporated the one dimensional example as a unit test for FNP
    
    commit cda0ce36ff39f49cc2c19ef27dba796ceaf47740
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 15:37:44 2021 -0500
    
        Got the RegressionFNP tested in one dimensional case
    
    commit 754c9093abae98915db50da1e81bbd1e0a3d6450
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 14:17:46 2021 -0500
    
        Moved onedim training and visualize to fnp.py
    
    commit 41aa5921416fcded2a55a97ef208dc0baf934fd1
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 14:11:03 2021 -0500
    
        Made od an argument rather than global
    
    commit 5dd7c50be4479d8c8b5bbf569d747e36c21ed75b
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 14:06:23 2021 -0500
    
        Moved OneDimDataset into fnp.py
    
    commit 263f8397be6ecba7faba758691ae501fa6b8161c
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 14:01:56 2021 -0500
    
        ensure that the indices match those from before
    
    commit 2c22b0d438a6fb81a76ceb0eb988ff48f24d4917
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 13:51:59 2021 -0500
    
        Deleted old onedim dataset code
    
    commit dc90af2fc86ab50e14d90a77e02f329456d3496e
    Author: Derek Hansen <dereklh@umich.edu>
    Date:   Fri Jan 22 13:18:31 2021 -0500
    
        Moved onedim dataset into a class

--- case_studies/fnp/one_dim.py
+++ case_studies/fnp/one_dim.py
@@ -43,7 +43,12 @@ path = os.path.abspath("..")
 if path not in sys.path:
     sys.path.insert(0, path)
 import numpy as np
-from bliss.models.fnp import RegressionFNP, PoolingFNP
+from bliss.models.fnp import (
+    RegressionFNP,
+    PoolingFNP,
+    OneDimDataset,
+    train_onedim_model,
+)
 from sklearn.preprocessing import StandardScaler
 import torch
 from torch.optim import Adam
@@ -53,222 +58,14 @@ import math
 import matplotlib.pyplot as plt
 
 plt.style.use(["seaborn-whitegrid", "seaborn-colorblind", "seaborn-notebook"])
+VISUALIZE = True
 warnings.filterwarnings("ignore")
 
-
 # %%
-def visualize(
-    model,
-    dx,
-    stdx,
-    stdy,
-    f,
-    cond_x=None,
-    cond_y=None,
-    all_x=None,
-    all_y=None,
-    samples=30,
-    range_y=(-100.0, 100.0),
-    title="",
-    train=False,
-):
-    """
-    Visualizes the predictive distribution
-    """
-    dxy = np.zeros((dx.shape[0], samples))
-    if not train:
-        model.eval()
-    with torch.no_grad():
-        dxi = torch.from_numpy(stdx.transform(dx).astype(np.float32))
-        if torch.cuda.is_available():
-            dxi = dxi.cuda()
-        for j in range(samples):
-            dxy[:, j] = (
-                model.predict(dxi, cond_x, cond_y.unsqueeze(0)).cpu().numpy().ravel()
-            )
-    print()
-
-    plt.figure()
-    mean_dxy, std_dxy = dxy.mean(axis=1), dxy.std(axis=1)
-    # smooth it in order to avoid the sampling jitter
-    mean_dxys = savgol_filter(mean_dxy, 61, 3)
-    std_dxys = savgol_filter(std_dxy, 61, 3)
-
-    if torch.cuda.is_available():
-        all_x, all_y, cond_x, cond_y = (
-            all_x.cpu(),
-            all_y.cpu(),
-            cond_x.cpu(),
-            cond_y.cpu(),
-        )
-
-    if stdy is None:
-        y_invt = lambda x: x
-    else:
-        y_invt = stdy.inverse_transform
-
-    plt.plot(dx.ravel(), mean_dxys, label="Mean function")
-    dxf = f(dx, 0.0)
-    plt.plot(dx.ravel(), dxf.ravel(), label="Actual function")
-    plt.plot(
-        stdx.inverse_transform(all_x.data.numpy()).ravel(),
-        y_invt(all_y.data.numpy()).ravel(),
-        "o",
-        label="Observations",
-    )
-    if cond_x is not None:
-        plt.plot(
-            stdx.inverse_transform(cond_x.data.numpy()).ravel(),
-            y_invt(cond_y.data.numpy()).ravel(),
-            "o",
-            label="Reference",
-        )
-    plt.fill_between(
-        dx.ravel(), mean_dxys - 1.0 * std_dxys, mean_dxys + 1.0 * std_dxys, alpha=0.1
-    )
-    plt.fill_between(
-        dx.ravel(), mean_dxys - 2.0 * std_dxys, mean_dxys + 2.0 * std_dxys, alpha=0.1
-    )
-    plt.fill_between(
-        dx.ravel(), mean_dxys - 3.0 * std_dxys, mean_dxys + 3.0 * std_dxys, alpha=0.1
-    )
-
-    plt.xlim([np.min(dx), np.max(dx)])
-    plt.ylim(range_y)
-    plt.legend(
-        loc="upper center",
-        bbox_to_anchor=(0.5, 1.10),
-        ncol=3,
-        fancybox=False,
-        shadow=False,
-    )
-    plt.title(title)
-    model.train()
-    plt.show()
-
-
-# %%
-def dataset():
-    N, num_extra = 20, 500
-    N_rows = 100
-    np.random.seed(1)
-    x = np.concatenate(
-        [
-            np.random.uniform(low=0, high=0.6, size=(N - 8, 1)),
-            np.random.uniform(low=0.8, high=1.0, size=(8, 1)),
-        ],
-        axis=0,
-    )
-    offset = 0.1
-    eps = np.random.normal(0.0, 0.03, size=(x.shape[0], 1))
-    f = lambda x, eps: x + np.sin(4 * (x + eps)) + np.sin(13 * (x + eps)) + eps
-    y = f(x, eps)
-
-    dx = np.linspace(-1.0, 2.0, num_extra).astype(np.float32)[:, np.newaxis]
-    return x, y, dx, f
-
-
-# %%
-def dataset_offset(x):
-    N, num_extra = 20, 500
-    N_rows = 100
-    x = x + np.random.normal()
-    offset = 0.1
-    eps = np.random.normal(0.0, 0.03, size=(x.shape[0], 1))
-    f = lambda x, eps: x + np.sin(4 * (x + eps)) + np.sin(13 * (x + eps)) + eps
-    y = f(x, eps)
-    return y
-
-
-# %%
-def train_model(model, epochs=10000, lr=1e-4):
-    if torch.cuda.is_available():
-        model = model.cuda()
-    optimizer = Adam(model.parameters(), lr=lr)
-    model.train()
-    holdout_loss_prev = np.infty
-    for i in range(epochs):
-        optimizer.zero_grad()
-
-        loss = model(XR, yR, XM, yM)
-        loss.backward()
-        optimizer.step()
-
-        if i % int(epochs / 10) == 0:
-            print("Epoch {}/{}, loss: {:.3f}".format(i, epochs, loss.item()))
-
-            visualize(
-                model,
-                dx,
-                stdx,
-                None,
-                f,
-                cond_x=XR,
-                cond_y=yR[0],
-                all_x=X,
-                all_y=y[0],
-                range_y=(-2.0, 3.0),
-                samples=100,
-            )
-            holdout_loss = model(XR, yhR, XM, yhM)[0]
-            print("Holdout loss: {:.3f}".format(holdout_loss))
-    visualize(
-        model,
-        dx,
-        stdx,
-        None,
-        f,
-        cond_x=XR,
-        cond_y=yR[0],
-        all_x=X,
-        all_y=y[0],
-        range_y=(-2.0, 3.0),
-        samples=100,
-    )
-    print("Done.")
-    return model
-
-
-# %%
-X, yb, dx, f = dataset()
-idx = np.arange(X.shape[0])
-# idxR = np.random.choice(idx, size=(10,), replace=False)
-idxR = np.array([2, 4, 6, 12, 16, 13, 3, 5, 9, 1])
-idxM = np.array([i for i in idx if i not in idxR.tolist()])
-ys = [yb]
-for i in range(99):
-    ys.append(dataset_offset(X))
-y = np.concatenate(ys, axis=1).transpose()
-ys = []
-for i in range(10):
-    ys.append(dataset_offset(X))
-yh = np.concatenate(ys, axis=1).transpose()
-
-stdx, stdy = StandardScaler().fit(X), StandardScaler().fit(y.reshape(-1, 1))
-# X, y = stdx.transform(X), stdy.transform(y)
-X = stdx.transform(X)
-idx = np.arange(X.shape[0])
-idxR = np.random.choice(idx, size=(10,), replace=False)
-idxM = np.array([i for i in idx if i not in idxR.tolist()])
-
-X, y = torch.from_numpy(X.astype(np.float32)), torch.from_numpy(y.astype(np.float32))
-y = y.unsqueeze(2)
-XR, yR = X[idxR], y[:, idxR]
-XM, yM = X[idxM], y[:, idxM]
-
-
-# %%
-yh = torch.from_numpy(yh.astype(np.float32))
-yh = yh.unsqueeze(2)
-yhR = yh[:, idxR]
-yhM = yh[:, idxM]
-
-
+od = OneDimDataset()
 # %%
 if torch.cuda.is_available():
-    XR, XM, X = XR.cuda(), XM.cuda(), X.cuda()
-    yR, yM, y = yR.cuda(), yM.cuda(), y.cuda()
-    yhR, yhM, yh = yhR.cuda(), yhM.cuda(), yh.cuda()
+    od.cuda()
 
 # %% [markdown]
 # ## FNP
@@ -281,11 +78,11 @@ torch.manual_seed(5)
 fnp = RegressionFNP(
     dim_x=1,
     dim_y=1,
-    transf_y=stdy,
+    transf_y=od.stdy,
     dim_h=100,
     dim_u=3,
     n_layers=1,
-    num_M=XM.size(0),
+    num_M=od.XM.size(0),
     dim_z=50,
     fb_z=1.0,
     use_plus=False,
@@ -294,7 +91,9 @@ fnp = RegressionFNP(
 if torch.cuda.is_available():
     fnp = fnp.cuda()
 
-fnp = train_model(fnp, epochs=10000, lr=1e-4)
+fnp, _, fnp_loss_final = train_onedim_model(
+    fnp, od, epochs=10000, lr=1e-4, visualize=VISUALIZE
+)
 
 # %% [markdown]
 # ## FNP+
@@ -303,16 +102,18 @@ fnp = train_model(fnp, epochs=10000, lr=1e-4)
 fnpp = RegressionFNP(
     dim_x=1,
     dim_y=1,
-    transf_y=stdy,
+    transf_y=od.stdy,
     dim_h=100,
     dim_u=3,
     n_layers=1,
-    num_M=XM.size(0),
+    num_M=od.XM.size(0),
     dim_z=50,
     fb_z=1.0,
     use_plus=True,
 )
-fnpp = train_model(fnpp, epochs=10000, lr=1e-4)
+fnpp, _, fnpp_loss_final = train_onedim_model(
+    fnpp, od, epochs=10000, lr=1e-4, visualize=VISUALIZE
+)
 
 # %% [markdown]
 # ## Attention FNP
@@ -325,14 +126,16 @@ attt = PoolingFNP(
     dim_h=100,
     dim_u=3,
     n_layers=1,
-    num_M=XM.size(0),
+    num_M=od.XM.size(0),
     dim_z=50,
     fb_z=1.0,
     use_plus=False,
     use_direction_mu_nu=True,
     set_transformer=True,
 )
-attt = train_model(attt, epochs=10000, lr=1e-4)
+attt, _, attt_loss_final = train_onedim_model(
+    attt, od, epochs=10000, lr=1e-4, visualize=VISUALIZE
+)
 
 # %% [markdown]
 # ## Deep-set FNP
@@ -341,15 +144,17 @@ attt = train_model(attt, epochs=10000, lr=1e-4)
 poolnp = PoolingFNP(
     dim_x=1,
     dim_y=1,
-    transf_y=stdy,
+    transf_y=od.stdy,
     dim_h=100,
     dim_u=3,
     n_layers=1,
-    num_M=XM.size(0),
+    num_M=od.XM.size(0),
     dim_z=50,
     fb_z=0.5,
     use_plus=False,
     use_direction_mu_nu=True,
     set_transformer=False,
 )
-poolnp = train_model(poolnp, epochs=10000, lr=1e-4)
+poolnp, _, poolnp_loss_final = train_onedim_model(
+    poolnp, od, epochs=10000, lr=1e-4, visualize=VISUALIZE
+)
--- tests/test_fnp.py
+++ tests/test_fnp.py
@@ -1,6 +1,79 @@
 import pytest
 
+import torch
+import bliss.models.fnp as fnp
+
 
 class TestFNP:
     def test_fnp(self, paths):
-        assert False
+        # One dimensional example
+        od = fnp.OneDimDataset()
+
+        vanilla_fnp = fnp.RegressionFNP(
+            dim_x=1,
+            dim_y=1,
+            transf_y=od.stdy,
+            dim_h=100,
+            dim_u=3,
+            n_layers=1,
+            num_M=od.XM.size(0),
+            dim_z=50,
+            fb_z=1.0,
+            use_plus=False,
+        )
+
+        fnpp = fnp.RegressionFNP(
+            dim_x=1,
+            dim_y=1,
+            transf_y=od.stdy,
+            dim_h=100,
+            dim_u=3,
+            n_layers=1,
+            num_M=od.XM.size(0),
+            dim_z=50,
+            fb_z=1.0,
+            use_plus=True,
+        )
+
+        attt = fnp.PoolingFNP(
+            dim_x=1,
+            dim_y=1,
+            transf_y=None,
+            dim_h=100,
+            dim_u=3,
+            n_layers=1,
+            num_M=od.XM.size(0),
+            dim_z=50,
+            fb_z=1.0,
+            use_plus=False,
+            use_direction_mu_nu=True,
+            set_transformer=True,
+        )
+
+        poolnp = fnp.PoolingFNP(
+            dim_x=1,
+            dim_y=1,
+            transf_y=od.stdy,
+            dim_h=100,
+            dim_u=3,
+            n_layers=1,
+            num_M=od.XM.size(0),
+            dim_z=50,
+            fb_z=0.5,
+            use_plus=False,
+            use_direction_mu_nu=True,
+            set_transformer=False,
+        )
+
+        model_names = ["fnp", "fnp plus", "pool - attention", "pool - deep set"]
+        thresholds = [0.5, 0.6, 0.5, 0.6]
+
+        for (i, model) in enumerate([vanilla_fnp, fnpp, attt, poolnp]):
+            print(model_names[i])
+            if torch.cuda.is_available():
+                od.cuda()
+                model = model.cuda()
+            model, loss_initial, loss_final = fnp.train_onedim_model(
+                model, od, epochs=1000, lr=1e-4
+            )
+            assert loss_final < loss_initial * thresholds[i]
