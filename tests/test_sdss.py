b7238379d34c107670423299628e11d5a9346561
Derek Hansen <dereklh@umich.edu>
2021-01-27 15:29:48 -0500

    Add test for SDSS psf number and size

--- tests/test_sdss.py
+++ tests/test_sdss.py
@@ -22,6 +22,10 @@ class TestSDSS:
         assert len(sdss_obj[0]["bright_stars"]) == 43
         assert sdss_obj[0]["bright_stars"].shape[1] == 5
         assert sdss_obj[0]["bright_stars"].shape[2] == 5
+
+        assert len(sdss_obj[0]["sdss_psfs"]) == 43
+        assert sdss_obj[0]["sdss_psfs"].shape[1] == 5
+        assert sdss_obj[0]["sdss_psfs"].shape[2] == 5
         super_star = sdss_obj[0]["bright_stars"].sum(axis=0)
         assert np.all(super_star[2, 2] + 1e-4 >= super_star)
 
@@ -33,6 +37,8 @@ class TestSDSS:
 
         assert sdss_obj9[0]["bright_stars"].shape[1] == 9
         assert sdss_obj9[0]["bright_stars"].shape[2] == 9
+        assert sdss_obj9[0]["sdss_psfs"].shape[1] == 9
+        assert sdss_obj9[0]["sdss_psfs"].shape[2] == 9
 
         sdss_obj9_cached = sdss.SloanDigitalSkySurvey(
             sdss_dir, run=3900, camcol=6, fields=[269, 745], bands=range(5), stampsize=9
