5cfdfc0fa879992292d89a8c4f70404a562367d3
Zachariah <46454028+ZachariahPang@users.noreply.github.com>
2021-01-31 22:35:18 -0500

    Fix test tune (#203)
    * Tune with 1 gpu
    
    * Ignore third party package deprecation warning
    
    * Fix max_concurent for cpu only environment
    
    * Ignore thread error when runing local_mode

--- tests/test_tune.py
+++ tests/test_tune.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from hydra.experimental import initialize, compose
 from bliss import tune
 
@@ -6,6 +7,17 @@ from bliss import tune
 class TestTune:
     @pytest.fixture(scope="class")
     def overrides(self, devices):
+        allocated_gpus = 0
+        gpus_per_trial = 0
+        max_concurrent = 1
+        if devices.use_cuda:
+            gpus_per_trial = 1
+            if torch.cuda.device_count() >= 2:
+                allocated_gpus = 2
+                max_concurrent = 2
+            else:
+                allocated_gpus = 1
+
         overrides = {
             "model": "m2",
             "dataset": "m2" if devices.use_cuda else "cpu",
@@ -13,9 +25,9 @@ class TestTune:
             "training": "m2" if devices.use_cuda else "cpu",
             "optimizer": "m2",
             "tuning.n_epochs": 2 if devices.use_cuda else 1,
-            "tuning.allocated_gpus": 2 if devices.use_cuda else 0,
-            "tuning.gpus_per_trial": 1 if devices.use_cuda else 0,
-            "tuning.max_concurrent": 1,
+            "tuning.allocated_gpus": allocated_gpus,
+            "tuning.gpus_per_trial": gpus_per_trial,
+            "tuning.max_concurrent": max_concurrent,
             "tuning.grace_period": 1,
             "tuning.verbose": 0,
             "tuning.save": False,
@@ -24,6 +36,7 @@ class TestTune:
         overrides = [f"{k}={v}" for k, v in overrides.items()]
         return overrides
 
+    @pytest.mark.filterwarnings("ignore:.*PytestUnhandledThreadExceptionWarning.*")
     def test_tune_run(self, overrides, devices):
         with initialize(config_path="../config"):
             cfg = compose("config", overrides=overrides)
