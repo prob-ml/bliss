db083c9961528d503c0f9b9caf11322d29a6649c
Zachariah <wild.zachariah@gmail.com>
2021-01-31 20:57:07 -0500

    Tune with 1 gpu

--- tests/test_tune.py
+++ tests/test_tune.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from hydra.experimental import initialize, compose
 from bliss import tune
 
@@ -6,6 +7,15 @@ from bliss import tune
 class TestTune:
     @pytest.fixture(scope="class")
     def overrides(self, devices):
+        allocated_gpus = 0
+        gpus_per_trial = 0
+        if devices.use_cuda:
+            gpus_per_trial = 1
+            if torch.cuda.device_count() >= 2:
+                allocated_gpus = 2
+            else:
+                allocated_gpus = 1
+
         overrides = {
             "model": "m2",
             "dataset": "m2" if devices.use_cuda else "cpu",
@@ -13,9 +23,9 @@ class TestTune:
             "training": "m2" if devices.use_cuda else "cpu",
             "optimizer": "m2",
             "tuning.n_epochs": 2 if devices.use_cuda else 1,
-            "tuning.allocated_gpus": 2 if devices.use_cuda else 0,
-            "tuning.gpus_per_trial": 1 if devices.use_cuda else 0,
-            "tuning.max_concurrent": 1,
+            "tuning.allocated_gpus": allocated_gpus,
+            "tuning.gpus_per_trial": gpus_per_trial,
+            "tuning.max_concurrent": allocated_gpus,
             "tuning.grace_period": 1,
             "tuning.verbose": 0,
             "tuning.save": False,
