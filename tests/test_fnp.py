d6b68b64cc164803703a5e131fe0c181619a11a0
Ismael Mendoza <imendoza@umich.edu>
2021-02-10 14:14:16 -0500

    improve speed of some tests like fnp and galaxy vae

--- tests/test_fnp.py
+++ tests/test_fnp.py
@@ -1,8 +1,9 @@
-import torch
 import numpy as np
-from torch.optim import Adam
 from pytorch_lightning import LightningModule, Trainer
+
+import torch
 from torch.utils.data.dataloader import DataLoader
+from torch.optim import Adam
 
 from bliss.models.fnp import (
     DepGraph,
@@ -24,7 +25,6 @@ class TestFNP:
         attt = OneDimFNP(use_set_pooler=True, use_attention=True)
         poolnp = OneDimFNP(use_set_pooler=True, use_attention=False, fb_z=0.5)
         model_names = ["fnp", "fnp plus", "pool - attention", "pool - deep set"]
-        thresholds = [0.5, 0.75, 0.5, 0.75]
         for (i, model) in enumerate([vanilla_fnp, fnpp, attt, poolnp]):
             print(model_names[i])
             train_loader = DataLoader(
@@ -37,7 +37,7 @@ class TestFNP:
                 gpus=devices.gpus,
                 max_epochs=1000 if devices.use_cuda else 2,
                 logger=None,
-                check_val_every_n_epoch=100,
+                check_val_every_n_epoch=100 if devices.use_cuda else 1,
                 checkpoint_callback=False,
             )
             trainer.fit(model, train_loader, val_loader)
@@ -46,8 +46,8 @@ class TestFNP:
             if devices.use_cuda:
                 assert min(model.valid_losses) < model.valid_losses[0] * thresholds[i]
             # Smoke test for prediction
-            pred = model.fnp.predict(od.XM, od.XR, od.yR[0].unsqueeze(0))
-            pred = model.fnp.predict(
+            model.fnp.predict(od.XM, od.XR, od.yR[0].unsqueeze(0))
+            model.fnp.predict(
                 od.XM, od.XR, od.yR[0].unsqueeze(0), sample=False, sample_Z=False
             )
 
@@ -128,9 +128,9 @@ class OneDimFNP(LightningModule):
         use_plus=False,
         use_set_pooler=False,
         pooling_rep_size=32,
-        pooling_layers=[64],
+        pooling_layers=(64,),
         use_attention=False,
-        st_numheads=[2, 2],
+        st_numheads=(2, 2),
         lr=1e-4,
     ):
         super().__init__()
@@ -198,7 +198,7 @@ class OneDimFNP(LightningModule):
         self.lr = lr
         self.valid_losses = []
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         XR, yR, XM, yM = batch
         loss = self.fnp(XR, yR, XM, yM)
         return loss
--- tests/test_galaxy_vae.py
+++ tests/test_galaxy_vae.py
@@ -3,6 +3,8 @@ def test_galaxy_vae(galaxy_vae_setup, devices):
     overrides = {
         "model": "galaxy_net",
         "dataset": "toy_gaussian",
+        "dataset.batch_size": 128 if use_cuda else 10,
+        "dataset.n_batches": 10 if use_cuda else 1,
         "training": "unittest",
         "training.n_epochs": 101 if use_cuda else 2,
         "training.trainer.check_val_every_n_epoch": 100 if use_cuda else 1,
