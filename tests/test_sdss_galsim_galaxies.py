163a59099546eb3054b2e23d4901fc7898821d35
Ismael Mendoza <imendoza@umich.edu>
2021-03-11 15:36:30 -0500

    updated smoke test for sdss galaxies and corresponding yaml files

--- tests/test_sdss_galsim_galaxies.py
+++ tests/test_sdss_galsim_galaxies.py
@@ -5,6 +5,7 @@ from bliss.datasets.galsim_galaxies import SDSSGalaxies, SavedGalaxies
 def test_sdss_galaxies():
     # just a smoke test for now, until we get VAE working.
     overrides = {"dataset": "sdss_galaxies", "dataset.batch_size": 10, "dataset.n_batches": 1}
+    overrides = [f"{k}={v}" for k, v in overrides.items()]
     with initialize(config_path="../config"):
         cfg = compose("config", overrides=overrides)
         ds = SDSSGalaxies(cfg)
@@ -19,6 +20,7 @@ def test_sdss_galaxies():
 def test_saved_galaxies():
     # just a smoke test
     overrides = {"dataset": "saved_galaxies", "dataset.batch_size": 64}
+    overrides = [f"{k}={v}" for k, v in overrides.items()]
     with initialize(config_path="../config"):
         cfg = compose("config", overrides=overrides)
         ds = SavedGalaxies(cfg)
