0fe38cda79c95f24b8afc63d98ec66aaf8b30168
Ismael Mendoza <imendoza@umich.edu>
2021-03-31 09:37:03 -0400

    added utility function to use different optimizres

---
+++ bliss/optimizer.py
@@ -0,0 +1,11 @@
+from torch.optim import Adam, AdamW
+
+
+def get_optimizer(name, model_params, optim_params):
+    if name == "Adam":
+        return Adam(model_params, **optim_params)
+
+    if name == "AdamW":
+        return AdamW(model_params, **optim_params)
+
+    raise NotImplementedError(f"The optimizer {name} is not implemented.")
