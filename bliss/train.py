d7662855ad43c5827e6463237f5c49038a8c4d62
Ismael Mendoza <imendoza@umich.edu>
2021-04-13 10:29:52 -0400

    fix bug in SavedGalaxies dataset;  SDSS dataset now only for use with generate.py because too slow

--- bliss/train.py
+++ bliss/train.py
@@ -17,7 +17,6 @@ from bliss.models import galaxy_net
 _datasets = [
     simulated.SimulatedDataset,
     galsim_galaxies.ToyGaussian,
-    galsim_galaxies.SDSSGalaxies,
     galsim_galaxies.SavedGalaxies,
 ]
 datasets = {cls.__name__: cls for cls in _datasets}
