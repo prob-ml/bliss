b8818f202bffde06acbcf79e0af92db0061a9ae6
Runjing Liu <runingli@deeplearning-01.stat.lsa.umich.edu>
2021-05-22 23:35:24 -0400

    optimizer doesnt work; psf too big

--- case_studies/sdss_galaxies_bl/catalog_optimizer_lib.py
+++ case_studies/sdss_galaxies_bl/catalog_optimizer_lib.py
@@ -1,18 +1,21 @@
 import torch 
 from torch import nn
+from torch import optim
+
+from torch.distributions import Normal
 
 import numpy as np
 
 # define parameters for the catalog
 class CatalogParameters(nn.Module):
-    def __init__(self, init_locs): 
+    def __init__(self, init_locs, init_fluxes): 
         
         super(CatalogParameters, self).__init__()
                 
         assert torch.all(init_locs < 1)
         assert torch.all(init_locs >= 0)
         
-        self.init_loc_free = torch.logit(init_locs)
+        self.init_loc_free = torch.logit(init_locs).contiguous()
         
         self.loc_param_free = nn.Parameter(self.init_loc_free.clone())
         
@@ -21,14 +24,14 @@ class CatalogParameters(nn.Module):
                                   init_locs.shape[1], 
                                   1)
         # star parameters
-        self.star_flux_param_free = nn.Parameter(init_params.clone())
+        self.star_flux_param_free = nn.Parameter(init_fluxes.clone())
         
         # galaxy parameters
-        self.gal_flux_param_free = nn.Parameter(init_params.clone())
+        self.gal_flux_param_free = nn.Parameter(init_fluxes.clone())
         self.theta_param_free = nn.Parameter(init_params.clone())
         self.ell_param_free = nn.Parameter(init_params.clone())
-        self.rdev_param_free = nn.Parameter(init_params.clone())
-        self.rexp_dev_free = nn.Parameter(init_params.clone())
+        self.rdev_free = nn.Parameter(init_params.clone())
+        self.rexp_free = nn.Parameter(init_params.clone())
         self.pdev_dev_free = nn.Parameter(init_params.clone())
         
     def get_locations(self): 
@@ -51,10 +54,10 @@ class CatalogParameters(nn.Module):
         ell = torch.sigmoid(self.ell_param_free) 
         
         # radii are positive 
-        r_dev = self.rdev_param_free.exp()
-        r_exp = self.rexp_dev_free.exp()
+        r_dev = self.rdev_free.exp()
+        r_exp = self.rexp_free.exp()
         
-        # between zero and 
+        # between zero and 1
         p_dev = torch.sigmoid(self.pdev_dev_free)
         
         return dict(flux = flux, 
@@ -63,3 +66,74 @@ class CatalogParameters(nn.Module):
                     r_dev = r_dev, 
                     r_exp = r_exp,
                     p_dev = p_dev)
+    
+def get_loss(image, 
+             background,
+             source_simulator,
+             my_catalog,
+             star_bool, 
+             gal_bool): 
+    
+    assert image.shape == background.shape
+    
+    locations = my_catalog.get_locations()
+    
+    stars = source_simulator.render_stars(locations, 
+                                          my_catalog.get_star_params(), 
+                                          star_bool)
+    
+    galaxies = source_simulator.render_galaxies(locations, 
+                                                my_catalog.get_galaxy_params(), 
+                                                gal_bool)
+    
+    recon_mean = background + stars # + galaxies
+    
+    normal = Normal(loc = recon_mean, scale = torch.sqrt(recon_mean))
+    
+    return - normal.log_prob(image).mean(), recon_mean
+
+
+def estimate_catalog(image, 
+                     background, 
+                     source_simulator,
+                     my_catalog, 
+                     star_bool, 
+                     gal_bool, 
+                     lr = 1, 
+                     max_outer_iter = 10,
+                     max_inner_iter = 50,
+                     tol = 1e-8,
+                     print_every = False):
+
+    optimizer = optim.LBFGS(my_catalog.parameters(), 
+                            lr = lr, 
+                            max_iter = max_inner_iter,
+                            line_search_fn = 'strong_wolfe')
+    
+    def closure():
+        optimizer.zero_grad()
+        loss = get_loss(image, 
+                        background, 
+                        source_simulator,
+                        my_catalog,
+                        star_bool, 
+                        gal_bool)[0]
+        loss.backward()
+        
+        return loss
+    
+    init_loss = closure()
+    
+    old_loss = 1e16
+    for i in range(max_outer_iter):
+        loss = optimizer.step(closure)
+
+        if print_every:
+            print(loss)
+
+        diff = (loss - old_loss).abs()
+        if diff < (tol * init_loss.abs()):
+            break
+
+        old_loss = loss
+        
