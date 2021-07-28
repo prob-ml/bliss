6057a77dbd142568c55130073ad672b5b9a5f6c8
Ismael Mendoza <imendoza@umich.edu>
2021-07-28 14:57:41 -0400

    test for galaxy encoder

---
+++ tests/test_galaxy_encoder.py
@@ -0,0 +1,23 @@
+import pytest
+import pytorch_lightning as pl
+from bliss.models.galaxy_encoder import GalaxyEncoder
+from bliss.datasets.simulated import SimulatedDataset
+
+
+class TestBasicGalaxyMeasure:
+    @pytest.fixture(scope="class")
+    def overrides(self, devices):
+        overrides = {
+            "model": "galenc_sdss",
+            "dataset": "default" if devices.use_cuda else "cpu",
+            "training": "cpu",
+            "dataset.kwargs.batch_size": 10,
+        }
+        return overrides
+
+    def test_simulated(self, devices, overrides, get_config):
+        cfg = get_config(overrides, devices)
+        galaxy_encoder = GalaxyEncoder(**cfg.model.kwargs, optimizer_params=cfg.optimizer)
+        dataset = SimulatedDataset(**cfg.dataset.kwargs)
+        trainer = pl.Trainer(**cfg.training.trainer)
+        trainer.fit(galaxy_encoder, datamodule=dataset)
