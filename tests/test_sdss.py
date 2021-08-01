12343d81dc84a75e9a3b85d4dc4f4f664a42cbf8
Jeffrey Regier <regier@umich.edu>
2021-08-01 12:44:39 -0400

    fix WPS204 Found overused expression

--- tests/test_sdss.py
+++ tests/test_sdss.py
@@ -17,28 +17,30 @@ class TestSDSS:
             overwrite_cache=True,
             overwrite_fits_cache=True,
         )
-        assert sdss_obj[0]["gain"][3] == pytest.approx(4.76)
+        an_obj = sdss_obj[0]
+        assert an_obj["gain"][3] == pytest.approx(4.76)
 
-        assert len(sdss_obj[0]["bright_stars"]) == 43
-        assert sdss_obj[0]["bright_stars"].shape[1] == 5
-        assert sdss_obj[0]["bright_stars"].shape[2] == 5
+        assert len(an_obj["bright_stars"]) == 43
+        assert an_obj["bright_stars"].shape[1] == 5
+        assert an_obj["bright_stars"].shape[2] == 5
 
-        assert len(sdss_obj[0]["sdss_psfs"]) == 43
-        assert sdss_obj[0]["sdss_psfs"].shape[1] == 5
-        assert sdss_obj[0]["sdss_psfs"].shape[2] == 5
-        super_star = sdss_obj[0]["bright_stars"].sum(axis=0)
+        assert len(an_obj["sdss_psfs"]) == 43
+        assert an_obj["sdss_psfs"].shape[1] == 5
+        assert an_obj["sdss_psfs"].shape[2] == 5
+        super_star = an_obj["bright_stars"].sum(axis=0)
         assert np.all(super_star[2, 2] + 1e-4 >= super_star)
 
-        assert len(sdss_obj[0]["sdss_psfs"]) == 43
+        assert len(an_obj["sdss_psfs"]) == 43
 
         sdss_obj9 = sdss.SloanDigitalSkySurvey(
             sdss_dir, run=3900, camcol=6, fields=[269, 745], bands=range(5), stampsize=9
         )
 
-        assert sdss_obj9[0]["bright_stars"].shape[1] == 9
-        assert sdss_obj9[0]["bright_stars"].shape[2] == 9
-        assert sdss_obj9[0]["sdss_psfs"].shape[1] == 9
-        assert sdss_obj9[0]["sdss_psfs"].shape[2] == 9
+        another_obj = sdss_obj9[0]
+        assert another_obj["bright_stars"].shape[1] == 9
+        assert another_obj["bright_stars"].shape[2] == 9
+        assert another_obj["sdss_psfs"].shape[1] == 9
+        assert another_obj["sdss_psfs"].shape[2] == 9
 
         sdss_obj9_cached = sdss.SloanDigitalSkySurvey(
             sdss_dir, run=3900, camcol=6, fields=[269, 745], bands=range(5), stampsize=9
