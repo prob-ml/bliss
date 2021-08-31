9f3f8e65b5017d71543540fb13254bdfb1837f3d
Derek Hansen <dereklh@umich.edu>
2021-08-31 15:43:18 -0400

    fixed up docstrings in galsim_galaxies

--- bliss/datasets/galsim_galaxies.py
+++ bliss/datasets/galsim_galaxies.py
@@ -16,13 +16,16 @@ def get_background(sky_brightness, filt, survey, B0=24):
     return get_flux(sky_brightness, filt, survey, B0=B0) * survey.pixel_scale ** 2
 
 
-def get_flux(ab_mag, filt, survey, B0=24):
-    """Convert source magnitude to flux.
+def get_flux(ab_mag: float, filt, survey, B0=24) -> float:
+    """Converts source magnitude to flux.
+
     The calculation includes the effects of atmospheric extinction.
+
     Args:
-        ab_magnitude(float): AB magnitude of source.
+        ab_mag: AB magnitude of source.
+
     Returns:
-        float: Flux in detected electrons.
+        Flux in detected electrons.
     """
     zeropoint = filt.zeropoint * survey.effective_area  # [s^-1]
     ab_mag += filt.extinction * (survey.airmass - survey.zeropoint_airmass)
