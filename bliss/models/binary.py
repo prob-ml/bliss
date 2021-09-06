4e7c655d412c15f76101c412370b00e348c53346
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2021-09-06 12:37:12 -0400

    Binary classifier (#300)
    * change the order of a couple of things
    
    * center_ptiles can be used by others
    
    * new module that uses same cnn architecture as sleep phase to only classify galaxy vs star. condition on everything else
    
    * add binary encoder
    
    * we dont need decoder, but keep for dataset. add optimizer to binary
    
    * config file for binary
    
    * fix a bunch of bugs
    
    * docstring improve
    
    * bugs when calculting binary loss are corrected
    
    * prob_galaxy is separate so easier to modify, turn off etc.
    
    * bug in the returned shapes
    
    * actually sum is better, add border
    
    * star bool needs to be estimated as well
    
    * binary works with predict now
    
    * predict now only optionally takes in additional encoders
    
    * clean bugs in predict.py
    
    * saving best models
    
    * updated latest reconstruction results with new models and binary classifier
    
    * working on comparing with photo and investigating leftover misclassifications
    
    * added some other interesting scenes to reconstruction results
    
    * no changes
    
    * expanded metrics and measurements done, more work will be done next PR
    
    * correction on plotting
    
    * correct file names in sdss.yaml
    
    * sorting
    
    * document binary and add test
    
    * fix in case of no logger
    
    * add type hints for complicated function
    
    * prevent error on repeated lines
    
    * we actually need very few epochs to test
    
    * solve darglint errors

---
+++ bliss/models/binary.py
@@ -0,0 +1,230 @@
+import pytorch_lightning as pl
+import torch
+from matplotlib import pyplot as plt
+from torch import nn
+from torch.nn import BCELoss
+
+from bliss.models.decoder import get_mgrid
+from bliss.models.encoder import EncoderCNN, get_images_in_tiles, get_is_on_from_n_sources
+from bliss.models.galaxy_encoder import center_ptiles, get_full_params
+from bliss.optimizer import get_optimizer
+from bliss.plotting import plot_image_and_locs
+
+
+class BinaryEncoder(pl.LightningModule):
+    def __init__(
+        self,
+        n_bands: int = 1,
+        tile_slen: int = 4,
+        ptile_slen: int = 52,
+        channel: int = 8,
+        hidden: int = 128,
+        spatial_dropout: float = 0,
+        dropout: float = 0,
+        decoder_kwargs: dict = None,  # pylint: disable=unused-argument
+        optimizer_params: dict = None,  # pylint: disable=unused-argument
+    ):
+        """Encoder which conditioned on other source params returns probability of galaxy vs. star.
+
+        This class implements the binary encoder, which is supposed to take in a synthetic image
+        along with true locations and source parameters and return whether each source in that
+        image is a star or a galaxy.
+
+        Arguments:
+            n_bands: number of bands
+            tile_slen: dimension (in pixels) of each tile.
+            ptile_slen: dimension (in pixels) of the individual image padded tiles.
+            channel: TODO (document this)
+            hidden: TODO (document this)
+            spatial_dropout: TODO (document this)
+            dropout: TODO (document this)
+            decoder_kwargs: TODO (document this)
+            optimizer_params: TODO (document this)
+        """
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.max_sources = 1  # by construction.
+
+        # extract useful info from image_decoder
+        self.n_bands = n_bands
+
+        # put image dimensions together
+        self.tile_slen = tile_slen
+        self.ptile_slen = ptile_slen
+        border_padding = (ptile_slen - tile_slen) / 2
+        assert tile_slen <= ptile_slen
+        assert border_padding % 1 == 0, "amount of border padding should be an integer"
+        self.border_padding = int(border_padding)
+        self.slen = self.ptile_slen - 2 * self.tile_slen  # will always crop 2 * tile_slen
+
+        dim_enc_conv_out = ((self.slen + 1) // 2 + 1) // 2
+        self.enc_conv = EncoderCNN(self.n_bands, channel, spatial_dropout)
+        self.enc_final = nn.Sequential(
+            nn.Flatten(1),
+            nn.Linear(channel * 4 * dim_enc_conv_out ** 2, hidden),
+            nn.BatchNorm1d(hidden),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(hidden, hidden),
+            nn.BatchNorm1d(hidden),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(hidden, 1),
+        )
+
+        # grid for center cropped tiles
+        self.register_buffer("cached_grid", get_mgrid(self.ptile_slen), persistent=False)
+        self.register_buffer("swap", torch.tensor([1, 0]), persistent=False)
+
+    def get_images_in_tiles(self, images):
+        """Divide a batch of full images into padded tiles similar to nn.conv2d."""
+        return get_images_in_tiles(images, self.tile_slen, self.ptile_slen)
+
+    def center_ptiles(self, image_ptiles, tile_locs):
+        """Return padded tiles centered on each corresponding source."""
+        return center_ptiles(
+            image_ptiles,
+            tile_locs,
+            self.tile_slen,
+            self.ptile_slen,
+            self.border_padding,
+            self.swap,
+            self.cached_grid,
+        )
+
+    def forward_image(self, images, tile_locs):
+        """Splits `images` into padded tiles and runs `self.forward()` on them."""
+        batch_size = images.shape[0]
+        ptiles = self.get_images_in_tiles(images)
+        prob_galaxy = self(ptiles, tile_locs)
+        return prob_galaxy.view(batch_size, -1, 1, 1)
+
+    def forward(self, image_ptiles, tile_locs):
+        """Centers padded tiles using `tile_locs` and runs the binary encoder on them."""
+        assert image_ptiles.shape[-1] == image_ptiles.shape[-2] == self.ptile_slen
+        n_ptiles = image_ptiles.shape[0]
+
+        # in each padded tile we need to center the corresponding galaxy/star
+        tile_locs = tile_locs.reshape(n_ptiles, self.max_sources, 2)
+        centered_ptiles = self.center_ptiles(image_ptiles, tile_locs)
+        assert centered_ptiles.shape[-1] == centered_ptiles.shape[-2] == self.slen
+
+        # forward to layer shared by all n_sources
+        log_img = torch.log(centered_ptiles - centered_ptiles.min() + 1.0)
+        h = self.enc_conv(log_img)
+        z = self.enc_final(h).reshape(n_ptiles, 1)
+        return torch.sigmoid(z).clamp(1e-4, 1 - 1e-4)
+
+    def get_prediction(self, batch):
+        """Return loss, accuracy, binary probabilities, and MAP classifications for given batch."""
+
+        images = batch["images"]
+        galaxy_bool = batch["galaxy_bool"].reshape(-1)
+        prob_galaxy = self.forward_image(images, batch["locs"]).reshape(-1)
+        tile_is_on_array = get_is_on_from_n_sources(batch["n_sources"], self.max_sources)
+        tile_is_on_array = tile_is_on_array.reshape(-1)
+
+        # we need to calculate cross entropy loss, only for "on" sources
+        loss = BCELoss(reduction="none")(prob_galaxy, galaxy_bool) * tile_is_on_array
+        loss = loss.sum()
+
+        # get predictions for calculating metrics
+        pred_galaxy_bool = (prob_galaxy > 0.5).float() * tile_is_on_array
+        correct = ((pred_galaxy_bool.eq(galaxy_bool)) * tile_is_on_array).sum()
+        total_n_sources = batch["n_sources"].sum()
+        acc = correct / total_n_sources
+
+        # finally organize quantities and return as a dictionary
+        pred_star_bool = (1 - pred_galaxy_bool) * tile_is_on_array
+
+        return {
+            "loss": loss,
+            "acc": acc,
+            "galaxy_bool": pred_galaxy_bool.reshape(images.shape[0], -1, 1, 1),
+            "star_bool": pred_star_bool.reshape(images.shape[0], -1, 1, 1),
+            "prob_galaxy": prob_galaxy.reshape(images.shape[0], -1, 1, 1),
+        }
+
+    def configure_optimizers(self):  # pylint: disable=empty-docstring
+        """Pytorch lightning method."""
+        assert self.hparams["optimizer_params"] is not None, "Need to specify 'optimizer_params'."
+        name = self.hparams["optimizer_params"]["name"]
+        kwargs = self.hparams["optimizer_params"]["kwargs"]
+        return get_optimizer(name, self.parameters(), kwargs)
+
+    def training_step(self, batch, batch_idx):  # pylint: disable=unused-argument,empty-docstring
+        """Pytorch lightning method."""
+        pred = self.get_prediction(batch)
+        self.log("train/loss", pred["loss"])
+        self.log("train/acc", pred["acc"])
+        return pred["loss"]
+
+    def validation_step(self, batch, batch_idx):  # pylint: disable=unused-argument,empty-docstring
+        """Pytorch lightning method."""
+        pred = self.get_prediction(batch)
+        self.log("val/loss", pred["loss"])
+        self.log("val/acc", pred["acc"])
+        return batch
+
+    def validation_epoch_end(self, outputs):  # pylint: disable=empty-docstring
+        """Pytorch lightning method."""
+        # Put all outputs together into a single batch
+        batch = {}
+        for b in outputs:
+            for k, v in b.items():
+                curr_val = batch.get(k, torch.tensor([], device=v.device))
+                batch[k] = torch.cat([curr_val, v])
+        self.make_plots(batch)
+
+    def test_step(self, batch, batch_idx):  # pylint: disable=unused-argument,empty-docstring
+        """Pytorch lightning method."""
+        pred = self.get_prediction(batch)
+        self.log("acc", pred["acc"])
+
+    def make_plots(self, batch, n_samples=16):
+        """Produced informative plots demonstrating encoder performance."""
+
+        assert n_samples ** (0.5) % 1 == 0
+        if n_samples > len(batch["n_sources"]):  # do nothing if low on samples.
+            return
+        nrows = int(n_samples ** 0.5)  # for figure
+
+        # extract non-params entries so that 'get_full_params' to works.
+        exclude = {"images", "slen", "background"}
+        slen = int(batch["slen"].unique().item())
+        tile_params = {k: v for k, v in batch.items() if k not in exclude}
+        true_params = get_full_params(tile_params, slen)
+
+        # prediction
+        pred = self.get_prediction(batch)
+        tile_est = dict(tile_params.items())
+        tile_est["galaxy_bool"] = pred["galaxy_bool"]
+        tile_est["star_bool"] = pred["star_bool"]
+        tile_est["prob_galaxy"] = pred["prob_galaxy"]
+        est = get_full_params(tile_est, slen)
+
+        # setup figure and axes
+        fig, axes = plt.subplots(nrows=nrows, ncols=nrows, figsize=(12, 12))
+        axes = axes.flatten()
+
+        for i in range(n_samples):
+            plot_image_and_locs(
+                i,
+                fig,
+                axes[i],
+                batch["images"],
+                slen,
+                true_params,
+                estimate=est,
+                labels=None if i > 0 else ("t. gal", "p. gal", "t. star", "p. star"),
+                annotate_axis=False,
+                add_borders=True,
+                prob_galaxy=est["prob_galaxy"],
+            )
+
+        fig.tight_layout()
+
+        title = f"Epoch:{self.current_epoch}/Validation Images"
+        if self.logger is not None:
+            self.logger.experiment.add_figure(title, fig)
--- bliss/predict.py
+++ bliss/predict.py
@@ -4,22 +4,21 @@ from einops import rearrange
 from omegaconf import DictConfig
 
 from bliss.datasets import sdss
-from bliss.models.encoder import get_full_params
+from bliss.models.binary import BinaryEncoder
+from bliss.models.encoder import get_full_params, get_is_on_from_n_sources, get_star_bool
 from bliss.models.galaxy_encoder import GalaxyEncoder
 from bliss.sleep import SleepPhase
 
 
-def prediction(image, image_encoder, galaxy_encoder):
-
+def prediction(image, image_encoder, galaxy_encoder=None, binary_encoder=None):
     # prepare and check consistency
+    assert not image_encoder.training
     assert len(image.shape) == 4
     assert image.shape[0] == 1
-    assert image.shape[1] == image_encoder.n_bands == galaxy_encoder.n_bands
-    image_encoder.eval()
-    galaxy_encoder.eval()
-    assert image_encoder.border_padding == galaxy_encoder.border_padding
-    assert image_encoder.tile_slen == galaxy_encoder.tile_slen
-    assert image_encoder.max_detections == galaxy_encoder.image_decoder.max_sources == 1
+    assert image.shape[1] == image_encoder.n_bands
+    assert image_encoder.max_detections == 1
+
+    # prepare dimensions
     h, w = image.shape[-2], image.shape[-1]
     bp = image_encoder.border_padding
 
@@ -28,20 +27,46 @@ def prediction(image, image_encoder, galaxy_encoder):
 
     # get MAP estimates
     tile_n_sources = image_encoder.tile_map_n_sources(ptiles)
+    tile_is_on_array = get_is_on_from_n_sources(tile_n_sources, 1).reshape(1, -1, 1, 1)
     tile_map = image_encoder.tile_map_estimate(image)
 
     # get var_params in tiles (excluding galaxy params)
     var_params = image_encoder(ptiles, tile_n_sources)
 
-    # get galaxy params per tile
-    galaxy_param_mean = galaxy_encoder(ptiles, tile_map["locs"])
+    if galaxy_encoder is not None:
+        # get galaxy params per tile
+        assert not galaxy_encoder.training
+        assert image.shape[1] == galaxy_encoder.n_bands
+        assert image_encoder.border_padding == galaxy_encoder.border_padding
+        assert galaxy_encoder.image_decoder.max_sources == 1
+        assert image_encoder.tile_slen == galaxy_encoder.tile_slen
+
+        galaxy_param_mean = galaxy_encoder(ptiles, tile_map["locs"])
+        latent_dim = galaxy_param_mean.shape[-1]
+        galaxy_param_mean = galaxy_param_mean.reshape(1, -1, 1, latent_dim)
+        galaxy_param_mean *= tile_is_on_array
+        var_params["galaxy_param_mean"] = galaxy_param_mean
+        tile_map["galaxy_params"] = galaxy_param_mean
+
+    if binary_encoder is not None:
+        # get classification params per tile
+        assert not binary_encoder.training
+        assert image.shape[1] == binary_encoder.n_bands
+        prob_galaxy = (
+            binary_encoder(ptiles, tile_map["locs"]).reshape(1, -1, 1, 1) * tile_is_on_array
+        )
+        galaxy_bool = (prob_galaxy > 0.5).float().reshape(1, -1, 1, 1) * tile_is_on_array
+        star_bool = get_star_bool(tile_map["n_sources"], galaxy_bool).reshape(1, -1, 1, 1)
+        var_params["galaxy_bool"] = galaxy_bool
+        tile_map["galaxy_bool"] = galaxy_bool
+        var_params["star_bool"] = star_bool
+        tile_map["star_bool"] = star_bool
+        tile_map["prob_galaxy"] = prob_galaxy
+        var_params["prob_galaxy"] = prob_galaxy
 
     # full parameters on chunk
     full_map = get_full_params(tile_map, h - 2 * bp, w - 2 * bp)
 
-    # collect all parameters into dictionaries
-    var_params["galaxy_param_mean"] = galaxy_param_mean
-    tile_map["galaxy_params"] = galaxy_param_mean
     return var_params, tile_map, full_map
 
 
@@ -52,6 +77,7 @@ def predict(cfg: DictConfig):
     sdss_obj = sdss.SloanDigitalSkySurvey(**cfg.predict.sdss_kwargs)
     sleep_net = SleepPhase.load_from_checkpoint(cfg.predict.sleep_checkpoint)
     galaxy_encoder = GalaxyEncoder.load_from_checkpoint(cfg.predict.galaxy_checkpoint)
+    binary_encoder = BinaryEncoder.load_from_checkpoint(cfg.predict.binary_checkpoint)
 
     # load images from SDSS for prediction.
     image = sdss_obj[0]["image"][0]
@@ -60,9 +86,9 @@ def predict(cfg: DictConfig):
 
     # move everything to specified GPU
     sleep_net.to(cfg.predict.device)
-    sleep_net.eval()
     image_encoder = sleep_net.image_encoder.eval()
     galaxy_encoder = galaxy_encoder.to(cfg.predict.device).eval()
+    binary_encoder = binary_encoder.to(cfg.predict.device).eval()
 
     list_var_params = []
     clen = 300  # sdss image is too big so we need to chunk it.
@@ -78,7 +104,7 @@ def predict(cfg: DictConfig):
                 chunk = chunk.to(cfg.predict.device)
 
                 # predict!
-                var_params, _, _ = prediction(chunk, image_encoder, galaxy_encoder)
+                var_params, _, _ = prediction(chunk, image_encoder, galaxy_encoder, binary_encoder)
 
                 # put everything in the cpu before saving
                 var_params = {key: value.cpu() for key, value in var_params.items()}
