084250774bef01dde52eee52735d084bef5dc252
Ismael Mendoza <imendoza@umich.edu>
2021-09-21 19:59:55 -0400

    more useful plot defaults and fix some bugs

--- case_studies/sdss_galaxies/figures/plot_defaults.py
+++ case_studies/sdss_galaxies/figures/plot_defaults.py
@@ -1,14 +1,26 @@
 # flake8: noqa
 import matplotlib as mpl
 
+CB_color_cycle = [
+    "#377eb8",
+    "#ff7f00",
+    "#4daf4a",
+    "#f781bf",
+    "#a65628",
+    "#984ea3",
+    "#999999",
+    "#e41a1c",
+    "#dede00",
+]
+
 
 def set_rc_params(
     figsize=(10, 10),
     fontsize=18,
-    label_size="medium",
     title_size="large",
-    tick_label_size="small",
+    label_size="medium",
     legend_fontsize="medium",
+    tick_label_size="small",
     major_tick_size=7,
     minor_tick_size=4,
     major_tick_width=0.8,
@@ -45,16 +57,14 @@ def set_rc_params(
     )
 
 
-def format_plot(ax, lims=None, ticks=None, xlabel=None, ylabel=None):
-
-    # format plots.
-    if lims:
-        ax.set_xlim(lims)
-        ax.set_ylim(lims)
-
-    if ticks:
-        ax.set_xticks(ticks)
-        ax.set_yticks(ticks)
+def format_plot(ax, xlims=None, ylims=None, xticks=None, yticks=None, xlabel="", ylabel=""):
+    ax.set_xlim(xlims)
+    ax.set_ylim(ylims)
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
+
+    if xticks:
+        ax.set_xticks(xticks)
+    if yticks:
+        ax.set_yticks(yticks)
