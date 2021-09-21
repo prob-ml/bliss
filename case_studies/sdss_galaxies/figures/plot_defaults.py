e7f8e2c17438ab26258afff61aa5c5b39cd32a4c
Ismael Mendoza <imendoza@umich.edu>
2021-09-21 14:49:50 -0400

    more utility functions for plotting

--- case_studies/sdss_galaxies/figures/plot_defaults.py
+++ case_studies/sdss_galaxies/figures/plot_defaults.py
@@ -1,34 +1,60 @@
 # flake8: noqa
 import matplotlib as mpl
-from matplotlib import pyplot as plt
-
-plt.style.use("seaborn-colorblind")
 
 
-mpl.rcParams.update(
-    {
-        # figure
-        "figure.figsize": (10, 10),
-        # axes
-        "axes.labelsize": 16,
-        "axes.titlesize": 20,
-        # ticks
-        "xtick.major.size": 7,
-        "xtick.minor.size": 4,
-        "xtick.major.width": 0.8,
-        "xtick.minor.width": 0.6,
-        "xtick.labelsize": 14,
-        "ytick.major.size": 7,
-        "ytick.minor.size": 6,
-        "ytick.major.width": 0.8,
-        "ytick.minor.width": 0.6,
-        "ytick.labelsize": 14,
-        # legend
-        "legend.fontsize": 12,
-    }
-)
+def set_rc_params(
+    figsize=(10, 10),
+    fontsize=18,
+    label_size="medium",
+    title_size="large",
+    tick_label_size="small",
+    legend_fontsize="medium",
+    major_tick_size=7,
+    minor_tick_size=4,
+    major_tick_width=0.8,
+    minor_tick_width=0.6,
+):
+    # named size options: 'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'.
+    mpl.rcParams.update(
+        {
+            # font.
+            "font.family": "STIXGeneral",
+            "font.sans-serif": "Helvetica",
+            "text.usetex": True,
+            "mathtext.fontset": "cm",
+            "font.size": fontsize,
+            # figure
+            "figure.figsize": figsize,
+            # axes
+            "axes.labelsize": label_size,
+            "axes.titlesize": title_size,
+            # ticks
+            "xtick.labelsize": tick_label_size,
+            "ytick.labelsize": tick_label_size,
+            "xtick.major.size": major_tick_size,
+            "ytick.major.size": major_tick_size,
+            "xtick.major.width": major_tick_width,
+            "ytick.major.width": major_tick_width,
+            "ytick.minor.size": minor_tick_size,
+            "xtick.minor.size": minor_tick_size,
+            "xtick.minor.width": minor_tick_width,
+            "ytick.minor.width": minor_tick_width,
+            # legend
+            "legend.fontsize": legend_fontsize,
+        }
+    )
 
-# font = {"family": "sans-serif", "sans-serif": ["Helvetica"]}
-# plt.rc("font", **font)
 
-plt.rc("text", usetex=True)
+def format_plot(ax, lims=None, ticks=None, xlabel=None, ylabel=None):
+
+    # format plots.
+    if lims:
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+
+    if ticks:
+        ax.set_xticks(ticks)
+        ax.set_yticks(ticks)
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
