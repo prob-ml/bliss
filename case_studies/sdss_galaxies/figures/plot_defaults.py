156bf46dc32a452d267bf6b8bf20864e8d0b5645
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2021-09-21 20:53:23 -0400

    create figures related to measuring quantities on galaxy images (#321)
    * add function to calculate measurements
    
    * add seaborn
    
    * add tqdm
    
    * added progress bar to single galaxy measurements
    
    * added convenience functions for converting to flux and magnitudes
    
    * more utility functions for plotting
    
    * first draft of autoencoder measurement figures
    
    * needed by seaborn
    
    * more useful plot defaults and fix some bugs
    
    * basically done with autoencoder figures in paper
    
    * update poetry
    
    * remove unused packages that might be causing errors
    
    * use latest poetry
    
    * use f strings
    
    * add test for coverage

--- case_studies/sdss_galaxies/figures/plot_defaults.py
+++ case_studies/sdss_galaxies/figures/plot_defaults.py
@@ -1,34 +1,70 @@
 # flake8: noqa
 import matplotlib as mpl
-from matplotlib import pyplot as plt
 
-plt.style.use("seaborn-colorblind")
+CB_color_cycle = [
+    "#377eb8",
+    "#ff7f00",
+    "#4daf4a",
+    "#f781bf",
+    "#a65628",
+    "#984ea3",
+    "#999999",
+    "#e41a1c",
+    "#dede00",
+]
 
 
-mpl.rcParams.update(
-    {
-        # figure
-        "figure.figsize": (10, 10),
-        # axes
-        "axes.labelsize": 16,
-        "axes.titlesize": 20,
-        # ticks
-        "xtick.major.size": 7,
-        "xtick.minor.size": 4,
-        "xtick.major.width": 0.8,
-        "xtick.minor.width": 0.6,
-        "xtick.labelsize": 14,
-        "ytick.major.size": 7,
-        "ytick.minor.size": 6,
-        "ytick.major.width": 0.8,
-        "ytick.minor.width": 0.6,
-        "ytick.labelsize": 14,
-        # legend
-        "legend.fontsize": 12,
-    }
-)
+def set_rc_params(
+    figsize=(10, 10),
+    fontsize=18,
+    title_size="large",
+    label_size="medium",
+    legend_fontsize="medium",
+    tick_label_size="small",
+    major_tick_size=7,
+    minor_tick_size=4,
+    major_tick_width=0.8,
+    minor_tick_width=0.6,
+):
+    # named size options: 'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'.
+    mpl.rcParams.update(
+        {
+            # font.
+            "font.family": "STIXGeneral",
+            "font.sans-serif": "Helvetica",
+            "text.usetex": True,
+            "mathtext.fontset": "cm",
+            "font.size": fontsize,
+            # figure
+            "figure.figsize": figsize,
+            # axes
+            "axes.labelsize": label_size,
+            "axes.titlesize": title_size,
+            # ticks
+            "xtick.labelsize": tick_label_size,
+            "ytick.labelsize": tick_label_size,
+            "xtick.major.size": major_tick_size,
+            "ytick.major.size": major_tick_size,
+            "xtick.major.width": major_tick_width,
+            "ytick.major.width": major_tick_width,
+            "ytick.minor.size": minor_tick_size,
+            "xtick.minor.size": minor_tick_size,
+            "xtick.minor.width": minor_tick_width,
+            "ytick.minor.width": minor_tick_width,
+            # legend
+            "legend.fontsize": legend_fontsize,
+        }
+    )
 
-# font = {"family": "sans-serif", "sans-serif": ["Helvetica"]}
-# plt.rc("font", **font)
 
-plt.rc("text", usetex=True)
+def format_plot(ax, xlims=None, ylims=None, xticks=None, yticks=None, xlabel="", ylabel=""):
+    ax.set_xlim(xlims)
+    ax.set_ylim(ylims)
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    if xticks:
+        ax.set_xticks(xticks)
+    if yticks:
+        ax.set_yticks(yticks)
