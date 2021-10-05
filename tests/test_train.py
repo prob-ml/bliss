43ce28181e8deeee4fc54f3aaad35d02c03e9034
Zachariah <wild.zachariah@gmail.com>
2021-10-05 17:49:42 -0400

    Add coverage by unittest

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,14 +1,19 @@
+import shutil
+
 from hydra import compose, initialize
 
 from bliss import train
 
 
-def test_train_run():
+def test_train_run(paths):
     overrides = {
         "training": "cpu",
+        "training.experiment": "unittest",
+        "training.trainer.logger": True,
         "dataset": "cpu",
     }
     overrides = [f"{k}={v}" for k, v in overrides.items()]
     with initialize(config_path="../config"):
         cfg = compose("config", overrides=overrides)
         train.train(cfg)
+    shutil.rmtree(f'{paths["output"]}/unittest')
