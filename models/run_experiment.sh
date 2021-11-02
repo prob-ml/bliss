3db8c2fb768589e8de3d73b49d112063022100b3
Derek Hansen <dereklh@umich.edu>
2021-11-02 10:23:26 -0400

    Got new Makefile working

--- models/run_experiment.sh
+++ models/run_experiment.sh
@@ -3,9 +3,10 @@ OUTPUT_DIR=$1
 EXPERIMENT=$2
 
 echo "Started $EXPERIMENT..."
-bliss +experiment=$EXPERIMENT training.save_top_k=1 paths.output=$OUTPUT_DIR training.version=$EXPERIMENT
+bliss +experiment=$EXPERIMENT training.save_top_k=1 paths.output=`realpath $OUTPUT_DIR` training.version=$EXPERIMENT
 CKPT=`find $OUTPUT_DIR/default/$EXPERIMENT/checkpoints | tail -n 1` 
-cp $CKPT ./models/$EXPERIMENT.ckpt
-echo $EXPERIMENT >> ./models/results
-basename $CKPT >> ./models/results
-echo >> ./models/results
+cp $CKPT ./$EXPERIMENT.ckpt
+RESULTS=${EXPERIMENT}_results
+echo $EXPERIMENT > ./${RESULTS}
+basename $CKPT >> ./${RESULTS}
+echo >> ./${RESULTS}
