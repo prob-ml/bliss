2979701efc3bb3f4c0b37a3da62ad01857338627
Derek Hansen <dereklh@umich.edu>
2021-11-03 11:36:31 -0400

    remove experiment directory before training

--- models/run_experiment.sh
+++ models/run_experiment.sh
@@ -2,9 +2,12 @@
 OUTPUT_DIR=$1
 EXPERIMENT=$2
 
-echo "Started $EXPERIMENT..."
+
+echo "Starting $EXPERIMENT..."
+EXP_DIR=$OUTPUT_DIR/default/$EXPERIMENT
+rm -rf $EXP_DIR
 bliss +experiment=$EXPERIMENT training.save_top_k=1 paths.output=`realpath $OUTPUT_DIR` training.version=$EXPERIMENT
-CKPT=`find $OUTPUT_DIR/default/$EXPERIMENT/checkpoints | tail -n 1` 
+CKPT=`find $EXP_DIR/checkpoints | tail -n 1`
 cp $CKPT ./$EXPERIMENT.ckpt
 RESULTS=${EXPERIMENT}_results
 echo $EXPERIMENT > ./${RESULTS}
