1d29bc56c5c5108cb5b715fc08fd77d6eea0a922
Runjing Liu <runingli@deeplearning-01.stat.lsa.umich.edu>
2021-12-08 23:40:11 -0500

    cleaning up decam code

---
+++ case_studies/starnet/decaps/data/wget_catalog.sh
@@ -0,0 +1,6 @@
+# dowloaded from decam plane survey website 
+wget -N https://faun.rc.fas.harvard.edu/decaps/release/cat/c4d_160811_031817_ooi_z_v1.cat.fits
+wget -N https://faun.rc.fas.harvard.edu/decaps/release/cat/c4d_160811_031918_ooi_i_v1.cat.fits
+
+
+
---
+++ case_studies/starnet/decaps/data/wget_images.sh
@@ -0,0 +1,7 @@
+# get the z band of galactic coordinates (0, 0)
+wget -O decaps_l0_b0_hdu08_z https://astroarchive.noirlab.edu/api/retrieve/197e57b38061a22fa2a616f1c21daf7a/?hdus=0,8
+
+# get the  i band 
+wget -O decaps_l0_b0_hdu08_i https://astroarchive.noirlab.edu/api/retrieve/7eb1b49ee60095b8f06e2fe277b641f4/?hdus=0,8
+
+
