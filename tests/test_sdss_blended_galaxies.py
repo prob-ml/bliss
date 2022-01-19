2b05319d95e9681ec1689f00a99c301801d18314
Derek Hansen <dereklh@umich.edu>
2022-01-19 13:58:48 -0500

    fix hydra option in tests

--- tests/test_sdss_blended_galaxies.py
+++ tests/test_sdss_blended_galaxies.py
@@ -8,7 +8,6 @@ class TestSdssBlendedGalaxies:
         return {
             "+experiment": "sdss_galaxy_encoder_real",
             "dataset.prerender_device": "cuda" if devices.use_cuda else "cpu",
-            "dataset.cache_path": None,
             "+dataset.slen": 40,
             "+dataset.h_start": 200,
             "+dataset.w_start": 1800,
