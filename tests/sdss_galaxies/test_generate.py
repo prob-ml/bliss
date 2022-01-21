a773031a282f596b1596e7cdccdeaba1fad53b29
Derek Hansen <dereklh@umich.edu>
2022-01-21 18:47:33 -0500

    improve generate test

--- tests/sdss_galaxies/test_generate.py
+++ tests/sdss_galaxies/test_generate.py
@@ -1,21 +1,23 @@
 import os
 
-from hydra import compose, initialize
+import pytest
 
 from bliss import generate
 
 
-def test_generate_run(devices, paths):
-    overrides = {
-        "generate.dataset": "${datasets.simulated}",
-        "datasets.simulated.generate_device": "cuda:0" if devices.use_cuda else "cpu",
-        "generate.file": f"{paths['root']}/example.pt",
-        "generate.common": ["background", "slen"],
-    }
-    overrides = [f"{k}={v}" for k, v in overrides.items()]
-    with initialize(config_path="../config"):
-        cfg = compose("config", overrides=overrides)
+class TestGenerate:
+    @pytest.fixture(scope="class")
+    def overrides(self, devices, paths):
+        return {
+            "generate.dataset": "${datasets.simulated}",
+            "datasets.simulated.generate_device": "cuda:0" if devices.use_cuda else "cpu",
+            "generate.file": f"{paths['root']}/example.pt",
+            "generate.common": ["background", "slen"],
+        }
+
+    def test_generate_run(self, devices, paths, overrides, get_config):
+        cfg = get_config(overrides, devices)
         generate.generate(cfg)
 
-    os.remove(f"{paths['root']}/example.pt")
-    os.remove(f"{paths['root']}/example_images.pdf")
+        os.remove(f"{paths['root']}/example.pt")
+        os.remove(f"{paths['root']}/example_images.pdf")
