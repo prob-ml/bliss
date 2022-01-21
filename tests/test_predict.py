5987986af694c867df35f32305e57d94052b06df
Derek Hansen <dereklh@umich.edu>
2022-01-21 17:14:40 -0500

    remove predict folder

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -4,7 +4,6 @@ from bliss import predict
 def test_predict_run(devices, get_config):
     overrides = {
         "mode": "predict",
-        "predict": "sdss",
         "predict.output_file": "null",
         "predict.device": f"cuda:{devices.device.index}" if devices.use_cuda else "cpu",
         "predict.testing": True,
