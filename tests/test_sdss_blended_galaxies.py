467738e38acbf8c68b7f29e15dc1d161208cb560
Derek Hansen <dereklh@umich.edu>
2022-01-21 16:44:06 -0500

    fix sdss_blended_galaxies

--- tests/test_sdss_blended_galaxies.py
+++ tests/test_sdss_blended_galaxies.py
@@ -1,24 +1,25 @@
 import pytest
 from hydra.utils import instantiate
 
+from bliss.train import train
+
 
 class TestSdssBlendedGalaxies:
     @pytest.fixture(scope="class")
     def overrides(self, devices):
         return {
-            "+experiment": "sdss_galaxy_encoder_real",
-            "dataset.prerender_device": "cuda" if devices.use_cuda else "cpu",
-            "dataset.cache_path": None,
-            "+dataset.slen": 40,
-            "+dataset.h_start": 200,
-            "+dataset.w_start": 1800,
-            "+dataset.scene_size": 100,
+            "mode": "train",
+            "training": "sdss_galaxy_encoder_real",
+            "datasets.sdss_blended_galaxies.prerender_device": "cuda"
+            if devices.use_cuda
+            else "cpu",
+            "+datasets.sdss_blended_galaxies.slen": 40,
+            "+datasets.sdss_blended_galaxies.h_start": 200,
+            "+datasets.sdss_blended_galaxies.w_start": 1800,
+            "+datasets.sdss_blended_galaxies.scene_size": 100,
             "training.n_epochs": 1,
         }
 
     def test_sdss_blended_galaxies(self, devices, overrides, get_config):
         cfg = get_config(overrides, devices)
-        galaxy_encoder = instantiate(cfg.model)
-        dataset = instantiate(cfg.dataset)
-        trainer = instantiate(cfg.training.trainer)
-        trainer.fit(galaxy_encoder, datamodule=dataset)
+        train(cfg)
