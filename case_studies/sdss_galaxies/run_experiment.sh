8590f0e4de925263423c187a7a262226982b7a0d
Derek Hansen <dereklh@umich.edu>
2022-01-26 18:27:32 -0500

    Updated SDSS galaxy Makefile

--- case_studies/sdss_galaxies/run_experiment.sh
+++ case_studies/sdss_galaxies/run_experiment.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-OUTPUT_DIR=$1
-EXPERIMENT=$2
-
-
+EXPERIMENT=$1
 echo "Starting $EXPERIMENT..."
-EXP_DIR=$OUTPUT_DIR/default/$EXPERIMENT
-rm -rf $EXP_DIR
-main.py mode=train training=$EXPERIMENT training.save_top_k=1 paths.output=`realpath $OUTPUT_DIR` training.version=$EXPERIMENT training.n_epochs=1 training.trainer.check_val_every_n_epoch=1
+./main.py mode=train training=$EXPERIMENT training.save_top_k=1 training.version=$EXPERIMENT
