25facb2707c507e195749c343e436ae9b2078a75
Derek Hansen <dereklh@umich.edu>
2022-03-02 16:22:01 -0500

    fixes a few flake8 warnings

--- bliss/models/galaxy_encoder.py
+++ bliss/models/galaxy_encoder.py
@@ -13,8 +13,8 @@ from torch.optim import Adam
 from bliss.models.decoder import ImageDecoder, get_mgrid
 from bliss.models.galaxy_net import OneCenteredGalaxyAE
 from bliss.models.location_encoder import get_full_params_from_tiles, get_images_in_tiles
-from bliss.models.vae.galaxy_vae import OneCenteredGalaxyVAE
 from bliss.models.vae.galaxy_flow import CenteredGalaxyLatentFlow
+from bliss.models.vae.galaxy_vae import OneCenteredGalaxyVAE
 from bliss.reporting import plot_image, plot_image_and_locs
 
 
