5e7978bfd85c9df04251f605338864d43a56f38f
Jeffrey Regier <regier@umich.edu>
2022-03-09 12:06:09 -0500

    fixed WPS122

--- case_studies/sdss_galaxies/tests/test_reporting.py
+++ case_studies/sdss_galaxies/tests/test_reporting.py
@@ -29,14 +29,14 @@ def test_coadd(devices, get_config):
     coadd_cat_file = Path(cfg.paths.data).joinpath("coadd_catalog_94_1_12.fits")
     coadd_cat = Table.read(coadd_cat_file)[:5]
     coadd_cat.remove_column("hlr")
-    _ = reporting.get_flux_coadd(coadd_cat)
+    reporting.get_flux_coadd(coadd_cat)
     coadd_cat["hlr"] = reporting.get_hlr_coadd(coadd_cat[:5], psf)
 
     # params for calculating metrics
     h = 1489
     w = 2048
     bp = 24
-    _ = reporting.get_params_from_coadd(coadd_cat, (bp, w - bp), (bp, h - bp))
+    reporting.get_params_from_coadd(coadd_cat, (bp, w - bp), (bp, h - bp))
 
 
 def test_measurements():
