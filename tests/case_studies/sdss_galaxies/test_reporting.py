0ce43a474e0e5bd73ef0529d64ba4ecc802b12ae
Derek Hansen <dereklh@umich.edu>
2022-03-31 09:17:53 -0400

    use same path object in test

--- tests/case_studies/sdss_galaxies/test_reporting.py
+++ tests/case_studies/sdss_galaxies/test_reporting.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import galsim
 import torch
 from astropy.table import Table
-from hydra.utils import instantiate
 
 from bliss import reporting
 from bliss.catalog import FullCatalog
@@ -44,10 +43,11 @@ def test_coadd(devices, get_sdss_galaxies_config):
 
     # get psf
     cfg = get_sdss_galaxies_config({}, devices)
-    psf = load_psf_from_file(cfg.paths.data + "/psField-000094-1-0012-PSF-image.npy", 0.396)
+    data_path = Path(cfg.paths.data)
+    psf = load_psf_from_file(data_path / "psField-000094-1-0012-PSF-image.npy", 0.396)
 
     # read file and get flux / hlr
-    coadd_cat_file = Path(cfg.paths.data).joinpath("coadd_catalog_94_1_12.fits")
+    coadd_cat_file = data_path / "coadd_catalog_94_1_12.fits"
     coadd_cat = Table.read(coadd_cat_file)
     coadd_cat.remove_column("hlr")
     reporting.get_flux_coadd(coadd_cat[:5])
