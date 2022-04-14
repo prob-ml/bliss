6ea917f611fd200847a9b006612ed6a136af51ee
Derek Hansen <dereklh@umich.edu>
2022-04-14 15:59:52 -0400

    Add n_source_weights argument to LocationEncoder (#543)
    * Add map_n_source_weights argument to LocationEncoder
    
    * don't need to normalize
    
    * map_n_source_weights -> n_source_weights
    
    * results using threshold=0.1
    
    * Added threshold stats
    
    * added expected/actual true positives, true negatives, etc
    
    * flake8
    
    * Fix linting errors

--- bliss/encoder.py
+++ bliss/encoder.py
@@ -1,5 +1,5 @@
-"""Scripts to produce BLISS estimates on survey images. Currently only SDSS is supported."""
-from typing import Optional
+"""Scripts to produce BLISS estimates on astronomical images."""
+from typing import Optional, Tuple
 
 import torch
 from torch import Tensor, nn
@@ -33,6 +33,7 @@ class Encoder(nn.Module):
         binary_encoder: Optional[BinaryEncoder] = None,
         galaxy_encoder: Optional[GalaxyEncoder] = None,
         eval_mean_detections: Optional[float] = None,
+        map_n_source_weights: Optional[Tuple[float, ...]] = None,
     ):
         """Initializes Encoder.
 
@@ -48,8 +49,11 @@ class Encoder(nn.Module):
                 returns a classification between stars and galaxies. Defaults to None.
             galaxy_encoder: Module that takes padded tiles and locations and returns the variational
                 distribution of the latent variable determining the galaxy shape. Defaults to None.
-            eval_mean_detections: Optional. Mean number of sources in each tile for test-time image.
-                If provided, probabilities in location_encoder are adjusted.
+            eval_mean_detections: Optional. See LocationEncoder. Mean number of sources in each tile
+                for test-time image. If provided, probabilities in location_encoder are adjusted.
+            map_n_source_weights: Optional. See LocationEncoder. If specified, weights the argmax in
+                MAP estimation of locations. Useful for raising/lowering the threshold for turning
+                sources on/off.
         """
         super().__init__()
         self._dummy_param = nn.Parameter(torch.empty(0))
@@ -58,6 +62,12 @@ class Encoder(nn.Module):
         self.binary_encoder = binary_encoder
         self.galaxy_encoder = galaxy_encoder
         self.eval_mean_detections = eval_mean_detections
+
+        if map_n_source_weights is None:
+            map_n_source_weights_tnsr = torch.ones(self.location_encoder.max_detections + 1)
+        else:
+            map_n_source_weights_tnsr = torch.tensor(map_n_source_weights)
+        self.register_buffer("map_n_source_weights", map_n_source_weights_tnsr, persistent=False)
 
     def forward(self, x):
         raise NotImplementedError(
@@ -90,9 +100,12 @@ class Encoder(nn.Module):
                 - 'galaxy_bools', 'star_bools', and 'galaxy_probs' from BinaryEncoder.
                 - 'galaxy_params' from GalaxyEncoder.
         """
+        assert isinstance(self.map_n_source_weights, Tensor)
         var_params = self.location_encoder.encode(image, background)
         tile_map = self.location_encoder.max_a_post(
-            var_params, eval_mean_detections=self.eval_mean_detections
+            var_params,
+            eval_mean_detections=self.eval_mean_detections,
+            n_source_weights=self.map_n_source_weights,
         )
 
         if self.binary_encoder is not None:
