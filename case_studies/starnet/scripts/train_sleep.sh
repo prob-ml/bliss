26b63f919661a6a9b4ab77d9656a1fd88ea6c69e
Runjing Liu <runingli@deeplearning-01.stat.lsa.umich.edu>
2022-04-17 15:49:39 -0400

    m2 results

--- case_studies/starnet/scripts/train_sleep.sh
+++ case_studies/starnet/scripts/train_sleep.sh
@@ -2,6 +2,7 @@
 
 outfolder='../fits/'
 encoder_name='starnet-m2'
+# encoder_name='tmp'
 
 python train_sleep.py \
   --outfolder $outfolder \
--- case_studies/starnet/starnet_utils/starnet_metrics.py
+++ case_studies/starnet/starnet_utils/starnet_metrics.py
@@ -1,6 +1,100 @@
 import torch
 import numpy as np
-from bliss.metrics import get_tpr_ppv
+
+import galsim
+import numpy as np
+import torch
+import tqdm
+from einops import rearrange, reduce
+from scipy import optimize as sp_optim
+
+def inner_join_locs(locs1, locs2):
+
+    # locs1 and locs2 are arrays of locations,
+    # each of shape (number of sources) x 2
+
+    # permutes locs2 to find minimal error between locs1 and locs2.
+    # The matching is done using scipy.optimize.linear_sum_assignment,
+    # which implements the Hungarian algorithm.
+
+    # if locs1 is less than locs2, not every locs2 is returned;
+    # if locs2 is less than locs1, not every locs1 is returned.
+    # Only those with a match is returned, hence the "inner_join."
+
+    assert len(locs1.shape) == 2
+    assert locs1.shape[1] == 2
+
+    assert len(locs2.shape) == 2
+    assert locs2.shape[1] == 2
+
+    # mse of locs:
+    # entry (i,j) is l1 distance between of ith loc in locs1
+    # and to jth loc in locs2
+    locs_err = (rearrange(locs1, "i j -> i 1 j") - rearrange(locs2, "i j -> 1 i j")).abs()
+    locs_err = reduce(locs_err, "i j k -> i j", "sum")
+
+    # find minimal permutation
+    row_indx, col_indx = sp_optim.linear_sum_assignment(locs_err.detach().cpu())
+
+    # return locations with their match
+    return locs1[row_indx], locs2[col_indx], row_indx, col_indx
+
+
+def inner_join_locs_and_fluxes(locs1, fluxes1, locs2, fluxes2):
+    # permutes locations and fluxes using the Hungarian algorithm
+    # on locations to find matches.
+    # see `inner_join_locs`.
+
+    # find matches based on locations,
+    # and permute locations.
+    locs1, locs2, row_indx, col_indx = inner_join_locs(locs1, locs2)
+
+    # permute fluxes based on how we permuted locations
+    fluxes1 = fluxes1[row_indx]
+    fluxes2 = fluxes2[col_indx]
+
+    return locs1, fluxes1, locs2, fluxes2
+
+
+def get_l1_error(true_locs, true_mag, est_locs, est_mag):
+
+    # returns a vector of absolute errors for locations and fluxes,
+    # one for each star.
+
+    # The number of estimated stars does not have to equal the number of true stars!
+
+    # We only evaluate the error on the sources that have a match:
+    # the returned vector of absolute erros have length min(num. true sources, num. est. sources).
+    # See `inner_join_locs_and_fluxes` above.
+
+    true_locs, true_mag, est_locs, est_mag = inner_join_locs_and_fluxes(
+        true_locs, true_mag, est_locs, est_mag
+    )
+
+    # L1 error over locations
+    locs_mae = (true_locs - est_locs).abs().sum(1)
+
+    # flux error (for all bands)
+    fluxes_mae = (true_mag - est_mag).abs().flatten()
+
+    return locs_mae, fluxes_mae
+
+
+def get_tpr_ppv(true_locs, true_mag, est_locs, est_mag, slack=1.0):
+
+    # l-infty error in location,
+    # matrix of true x est error
+    locs_error = torch.abs(est_locs.unsqueeze(0) - true_locs.unsqueeze(1)).max(-1)[0]
+
+    # worst error in either band
+    mag_error = torch.abs(est_mag.unsqueeze(0) - true_mag.unsqueeze(1)).max(-1)[0]
+
+    tpr_bool = torch.any((locs_error < slack) * (mag_error < slack), dim=1).float()
+    ppv_bool = torch.any((locs_error < slack) * (mag_error < slack), dim=0).float()
+
+    return tpr_bool.mean(), ppv_bool.mean()
+
+
 
 def get_tpr_vec(
     true_locs, true_mags, est_locs, est_mags, mag_bins
