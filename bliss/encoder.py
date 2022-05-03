759ee850699bc47343d761e7ddfa3d10f00cab6d
Derek Hansen <dereklh@umich.edu>
2022-05-03 08:55:03 -0400

    Allow Encoder to handle entire frame and remove ChunkedScene (#597)
    * encapsulate part of encoder that works on ptiles
    
    * Pipeline still works
    
    * got it working
    
    * clean some things up
    
    * get new encoder code working
    
    * Eliminate ChunkedScene code
    
    * fix flake and mypy
    
    * a couple of small fixes, figures stayed the same
    
    Co-authored-by: Ismael Mendoza <imendoza@umich.edu>

--- bliss/encoder.py
+++ bliss/encoder.py
@@ -1,9 +1,9 @@
 """Scripts to produce BLISS estimates on astronomical images."""
-from typing import Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import torch
-from einops import rearrange
 from torch import Tensor, nn
+from tqdm import tqdm
 
 from bliss.catalog import TileCatalog, get_images_in_tiles, get_is_on_from_n_sources
 from bliss.models.binary import BinaryEncoder
@@ -34,6 +34,7 @@ class Encoder(nn.Module):
         binary_encoder: Optional[BinaryEncoder] = None,
         galaxy_encoder: Optional[GalaxyEncoder] = None,
         map_n_source_weights: Optional[Tuple[float, ...]] = None,
+        batch_size: Optional[int] = None,
     ):
         """Initializes Encoder.
 
@@ -52,6 +53,8 @@ class Encoder(nn.Module):
             map_n_source_weights: Optional. See LocationEncoder. If specified, weights the argmax in
                 MAP estimation of locations. Useful for raising/lowering the threshold for turning
                 sources on/off.
+            batch_size: How many padded tiles can be rendered at a time on the GPU?
+                If not specified, defaults to an amount known to fit on my GPU.
         """
         super().__init__()
         self._dummy_param = nn.Parameter(torch.empty(0))
@@ -64,6 +67,8 @@ class Encoder(nn.Module):
             map_n_source_weights_tnsr = torch.ones(self.location_encoder.max_detections + 1)
         else:
             map_n_source_weights_tnsr = torch.tensor(map_n_source_weights)
+
+        self.batch_size = batch_size if batch_size is not None else 75**2 + 500 * 5
         self.register_buffer("map_n_source_weights", map_n_source_weights_tnsr, persistent=False)
 
     def forward(self, x):
@@ -95,14 +100,36 @@ class Encoder(nn.Module):
                 - 'galaxy_bools', 'star_bools', and 'galaxy_probs' from BinaryEncoder.
                 - 'galaxy_params' from GalaxyEncoder.
         """
-        assert isinstance(self.map_n_source_weights, Tensor)
-        image_ptiles = get_images_in_tiles(
-            torch.cat((image, background), dim=1),
-            self.location_encoder.tile_slen,
-            self.location_encoder.ptile_slen,
+        n_tiles_h = (image.shape[2] - 2 * self.border_padding) // self.location_encoder.tile_slen
+        n_tiles_w = (image.shape[3] - 2 * self.border_padding) // self.location_encoder.tile_slen
+        ptile_loader = self._make_ptile_loader(image, background, n_tiles_h, n_tiles_w)
+        tile_map_list: List[Dict[str, Tensor]] = []
+        with torch.no_grad():
+            for ptiles in tqdm(ptile_loader, desc="Encoding ptiles"):
+                assert isinstance(ptiles, Tensor)
+                out_ptiles = self._encode_ptiles(ptiles)
+                tile_map_list.append(out_ptiles)
+        tile_map_dict = self._collate(tile_map_list)
+        return TileCatalog.from_flat_dict(
+            self.location_encoder.tile_slen, n_tiles_h, n_tiles_w, tile_map_dict
         )
-        _, n_tiles_h, n_tiles_w, _, _, _ = image_ptiles.shape
-        image_ptiles = rearrange(image_ptiles, "n nth ntw b h w -> (n nth ntw) b h w")
+
+    def _make_ptile_loader(self, image: Tensor, background: Tensor, n_tiles_h: int, n_tiles_w: int):
+        img_bg = torch.cat((image, background), dim=1).to(self.device)
+        n_rows_per_batch = self.batch_size // n_tiles_w
+        for row in range(0, n_tiles_h, n_rows_per_batch):
+            end_row = row + n_rows_per_batch
+            start_h = row * self.location_encoder.tile_slen
+            end_h = end_row * self.location_encoder.tile_slen + 2 * self.border_padding
+            img_bg_cropped = img_bg[:, :, start_h:end_h, :]
+            image_ptiles = get_images_in_tiles(
+                img_bg_cropped, self.location_encoder.tile_slen, self.location_encoder.ptile_slen
+            )
+            image_ptiles = image_ptiles.view(-1, *image_ptiles.shape[-3:])
+            yield image_ptiles
+
+    def _encode_ptiles(self, image_ptiles: Tensor):
+        assert isinstance(self.map_n_source_weights, Tensor)
         dist_params = self.location_encoder.encode(image_ptiles)
         tile_map_dict = self.location_encoder.variational_mode(
             dist_params, n_source_weights=self.map_n_source_weights
@@ -129,9 +156,14 @@ class Encoder(nn.Module):
             galaxy_params *= is_on_array.unsqueeze(-1) * galaxy_bools
             tile_map_dict.update({"galaxy_params": galaxy_params})
 
-        return TileCatalog.from_flat_dict(
-            self.location_encoder.tile_slen, n_tiles_h, n_tiles_w, tile_map_dict
-        )
+        return tile_map_dict
+
+    @staticmethod
+    def _collate(tile_map_list: List[Dict[str, Tensor]]) -> Dict[str, Tensor]:
+        out: Dict[str, Tensor] = {}
+        for k in tile_map_list[0]:
+            out[k] = torch.cat([d[k] for d in tile_map_list], dim=0)
+        return out
 
     def get_images_in_ptiles(self, images):
         """Run get_images_in_ptiles with correct tile_slen and ptile_slen."""
