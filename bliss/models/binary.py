0695abb44d466388b62879522eb492c7f1f1bdbd
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2022-05-27 00:33:09 -0400

    Refactoring towards metrics on simulated galsim blends (#608)
    * change name to GalsimBlends, start figure for metrics
    
    * add types
    
    * not used arguments
    
    * add slen to blend sim dataset
    
    * change galsim sim blend config dataset defaults
    
    * start evaluation on galsim blends figure
    
    * update git ignore
    
    * tests
    
    * finished testing
    
    * automatically set star_bools in TileCatalog
    PhotFullCatalog lives in catalog.py
    
    * simplify get_single_galaxy_measurements
    
    * update plots.py with changes in catalog, etc.
    
    * star bool gets updated automatically
    
    * inference does not need to know about galaxy_fluxes
    
    * scratch
    
    * decoder updated to give galaxy_fluxes if requested
    
    * simplify
    
    * imports fixed
    
    * fix tests
    
    * remove commented out
    
    * flake8 not everywhere
    
    * vastly simplify plotting code
    
    * enfoce calculations are all tensors
    
    * propagate new simpler way to plot
    
    * enforce a standard return type for all compute_data figures
    
    * do not propagate fullcatalog rather precompute quantities need from it
    
    * small corrections, images updated
    
    * update images
    
    * last few tests passing; enforce fullcatalog uses float for bools
    
    * small type bug in mypy
    
    * fix weird import error

--- bliss/models/binary.py
+++ bliss/models/binary.py
@@ -8,6 +8,7 @@ from torch import Tensor
 from torch.nn import BCELoss
 from torch.optim import Adam
 
+from bliss import reporting
 from bliss.catalog import TileCatalog, get_images_in_tiles, get_is_on_from_n_sources
 from bliss.models.detection_encoder import (
     ConcatBackgroundTransform,
@@ -16,7 +17,6 @@ from bliss.models.detection_encoder import (
     make_enc_final,
 )
 from bliss.models.galaxy_encoder import CenterPaddedTilesTransform
-from bliss.reporting import plot_image_and_locs
 
 
 class BinaryEncoder(pl.LightningModule):
@@ -200,14 +200,21 @@ class BinaryEncoder(pl.LightningModule):
         fig, axes = plt.subplots(nrows=nrows, ncols=nrows, figsize=(12, 12))
         axes = axes.flatten()
 
-        for i in range(n_samples):
-            labels = None if i > 0 else ("t. gal", "p. gal", "t. star", "p. star")
+        for ii in range(n_samples):
+            ax = axes[ii]
+            labels = None if ii > 0 else ("t. gal", "p. gal", "t. star", "p. star")
             bp = self.border_padding
-            images = batch["images"]
-            plot_image_and_locs(
-                fig, axes[i], i, images, bp, true_params, est, labels=labels, annotate_probs=True
-            )
-
+            image = batch["images"][ii, 0].numpy()
+            true_plocs = true_params.plocs[ii].numpy().reshape(-1, 2)
+            true_gbools = true_params["galaxy_bools"][ii].numpy().reshape(-1)
+            est_plocs = est.plocs[ii].numpy().reshape(-1, 2)
+            est_gprobs = est["galaxy_probs"][ii].numpy().reshape(-1)
+            slen, _ = image.shape
+            reporting.plot_image(fig, ax, image, colorbar=False)
+            reporting.plot_locs(ax, bp, slen, true_plocs, true_gbools, m="+", s=30, cmap="cool")
+            reporting.plot_locs(ax, bp, slen, est_plocs, est_gprobs, m="x", s=20, cmap="bwr")
+            if ii == 0:
+                reporting.add_legend(ax, labels)
         fig.tight_layout()
 
         title = f"Epoch:{self.current_epoch}/Validation Images"
