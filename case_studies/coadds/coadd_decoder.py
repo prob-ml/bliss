7c1aacd47b36cc1942450cada663da85a2613685
wangmallory <mallory.wang@gmail.com>
2022-07-12 16:04:43 -0400

    update coadd_decoder and add test_notebook

--- case_studies/coadds/coadd_decoder.py
+++ case_studies/coadds/coadd_decoder.py
@@ -9,13 +9,12 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Dataset
 from torch import Tensor
-from hydra import compose, initialize
-from hydra.utils import instantiate
 from einops import rearrange, reduce
 from bliss.catalog import FullCatalog
 from bliss.catalog import TileCatalog, get_is_on_from_n_sources
 from bliss.models.galaxy_net import OneCenteredGalaxyAE
 from bliss.encoder import Encoder
+from bliss.datasets.background import ConstantBackground
 from bliss.datasets.galsim_galaxies import SingleGalsimGalaxies
 from bliss.models.decoder import GalaxyTileDecoder
 from bliss.datasets.galsim_galaxies import GalsimBlends
@@ -24,6 +23,53 @@ from bliss.catalog import FullCatalog, TileCatalog
 from bliss.models.decoder import get_mgrid
 from bliss.models.galsim_decoder import SingleGalsimGalaxyDecoder, load_psf_from_file
 
+def _sample_n_sources(max_n_sources) -> int:
+    return int(torch.randint(1, max_n_sources + 1, (1,)).int().item())
+
+def _uniform(a, b, n_samples=1) -> Tensor:
+    # uses pytorch to return a single float ~ U(a, b)
+    return (a - b) * torch.rand(n_samples) + b
+
+class CoaddUniformGalsimGalaxiesPrior(UniformGalsimGalaxiesPrior):
+    def __init__(
+        self,
+        single_galaxy_prior: SingleGalsimGalaxyPrior,
+        max_n_sources: int,
+        max_shift: float,
+        num_dithers: int,
+    ):
+        super().__init__(
+            single_galaxy_prior,
+            max_n_sources,
+            max_shift,
+        )
+    def sample(self) -> Dict[str, Tensor]:
+        """Returns a single batch of source parameters."""
+        n_sources = _sample_n_sources(self.max_n_sources)
+
+        params = torch.zeros(self.max_n_sources, self.dim_latents)
+        params[:n_sources, :] = self.single_galaxy_prior.sample(n_sources)
+
+        locs = torch.zeros(self.max_n_sources, 2)
+        locs[:n_sources, 0] = _uniform(-self.max_shift, self.max_shift, n_sources) + 0.5
+        locs[:n_sources, 1] = _uniform(-self.max_shift, self.max_shift, n_sources) + 0.5
+
+        # for now, galaxies only
+        galaxy_bools = torch.zeros(self.max_n_sources, 1)
+        galaxy_bools[:n_sources, :] = 1
+        star_bools = torch.zeros(self.max_n_sources, 1)
+
+        dithers = [((-0.5 - 0.5) * torch.rand((2,)) + 0.5).numpy() for x in range(num_dithers)]
+
+        return {
+            "n_sources": torch.tensor(n_sources),
+            "galaxy_params": params,
+            "locs": locs,
+            "galaxy_bools": galaxy_bools,
+            "star_bools": star_bools,
+            "dithers": dithers,
+        }
+
 class CoaddSingleGalaxyDecoder(SingleGalsimGalaxyDecoder):
     def __init__(
         self,
@@ -31,7 +77,13 @@ class CoaddSingleGalaxyDecoder(SingleGalsimGalaxyDecoder):
         n_bands: int,
         pixel_scale: float,
         psf_image_file: str,
-    ) -> None:
+    ):
+        super().__init__(
+            slen,
+            n_bands,
+            pixel_scale,
+            psf_image_file,
+        )
         assert n_bands == 1, "Only 1 band is supported"
         self.slen = slen
         self.n_bands = 1
@@ -74,18 +126,19 @@ class CoaddSingleGalaxyDecoder(SingleGalsimGalaxyDecoder):
         galaxy = galsim.Add(components)
         gal_conv = galsim.Convolution(galaxy, psf)
         offset = offset if offset is None else offset.numpy()
-        shift = torch.add(torch.Tensor(dither), torch.Tensor(offset))
+        shift = torch.add(torch.Tensor(dithers), torch.Tensor(offset))
         images = []
         for i in shift:
             image = gal_conv.drawImage(
-                nx=slen, ny=slen, method="auto", scale=self.pixel_scale, offset=shift[i]
+                nx=slen, ny=slen, method="auto", scale=self.pixel_scale, offset=i
             )
+            image = image.array
             images.append(image)
-        return torch.from_numpy(images.array).reshape(len(dithers), 1, slen, slen)
+        return torch.tensor(images[:]).reshape(len(dithers), 1, slen, slen)
 
 class FullCatalogDecoder:
     def __init__(
-        self, single_galaxy_decoder: SingleGalsimGalaxyDecoder, slen: int, bp: int
+        self, single_galaxy_decoder: CoaddSingleGalaxyDecoder, slen: int, bp: int
     ) -> None:
         self.single_decoder = single_galaxy_decoder
         self.slen = slen
@@ -120,26 +173,6 @@ class FullCatalogDecoder:
             image += uncentered
         return image, noiseless_centered, noiseless_uncentered
 
-class PsfSampler:
-    def __init__(
-        self,
-        psf_rmin: float = 0.7,
-        psf_rmax: float = 0.9,
-    ) -> None:
-        self.rmin = psf_rmin
-        self.rmax = psf_rmax
-
-    def sample(self) -> galsim.GSObject:
-        # sample psf from galsim Gaussian distribution
-        if self.rmin == self.rmax:
-            fwhm = self.rmin
-        elif self.rmin > self.rmax:
-            raise ValueError("invalid argument!!!")
-        else:
-            fwhm = torch.distributions.uniform.Uniform(self.rmin, self.rmax).sample([1]).item()
-        
-        return galsim.Gaussian(fwhm=fwhm)
-
 class CoaddGalsimBlends(GalsimBlends):
     """Dataset of coadd galsim blends."""
 
@@ -152,7 +185,6 @@ class CoaddGalsimBlends(GalsimBlends):
         num_workers: int,
         batch_size: int,
         n_batches: int,
-        psf_sampler: PsfSampler,
         fix_validation_set: bool = False,
         valid_n_batches: Optional[int] = None,
     ):
@@ -170,9 +202,7 @@ class CoaddGalsimBlends(GalsimBlends):
         )
         self.slen = self.decoder.slen
         self.pixel_scale = self.decoder.single_decoder.pixel_scale
-        self.psf = psf_sampler
         
-
     def _get_images(self, full_cat, dithers):
         psf_obj = self.psf.sample()
         noiseless, noiseless_centered, noiseless_uncentered = FullCatalogDecoder.render_catalog(
