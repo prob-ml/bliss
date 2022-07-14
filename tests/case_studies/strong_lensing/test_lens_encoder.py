b78f1e512625236507f43c83ada7881dffa5eb26
yashpatel5400 <ypatel5400@gmail.com>
2022-07-13 20:34:01 -0400

    Add lensing tests

---
+++ tests/case_studies/strong_lensing/test_lens_encoder.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def overrides(devices):
+    overrides = {
+        "mode": "train",
+        "training": "sdss_lens_encoder",
+    }
+    if devices.use_cuda:
+        overrides.update({"training.n_epochs": 50})
+    else:
+        overrides.update(
+            {
+                "datasets.simulated.n_batches": 1,
+                "datasets.simulated.batch_size": 2,
+                "datasets.simulated.generate_device": "cpu",
+                "training.n_epochs": 2,
+            }
+        )
+    return overrides
+
+
+def test_lens_encoder(strong_lensing_setup, devices, overrides):
+    trained_lens_encoder = strong_lensing_setup.get_trained_model(overrides)
+    results = strong_lensing_setup.test_model(overrides, trained_lens_encoder)
