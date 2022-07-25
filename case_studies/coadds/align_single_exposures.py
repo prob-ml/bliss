c8b030237f5ef8b5697e70fec39a99551644df93
wangmallory <mallory.wang@gmail.com>
2022-07-25 12:13:46 -0400

    update with changes from mallory/align branch

--- case_studies/coadds/align_single_exposures.py
+++ case_studies/coadds/align_single_exposures.py
@@ -1,38 +1,49 @@
 import numpy as np
-import matplotlib.pyplot as plt 
-from pathlib import Path
 import galsim
 import torch
 import torch.nn.functional as F
-from torch import Tensor
 from bliss.models.decoder import get_mgrid
 
-def align_single_exposures(img0, img1, slen, pixel_scale, dithers):
-    img0 = galsim.Image(np.array(img0), wcs=galsim.PixelScale(pixel_scale)) if type(img0) is not galsim.image.Image else img0
+
+def align_single_exposures(img0, img1, slen, dithers, scale=0.393):
+    """Aligns multiple single exposure images that are dithered by some sub-pixel amount.
+    Args:
+        img0: Tensor of shape `(1 x C x H x W)` C is the number of band, H is height and W is weight,
+            containing image data.
+        img1: Tensor of shape `(N x C x H x W)` where N is the number of dithers (len(dithers)),
+            C is the number of band, H is height and W is weight, containing image data.
+        slen: size of images (H and W)
+        dithers: List of pairs of sub-pixel amounts that img0 is shifted by in x and y directions
+        scale: pixel_scale
+    Returns:
+        Tensor of shape `(N x H-2 x W-2)`. Aligned images with 1 pixel cropped from from each
+            direction of height and width.
+    """
+    img0 = galsim.Image(np.array(img0), wcs=galsim.PixelScale(scale))
     wcs0 = img0.wcs
     images = img1
 
-    sgrid = (get_mgrid(slen) - (-1))/(1-(-1)) * (slen)
-    grid_x = wcs0.xyTouv(np.array(sgrid.reshape(slen*slen,2)[:,0]), np.array(sgrid.reshape(slen*slen,2)[:,1]))[0]
-    grid_y = wcs0.xyTouv(np.array(sgrid.reshape(slen*slen,2)[:,0]), np.array(sgrid.reshape(slen*slen,2)[:,1]))[1]
+    sgrid = (get_mgrid(slen) - (-1)) / 2 * (slen)
+    sgrid = sgrid.reshape(slen**2, 2)
+    grid_x = wcs0.xyTouv(sgrid[:, 0], sgrid[:, 1])[0]
+    grid_y = wcs0.xyTouv(sgrid[:, 0], sgrid[:, 1])[1]
 
     grid = torch.empty(size=(0, 2))
     for i in dithers:
-        wcs1 = galsim.OffsetWCS(scale = 0.393, origin = galsim.PositionD(i))
-        x, y = wcs1.uvToxy(grid_x,grid_y)
-        x_grid = (x/slen) * (1-(-1)) + (-1)
-        y_grid = (y/slen) * (1-(-1)) + (-1)
-        grid = torch.cat([grid, torch.stack((torch.tensor(x_grid),torch.tensor(y_grid)),-1)], dim=0)
+        wcs1 = galsim.OffsetWCS(scale=scale, origin=galsim.PositionD(i))
+        x, y = wcs1.uvToxy(grid_x, grid_y)
+        x_grid = (x / slen) * 2 + (-1)
+        y_grid = (y / slen) * 2 + (-1)
+        grid = torch.cat(
+            [grid, torch.stack((torch.tensor(x_grid), torch.tensor(y_grid)), -1)], dim=0
+        )
 
     iplots = []
-    input = torch.tensor(images[:]).reshape(len(dithers),1,slen,slen).float()
-    grids = grid.reshape(len(dithers),1,slen*slen,2).float()
-    iplots.append(F.grid_sample(input, grids, align_corners = False))
-    
-    tenplot = torch.tensor(iplots[:][0])
-    crop_images = []
-    for i in range(tenplot.shape[0]):
-        im = np.array(tenplot[i].reshape(slen,slen))
-        crop_im = im[1:slen-1, 1:slen-1]
-        crop_images.append(crop_im)
-    return torch.tensor(crop_images)
+    input = torch.tensor(images[:]).reshape(len(dithers), 1, slen, slen).float()
+    grids = grid.reshape(len(dithers), 1, slen * slen, 2).float()
+    iplots.append(F.grid_sample(input, grids, align_corners=False))
+
+    # reshape and crop 1 pixel on each side
+    iplots = torch.tensor(iplots[:][0]).reshape(len(dithers), slen, slen)
+    iplots_cropped = iplots[:, 1 : slen - 1, 1 : slen - 1]
+    return iplots_cropped
--- tests/case_studies/coadds/test_align_single_exposures.py
+++ tests/case_studies/coadds/test_align_single_exposures.py
@@ -1,18 +1,20 @@
-import case_studies.coadds.align_single_exposures
-import galsim 
+import galsim
 import torch
+
+from case_studies.coadds.align_single_exposures import align_single_exposures
+
 
 def test_galsim_align():
     pixel_scale = 0.393
-    slen = 9 
+    slen = 9
     g0 = galsim.Gaussian(sigma=1.0)
     g0 = g0.shear(e1=0, e2=0)
-    img0 = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale)
+    img0 = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, bandpass=None)
+    img0 = img0.array
     dithers = [((-0.5 - 0.5) * torch.rand((2,)) + 0.5).numpy() for x in range(10)]
     img = []
     for i in dithers:
-        wcs1 = galsim.OffsetWCS(scale = 0.393, origin = galsim.PositionD(i))
-        im = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset = i)
+        im = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=i, bandpass=None)
         im = im.array
         img.append(im)
-    case_studies.coadds.align_single_exposures.align_single_exposures(img0, img, slen, pixel_scale, dithers)
+    align_single_exposures(img0, img, slen, dithers)
