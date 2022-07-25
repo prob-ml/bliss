6c0d10d92f04c67479e7836a956a73a06d8c8a84
wangmallory <mallory.wang@gmail.com>
2022-07-25 11:54:32 -0400

    fix scale in test_

--- tests/case_studies/coadds/test_align_single_exposures.py
+++ tests/case_studies/coadds/test_align_single_exposures.py
@@ -10,10 +10,11 @@ def test_galsim_align():
     g0 = galsim.Gaussian(sigma=1.0)
     g0 = g0.shear(e1=0, e2=0)
     img0 = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, bandpass=None)
+    img0 = img0.array
     dithers = [((-0.5 - 0.5) * torch.rand((2,)) + 0.5).numpy() for x in range(10)]
     img = []
     for i in dithers:
         im = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=i, bandpass=None)
         im = im.array
         img.append(im)
-    align_single_exposures(img0, img, slen, pixel_scale, dithers)
+    align_single_exposures(img0, img, slen, dithers)
