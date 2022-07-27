8ee6dced785e8e807db89f83a40be48055475fb5
wangmallory <mallory.wang@gmail.com>
2022-07-27 17:47:22 -0400

    update snr

--- case_studies/coadds/signal_noise_ratio.py
+++ case_studies/coadds/signal_noise_ratio.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 
-def snr(img):
+def snr(img, background):
     """Calculate Signal-to-Noise Ratio.
     Args:
         img: Tensor of shape `(N x C x H x W)` where N is the batch_size, C is the
@@ -10,8 +10,8 @@ def snr(img):
     Returns:
         Tensor of shape `(N x 1)`. The Signal-to-Noise Ratio.
     """
+    assert img.shape == background.shape
 
-    # sn = torch.sqrt(torch.sum((img)**2 / (img), dim=(3, 2, 1)))
-    sn = torch.mean(img, dim=(3, 2, 1)) / torch.std(img, dim=(3, 2, 1))
+    sn = torch.sqrt(torch.sum((img - background)**2 / (img), dim=(3, 2, 1)))
 
-    return sn
+    return 1 / sn
