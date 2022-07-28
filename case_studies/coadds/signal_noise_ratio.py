c6f9f335101748d5b852e05994e631b89939a28c
Ismael Mendoza <imendoza@umich.edu>
2022-07-28 00:03:53 -0400

    SNR fixes

--- case_studies/coadds/signal_noise_ratio.py
+++ case_studies/coadds/signal_noise_ratio.py
@@ -1,17 +1,17 @@
-import numpy as np
 import torch
 
 
-def snr(img, background):
+def get_snr(img_no_background, background):
     """Calculate Signal-to-Noise Ratio.
+
     Args:
-        img: Tensor of shape `(N x C x H x W)` where N is the batch_size, C is the
+        background: Tensor of shape `(N x C x H x W)` containing the background for the image.
+        img_no_background: Tensor of shape `(N x C x H x W)` where N is the batch_size, C is the
             number of band, H is height and W is weight, containing image data.
+
     Returns:
-        Tensor of shape `(N x 1)`. The Signal-to-Noise Ratio.
+        Tensor of shape `(N,)`. The Signal-to-Noise Ratio.
     """
-    assert img.shape == background.shape
-
-    sn = torch.sqrt(torch.sum((img - background) ** 2 / (img), dim=(3, 2, 1)))
-
-    return sn
+    assert img_no_background.shape == background.shape
+    image = img_no_background + background
+    return torch.sqrt(torch.sum(img_no_background**2 / image, dim=(3, 2, 1)))
