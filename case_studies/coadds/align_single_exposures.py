429f41e994bc91800a42fa81ce5bfe990adcf8cc
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2022-07-29 17:48:58 -0400

    Mallory/align (#652)
    * adding align_single_exposures function, snr function and galsim_galaxy dataset
    
    * add align_single_exposures, snr functions and galsim_galaxies dataset -- incomplete
    
    * update config
    
    * update coadd_decoder and add test_notebook
    
    * update signal_noise_ratio
    
    * update render_galaxy in coadd_decoder, still need to fix render_catalog
    
    * fixed render_catalog
    
    * update notebook
    
    * add cropping to align_single_exposures
    
    * add crop to align_single_exposures and unit test
    
    * branch with only align and snr
    
    * fixed align_single_exposure and test
    
    * remove test_notebook from this branch
    
    * resolved comments per PR, added notebook to show align_single_exposure
    
    * notebooks
    
    * update formatting
    
    * update formatting
    
    * added bandpass
    
    * added bandpass
    
    * fix scale in test_
    
    * adding assert shape to test, fix naming conventions in align_, and change formula for snr
    
    * update formatting per black
    
    * update snr
    
    * remove test_notebook
    
    * cleaned up align_
    
    * Update case_studies/coadds/align_single_exposures.py
    
    * Update case_studies/coadds/align_single_exposures.py
    
    * SNR fixes
    
    * align single exposures fix
    
    Co-authored-by: wangmallory <mallory.wang@gmail.com>

---
+++ case_studies/coadds/align_single_exposures.py
@@ -0,0 +1,51 @@
+import galsim
+import numpy as np
+import torch
+from torch.nn import functional as F
+
+from bliss.models.decoder import get_mgrid
+
+
+def align_single_exposures(img0, images, slen, dithers, scale=0.393):
+    """Aligns multiple single exposure images that are dithered by some sub-pixel amount.
+
+    Args:
+        slen: size of images (H and W)
+        dithers: List of pairs of sub-pixel amounts that img0 is shifted by in x and y directions
+        scale: pixel_scale
+        img0: Tensor of shape `(1 x C x H x W)` C is the number of band, H is height and W is
+            weight, containing image data.
+        images: Tensor of shape `(N x C x H x W)` where N is the number of dithers (len(dithers)),
+            C is the number of band, H is height and W is weight, containing image data.
+
+    Returns:
+        Tensor of shape `(N x H-2 x W-2)`. Aligned images with 1 pixel cropped from from each
+            direction of height and width.
+    """
+    img0 = galsim.Image(np.array(img0), wcs=galsim.PixelScale(scale))
+    wcs0 = img0.wcs
+
+    sgrid = (get_mgrid(slen) - (-1)) / 2 * (slen)
+    sgrid = sgrid.reshape(slen**2, 2)
+    grid_x = wcs0.xyTouv(sgrid[:, 0], sgrid[:, 1])[0]
+    grid_y = wcs0.xyTouv(sgrid[:, 0], sgrid[:, 1])[1]
+
+    grid = torch.empty(size=(0, 2))
+    for dither in dithers:
+        wcs1 = galsim.OffsetWCS(scale=scale, origin=galsim.PositionD(dither))
+        x, y = wcs1.uvToxy(grid_x, grid_y)
+        x_grid = (x / slen) * 2 + (-1)
+        y_grid = (y / slen) * 2 + (-1)
+        grid = torch.cat(
+            [grid, torch.stack((torch.tensor(x_grid), torch.tensor(y_grid)), -1)], dim=0
+        )
+
+    interped_images = []
+    input = torch.tensor(images[:]).reshape(len(dithers), 1, slen, slen).float()
+    grids = grid.reshape(len(dithers), slen, slen, 2).float()
+    interped_images.append(F.grid_sample(input, grids, align_corners=False))
+
+    # reshape and crop 1 pixel on each side
+    interped_images = torch.tensor(interped_images[:][0]).reshape(len(dithers), slen, slen)
+    interped_images_cropped = interped_images[:, 1 : slen - 1, 1 : slen - 1]
+    return interped_images_cropped
---
+++ tests/case_studies/coadds/test_align_single_exposures.py
@@ -0,0 +1,22 @@
+import galsim
+import torch
+
+from case_studies.coadds.align_single_exposures import align_single_exposures
+
+
+def test_galsim_align():
+    pixel_scale = 0.393
+    slen = 9
+    g0 = galsim.Gaussian(sigma=1.0)
+    g0 = g0.shear(e1=0, e2=0)
+    img0 = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, bandpass=None)
+    img0 = img0.array
+    dithers = [((-0.5 - 0.5) * torch.rand((2,)) + 0.5).numpy() for x in range(10)]
+    img = []
+    for i in dithers:
+        im = g0.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=i, bandpass=None)
+        im = im.array
+        img.append(im)
+    assert align_single_exposures(img0, img, slen, dithers).shape == torch.Size(
+        [len(dithers), slen - 2, slen - 2]
+    )
