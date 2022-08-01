1aa596f98ffde48e98fe5bd9d3febc18ca112891
wangmallory <mallory.wang@gmail.com>
2022-08-01 12:12:55 -0400

    test

--- tests/case_studies/coadds/test_coadd_decoder.py
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -1,20 +1,11 @@
-from hydra import compose, initialize
-from hydra.utils import instantiate
-
 from case_studies.coadds.coadd_decoder import CoaddUniformGalsimGalaxiesPrior
 
 
-def get_coadds_cfg(get_config, devices):
-    with initialize(config_path="../coadds/config"):
-        cfg = compose("config")
-    return cfg
-
-
-def test_coadd_prior(devices):
+def test_coadd_prior(get_coadds_config, devices):
     max_n_sources = 1
     max_shift = 0.5
     num_dithers = 4
-    cfg = get_coadds_cfg({}, devices)
+    cfg = get_coadds_config({}, devices)
     prior = instantiate(cfg.datasets.sdss_galaxies_coadd.prior)
 
     CoaddUniformGalsimGalaxiesPrior(prior, max_n_sources, max_shift, num_dithers).sample(
