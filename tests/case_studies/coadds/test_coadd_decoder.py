7319e990fb612a0f2531ae6cda943ebc1143e9b9
wangmallory <mallory.wang@gmail.com>
2022-08-01 12:40:53 -0400

    test

--- tests/case_studies/coadds/test_coadd_decoder.py
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -1,6 +1,6 @@
 from hydra.utils import instantiate
 
-from case_studies.coadds.coadd_decoder import CoaddUniformGalsimGalaxiesPrior
+from case_studies.coadds.coadd_decoder import CoaddUniformGalsimGalaxiesPrior, CoaddSingleGalaxyDecoder
 
 
 def test_coadd_prior(get_coadds_config, devices):
