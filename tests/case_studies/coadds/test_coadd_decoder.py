472e4d204601f9073fee3c725cfc96d9292b1b55
wangmallory <mallory.wang@gmail.com>
2022-08-01 08:33:58 -0400

    fix test_coadd_decoder

--- tests/case_studies/coadds/test_coadd_decoder.py
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -1,38 +1,36 @@
 from hydra.utils import instantiate
 
 from case_studies.coadds.coadd_decoder import (
-    CoaddGalsimBlends,
     CoaddSingleGalaxyDecoder,
     CoaddUniformGalsimGalaxiesPrior,
 )
 
 
-def test_coadd_prior(get_config, devices):
+with initialize(config_path="./case_studies/sdss_galaxies/config"):
+    cfg = compose("config", overrides=[])
+prior = instantiate(cfg.datasets.sdss_galaxies_coadd.prior)
+decoder = instantiate(cfg.datasets.sdss_galaxies_coadd.decoder)
+
+
+def test_coadd_prior(prior):
     max_n_sources = 1
     max_shift = 0.5
     num_dithers = 4
-    cfg = get_config({}, devices)
-    prior = instantiate(cfg.datasets.sdss_galaxies_coadd.prior)
     CoaddUniformGalsimGalaxiesPrior(prior, max_n_sources, max_shift, num_dithers).sample(
         num_dithers
     )
 
 
-def test_coadd_single_decoder(get_config, devices):
+def test_coadd_single_decoder(prior):
     max_n_sources = 1
     max_shift = 0.5
     num_dithers = 4
-    cfg = get_config({}, devices)
-    prior = instantiate(cfg.datasets.sdss_galaxies_coadd.prior)
     sampled_cuggp = CoaddUniformGalsimGalaxiesPrior(
         prior, max_n_sources, max_shift, num_dithers
     ).sample(num_dithers)
     galaxy_params = sampled_cuggp["galaxy_params"]
     dithers = sampled_cuggp["dithers"]
     offset = None
-
-    cfg = get_config({}, devices)
-    decoder = instantiate(cfg.datasets.sdss_galaxies_coadd.decoder)
 
     csgd = CoaddSingleGalaxyDecoder(
         decoder,
@@ -46,29 +44,4 @@ def test_coadd_single_decoder(get_config, devices):
         psf=decoder.psf,
         offset=offset,
         dithers=dithers,
-    )
-
-
-def test_coadd_galsim_blend(get_config, devices):
-    cfg = get_config({}, devices)
-    decoder = instantiate(cfg.datasets.galsim_blended_galaxies_coadd.decoder)
-    prior = instantiate(cfg.datasets.galsim_blended_galaxies_coadd.prior)
-    decoder = instantiate(cfg.datasets.galsim_blended_galaxies_coadd.decoder)
-    background = instantiate(cfg.datasets.galsim_blended_galaxies_coadd.background)
-
-    tile_slen = 4
-    max_tile_n_sources = 1
-    num_workers = 5
-    batch_size = 1000
-    n_batches = 1
-
-    CoaddGalsimBlends(
-        prior=prior,
-        decoder=decoder,
-        background=background,
-        tile_slen=tile_slen,
-        max_sources_per_tile=max_tile_n_sources,
-        num_workers=num_workers,
-        batch_size=batch_size,
-        n_batches=n_batches,
     )
