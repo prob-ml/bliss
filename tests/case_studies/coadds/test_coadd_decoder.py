f520cf73a116cf4a1cc0a291cf7ed235702474a8
wangmallory <mallory.wang@gmail.com>
2022-08-01 10:45:15 -0400

    fix test

--- tests/case_studies/coadds/test_coadd_decoder.py
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -14,11 +14,11 @@ def get_coadds_cfg(overrides, devices):
     return cfg
 
 
-def test_coadd_prior(get_coadds_cfg, devices):
+def test_coadd_prior(overrides, devices):
     max_n_sources = 1
     max_shift = 0.5
     num_dithers = 4
-    cfg = get_coadds_cfg({}, devices)
+    cfg = get_coadds_cfg(overrides, devices)
     prior = instantiate(cfg.datasets.sdss_galaxies_coadd.prior)
 
     CoaddUniformGalsimGalaxiesPrior(prior, max_n_sources, max_shift, num_dithers).sample(
