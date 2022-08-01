2482de8c9a67497aaac8e72f20b7faf1d3b25abd
wangmallory <mallory.wang@gmail.com>
2022-08-01 08:45:36 -0400

    chnage order of import in test_

--- tests/case_studies/coadds/test_coadd_decoder.py
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -1,5 +1,5 @@
-from hydra.utils import instantiate
 from hydra import compose, initialize
+from hydra.utils import instantiate
 
 from case_studies.coadds.coadd_decoder import (
     CoaddSingleGalaxyDecoder,
