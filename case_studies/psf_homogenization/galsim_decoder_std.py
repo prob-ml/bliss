864e1e09d7f9b840528095c18f5f9d9214a06ebb
cheng wang <wangchv@umich.edu>
2022-08-14 15:46:13 -0400

    update case_studies accordingly

--- case_studies/psf_homogenization/galsim_decoder_std.py
+++ case_studies/psf_homogenization/galsim_decoder_std.py
@@ -6,9 +6,8 @@ from torch import Tensor
 
 from bliss.datasets.background import ConstantBackground
 from bliss.datasets.galsim_galaxies import GalsimBlends
-from bliss.models.galsim_decoder import FullCatalogDecoder, UniformGalsimPrior
+from bliss.models.galsim_decoder import FullCatalogDecoder, PsfSampler, UniformGalsimPrior
 from case_studies.psf_homogenization.homogenization import psf_homo
-from case_studies.psf_homogenization.psf_sampler import PsfSampler
 
 
 def _add_noise_and_background(image: Tensor, background: Tensor) -> Tensor:
@@ -28,10 +27,10 @@ class GalsimBlendswithPSF(GalsimBlends):
         num_workers: int,
         batch_size: int,
         n_batches: int,
+        psf_sampler: PsfSampler,
         std_psf_fwhm: float = 1.0,
         fix_validation_set: bool = False,
         valid_n_batches: Optional[int] = None,
-        psf_sampler: Optional[PsfSampler] = None,
     ):
         super().__init__(
             prior,
@@ -52,10 +51,9 @@ class GalsimBlendswithPSF(GalsimBlends):
             .array  # noqa: WPS348
         )
 
-    def _get_images(self, full_cat):
-        psf_obj = self.psf.sample()
+    def _get_images(self, full_cat, psf):
         noiseless, noiseless_centered, noiseless_uncentered = self.decoder.render_catalog(
-            full_cat, psf_obj
+            full_cat, psf
         )
 
         # get background and noisy image
@@ -65,7 +63,7 @@ class GalsimBlendswithPSF(GalsimBlends):
         # homogenization
         std_psf = self.std_psf.reshape(1, 1, self.slen, self.slen)
         psf = torch.from_numpy(
-            psf_obj.drawImage(nx=self.slen, ny=self.slen, scale=self.pixel_scale).array
+            psf.drawImage(nx=self.slen, ny=self.slen, scale=self.pixel_scale).array
         ).reshape(1, 1, self.slen, self.slen)
         homo_image, _ = psf_homo(
             noisy_image.reshape(1, *noisy_image.shape),
@@ -80,9 +78,10 @@ class GalsimBlendswithPSF(GalsimBlends):
         return noisy_image, *noiseless_images, background, homo_image, psf
 
     def __getitem__(self, idx):
+        psf_obj = self.psf_sampler.get_galsim_psf()
         full_cat = self._sample_full_catalog()
-        images, *metric_images, background, homo_image, psf = self._get_images(full_cat)
-        full_cat = self._add_metrics(full_cat, *metric_images, background)
+        images, *metric_images, background, homo_image, psf = self._get_images(full_cat, psf_obj)
+        full_cat = self._add_metrics(full_cat, *metric_images, background, psf_obj)
         tile_params = self._get_tile_params(full_cat)
         return {
             "images": homo_image,
