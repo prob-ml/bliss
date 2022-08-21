e854c9c26f6e900b51bba473e28c938fde411dbe
cheng wang <wangchv@umich.edu>
2022-08-21 10:00:07 -0400

    add _add_metrics() to train model

--- case_studies/psf_homogenization/galsim_galaxies_homo.py
+++ case_studies/psf_homogenization/galsim_galaxies_homo.py
@@ -52,7 +52,7 @@ class HomoGalsimBlends(GalsimBlends):
         return torch.from_numpy(psf.array).reshape(1, 1, self._size, self._size)
 
     def _get_images(self, full_cat):
-        noiseless, _, _ = self.decoder.render_catalog(full_cat)
+        noiseless, noiseless_centered, noiseless_uncentered = self.decoder.render_catalog(full_cat)
 
         # get background and noisy image
         background = self.background.sample((1, *noiseless.shape)).squeeze(0)
@@ -68,11 +68,13 @@ class HomoGalsimBlends(GalsimBlends):
         homo_image, _ = psf_homo(noisy_image, psf, self.std_psf, background)
 
         homo_image = homo_image.reshape(*noisy_image.shape)
-        return noisy_image, background, homo_image
+        metric_images = noiseless, noiseless_centered, noiseless_uncentered
+        return noisy_image, *metric_images,background, homo_image
 
     def __getitem__(self, idx):
         full_cat = self._sample_full_catalog()
-        images, background, homo_image = self._get_images(full_cat)
+        images, *metric_images, background, homo_image = self._get_images(full_cat)
+        full_cat = self._add_metrics(full_cat, *metric_images, background)
         tile_params = self._get_tile_params(full_cat)
         return {
             "images": homo_image.reshape(1, self._size, self._size),
