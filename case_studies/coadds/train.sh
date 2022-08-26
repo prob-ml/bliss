e391a1f09fb1c1170622a8c5a865403b45bccfd4
Ismael Mendoza <imendoza@umich.edu>
2022-08-26 15:10:06 -0400

    train script

--- case_studies/coadds/train.sh
+++ case_studies/coadds/train.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
-./main.py mode=train experiment='coadd_encoder' training.name=coadd_encoder_1
+export CUDA_VISIBLE_DEVICES=5
+./main.py mode=train training.experiment='coadd_encoder' training.name=coadd_encoder_1
 # ./main.py mode=train training.name="second_run" training.n_epochs=2000
