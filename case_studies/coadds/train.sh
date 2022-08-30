be62a17d280a5bca2800f37d84205db4907fcda2
wangmallory <mallory.wang@gmail.com>
2022-08-30 10:14:25 -0400

    add models and notebook

--- case_studies/coadds/train.sh
+++ case_studies/coadds/train.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export CUDA_VISIBLE_DEVICES=0
-./main.py mode=train training.name="first_run" 
-=======
 export CUDA_VISIBLE_DEVICES=5
-./main.py mode=train training.experiment='coadd_encoder' training.name=coadd_encoder_1
->>>>>>> 8b7e27cb02660d15f134289fd6501afa0a04be24
+./main.py mode=train training.experiment='coadd_encoder' training.name=coadd_encoder_3
 # ./main.py mode=train training.name="second_run" training.n_epochs=2000
