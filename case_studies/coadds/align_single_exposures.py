3d2128b50b7c9289077bcfef48f99105851f3b57
Ismael Mendoza <imendoza@umich.edu>
2022-09-11 17:42:00 -0400

    refactor and fix test

--- case_studies/coadds/align_single_exposures.py
+++ case_studies/coadds/align_single_exposures.py
@@ -8,12 +8,12 @@ from torch.nn import functional as F
 from bliss.models.decoder import get_mgrid
 
 
-def align_single_exposures(img0, images: Tensor, slen, dithers, scale=0.393):
+def align_single_exposures(img0: Tensor, images: Tensor, slen: int, dithers: Tensor, scale=0.393):
     """Aligns multiple single exposure images that are dithered by some sub-pixel amount.
 
     Args:
         slen: size of images (H and W)
-        dithers: List of pairs of sub-pixel amounts that img0 is shifted by in x and y directions
+        dithers: Tensor of pairs of sub-pixel amounts that img0 is shifted by in x and y directions
         scale: pixel_scale
         img0: Tensor of shape `(H x W)` H height and W width, containing reference image data.
         images: Tensor of shape `(N x C x H x W)` where N is the number of dithers (len(dithers)),
@@ -39,11 +39,10 @@ def align_single_exposures(img0, images: Tensor, slen, dithers, scale=0.393):
         y_grid = (y / slen) * 2 - 1
         grid = torch.cat([grid, torch.stack((x_grid, y_grid), -1)], dim=0)
 
-    interped_images = []
     images = images.reshape(len(dithers), 1, slen, slen)
     grids = grid.reshape(len(dithers), slen, slen, 2).float()
-    interped_images.append(F.grid_sample(images, grids, align_corners=False))
+    interped_images = F.grid_sample(images, grids, align_corners=False)
 
     # reshape and crop 1 pixel on each side
-    interped_images = rearrange(interped_images[:][0], "d 1 h w -> d h w")
+    interped_images = rearrange(interped_images, "d 1 h w -> d h w")
     return interped_images[:, 1 : slen - 1, 1 : slen - 1]
