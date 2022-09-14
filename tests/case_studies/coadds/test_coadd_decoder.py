775b49877414bbd842ef3079d6e753eceea5704b
Ismael Mendoza <imendoza@umich.edu>
2022-09-14 15:46:58 -0400

    revive test

---
+++ tests/case_studies/coadds/test_coadd_decoder.py
@@ -0,0 +1,13 @@
+import torch
+from hydra.utils import instantiate
+
+
+def test_coadd_galsim_blend(get_coadds_config, devices):
+    cfg = get_coadds_config({}, devices)
+    cgb = instantiate(cfg.datasets.galsim_blends_coadds)
+    ds = cgb[0]
+    size = cgb.slen + 2 * cgb.bp
+
+    assert ds["noisy"].shape == torch.Size([1, size - 2, size - 2])
+    assert ds["background"].shape == torch.Size([cgb.prior.n_dithers, 1, size - 2, size - 2])
+    assert ds["images"].shape == torch.Size([1, size - 2, size - 2])
