14b383c9210ccf59ba09a5ab1115a41b75352c4c
Ismael Mendoza <imendoza@umich.edu>
2022-11-02 11:52:55 -0400

    supersede messy training files

--- case_studies/coadds/train.sh
+++ case_studies/coadds/train.sh
@@ -1,12 +1,18 @@
 # ./main.py mode=train training.name='encoder'
-export CUDA_VISIBLE_DEVICES="7"
-for i in 1 2 3 4 5
+export TRAIN_EPOCHS=30
+export EPOCH_SIZE=10000
+export MODEL_NAME=$1 #options single / coadd_XYZ
+export CUDA_VISIBLE_DEVICES=$2
+# for i in 1 2 3 4 5
+for i in 1
 do
-    ./main.py mode=train training.name="single_encoder" training.experiment="single_encoder" datasets.saved_coadd_module.train_ds.coadd_name="single" datasets.saved_coadd_module.val_ds.coadd_name="single" 'training.dataset="${datasets.saved_coadd_module}"' training.seed=$i training.n_epochs=301
-
-    ./main.py mode=train training.name="coadd_encoder_saved_10" training.experiment="coadd_encoder_saved10" datasets.saved_coadd_module.train_ds.coadd_name="coadd_10" datasets.saved_coadd_module.val_ds.coadd_name="coadd_10" 'training.dataset="${datasets.saved_coadd_module}"' training.seed=$i training.n_epochs=301
-
-    ./main.py mode=train training.name="coadd_encoder_saved_25" training.experiment="coadd_encoder_saved25" datasets.saved_coadd_module.train_ds.coadd_name="coadd_25" datasets.saved_coadd_module.val_ds.coadd_name="coadd_25" 'training.dataset="${datasets.saved_coadd_module}"' training.seed=$i training.n_epochs=301
-
-    ./main.py mode=train training.name="coadd_encoder_saved_50" training.experiment="coadd_encoder_saved50" datasets.saved_coadd_module.train_ds.coadd_name="coadd_50" datasets.saved_coadd_module.val_ds.coadd_name="coadd_50" 'training.dataset="${datasets.saved_coadd_module}"' training.seed=$i training.n_epochs=301
+    ./main.py mode=train training.name="${MODEL_NAME}_encoder" \
+    training.experiment="${MODEL_NAME}_encoder" \
+    datasets.saved_coadd_module.train_ds.coadd_name="${MODEL_NAME}" \
+    datasets.saved_coadd_module.val_ds.coadd_name="${MODEL_NAME}" \
+    training.seed=$i training.n_epochs=$TRAIN_EPOCHS \
+    training.trainer.check_val_every_n_epoch=1 \
+    datasets.saved_coadd_module.train_ds.epoch_size=$EPOCH_SIZE \
+    datasets.saved_coadd_module.val_ds.epoch_size=$EPOCH_SIZE \
+    'paths.output="${paths.project}/output/training/"'
 done
