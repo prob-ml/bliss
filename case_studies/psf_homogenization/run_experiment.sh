9c68e3ce401279677fad64f6249c64b421ba6321
Ismael Mendoza <imendoza@umich.edu>
2022-12-08 09:28:16 -0500

    revive later if we want to retry homogenization but no immediate plans to do so

--- case_studies/psf_homogenization/run_experiment.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-GPU=4
-export CUDA_VISIBLE_DEVICES=$GPU
-echo "Starting $EXPERIMENT..."
-python main.py mode=train training=sdss_detection_encoder_homo training.save_top_k=1 training.experiment=sdss_detection_encoder_homo
