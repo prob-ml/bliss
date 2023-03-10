e98f7bf6205fba2ddac3b24bb9f853a1dbae4fa5
Jeffrey Regier <regier@umich.edu>
2023-03-10 09:33:50 -0500

    changes based on code review

--- case_studies/new_detection_encoder/main.py
+++ case_studies/new_detection_encoder/main.py
@@ -3,8 +3,6 @@ from os import environ, getenv
 from pathlib import Path
 
 import hydra
-import numpy as np
-import torch
 from einops import rearrange
 from omegaconf import OmegaConf
 from yolov5.models.yolo import DetectionModel
@@ -14,29 +12,24 @@ from bliss.train import train
 
 
 class NewDetectionEncoder(DetectionEncoder):
-    def __init__(self, **ode_args):
-        architecture = ode_args.pop("architecture")
-        super().__init__(**ode_args)
-
-        # the checkerboard sizes below only make sense for images composed of an
-        # 8x8 grid of 4x4 tiles. It's straightforward to generalize it to arbitrary
-        # image/tile sizes, but I done so because I haven't determined yet whether
-        # this checkerboard is helpful/necessary
-        shape = (20, 20)
-        base_checkerboard = np.indices(shape).sum(axis=0) % 2
-        self.checkerboard = torch.from_numpy(base_checkerboard).unsqueeze(2).repeat([1, 4, 4])
-        self.checkerboard = self.checkerboard.view(1, 1, 80, 80).float().cuda()
+    def __init__(self, **args):
+        architecture = args.pop("architecture")
+        super().__init__(**args)
 
         arch_dict = OmegaConf.to_container(architecture)
-        self.model = DetectionModel(cfg=arch_dict, ch=3)
+        self.model = DetectionModel(cfg=arch_dict, ch=2)
+
+        self.tiles_to_crop = (args["ptile_slen"] - args["tile_slen"]) // (2 * args["tile_slen"])
 
     def do_encode_batch(self, images_with_background):
-        b = images_with_background.size(0)
-        batch_checkerboard = self.checkerboard.expand([b, 1, -1, -1])
-        x = torch.cat([images_with_background, batch_checkerboard], dim=1)
+        # setting this to true every time is a hack to make yolo DetectionModel
+        # give us output of the right dimension
         self.model.model[-1].training = True
-        output = self.model(x)
-        output_cropped = output[0][:, :, 6:-6, 6:-6, :]
+
+        output = self.model(images_with_background)
+
+        ttc = self.tiles_to_crop
+        output_cropped = output[0][:, :, ttc:-ttc, ttc:-ttc, :]
         return rearrange(output_cropped, "b c h w p -> (b c h w) p")
 
 
