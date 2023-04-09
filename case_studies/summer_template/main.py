fff8e394d7a2ea0315d7033d48ee8d9c4ace4bc0
Jeffrey Regier <regier@umich.edu>
2023-04-08 20:54:57 -0400

    inference/prediction runs with one third of an sdss frame

--- case_studies/summer_template/main.py
+++ case_studies/summer_template/main.py
@@ -3,6 +3,8 @@ from os import environ, getenv
 from pathlib import Path
 
 import hydra
+import torch
+from hydra.utils import instantiate
 
 from bliss.train import train
 
@@ -12,16 +14,31 @@ if not getenv("BLISS_HOME"):
     environ["BLISS_HOME"] = bliss_home.as_posix()
 
 
-def infer(cfg):
-    pass
+def prepare_image(x):
+    x = torch.from_numpy(x).cuda().unsqueeze(0)
+    return x[:, :, :1488, :720]
+
+
+def predict(cfg):
+    sdss = instantiate(cfg.inference.dataset)
+    encoder = instantiate(cfg.encoder).cuda()
+    # TODO: load saved weights
+    encoder.eval()
+    batch = {
+        "images": prepare_image(sdss[0]["image"]),
+        "background": prepare_image(sdss[0]["background"]),
+    }
+    pred = encoder.encode_batch(batch)
+    est_cat = encoder.variational_mode(pred)
+    print(est_cat)
 
 
 @hydra.main(config_path=".", config_name="config", version_base=None)
 def main(cfg):
     if cfg.mode == "train":
         train(cfg)
-    elif cfg.mode == "infer":
-        infer(cfg)
+    elif cfg.mode == "predict":
+        predict(cfg)
     else:
         raise KeyError
 
