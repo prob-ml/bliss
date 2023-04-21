b94b328a34377539ac4b91804baddeba45343985
Jeffrey Regier <regier@umich.edu>
2023-04-21 13:40:18 -0400

    added nms case study

---
+++ case_studies/nms/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from os import environ, getenv
+from pathlib import Path
+
+import hydra
+
+from bliss.predict import predict
+from bliss.train import train
+
+if not getenv("BLISS_HOME"):
+    project_path = Path(__file__).resolve()
+    bliss_home = project_path.parents[2]
+    environ["BLISS_HOME"] = bliss_home.as_posix()
+
+
+@hydra.main(config_path=".", config_name="config", version_base=None)
+def main(cfg):
+    if cfg.mode == "train":
+        train(cfg)
+    elif cfg.mode == "predict":
+        predict(cfg)
+    else:
+        raise KeyError
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
