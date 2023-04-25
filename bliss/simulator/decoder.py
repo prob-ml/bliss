c5056d022cd007a195229cdc7d73f9064c4ed480
Jeffrey Regier <jeff-regier@users.noreply.github.com>
2023-04-25 10:00:29 -0400

    refactor simulator (prior and decoder) (#734)
    * moved trained encoder to pretrained_models
    
    * changed decoder to generate non-square images
    
    * simplified overly complicated inheritance structure
    
    * merge ImagePrior and GalaxyPrior in the process of resolving a log-of-zero issue
    
    * more refactoring of prior.py
    
    * removed unused argument from Encoder
    
    * switch back to reporting mean of loss terms
    
    * fix project path
    
    * back to loss terms as a fraction of num relevant sources
    
    * draw on existing image and don't sample bulge_q == 0

--- bliss/simulator/decoder.py
+++ bliss/simulator/decoder.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Callable, Optional, Tuple
+from typing import Tuple
 
 import galsim
 import numpy as np
@@ -11,25 +11,7 @@ from torch import Tensor, nn
 from bliss.catalog import TileCatalog
 
 
-class PSFDecoder(nn.Module):
-    """Abstract decoder class to subclass whenever the decoded result will go through a PSF.
-
-    PSF (point-spread function) use is common for decoding the final realistic astronomical
-    image to account for sensor lens effects. PSF loading is suppported as a direct image (npy)
-    or through attributes (npy or fits) file.
-
-    For parameter loading, a psf_params_file should be indicated, with the corresponding
-    psf_slen and sdss_bands.
-
-    Attributes:
-        psf_params_file: PSF parameters (saved either in a numpy file or fits file from SDSS)
-        psf_slen: Side-length of the PSF.
-        sdss_bands: Bands to retrieve from PSF.
-        n_bands: Number of bands to retrieve from PSF.
-    """
-
-    forward: Callable[..., Tensor]
-
+class ImageDecoder(nn.Module):
     def __init__(
         self,
         n_bands: int,
@@ -37,27 +19,31 @@ class PSFDecoder(nn.Module):
         psf_params_file: str,
         psf_slen: int,
         sdss_bands: Tuple[int, ...],
-    ):
+    ) -> None:
         super().__init__()
+        assert n_bands == 1, "Only 1 band is supported"
+
         self.n_bands = n_bands
+        self.pixel_scale = pixel_scale
+
         self.params = None  # psf params from fits file
 
         assert Path(psf_params_file).suffix == ".fits"
         assert len(sdss_bands) == n_bands
         psf_params = self._get_fit_file_psf_params(psf_params_file, sdss_bands)
         self.params = nn.Parameter(psf_params.clone(), requires_grad=True)
+
         self.psf_slen = psf_slen
+
         grid = self._get_mgrid(self.psf_slen) * (self.psf_slen - 1) / 2
         # Bryan: extra factor to be consistent with old repo, probably unimportant...
         grid *= self.psf_slen / (self.psf_slen - 1)
         self.register_buffer("cached_radii_grid", (grid**2).sum(2).sqrt())
 
         self.psf = self.forward_psf_from_params().detach().numpy()
+        assert len(self.psf.shape) == 3 and self.psf.shape[0] == 1
         psf_image = galsim.Image(self.psf[0], scale=pixel_scale)
         self.psf_galsim = galsim.InterpolatedImage(psf_image).withFlux(1.0)
-
-    def forward(self, x):  # type: ignore
-        raise NotImplementedError("Please extend this class and implement forward()")
 
     @staticmethod
     def _get_mgrid(slen: int):
@@ -113,41 +99,7 @@ class PSFDecoder(nn.Module):
         term3 = p0 * (1 + r**2 / (beta * sigmap)) ** (-beta / 2)
         return (term1 + term2 + term3) / (1 + b + p0)
 
-
-class GalaxyDecoder(PSFDecoder):
-    def __init__(
-        self,
-        slen: int,
-        ptile_slen: int,
-        n_bands: int,
-        pixel_scale: float,
-        psf_params_file: str,
-        psf_slen: int,
-        sdss_bands: Tuple[int, ...],
-    ) -> None:
-        super().__init__(
-            psf_params_file=psf_params_file,
-            psf_slen=psf_slen,
-            sdss_bands=sdss_bands,
-            n_bands=n_bands,
-            pixel_scale=pixel_scale,
-        )
-        assert len(self.psf.shape) == 3 and self.psf.shape[0] == 1
-
-        assert n_bands == 1, "Only 1 band is supported"
-        self.slen = slen
-        self.n_bands = 1
-        self.pixel_scale = pixel_scale
-        self.ptile_slen = ptile_slen
-
-    def _render_galaxy_np(
-        self,
-        galaxy_params: Tensor,
-        psf: galsim.GSObject,
-        slen: int,
-        offset: Optional[Tensor] = None,
-    ) -> Tensor:
-        assert offset is None or offset.shape == (2,)
+    def render_galaxy(self, galaxy_params: Tensor):
         galaxy_params = galaxy_params.cpu().detach()
         total_flux, disk_frac, beta_radians, disk_q, a_d, bulge_q, a_b = galaxy_params
         bulge_frac = 1 - disk_frac
@@ -167,60 +119,34 @@ class GalaxyDecoder(PSFDecoder):
         if bulge_flux > 0:
             b_b = bulge_q * a_b
             bulge_hlr_arcsecs = np.sqrt(a_b * b_b)
-            bulge = galsim.DeVaucouleurs(
-                flux=bulge_flux, half_light_radius=bulge_hlr_arcsecs
-            ).shear(q=bulge_q, beta=beta_radians * galsim.radians)
-            components.append(bulge)
+            bulge = galsim.DeVaucouleurs(flux=bulge_flux, half_light_radius=bulge_hlr_arcsecs)
+            sheared_bulge = bulge.shear(q=bulge_q, beta=beta_radians * galsim.radians)
+            components.append(sheared_bulge)
         galaxy = galsim.Add(components)
-        gal_conv = galsim.Convolution(galaxy, psf)
-        offset = offset if offset is None else offset.numpy()
-        return gal_conv.drawImage(nx=slen, ny=slen, scale=self.pixel_scale, offset=offset).array
-
-    def render_galaxy(
-        self,
-        galaxy_params: Tensor,
-        slen: int,
-        offset: Optional[Tensor] = None,
-    ) -> Tensor:
-        image = self._render_galaxy_np(galaxy_params, self.psf_galsim, slen, offset)
-        return torch.from_numpy(image).reshape(1, slen, slen)
-
-
-class ImageDecoder:
-    def __init__(self, galaxy_decoder: GalaxyDecoder) -> None:
-        self.galaxy_decoder = galaxy_decoder
-        self.pixel_scale = self.galaxy_decoder.pixel_scale
-
-    def _render_star(self, flux: float, slen: int, offset: Optional[Tensor] = None) -> Tensor:
-        assert offset is None or offset.shape == (2,)
-        star = self.galaxy_decoder.psf_galsim.withFlux(flux)  # creates a copy
-        offset = offset if offset is None else offset.numpy()
-        image = star.drawImage(nx=slen, ny=slen, scale=self.pixel_scale, offset=offset)
-        return torch.from_numpy(image.array).reshape(1, slen, slen)
+        return galsim.Convolution(galaxy, self.psf_galsim)
 
     def render_images(self, tile_cat: TileCatalog):
         batch_size, n_tiles_h, n_tiles_w = tile_cat.n_sources.shape
-        assert n_tiles_h == n_tiles_w
         slen_h = tile_cat.tile_slen * n_tiles_h
+        slen_w = tile_cat.tile_slen * n_tiles_w
+        images = np.zeros((batch_size, self.n_bands, slen_h, slen_w), dtype=np.float32)
 
         full_cat = tile_cat.to_full_params()
-        assert self.galaxy_decoder.n_bands == 1, "only 1 band supported for now"
-
-        images = torch.zeros(batch_size, self.galaxy_decoder.n_bands, slen_h, slen_h)
 
         for b in range(batch_size):
             n_sources = int(full_cat.n_sources[b].item())
+            gs_img = galsim.Image(array=images[b, 0], scale=self.pixel_scale)
             for s in range(n_sources):
-                offset_yx = full_cat.plocs[b][s] - (slen_h / 2)
-                # I don't think we should have to do this swap, though it does give us consistency
-                # btw tile_cat and images...is plocs backwards?
-                offset_xy = torch.tensor([offset_yx[1], offset_yx[0]])
-
                 if full_cat["galaxy_bools"][b][s] == 1:
-                    gp = full_cat["galaxy_params"][b][s]
-                    images[b] += self.galaxy_decoder.render_galaxy(gp, slen_h, offset_xy)
+                    galsim_obj = self.render_galaxy(full_cat["galaxy_params"][b][s])
                 elif full_cat["star_bools"][b][s] == 1:
-                    sp = full_cat["star_fluxes"][b][s].item()
-                    images[b] += self._render_star(sp, slen_h, offset_xy)
+                    galsim_obj = self.psf_galsim.withFlux(full_cat["star_fluxes"][b][s].item())
+                else:
+                    raise AssertionError("Every source is a star or galaxy")
 
-        return images
+                plocs = full_cat.plocs[b][s]
+                offset = np.array([plocs[1] - (slen_w / 2), plocs[0] - (slen_h / 2)])
+                # essentially all the runtime of the simulator is incurred by this call to drawImage
+                galsim_obj.drawImage(offset=offset, method="auto", add_to_image=True, image=gs_img)
+
+        return torch.from_numpy(images)
--- bliss/simulator/prior.py
+++ bliss/simulator/prior.py
@@ -1,53 +1,10 @@
-from typing import Optional
-
 import numpy as np
 import pytorch_lightning as pl
 import torch
-from einops import rearrange
 from torch import Tensor
-from torch.distributions import Poisson
+from torch.distributions import Gamma, Poisson, Uniform
 
 from bliss.catalog import TileCatalog, get_is_on_from_n_sources
-
-
-class GalaxyPrior:
-    def __init__(
-        self,
-        min_flux: float,
-        max_flux: float,
-        alpha: float,
-        a_concentration: float,
-        a_loc: float,
-        a_scale: float,
-        a_bulge_disk_ratio: float,
-    ) -> None:
-        self.min_flux = min_flux
-        self.max_flux = max_flux
-        self.alpha = alpha
-
-        self.a_concentration = a_concentration
-        self.a_loc = a_loc
-        self.a_scale = a_scale
-        self.a_bulge_disk_ratio = a_bulge_disk_ratio
-
-    def sample(self, total_latent, device="cpu"):
-        total_flux = _draw_pareto(self.alpha, self.min_flux, self.max_flux, n_samples=total_latent)
-
-        disk_frac = _uniform(0, 1, n_samples=total_latent)
-        beta_radians = _uniform(0, 2 * np.pi, n_samples=total_latent)
-        disk_q = _uniform(0, 1, n_samples=total_latent)
-        bulge_q = _uniform(0, 1, n_samples=total_latent)
-
-        disk_a = _gamma(self.a_concentration, self.a_loc, self.a_scale, n_samples=total_latent)
-        bulge_a = _gamma(
-            self.a_concentration,
-            self.a_loc / self.a_bulge_disk_ratio,
-            self.a_scale / self.a_bulge_disk_ratio,
-            n_samples=total_latent,
-        )
-
-        param_lst = [total_flux, disk_frac, beta_radians, disk_q, disk_a, bulge_q, bulge_a]
-        return torch.stack(param_lst, dim=1).to(device)
 
 
 class ImagePrior(pl.LightningModule):
@@ -56,16 +13,6 @@ class ImagePrior(pl.LightningModule):
     After the module is initialized, sampling is done with the sample_prior method.
     The input parameters correspond to the number of sources, the fluxes, whether an
     object is a galaxy or star, and the distributions of galaxy and star shapes.
-
-    Attributes:
-        n_bands: Number of bands (colors) in the image
-        min_sources: Minimum number of sources in a tile
-        max_sources: Maximum number of sources in a tile
-        mean_sources: Mean rate of sources appearing in a tile
-        f_min: Prior parameter on fluxes
-        f_max: Prior parameter on fluxes
-        alpha: Prior parameter on fluxes
-        prob_galaxy: Prior probability a source is a galaxy
     """
 
     def __init__(
@@ -78,11 +25,17 @@ class ImagePrior(pl.LightningModule):
         min_sources: int,
         max_sources: int,
         mean_sources: float,
-        f_min: float,
-        f_max: float,
-        alpha: float,
         prob_galaxy: float,
-        galaxy_prior: Optional[GalaxyPrior] = None,
+        star_flux_min: float,
+        star_flux_max: float,
+        star_flux_alpha: float,
+        galaxy_flux_min: float,
+        galaxy_flux_max: float,
+        galaxy_alpha: float,
+        galaxy_a_concentration: float,
+        galaxy_a_loc: float,
+        galaxy_a_scale: float,
+        galaxy_a_bd_ratio: float,
     ):
         """Initializes ImagePrior.
 
@@ -95,11 +48,17 @@ class ImagePrior(pl.LightningModule):
             min_sources: Minimum number of sources in a tile
             max_sources: Maximum number of sources in a tile
             mean_sources: Mean rate of sources appearing in a tile
-            f_min: Prior parameter on fluxes
-            f_max: Prior parameter on fluxes
-            alpha: Prior parameter on fluxes (pareto parameter)
             prob_galaxy: Prior probability a source is a galaxy
-            galaxy_prior: Object from which galaxy latents are sampled
+            star_flux_min: Prior parameter on fluxes
+            star_flux_max: Prior parameter on fluxes
+            star_flux_alpha: Prior parameter on fluxes (pareto parameter)
+            galaxy_flux_min: Minimum flux of a galaxy
+            galaxy_flux_max: Maximum flux of a galaxy
+            galaxy_alpha: ?
+            galaxy_a_concentration: ?
+            galaxy_a_loc: ?
+            galaxy_a_scale: galaxy scale
+            galaxy_a_bd_ratio: galaxy bulge-to-disk ratio
         """
         super().__init__()
         self.n_tiles_h = n_tiles_h
@@ -112,14 +71,20 @@ class ImagePrior(pl.LightningModule):
         self.max_sources = max_sources
         self.mean_sources = mean_sources
 
-        self.f_min = f_min
-        self.f_max = f_max
-        self.alpha = alpha
+        self.prob_galaxy = prob_galaxy
 
-        self.prob_galaxy = float(prob_galaxy)
-        self.galaxy_prior = galaxy_prior
-        if self.prob_galaxy > 0.0:
-            assert self.galaxy_prior is not None
+        self.star_flux_min = star_flux_min
+        self.star_flux_max = star_flux_max
+        self.star_flux_alpha = star_flux_alpha
+
+        self.galaxy_flux_min = galaxy_flux_min
+        self.galaxy_flux_max = galaxy_flux_max
+        self.galaxy_alpha = galaxy_alpha
+
+        self.galaxy_a_concentration = galaxy_a_concentration
+        self.galaxy_a_loc = galaxy_a_loc
+        self.galaxy_a_scale = galaxy_a_scale
+        self.galaxy_a_bd_ratio = galaxy_a_bd_ratio
 
     def sample_prior(self) -> TileCatalog:
         """Samples latent variables from the prior of an astronomical image.
@@ -130,20 +95,19 @@ class ImagePrior(pl.LightningModule):
             `(batch_size, self.n_tiles_h, self.n_tiles_w)`.
             The remaining dimensions are variable-specific.
         """
-        n_sources = self._sample_n_sources(self.batch_size, self.n_tiles_h, self.n_tiles_w)
-        is_on_array = get_is_on_from_n_sources(n_sources, self.max_sources)
-        locs = self._sample_locs(is_on_array)
+        locs = self._sample_locs()
+        galaxy_params = self._sample_galaxy_prior()
+        star_fluxes = self._sample_star_fluxes()
+        star_log_fluxes = star_fluxes.log()
 
-        galaxy_bools, star_bools = self._sample_n_galaxies_and_stars(is_on_array)
-        galaxy_params = self._sample_galaxy_params(self.galaxy_prior, galaxy_bools)
-        star_fluxes = self._sample_star_fluxes(star_bools)
-        star_log_fluxes = self._get_log_fluxes(star_fluxes)
+        n_sources = self._sample_n_sources()
+        galaxy_bools, star_bools = self._sample_n_galaxies_and_stars(n_sources)
 
         catalog_params = {
             "n_sources": n_sources,
-            "locs": locs,
             "galaxy_bools": galaxy_bools,
             "star_bools": star_bools,
+            "locs": locs,
             "galaxy_params": galaxy_params,
             "star_fluxes": star_fluxes,
             "star_log_fluxes": star_log_fluxes,
@@ -151,133 +115,62 @@ class ImagePrior(pl.LightningModule):
 
         return TileCatalog(self.tile_slen, catalog_params)
 
-    @staticmethod
-    def _get_log_fluxes(fluxes):
-        log_fluxes = torch.where(
-            fluxes > 0, fluxes, torch.ones_like(fluxes)
-        )  # prevent log(0) errors.
-        return torch.log(log_fluxes)
-
-    def _sample_n_sources(self, batch_size, n_tiles_h, n_tiles_w):
-        # returns number of sources for each batch x tile
-        # output dimension is batch_size x n_tiles_h x n_tiles_w
-
-        # always poisson distributed.
-        p = torch.full((1,), self.mean_sources, device=self.device, dtype=torch.float)
-        m = Poisson(p)
-        n_sources = m.sample([batch_size, n_tiles_h, n_tiles_w])
-
-        # long() here is necessary because used for indexing and one_hot encoding.
+    def _sample_n_sources(self):
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w)
+        n_sources = Poisson(self.mean_sources).sample(latent_dims)
         n_sources = n_sources.clamp(max=self.max_sources, min=self.min_sources)
-        return rearrange(n_sources.long(), "b nth ntw 1 -> b nth ntw")
+        # long() is necessary here because the return value is used for indexing subsequently
+        return n_sources.long()
 
-    def _sample_locs(self, is_on_array):
-        # output dimension is batch_size x n_tiles_h x n_tiles_w x max_sources x 2
+    def _sample_locs(self):
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 2)
+        return Uniform(0, 1).sample(latent_dims)
 
-        # 2 = (x,y)
-        batch_size, n_tiles_h, n_tiles_w, max_sources = is_on_array.shape
-        shape = (batch_size, n_tiles_h, n_tiles_w, max_sources, 2)
-        locs = torch.rand(*shape, device=is_on_array.device)
-        locs *= is_on_array.unsqueeze(-1)
-
-        return locs
-
-    def _sample_n_galaxies_and_stars(self, is_on_array):
-        # the counts returned (n_galaxies, n_stars) are of
-        # shape (batch_size x n_tiles_h x n_tiles_w)
-        # the booleans returned (galaxy_bools, star_bools) are of shape
-        # (batch_size x n_tiles_h x n_tiles_w x max_sources x 1)
-        # this last dimension is so it is consistent with other catalog values.
-        batch_size, n_tiles_h, n_tiles_w, max_sources = is_on_array.shape
-        uniform = torch.rand(
-            batch_size,
-            n_tiles_h,
-            n_tiles_w,
-            max_sources,
-            1,
-            device=is_on_array.device,
-        )
-        galaxy_bools = uniform < self.prob_galaxy
+    def _sample_n_galaxies_and_stars(self, n_sources):
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 1)
+        uniform_aux_var = torch.rand(*latent_dims)
+        galaxy_bools = uniform_aux_var < self.prob_galaxy
         star_bools = galaxy_bools.bitwise_not()
+
+        # gate galaxy/star booleans according n_sources
+        is_on_array = get_is_on_from_n_sources(n_sources, self.max_sources)
         galaxy_bools *= is_on_array.unsqueeze(-1)
         star_bools *= is_on_array.unsqueeze(-1)
 
         return galaxy_bools, star_bools
 
-    def _sample_star_fluxes(self, star_bools: Tensor):
-        """Samples star fluxes.
-
-        Arguments:
-            star_bools: Tensor indicating whether each object is a star or not.
-                Has shape (batch_size x n_tiles_h x n_tiles_w x max_sources x 1)
-
-        Returns:
-            fluxes, tensor shape
-            (batch_size x n_tiles_h x n_tiles_w x max_sources x n_bands)
-        """
-        device = star_bools.device
-        batch_size, n_tiles_h, n_tiles_w, max_sources, _ = star_bools.shape
-        shape = (batch_size, n_tiles_h, n_tiles_w, max_sources, 1)
-        base_fluxes = self._draw_pareto_maxed(shape, device)
-
-        if self.n_bands > 1:
-            shape = (
-                batch_size,
-                n_tiles_h,
-                n_tiles_w,
-                max_sources,
-                self.n_bands - 1,
-            )
-            colors = torch.randn(*shape, device=device)
-            fluxes = 10 ** (colors / 2.5) * base_fluxes
-            fluxes = torch.cat((base_fluxes, fluxes), dim=-1)
-            fluxes *= star_bools.float()
-        else:
-            fluxes = base_fluxes * star_bools.float()
-
-        return fluxes
-
-    def _draw_pareto_maxed(self, shape, device):
-        # draw pareto conditioned on being less than f_max
-
-        u_max = self._pareto_cdf(self.f_max)
-        uniform_samples = torch.rand(*shape, device=device) * u_max
-        return self.f_min / (1.0 - uniform_samples) ** (1 / self.alpha)
-
-    def _pareto_cdf(self, x):
-        return 1 - (self.f_min / x) ** self.alpha
-
-    def _sample_galaxy_params(self, galaxy_prior, galaxy_bools):
-        """Sample latent galaxy params from GalaxyPrior object."""
-        batch_size, n_tiles_h, n_tiles_w, max_sources, _ = galaxy_bools.shape
-        total_latent = batch_size * n_tiles_h * n_tiles_w * max_sources
-        if self.prob_galaxy > 0.0:
-            samples = galaxy_prior.sample(total_latent, galaxy_bools.device)
-        else:
-            samples = torch.zeros((total_latent, 1), device=galaxy_bools.device)
-        return rearrange(
-            samples,
-            "(b nth ntw s) g -> b nth ntw s g",
-            b=batch_size,
-            nth=n_tiles_h,
-            ntw=n_tiles_w,
-            s=max_sources,
+    def _sample_star_fluxes(self):
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 1)
+        return self._draw_truncated_pareto(
+            self.star_flux_alpha, self.star_flux_min, self.star_flux_max, latent_dims
         )
 
+    @staticmethod
+    def _draw_truncated_pareto(alpha, min_x, max_x, n_samples) -> Tensor:
+        # draw pareto conditioned on being less than f_max
+        u_max = 1 - (min_x / max_x) ** alpha
+        uniform_samples = torch.rand(n_samples) * u_max
+        return min_x / (1.0 - uniform_samples) ** (1 / alpha)
 
-def _uniform(a, b, n_samples=1) -> Tensor:
-    # uses pytorch to return a single float ~ U(a, b)
-    return (a - b) * torch.rand(n_samples) + b
+    def _sample_galaxy_prior(self):
+        """Sample latent galaxy params from GalaxyPrior object."""
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 1)
 
+        total_flux = self._draw_truncated_pareto(
+            self.galaxy_alpha, self.galaxy_flux_min, self.galaxy_flux_max, latent_dims
+        )
 
-def _draw_pareto(alpha, min_x, max_x, n_samples=1) -> Tensor:
-    # draw pareto conditioned on being less than f_max
-    assert alpha is not None
-    u_max = 1 - (min_x / max_x) ** alpha
-    uniform_samples = torch.rand(n_samples) * u_max
-    return min_x / (1.0 - uniform_samples) ** (1 / alpha)
+        disk_frac = Uniform(0, 1).sample(latent_dims)
+        beta_radians = Uniform(0, 2 * np.pi).sample(latent_dims)
+        disk_q = Uniform(1e-8, 1).sample(latent_dims)
+        bulge_q = Uniform(1e-8, 1).sample(latent_dims)
 
+        base_dist = Gamma(self.galaxy_a_concentration, rate=1.0)
+        disk_a = base_dist.sample(latent_dims) * self.galaxy_a_scale + self.galaxy_a_loc
 
-def _gamma(concentration, loc, scale, n_samples=1):
-    x = torch.distributions.Gamma(concentration, rate=1.0).sample((n_samples,))
-    return x * scale + loc
+        bulge_loc = self.galaxy_a_loc / self.galaxy_a_bd_ratio
+        bulge_scale = self.galaxy_a_scale / self.galaxy_a_bd_ratio
+        bulge_a = base_dist.sample(latent_dims) * bulge_scale + bulge_loc
+
+        param_lst = [total_flux, disk_frac, beta_radians, disk_q, disk_a, bulge_q, bulge_a]
+        return torch.cat(param_lst, dim=4)
