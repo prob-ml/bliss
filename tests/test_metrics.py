8f6a494d2b06a2bc68907272d8572df0aadd2d9d
Aakash Patel <aakashdp@umich.edu>
2023-05-04 10:16:33 -0400

    Added decals RA/DEC to plocs conversion, more integration tests.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,10 +1,48 @@
+from pathlib import Path
+
 import torch
+from astropy.io import fits
+from astropy.wcs import WCS
 
 from bliss.catalog import FullCatalog
 from bliss.metrics import BlissMetrics
+from bliss.surveys.decals import DecalsFullCatalog
+from bliss.surveys.sdss import PhotoFullCatalog, SloanDigitalSkySurvey
+
+RA_LIM = (336.62564677, 336.64428049)
+DEC_LIM = (-0.96927915, -0.95064804)
+
+
+def get_decals_data(filename, wcs=None):
+    """Helper function to load DECaLS data for test cases."""
+
+    cat = DecalsFullCatalog.from_file(filename, RA_LIM, DEC_LIM)
+
+    # if provided, use WCS to convert RA and DEC to plocs
+    if wcs is not None:
+        plocs = cat.get_plocs_from_ra_dec(wcs)
+        cat.plocs = plocs
+        cat.height, cat.width = wcs.array_shape
+
+    return cat
+
+
+def constrain_photo_cat(photo_cat):
+    """Helper function to restrict photo catalog to within RA and DEC limits."""
+    ra = photo_cat["ra"].numpy().squeeze()
+    dec = photo_cat["dec"].numpy().squeeze()
+
+    keep = (ra > RA_LIM[0]) & (ra < RA_LIM[1]) & (dec >= DEC_LIM[0]) & (dec <= DEC_LIM[1])
+    plocs = photo_cat.plocs[:, keep]
+    galaxy_bools = photo_cat["galaxy_bools"][:, keep]
+    n_sources = torch.tensor([plocs.size()[1]])
+
+    d = {"plocs": plocs, "n_sources": n_sources, "galaxy_bools": galaxy_bools}
+    return PhotoFullCatalog(photo_cat.height, photo_cat.width, d)
 
 
 def test_metrics():
+    """Tests basic metrics using simple toy data."""
     slen = 50
     slack = 1.0
     bliss_metrics = BlissMetrics(slack)
@@ -47,3 +85,71 @@ def test_metrics():
     assert gal_fn == torch.tensor([0])
     assert gal_tn == torch.tensor([0])
     assert avg_distance.item() == 50 * (0.01 + (0.01 + 0.09) / 2) / 2
+
+
+def test_photo_self_agreement(cfg):
+    """Compares PhotoFullCatalog to itself as safety check for metrics."""
+    slack = 1.0
+    metrics = BlissMetrics(slack)
+
+    sdss_path = cfg.paths.sdss
+    photo_cat = PhotoFullCatalog.from_file(sdss_path, run=94, camcol=1, field=12, band=2)  # R band
+
+    results_detection = metrics(photo_cat, photo_cat)
+    f1_score = results_detection["f1"]
+    assert f1_score == 1
+
+
+def test_decals_self_agreement(cfg):
+    """Compares Decals catalog to itself as safety check for metrics."""
+    slack = 1.0
+    metrics = BlissMetrics(slack)
+
+    # load file and WCS
+    data_file = Path(cfg.paths.decals).joinpath("tractor-3366m010.fits")
+    image_file = Path(cfg.paths.decals).joinpath("cutout_336.635_-0.9600.fits")
+
+    with fits.open(image_file) as f:
+        wcs = WCS(f[0].header)  # pylint: disable=E1101
+        true_params = get_decals_data(data_file, wcs)
+
+    results_detection = metrics(true_params, true_params)
+    f1_score = results_detection["f1"]
+    assert f1_score == 1
+
+
+def test_photo_decals_agree(cfg):
+    """Compares metrics for agreement between Photo catalog and Decals catalog."""
+    slack = 1.0
+    metrics = BlissMetrics(slack)
+
+    decals_file = Path(cfg.paths.decals).joinpath("tractor-3366m010.fits")
+    sdss_path = cfg.paths.sdss
+
+    # load SDSS WCS and use to get plocs for DECaLS catalog
+    sdss = SloanDigitalSkySurvey(sdss_path, 94, 1, fields=(12,), bands=(2,))
+    wcs: WCS = sdss[0]["wcs"][0]
+    decals_cat = get_decals_data(decals_file, wcs)
+
+    # get photo catalog constrained to region
+    photo_cat = PhotoFullCatalog.from_file(sdss_path, run=94, camcol=1, field=12, band=2)  # R band
+    photo_cat = constrain_photo_cat(photo_cat)
+
+    results_detection = metrics(decals_cat, photo_cat)
+    precision = results_detection["precision"]
+    assert precision == 1
+
+
+def test_bliss_photo_agree():
+    """Compares metrics for agreement between BLISS-inferred catalog and Photo catalog."""
+    raise NotImplementedError()
+
+
+def test_bliss_photo_agree_comp_decals():
+    """Tests that metrics agree between BLISS and Photo catalog when computed with decals as GT."""
+    raise NotImplementedError()
+
+
+def test_bliss_hst_agree():
+    """Compares metrics for agreement between BLISS-inferred catalog and Hst catalog."""
+    raise NotImplementedError()
