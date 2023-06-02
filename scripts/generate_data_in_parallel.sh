6f81117c1f6f35d189da85a945b859530b0665e6
zhixiangteoh <47135468+zhixiangteoh@users.noreply.github.com>
2023-06-02 10:14:37 -0400

    Allow config overrides in parallel generate script

--- scripts/generate_data_in_parallel.sh
+++ scripts/generate_data_in_parallel.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# gracefully exit
+trap "cleanup" SIGINT SIGTERM EXIT
+
 # Usage: generate_data_in_parallel.sh <relative config_path> <num_files_per_process> <num_processes> <num_workers_per_process>
 CONFIG_PATH=$1
 N_FILES_PER_PROCESS=$2
-NUM_PROCESSES=${3:-32}
-N_WORKERS_PER_PROCESS=${4:-0}
+CONFIG_OVERRIDES=${3:-""}
+NUM_PROCESSES=${4:-32}
+N_WORKERS_PER_PROCESS=${5:-0}
 
 # Gracefully exit when interrupted
 pids=()
@@ -23,10 +27,22 @@ if [ -z "$CONFIG_PATH" ] || [ -z "$N_FILES_PER_PROCESS" ]; then
     exit 1
 fi
 
+pids=()
 for ((i=0; i<$NUM_PROCESSES; i++));
 do
     files_start_idx=$((i * N_FILES_PER_PROCESS))
-    bliss -cp $CONFIG_PATH 'mode=generate' "simulator.num_workers=$N_WORKERS_PER_PROCESS" "generate.files_start_idx=$files_start_idx" &
+    bliss -cp $CONFIG_PATH 'mode=generate' "simulator.num_workers=$N_WORKERS_PER_PROCESS" "generate.files_start_idx=$files_start_idx" $CONFIG_OVERRIDES &
     pids+=($!) # store PID in array
 done
-wait
+
+# wait for all bliss processes to finish
+for pid in "${pids[@]}"; do
+    wait $pid
+done
+
+cleanup() {
+    echo "Killing bliss data generation processes..."
+    for pid in "${pids[@]}"; do
+        kill $pid
+    done
+}
