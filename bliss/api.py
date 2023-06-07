ee0844b3edc5586d27582e619217c89d911247da
zhixiangteoh <47135468+zhixiangteoh@users.noreply.github.com>
2023-06-07 14:12:29 -0400

    Complete basic E2E user API

--- bliss/api.py
+++ bliss/api.py
@@ -1,14 +1,19 @@
 import base64
+import subprocess  # noqa: S404
+from pathlib import Path
 from typing import Literal
 
 import requests
-from astropy import coordinates as coords
-from astroquery.sdss import SDSS
+import torch
+from astropy import units as u
+from astropy.table import Table
 from omegaconf import OmegaConf
 
+from bliss.catalog import FullCatalog
 from bliss.conf.igs import base_config
 from bliss.generate import generate as _generate
 from bliss.predict import predict_sdss as _predict_sdss
+from bliss.surveys import sdss_download
 from bliss.train import train as _train
 
 SurveyType = Literal["decals", "hst", "lsst", "sdss"]
@@ -155,16 +160,114 @@ def train_on_cached_data(
     _train(cfg)
 
 
-def load_survey(survey: SurveyType):
-    pos = coords.SkyCoord("0h8m05.63s +14d50m23.3s", frame="icrs")
-    xid = SDSS.query_region(pos, radius="5 arcsec", spectro=True)  # pylint: disable=E1101
-    print(xid)
-    sp = SDSS.get_spectra(matches=xid)
-    im = SDSS.get_images(matches=xid, band="g")
-    return sp, im
+def load_survey_via_makefile(survey: SurveyType, run, camcol, field, download_dir: Path):
+    with subprocess.Popen(  # noqa: S603, S607
+        [
+            "make",
+            f"RUN={run}",
+            f"CAMCOL={camcol}",
+            f"FIELD={field}",
+            f"DOWNLOAD_DIR={download_dir}",
+        ],
+        stdout=subprocess.PIPE,
+        cwd=Path(__file__).parent / "surveys",
+    ) as process:
+        assert process.wait() == 0
+        # assert files downloaded at download_dir
 
 
-def predict():
+def load_survey(survey: SurveyType, run, camcol, field, download_dir: Path):
+    sdss_download.download_all(run, camcol, field, str(download_dir))
+    # assert files downloaded at download_dir
+
+
+def to_astropy_table(est_cat: FullCatalog):
+    assert list(est_cat.keys()) == [
+        "star_log_fluxes",
+        "star_fluxes",
+        "galaxy_bools",
+        "star_bools",
+        "galaxy_params",
+    ]
+
+    # Convert dictionary of tensors to list of dictionaries
+    n = [torch.squeeze(v).shape[0] for v in est_cat.values()][0]  # number of rows
+    est_cat_list = [{k: torch.squeeze(v)[i].cpu() for k, v in est_cat.items()} for i in range(n)]
+
+    # Convert to astropy table
+    est_cat_table = Table(est_cat_list)
+    # Convert all _fluxes columns to u.Quantity
+    log_nmgy = u.LogUnit(u.nmgy)
+    est_cat_table["star_log_fluxes"] = est_cat_table["star_log_fluxes"] * log_nmgy
+    est_cat_table["star_fluxes"] = est_cat_table["star_fluxes"] * u.nmgy
+
+    # Create inner table for galaxy_params
+    # Convert list of tensors to list of dictionaries
+    galaxy_params_names = [
+        "galaxy_flux",
+        "galaxy_disk_frac",
+        "galaxy_beta_radians",
+        "galaxy_disk_q",
+        "galaxy_a_d",
+        "galaxy_bulge_q",
+        "galaxy_a_b",
+    ]
+    galaxy_params_list = []
+    for galaxy_params in est_cat_table["galaxy_params"]:
+        galaxy_params_dic = {}
+        for i, name in enumerate(galaxy_params_names):
+            galaxy_params_dic[name] = galaxy_params[i]
+        galaxy_params_dic["galaxy_flux"] = galaxy_params_dic["galaxy_flux"] * u.nmgy
+        galaxy_params_dic["galaxy_disk_frac"] = (
+            galaxy_params_dic["galaxy_disk_frac"] * u.dimensionless_unscaled
+        )
+        galaxy_params_dic["galaxy_beta_radians"] = (
+            galaxy_params_dic["galaxy_beta_radians"] * u.radian
+        )
+        galaxy_params_dic["galaxy_disk_q"] = (
+            galaxy_params_dic["galaxy_disk_q"] * u.dimensionless_unscaled
+        )
+        galaxy_params_dic["galaxy_a_d"] = galaxy_params_dic["galaxy_a_d"] * u.arcsec
+        galaxy_params_dic["galaxy_bulge_q"] = (
+            galaxy_params_dic["galaxy_bulge_q"] * u.dimensionless_unscaled
+        )
+        galaxy_params_dic["galaxy_a_b"] = galaxy_params_dic["galaxy_a_b"] * u.arcsec
+        galaxy_params_list.append(galaxy_params_dic)
+    galaxy_params_table = Table(galaxy_params_list)
+    est_cat_table["galaxy_params"] = galaxy_params_table
+
+    return est_cat_table, galaxy_params_table
+
+
+def predict_sdss(data_path: str, weight_save_path: str, **kwargs):
     cfg = base_config()
+    # apply overrides
+    cfg.predict.dataset.sdss_dir = data_path
+    cfg.predict.weight_save_path = weight_save_path
+    cfg.paths.sdss = cfg.predict.dataset.sdss_dir
+    for k, v in kwargs.items():
+        OmegaConf.update(cfg, k, v)
 
-    _predict_sdss(cfg)
+    # download survey images if not already downloaded
+    run, camcol, field = (
+        cfg.predict.dataset.run,
+        cfg.predict.dataset.camcol,
+        cfg.predict.dataset.fields[0],
+    )
+    bands = ["u", "g", "r", "i", "z"]
+    for band in bands:
+        sdss_data_file_path = (
+            Path(cfg.predict.dataset.sdss_dir)
+            / f"{run}"
+            / f"{camcol}"
+            / f"{field}"
+            / f"frame-{band}-{'{:06d}'.format(run)}-{camcol}-{'{:04d}'.format(field)}.fits"
+        )
+        if not sdss_data_file_path.exists():
+            download_dir = Path(cfg.predict.dataset.sdss_dir)
+            load_survey("sdss", run, camcol, field, download_dir)
+            break
+
+    est_cat, _, _, _ = _predict_sdss(cfg)
+    est_cat_table, galaxy_params_table = to_astropy_table(est_cat)
+    return est_cat, est_cat_table, galaxy_params_table
