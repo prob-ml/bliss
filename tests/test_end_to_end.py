9c3fc98f94253bff568f52e41a9c8e3959722b91
XinyueLi1012 <131332845+XinyueLi1012@users.noreply.github.com>
2023-06-16 16:22:00 -0400

    avoid create new file when tesing plot (#800)
    Co-authored-by: Xinyue Li <lxinyue@deeplearning-01.stat.lsa.umich.edu>

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,3 +1,5 @@
+import shutil
+
 from bliss.generate import generate
 from bliss.predict import predict_sdss
 from bliss.train import train
@@ -10,5 +12,13 @@ class TestEndToEnd:
     def test_train(self, cfg):
         train(cfg)
 
-    def test_predict(self, cfg):
-        predict_sdss(cfg)
+    def test_predict(self, cfg, tmpdir_factory):
+        the_cfg = cfg.copy()
+        temp_dir = tmpdir_factory.mktemp("plot")
+        temp_file = str(temp_dir) + "/" + the_cfg.predict.plot.out_file_name
+        the_cfg.predict.plot.out_file_name = temp_file
+        the_cfg.predict.plot.show_plot = True
+        predict_sdss(the_cfg)
+
+        # delete the tmpdir
+        shutil.rmtree(str(temp_dir))
