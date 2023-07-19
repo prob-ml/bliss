891c4c4e9377e6d6d058eb6d3d38101cd923bb16
Jeffrey Regier <jeff-regier@users.noreply.github.com>
2023-07-19 11:20:06 -0400

    truncate location distribution in the unit box (#884)
    * truncate location distribution
    
    * formatting
    
    * added mode property

--- bliss/encoder.py
+++ bliss/encoder.py
@@ -21,9 +21,9 @@ from bliss.surveys.sdss import SloanDigitalSkySurvey as SDSS
 from bliss.transforms import z_score
 from bliss.unconstrained_dists import (
     UnconstrainedBernoulli,
-    UnconstrainedDiagonalBivariateNormal,
     UnconstrainedLogitNormal,
     UnconstrainedLogNormal,
+    UnconstrainedTDBN,
 )
 
 
@@ -98,7 +98,7 @@ class Encoder(pl.LightningModule):
     def dist_param_groups(self):
         d = {
             "on_prob": UnconstrainedBernoulli(),
-            "loc": UnconstrainedDiagonalBivariateNormal(),
+            "loc": UnconstrainedTDBN(),
             "galaxy_prob": UnconstrainedBernoulli(),
             # galsim parameters
             "galsim_disk_frac": UnconstrainedLogitNormal(),
--- bliss/unconstrained_dists.py
+++ bliss/unconstrained_dists.py
@@ -2,6 +2,7 @@ import torch
 from torch.distributions import (
     AffineTransform,
     Categorical,
+    Distribution,
     Independent,
     LogNormal,
     Normal,
@@ -32,15 +33,49 @@ class UnconstrainedNormal:
         return Normal(mean, sd)
 
 
-class UnconstrainedDiagonalBivariateNormal:
+class TruncatedDiagonalMVN(Distribution):
+    """A truncated diagonal multivariate normal distribution."""
+
+    def __init__(self, mu, sigma):
+        super().__init__()
+
+        multiple_normals = Normal(mu, sigma)
+        prob_in_unit_box_hw = multiple_normals.cdf(torch.ones_like(mu))
+        prob_in_unit_box_hw -= multiple_normals.cdf(torch.zeros_like(mu))
+        self.log_prob_in_unit_box = prob_in_unit_box_hw.log().sum(dim=-1)
+
+        self.base_dist = Independent(multiple_normals, 1)
+
+    def sample(self, **args):
+        # some ideas for how to sample it here, if we need to:
+        # https://cran.r-project.org/web/packages/truncnorm/
+        raise NotImplementedError("sampling a truncated normal isn't straightforward")
+
+    @property
+    def mode(self):
+        mu = self.base_dist.mean
+        # a mode still exists if this assertion is false, but I haven't implemented code
+        # to compute it because I don't think we need it
+        assert (mu >= 0).all() and (mu <= 1).all()
+        return self.base_dist.mode
+
+    def log_prob(self, value):
+        assert (value >= 0).all() and (value <= 1).all()
+        # subtracting log probability that the base RV is in the unit box
+        # is equivalent in log space to dividing the normal pdf by the normalizing constant
+        return self.base_dist.log_prob(value) - self.log_prob_in_unit_box
+
+
+class UnconstrainedTDBN:
+    """Produces truncated bivariate normal distributions from unconstrained parameters."""
+
     def __init__(self):
         self.dim = 4
 
     def get_dist(self, params):
-        mean = params[:, :, :, :2].sigmoid()
-        sd = params[:, :, :, 2:].clamp(-6, 3).exp().sqrt()
-        base_dist = Normal(mean, sd)
-        return Independent(base_dist, 1)
+        mu = params[:, :, :, :2].sigmoid()
+        sigma = params[:, :, :, 2:].clamp(-6, 3).exp().sqrt()
+        return TruncatedDiagonalMVN(mu, sigma)
 
 
 class UnconstrainedLogNormal:
