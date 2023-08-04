6983a5e6cf3ea7fa0bafcd01ffb1081d87784b79
Sawan Patel <68625173+sawanp813@users.noreply.github.com>
2023-08-04 14:46:31 -0400

    #829 input transforms (#878)
    * rolling z-score model standard + unalign-realign case study
    
    * removing unnecessary transforms
    
    ---------
    
    Co-authored-by: Sawan Patel <sawanpa@deeplearning-01.stat.lsa.umich.edu>

--- bliss/encoder.py
+++ bliss/encoder.py
@@ -17,7 +17,7 @@ from bliss.backbone import Backbone
 from bliss.catalog import FullCatalog, SourceType, TileCatalog
 from bliss.metrics import BlissMetrics, MetricsMode
 from bliss.plotting import plot_detections
-from bliss.transforms import log_transform, z_score
+from bliss.transforms import log_transform, rolling_z_score
 from bliss.unconstrained_dists import (
     UnconstrainedBernoulli,
     UnconstrainedLogitNormal,
@@ -127,6 +127,8 @@ class Encoder(pl.LightningModule):
             num_features_per_band += 1
         if self.input_transform_params.get("concat_psf_params"):
             num_features_per_band += 6
+        if self.input_transform_params.get("clahe"):
+            num_features_per_band += 1
         return num_features_per_band
 
     def get_input_tensor(self, batch):
@@ -136,7 +138,7 @@ class Encoder(pl.LightningModule):
         in self.input_transform_params. Supported options are:
             use_deconv_channel: add channel for image deconvolved with PSF
             concat_psf_params: add each PSF parameter as a channel
-            z_score: z-score both the images and background
+            rolling_z_score: rolling z-score both the images and background
             log_transform: apply pixel-wise natural logarithm to background-subtracted image.
 
         Args:
@@ -170,14 +172,12 @@ class Encoder(pl.LightningModule):
             n, c, i, h, w = imgs.shape
             psf_params = batch["psf_params"][:, self.bands]
             inputs.append(psf_params.view(n, c, 6 * i, 1, 1).expand(n, c, 6 * i, h, w))
-        if self.input_transform_params.get("z_score"):
-            assert (
-                batch["background"][0, 0].std() > 0
-            ), "Constant backgrounds not supported for multi-band encoding"
-            inputs[0] = z_score(inputs[0])
-            inputs[1] = z_score(inputs[1])
-        elif self.input_transform_params.get("log_transform"):
+        if self.input_transform_params.get("log_transform"):
+            # untransformed background
             inputs[0] = log_transform(torch.clamp(inputs[0] - inputs[1], min=1))
+        elif self.input_transform_params.get("rolling_z_score"):
+            inputs[0] = rolling_z_score(inputs[0], 9, 200, 4)
+            inputs[1] = rolling_z_score(inputs[1], 9, 200, 4)
         return torch.cat(inputs, dim=2)
 
     def encode_batch(self, batch):
