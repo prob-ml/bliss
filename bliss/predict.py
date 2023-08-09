4c567c4a220812319ac069d04402e17d422cc05f
zhixiangteoh <47135468+zhixiangteoh@users.noreply.github.com>
2023-08-09 18:19:18 -0400

    Fix flux calibrations for DES/DECaLS

--- bliss/predict.py
+++ bliss/predict.py
@@ -59,23 +59,24 @@ def align(img, wcs_list, ref_band, ref_depth=0):
     coadd_depth, n_bands, h, w = img.shape
 
     # align with (`ref_depth`, `ref_band`) WCS
+    target_wcs = wcs_list[ref_depth][ref_band]
     for d in range(coadd_depth):
         for bnd in range(n_bands):
             inputs = (img[d, bnd], wcs_list[d][bnd])
-            reproj, footprint = reproject_interp(  # noqa: WPS317
-                inputs, wcs_list[ref_depth][ref_band], order="bicubic", shape_out=(h, w)
+            reproj, footprint = reproject_interp(
+                inputs, target_wcs, order="bicubic", shape_out=(h, w)
             )
             reproj_d[(d, bnd)] = reproj
             footprint_d[(d, bnd)] = footprint
 
     # use footprints to handle NaNs from reprojection
-    h, w = footprint_d[(0, 0)].shape
-    out_print = np.ones((h, w))
+    fp_h, fp_w = footprint_d[(0, 0)].shape
+    out_print = np.ones((fp_h, fp_w))
     for fp in footprint_d.values():
-        out_print = out_print * fp  # noqa: WPS350
+        out_print *= fp
 
     out_print = np.expand_dims(out_print, axis=(0, 1))
-    reproj_out = np.zeros((coadd_depth, n_bands, h, w))
+    reproj_out = np.zeros(img.shape)
     for d in range(coadd_depth):
         for bnd in range(n_bands):
             reproj_d[(d, bnd)] = np.multiply(reproj_d[(d, bnd)], out_print)
