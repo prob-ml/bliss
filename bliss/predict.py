dc2a512220ef8389b9fe2ca76c86913caafcfae7
Jeffrey Regier <regier@umich.edu>
2023-11-18 13:11:04 -0500

    crop within sdss class; prediction associated with image_ids

--- bliss/predict.py
+++ bliss/predict.py
@@ -4,20 +4,10 @@ from hydra.utils import instantiate
 
 def predict(cfg):
     encoder = instantiate(cfg.encoder)
-    encoder.eval()
     encoder.load_state_dict(torch.load(cfg.predict.weight_save_path))
-
     dataset = instantiate(cfg.predict.dataset)
-
     trainer = instantiate(cfg.training.trainer)
     enc_output = trainer.predict(encoder, datamodule=dataset)
 
-    est_cat_tables = []
-    for batch_output in enc_output:
-        est_cat = batch_output["est_cat"]
-        full_cat = est_cat.to_full_catalog()
-        astropy_cat = full_cat.to_astropy_table(cfg.encoder.survey_bands)
-        est_cat_tables.append(astropy_cat)
-
-    # eventually we should return variational distribution samples too
-    return est_cat_tables
+    est_cats = [b["est_cat"].to_full_catalog() for b in enc_output]
+    return dict(zip(dataset.image_ids(), est_cats))
