d09b349dea5c38fcab83511bff36f4ea87668207
Aakash Patel <aakashdp@umich.edu>
2023-12-03 09:37:00 -0500

    Add scripts and notebook for psf experiments (#948)
    scripts and notebook for psf experiments

---
+++ case_studies/psf_variation/train_models.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Runs the experiments for the BLISS PSF variation paper.
+
+# train base model
+bliss -cp ~/bliss/case_studies/psf_variation -cn config mode=train \
+    train.name=PSF_MODELS \
+    train.version=base_model \
+    encoder.image_normalizer.use_deconv_channel=False \
+    encoder.image_normalizer.concat_psf_params=False \
+    cached_simulator.cached_data_path=/data/scratch/aakash/train_single_94-1-12
+
+# train psf-unaware model
+bliss -cp ~/bliss/case_studies/psf_variation -cn config mode=train \
+    train.name=PSF_MODELS \
+    train.version=multi_field_psf_unaware \
+    encoder.image_normalizer.use_deconv_channel=False \
+    encoder.image_normalizer.concat_psf_params=False \
+    cached_simulator.cached_data_path=/data/scratch/aakash/train_multi_field
+
+# train deconv-only model
+bliss -cp ~/bliss/case_studies/psf_variation -cn config mode=train \
+    train.name=PSF_MODELS \
+    train.version=multi_field_deconv_only \
+    encoder.image_normalizer.use_deconv_channel=True \
+    encoder.image_normalizer.concat_psf_params=False \
+    cached_simulator.cached_data_path=/data/scratch/aakash/train_multi_field
+
+# train concat params only model
+bliss -cp ~/bliss/case_studies/psf_variation -cn config mode=train \
+    train.name=PSF_MODELS \
+    train.version=multi_field_psf_params_only \
+    encoder.image_normalizer.use_deconv_channel=False \
+    encoder.image_normalizer.concat_psf_params=True \
+    cached_simulator.cached_data_path=/data/scratch/aakash/train_multi_field
+
+# train both params only model
+bliss -cp ~/bliss/case_studies/psf_variation -cn config mode=train \
+    train.name=PSF_MODELS \
+    train.version=multi_field_deconv_and_psf_param \
+    encoder.image_normalizer.use_deconv_channel=True \
+    encoder.image_normalizer.concat_psf_params=True \
+    cached_simulator.cached_data_path=/data/scratch/aakash/train_multi_field
+
+
+# run the evaluation notebook
+jupyter nbconvert --execute evaluate_models.ipynb --to html
