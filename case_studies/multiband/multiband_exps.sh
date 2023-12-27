4cef37e41ae527e57d6441d37535e79bfefced8e
Sawan Patel <sawanpa@deeplearning-01.stat.lsa.umich.edu>
2023-12-26 21:47:16 -0500

    case study runs e2e

--- case_studies/multiband/multiband_exps.sh
+++ case_studies/multiband/multiband_exps.sh
@@ -13,14 +13,14 @@ bliss -cp ~/bliss/case_studies/multiband -cn config mode=train \
 bliss -cp ~/bliss/case_studies/multiband -cn config mode=train \
     train.trainer.logger.name=MULTIBAND \
     train.trainer.logger.version=gri_band \
-    encoder.image_normalizer.bands=[1, 2, 3] \
+    encoder.image_normalizer.bands=[1,2,3] \
     cached_simulator.cached_data_path=/data/scratch/regier/2percent
 
 # train five-band model
 bliss -cp ~/bliss/case_studies/multiband -cn config mode=train \
     train.trainer.logger.name=MULTIBAND \
     train.trainer.logger.version=ugriz_band \
-    encoder.image_normalizer.bands=[0, 1, 2, 3, 4] \
+    encoder.image_normalizer.bands=[0,1,2,3,4] \
     cached_simulator.cached_data_path=/data/scratch/regier/2percent
 
 # run the evaluation notebook
