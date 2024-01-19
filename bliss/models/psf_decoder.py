37ceb9bd320b77fc8723f015baa72b70db065994
Ismael Mendoza <imendoza@umich.edu>
2024-01-19 09:35:08 -0500

    jk

--- bliss/models/psf_decoder.py
+++ bliss/models/psf_decoder.py
@@ -7,6 +7,18 @@ from torch import Tensor, nn
 from bliss.datasets.lsst import get_default_lsst_psf
 
 
+def get_mgrid(slen: int):
+    offset = (slen - 1) / 2
+    # Currently type-checking with mypy doesn't work with np.mgrid
+    # See https://github.com/python/mypy/issues/11185.
+    x, y = np.mgrid[-offset : (offset + 1), -offset : (offset + 1)]  # type: ignore
+    mgrid = torch.tensor(np.dstack((y, x))) / offset
+    # mgrid is between -1 and 1
+    # then scale slightly because of the way f.grid_sample
+    # parameterizes the edges: (0, 0) is center of edge pixel
+    return mgrid.float() * (slen - 1) / slen
+
+
 class PSFDecoder(nn.Module):
     """Abstract decoder class to subclass whenever the decoded result will go through a PSF.
 
