23b58421934b6a6392b1e014570567e30e0a478f
Tim White <twhit@umich.edu>
2024-01-23 15:49:53 -0500

    Update variational distribution subclasses and create example notebook with simple training loop for shear and convergence

--- case_studies/weak_lensing/lensing_variational_dist.py
+++ case_studies/weak_lensing/lensing_variational_dist.py
@@ -1,8 +1,6 @@
-import torch
 from lensing_catalog import LensingTileCatalog
 
-from bliss.catalog import SourceType
-from bliss.encoder.unconstrained_dists import UnconstrainedNormal, UnconstrainedTDBN
+from bliss.encoder.unconstrained_dists import UnconstrainedLogitNormal, UnconstrainedTDBN
 from bliss.encoder.variational_dist import VariationalDist, VariationalDistSpec
 
 
@@ -13,8 +11,10 @@ class LensingVariationalDistSpec(VariationalDistSpec):
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        self.factor_specs["shear"] = UnconstrainedTDBN()
-        self.factor_specs["convergence"] = UnconstrainedNormal()
+        self.factor_specs = {
+            "shear": UnconstrainedTDBN(),
+            "convergence": UnconstrainedLogitNormal(),
+        }
 
     def make_dist(self, x_cat):
         # override this method to instantiate a subclass of VariationalGrid, e.g.,
@@ -35,46 +35,18 @@ class LensingVariationalDist(VariationalDist):
         """
         q = self.factors
 
-        locs = q["loc"].mode if use_mode else q["loc"].sample().squeeze(0)
-        est_cat = {"locs": locs}
-
-        # populate catalog with per-band (log) star fluxes
-        sf_factors = [q[f"star_flux_{band}"] for band in self.survey_bands]
-        sf_lst = [p.mode if use_mode else p.sample() for p in sf_factors]
-        est_cat["star_fluxes"] = torch.stack(sf_lst, dim=3)
-
-        # populate catalog with source type
-        galaxy_bools = q["galaxy_prob"].mode if use_mode else q["galaxy_prob"].sample()
-        galaxy_bools = galaxy_bools.unsqueeze(3)
-        star_bools = 1 - galaxy_bools
-        est_cat["source_type"] = SourceType.STAR * star_bools + SourceType.GALAXY * galaxy_bools
-
-        # populate catalog with galaxy parameters
-        gs_dists = [q[f"galsim_{name}"] for name in self.GALSIM_NAMES]
-        gs_param_lst = [d.icdf(torch.tensor(0.5)) if use_mode else d.sample() for d in gs_dists]
-        est_cat["galaxy_params"] = torch.stack(gs_param_lst, dim=3)
-
-        # populate catalog with per-band galaxy fluxes
-        gf_dists = [q[f"galaxy_flux_{band}"] for band in self.survey_bands]
-        gf_lst = [d.icdf(torch.tensor(0.5)) if use_mode else d.sample() for d in gf_dists]
-        est_cat["galaxy_fluxes"] = torch.stack(gf_lst, dim=3)
-
-        # we have to unsqueeze these tensors because a TileCatalog can store multiple
-        # light sources per tile, but we sample only one source per tile
-        for k, v in est_cat.items():
-            est_cat[k] = v.unsqueeze(3)
-
-        # n_sources is not unsqueezed because it is a single integer per tile regardless of
-        # how many light sources are stored per tile
-        est_cat["n_sources"] = q["on_prob"].mode if use_mode else q["on_prob"].sample()
-
+        est_cat = {}
         est_cat["shear"] = q["shear"].mode if use_mode else q["shear"].sample()
         est_cat["convergence"] = q["convergence"].mode if use_mode else q["convergence"].sample()
 
         return LensingTileCatalog(self.tile_slen, est_cat)
 
     def compute_nll(self, true_tile_cat: LensingTileCatalog):
-        non_lensing_loss = super().compute_nll(true_tile_cat)
-        lensing_loss = 1.0
+        q = self.factors
 
-        return non_lensing_loss + lensing_loss
+        true_shear = true_tile_cat.data["shear"].squeeze()
+        true_convergence = true_tile_cat.data["convergence"].squeeze()
+        lensing_loss = -q["shear"].log_prob(true_shear)
+        lensing_loss -= q["convergence"].log_prob(true_convergence)
+
+        return lensing_loss
