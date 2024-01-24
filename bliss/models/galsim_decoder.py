7038b84cbd19a421d7f90535ae2e3f9da7784583
Ismael Mendoza <imendoza@umich.edu>
2024-01-24 16:22:56 -0500

    remove prior

--- bliss/models/galsim_decoder.py
+++ bliss/models/galsim_decoder.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Tuple
+from typing import Optional
 
 import galsim
 import numpy as np
@@ -9,109 +9,16 @@ from bliss.catalog import FullCatalog, TileCatalog
 from bliss.models.psf_decoder import PSFDecoder
 
 
-class SingleGalsimGalaxyPrior:
-    dim_latents = 7
-
-    def __init__(
-        self,
-        flux_sample: str,
-        min_flux: float,
-        max_flux: float,
-        a_sample: str,
-        alpha: Optional[float] = None,
-        min_a_d: Optional[float] = None,
-        max_a_d: Optional[float] = None,
-        min_a_b: Optional[float] = None,
-        max_a_b: Optional[float] = None,
-        a_concentration: Optional[float] = None,
-        a_loc: Optional[float] = None,
-        a_scale: Optional[float] = None,
-        a_bulge_disk_ratio: Optional[float] = None,
-    ) -> None:
-        self.flux_sample = flux_sample
-        self.min_flux = min_flux
-        self.max_flux = max_flux
-        self.alpha = alpha
-        if self.flux_sample == "pareto":
-            assert self.alpha is not None
-
-        self.a_sample = a_sample
-        self.min_a_d = min_a_d
-        self.max_a_d = max_a_d
-        self.min_a_b = min_a_b
-        self.max_a_b = max_a_b
-
-        self.a_concentration = a_concentration
-        self.a_loc = a_loc
-        self.a_scale = a_scale
-        self.a_bulge_disk_ratio = a_bulge_disk_ratio
-
-        if self.a_sample == "uniform":
-            assert self.min_a_d is not None
-            assert self.max_a_d is not None
-            assert self.min_a_b is not None
-            assert self.max_a_b is not None
-        elif self.a_sample == "gamma":
-            assert self.a_concentration is not None
-            assert self.a_loc is not None
-            assert self.a_scale is not None
-            assert self.a_bulge_disk_ratio is not None
-        else:
-            raise NotImplementedError()
-
-    def sample(self, total_latent, device="cpu"):
-        # create galaxy as mixture of Exponential + DeVacauleurs
-        if self.flux_sample == "uniform":
-            total_flux = _uniform(self.min_flux, self.max_flux, n_samples=total_latent)
-        elif self.flux_sample == "log_uniform":
-            log_flux = _uniform(
-                torch.log10(self.min_flux), torch.log10(self.max_flux), n_samples=total_latent
-            )
-            total_flux = 10**log_flux
-        elif self.flux_sample == "pareto":
-            total_flux = _draw_pareto(
-                self.alpha, self.min_flux, self.max_flux, n_samples=total_latent
-            )
-        else:
-            raise NotImplementedError()
-        disk_frac = _uniform(0, 1, n_samples=total_latent)
-        beta_radians = _uniform(0, 2 * np.pi, n_samples=total_latent)
-        disk_q = _uniform(0, 1, n_samples=total_latent)
-        bulge_q = _uniform(0, 1, n_samples=total_latent)
-        if self.a_sample == "uniform":
-            disk_a = _uniform(self.min_a_d, self.max_a_d, n_samples=total_latent)
-            bulge_a = _uniform(self.min_a_b, self.max_a_b, n_samples=total_latent)
-        elif self.a_sample == "gamma":
-            disk_a = _gamma(self.a_concentration, self.a_loc, self.a_scale, n_samples=total_latent)
-            bulge_a = _gamma(
-                self.a_concentration,
-                self.a_loc / self.a_bulge_disk_ratio,
-                self.a_scale / self.a_bulge_disk_ratio,
-                n_samples=total_latent,
-            )
-        else:
-            raise NotImplementedError()
-        return torch.stack(
-            [total_flux, disk_frac, beta_radians, disk_q, disk_a, bulge_q, bulge_a], dim=1
-        ).to(device)
-
-
 class SingleGalsimGalaxyDecoder(PSFDecoder):
     def __init__(
         self,
         slen: int,
         n_bands: int,
         pixel_scale: float,
-        psf_params_file: Optional[str] = None,
         psf_slen: Optional[int] = None,
-        sdss_bands: Optional[Tuple[int, ...]] = None,
-        psf_gauss_fwhm: Optional[float] = None,
     ) -> None:
         super().__init__(
-            psf_gauss_fwhm=psf_gauss_fwhm,
-            psf_params_file=psf_params_file,
             psf_slen=psf_slen,
-            sdss_bands=sdss_bands,
             n_bands=n_bands,
             pixel_scale=pixel_scale,
         )
@@ -185,67 +92,6 @@ class SingleGalsimGalaxyDecoder(PSFDecoder):
         return torch.from_numpy(image).reshape(1, slen, slen)
 
 
-class DefaultGalsimPrior:
-    def __init__(
-        self,
-        single_galaxy_prior: SingleGalsimGalaxyPrior,
-        max_n_sources: int,
-        mean_sources: float,
-        max_shift: float,
-        galaxy_prob: float,
-    ):
-        self.single_galaxy_prior = single_galaxy_prior
-        self.max_shift = max_shift  # between 0 and 0.5, from center.
-        self.max_n_sources = max_n_sources
-        self.mean_sources = mean_sources
-        self.dim_latents = self.single_galaxy_prior.dim_latents
-        self.galaxy_prob = galaxy_prob
-        assert 0 <= self.max_shift <= 0.5
-
-    def sample(self) -> Dict[str, Tensor]:
-        """Returns a single batch of source parameters."""
-        n_sources = _sample_poisson_n_sources(self.mean_sources, self.max_n_sources)
-
-        params = torch.zeros(self.max_n_sources, self.dim_latents)
-        params[:n_sources, :] = self.single_galaxy_prior.sample(n_sources)
-
-        locs = torch.zeros(self.max_n_sources, 2)
-        locs[:n_sources, 0] = _uniform(-self.max_shift, self.max_shift, n_sources) + 0.5
-        locs[:n_sources, 1] = _uniform(-self.max_shift, self.max_shift, n_sources) + 0.5
-
-        galaxy_bools = torch.zeros(self.max_n_sources, 1)
-        galaxy_bools[:n_sources, :] = _bernoulli(self.galaxy_prob, n_sources)[:, None]
-        star_bools = torch.zeros(self.max_n_sources, 1)
-        star_bools[:n_sources, :] = 1 - galaxy_bools[:n_sources, :]
-
-        star_fluxes = torch.zeros(self.max_n_sources, 1)
-        star_log_fluxes = torch.zeros(self.max_n_sources, 1)
-        star_fluxes[:n_sources, 0] = params[:n_sources, 0]
-        star_log_fluxes[:n_sources, 0] = torch.log(star_fluxes[:n_sources, 0])
-
-        return {
-            "n_sources": torch.tensor(n_sources),
-            "locs": locs,
-            "galaxy_bools": galaxy_bools,
-            "star_bools": star_bools,
-            "galaxy_params": params * galaxy_bools,
-            "star_fluxes": star_fluxes * star_bools,
-            "star_log_fluxes": star_log_fluxes * star_bools,
-        }
-
-
-class BCGPrior(DefaultGalsimPrior):
-    def sample(self) -> Dict[str, Tensor]:
-        """Returns a single batch of source parameters where brightest galaxy is centered."""
-        sample = super().sample()
-        n_sources = sample.pop("n_sources")
-        flux = sample["galaxy_params"][:, 0].reshape(-1)
-        idx_order = torch.argsort(-flux)
-        reordered_sample = {k: v[idx_order] for k, v in sample.items()}
-        reordered_sample["locs"][0, :] = 0.5 + _uniform(0.015, 0.03, 2)  # slight off-center.
-        return {"n_sources": n_sources, **reordered_sample}
-
-
 class FullCatalogDecoder:
     def __init__(
         self, single_galaxy_decoder: SingleGalsimGalaxyDecoder, slen: int, bp: int
@@ -306,31 +152,3 @@ class FullCatalogDecoder:
     def forward_tile(self, tile_cat: TileCatalog):
         full_cat = tile_cat.to_full_params()
         return self(full_cat)
-
-
-def _sample_poisson_n_sources(mean_sources, max_n_sources) -> int:
-    n_sources = torch.distributions.Poisson(mean_sources).sample([1])
-    return int(torch.clamp(n_sources, max=torch.tensor(max_n_sources)))
-
-
-def _uniform(a, b, n_samples=1) -> Tensor:
-    # uses pytorch to return a single float ~ U(a, b)
-    return (a - b) * torch.rand(n_samples) + b
-
-
-def _draw_pareto(alpha, min_x, max_x, n_samples=1) -> Tensor:
-    # draw pareto conditioned on being less than f_max
-    assert alpha is not None
-    u_max = 1 - (min_x / max_x) ** alpha
-    uniform_samples = torch.rand(n_samples) * u_max
-    return min_x / (1.0 - uniform_samples) ** (1 / alpha)
-
-
-def _gamma(concentration, loc, scale, n_samples=1):
-    x = torch.distributions.Gamma(concentration, rate=1.0).sample((n_samples,))
-    return x * scale + loc
-
-
-def _bernoulli(prob, n_samples=1) -> Tensor:
-    prob_list = [float(prob) for _ in range(n_samples)]
-    return torch.bernoulli(torch.tensor(prob_list))
