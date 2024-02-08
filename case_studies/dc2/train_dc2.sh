83fe6b0471109ba60be3dc87c76ead5e44efdf5e
Xinyue Li <lxinyue@deeplearning-01.stat.lsa.umich.edu>
2024-02-08 14:22:14 -0500

    fix some bugs

--- case_studies/dc2/train_dc2.sh
+++ case_studies/dc2/train_dc2.sh
@@ -7,37 +7,37 @@ jupyter nbconvert --execute DC2_galaxy_psf_params.ipynb --to html
 jupyter nbconvert --execute DC2_deconv.ipynb --to html
 
 # train base model
-bliss -cp ~/bliss/case_studies/bliss_dc2 -cn config mode=train \
+bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=base_model \
     encoder.image_normalizer.use_deconv_channel=False \
     encoder.image_normalizer.concat_psf_params=False \
-    encoder.do_data_augmentation = False \
-    train.pretrained_weights = None
+    encoder.do_data_augmentation=False \
+    train.pretrained_weights=None
 
 # train both PSF params and deconvolution channel model
-bliss -cp ~/bliss/case_studies/bliss_dc2 -cn config mode=train \
+bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=dc2_psf_deconv \
     encoder.image_normalizer.use_deconv_channel=True \
     encoder.image_normalizer.concat_psf_params=True \
-    encoder.do_data_augmentation = False \
-    train.pretrained_weights = None
+    encoder.do_data_augmentation=False \
+    train.pretrained_weights=None
 
 # train with data augmentation
-bliss -cp ~/bliss/case_studies/bliss_dc2 -cn config mode=train \
+bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=dc2_data_aug \
     encoder.image_normalizer.use_deconv_channel=False \
     encoder.image_normalizer.concat_psf_params=False \
-    encoder.do_data_augmentation = True \
-    train.pretrained_weights = None
+    encoder.do_data_augmentation=True \
+    train.pretrained_weights=None
 
 # train with data augmentation and psf, deconvolution
-bliss -cp ~/bliss/case_studies/bliss_dc2 -cn config mode=train \
+bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=dc2_psf_deconv_da \
     encoder.image_normalizer.use_deconv_channel=True \
     encoder.image_normalizer.concat_psf_params=True \
-    encoder.do_data_augmentation = True \
-    train.pretrained_weights = None
+    encoder.do_data_augmentation=True \
+    train.pretrained_weights=None
