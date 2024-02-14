31e35908ee7f1f9b15d438eb762235b9141b6d0e
Steve Jeffery Fan <stevefan@deeplearning-01.stat.lsa.umich.edu>
2024-02-14 17:07:28 -0500

    committing work on metrics

--- case_studies/weak_lensing/lensing_metrics.py
+++ case_studies/weak_lensing/lensing_metrics.py
@@ -1,5 +1,76 @@
 from torchmetrics import Metric
+import torch
 
-class LensingMapMetrics(Metric):
-    def __init__(self,):
-        super().__init__()
+class LensingMapCorrCoef(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_state("preds_shear", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("target_shear", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("preds_convergence", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("target_convergence", default=torch.Tensor(), dist_reduce_fx="cat")
+    
+    def update(self, true_cat, est_cat, matching) -> None:
+        for i in range(len(matching)):
+            true_idx, est_idx = matching[i]
+
+            self.target_shear = torch.cat((self.target_shear, true_cat["shear"][i,true_idx,:].flatten(end_dim=-2)),)
+            self.preds_shear = torch.cat((self.preds_shear, est_cat["shear"][i, est_idx, :].flatten(end_dim=-2)),)
+
+            self.target_convergence = torch.cat((self.target_convergence, true_cat["convergence"][i,true_idx,:].flatten()),)
+            self.preds_convergence = torch.cat((self.preds_convergence, est_cat["convergence"][i,est_idx,:].flatten()),)
+
+    def compute(self,):
+        posterior_shear1 = self.preds_shear[:,0]
+        posterior_shear2 = self.preds_shear[:,1]
+        posterior_convergence = self.preds_convergence
+
+        true_shear1 = self.target_shear[:,0]
+        true_shear2 = self.target_shear[:,1]
+        true_convergence = self.target_convergence
+
+        shear_1_corr_coef = torch.corrcoef(torch.Tensor([posterior_shear1, true_shear1]))
+        shear_2_corr_coef = torch.corrcoef(torch.Tensor([posterior_shear2, true_shear2]))
+        convergence_corr_coef = torch.corrcoef(torch.Tensor([posterior_convergence, true_convergence]))
+
+        return {
+            "Shear 1 corr_coef" : shear_1_corr_coef,
+            "Shear 2 corr_coef" : shear_2_corr_coef,
+            "Convergence corr_coef" : convergence_corr_coef
+        }
+
+class LensingMapRMSE(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_state("preds_shear", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("target_shear", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("preds_convergence", default=torch.Tensor(), dist_reduce_fx="cat")
+        self.add_state("target_convergence", default=torch.Tensor(), dist_reduce_fx="cat")
+
+    def update(self, true_cat, est_cat, matching) -> None:
+        for i in range(len(matching)):
+            true_idx, est_idx = matching[i]
+
+            self.target_shear = torch.cat((self.target_shear, true_cat["shear"][i,true_idx,:].flatten(end_dim=-2)),)
+            self.preds_shear = torch.cat((self.preds_shear, est_cat["shear"][i, est_idx, :].flatten(end_dim=-2)),)
+
+            self.target_convergence = torch.cat((self.target_convergence, true_cat["convergence"][i,true_idx,:].flatten()),)
+            self.preds_convergence = torch.cat((self.preds_convergence, est_cat["convergence"][i,est_idx,:].flatten()),)
+
+    def compute(self,):
+        posterior_shear1 = self.preds_shear[:,0]
+        posterior_shear2 = self.preds_shear[:,1]
+        posterior_convergence = self.preds_convergence
+
+        true_shear1 = self.target_shear[:,0]
+        true_shear2 = self.target_shear[:,1]
+        true_convergence = self.target_convergence
+
+        shear_1_RMSE = torch.sqrt(torch.mean((true_shear1.flatten()-posterior_shear1.flatten())**2))
+        shear_2_RMSE = torch.sqrt(torch.mean((true_shear2.flatten()-posterior_shear2.flatten())**2))
+        convergence_RMSE = torch.sqrt(torch.mean((true_convergence-posterior_convergence)**2))
+
+        return {
+            "Shear 1 RMSE" : shear_1_RMSE,
+            "Shear 2 RMSE" : shear_2_RMSE,
+            "Convergence RMSE" : convergence_RMSE
+        }
