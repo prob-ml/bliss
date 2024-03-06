861f5a7bb56339d729f99e2358821b7924c8a6df
Jackson Loper <jaloper@umich.edu>
2024-03-06 13:30:50 -0500

    Black fix

--- tests/test_toy_simulator.py
+++ tests/test_toy_simulator.py
@@ -1,22 +1,19 @@
 import torch
-import numpy as np
 
 import bliss.simulator.toy_simulated_dataset
 
+
 def test_toy_simulator():
-    simulator = bliss.simulator.toy_simulated_dataset.ToySimulator(height=80,width=80,n_bands=2)
+    simulator = bliss.simulator.toy_simulated_dataset.ToySimulator(height=80, width=80, n_bands=2)
 
-    ds = bliss.simulator.toy_simulated_dataset.ToySimulatedDataset(simulator,batch_size=64)
-    for batch in ds:
-        break
-    assert batch['images'].shape == (64, 2, 80,80)
+    ds = bliss.simulator.toy_simulated_dataset.ToySimulatedDataset(simulator, batch_size=64)
+    batch = next(iter(ds))
+    assert batch["images"].shape == (64, 2, 80, 80)
 
-
-    ds = bliss.simulator.toy_simulated_dataset.ToySimulatedDataset(simulator,
-                                                                   batch_size=1,epoch_size=1,cache=True)
-    for batch1 in ds:
-        break
-    for batch2 in ds:
-        break
-    assert batch1['images'].shape == (1, 2, 80,80)
-    assert torch.all(batch1['images']==batch2['images'])
+    ds = bliss.simulator.toy_simulated_dataset.ToySimulatedDataset(
+        simulator, batch_size=1, epoch_size=1, cache=True
+    )
+    batch1 = next(iter(ds))
+    batch2 = next(iter(ds))
+    assert batch1["images"].shape == (1, 2, 80, 80)
+    assert torch.all(batch1["images"] == batch2["images"])
