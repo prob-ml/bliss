c03155a48401b28546923f03baee1adb8368738a
Ismael Mendoza <imendoza@umich.edu>
2024-03-07 14:30:29 -0500

    not sure why we evver need such complicated background class

--- bliss/datasets/background.py
+++ bliss/datasets/background.py
@@ -2,17 +2,10 @@ from typing import Tuple
 
 import torch
 from einops import rearrange
-from torch import Tensor, nn
 
 
-class ConstantBackground(nn.Module):
-    def __init__(self, background: Tuple[float, ...]):
-        super().__init__()
-        bg: Tensor = torch.tensor(background)
-        bg = rearrange(bg, "c -> 1 c 1 1")
-        self.register_buffer("background", bg, persistent=False)
-
-    def sample(self, shape) -> Tensor:
-        assert isinstance(self.background, Tensor)
-        batch_size, c, hlen, wlen = shape
-        return self.background.expand(batch_size, c, hlen, wlen)
+def get_constant_background(value: float, shape: Tuple[int, ...]):
+    b, c, h, w = shape
+    bg = torch.tensor(value)
+    bg = rearrange(bg, "c -> 1 c 1 1")
+    return bg.expand(b, c, h, w)
