4b336984bcc166d7f22f48f66a29a9c2bd30c649
Ismael Mendoza <imendoza@umich.edu>
2024-03-07 14:14:04 -0500

    split into two

--- bliss/datasets/lsst.py
+++ bliss/datasets/lsst.py
@@ -1,9 +1,11 @@
+"""Utilites for calculating LSST survey related quantities."""
+
 import galcheat
 import galsim
-import numpy as np
 import torch
 from astropy import units as u  # noqa: WPS347
 from btk.survey import get_surveys
+from einops import rearrange
 from galcheat.utilities import mag2counts, mean_sky_level
 from torch import Tensor
 
@@ -40,52 +42,12 @@ def get_default_lsst_psf() -> galsim.GSObject:
     return i_band.psf
 
 
+def get_default_lsst_psf_tensor(slen: int) -> Tensor:
+    psf_obj = get_default_lsst_psf()
+    psf_array = psf_obj.drawImage(nx=slen, ny=slen, scale=PIXEL_SCALE).array
+    psf_tensor = torch.from_numpy(psf_array)
+    return rearrange(psf_tensor, "h w -> 1 h w", h=slen, w=slen).float()
+
+
 def get_default_lsst_background() -> float:
     return mean_sky_level("LSST", "i").to_value("electron")
-
-
-def column_to_tensor(table, colname):
-    dtypes = {
-        np.dtype(">i2"): int,
-        np.dtype(">i4"): int,
-        np.dtype(">i8"): int,
-        np.dtype("bool"): bool,
-        np.dtype(">f4"): np.float32,
-        np.dtype(">f8"): np.float32,
-        np.dtype("float32"): np.float32,
-        np.dtype("float64"): np.dtype("float64"),
-    }
-    x = np.array(table[colname])
-    dtype = dtypes[x.dtype]
-    x = x.astype(dtype)
-    return torch.from_numpy(x)
-
-
-def table_to_dict(table):
-    d = {}
-    for p in table.columns:
-        d[p] = column_to_tensor(table, p)
-    return d
-
-
-def catsim_row_to_galaxy_params(table, max_n_sources):
-    names = (
-        "fluxnorm_bulge",
-        "fluxnorm_disk",
-        "fluxnorm_agn",
-        "a_b",
-        "a_d",
-        "b_b",
-        "b_d",
-        "pa_bulge",
-        "i_ab",
-        "flux",
-    )
-
-    params = torch.zeros((max_n_sources, len(names)))
-
-    for ii, col in enumerate(table):
-        for jj, n in enumerate(names):
-            params[ii, jj] = column_to_tensor(col, n)
-
-    return params
