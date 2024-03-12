5b25eefff1bacbee39b3687f6135f53c67eafd7d
Ismael Mendoza <imendoza@umich.edu>
2024-03-12 09:29:16 -0400

    refactor and remove pytorch lightning, validation can be overwritten in main train.py if desired

--- bliss/models/galaxy_net.py
+++ bliss/models/galaxy_net.py
@@ -1,51 +1,39 @@
-from typing import List, Optional, Tuple
+from pathlib import Path
+from typing import Tuple
 
-import pytorch_lightning as pl
+import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from torch import Tensor, nn
 from torch.distributions import Normal
 from torch.nn.functional import relu
-from torch.optim import Adam
 
 from bliss.plotting import plot_image
 
 
-def loss_fn(image: Tensor, recon_mean: Tensor, pq_z: Tensor):
+def vae_loss(image: Tensor, recon_mean: Tensor, pq_z: Tensor):
     recon_loss = -Normal(recon_mean, recon_mean.sqrt()).log_prob(image).sum()
     pq_z_loss = -pq_z.sum()
     return recon_loss + pq_z_loss
 
 
-class OneCenteredGalaxyAE(pl.LightningModule):
-    # ---------------
-    # Model
-    # ----------------
-
+class OneCenteredGalaxyAE(nn.Module):
     def __init__(
         self,
         slen: int,
         latent_dim: int,
         hidden: int,
         n_bands: int = 1,
-        optimizer_params: Optional[dict] = None,
-        ckpt: Optional[str] = None,
+        ckpt: str | Path | None = None,
     ):
         super().__init__()
-        self.save_hyperparameters()
-        self.optimizer_params = optimizer_params
 
         self.enc = self.make_encoder(slen, latent_dim, n_bands, hidden)
         self.dec = self.make_decoder(slen, latent_dim, n_bands, hidden)
         self.latent_dim = latent_dim
 
-        self.register_buffer("zero", torch.zeros(1))
-        self.register_buffer("one", torch.ones(1))
-
         if ckpt is not None:
-            self.load_state_dict(torch.load(ckpt, map_location=self.device))  # type: ignore
-
-        self.validation_step_outputs: List[dict] = []
+            self.load_state_dict(torch.load(ckpt, map_location=self.device))
 
     def forward(self, image, background):
         return self.reconstruct(image, background)
@@ -61,128 +49,6 @@ class OneCenteredGalaxyAE(pl.LightningModule):
 
     def make_decoder(self, slen: int, latent_dim: int, n_bands: int, hidden: int):
         return CenteredGalaxyDecoder(slen, latent_dim, n_bands, hidden)
-
-    def get_encoder(self):
-        return self.enc
-
-    def get_decoder(self):
-        return self.dec
-
-    # ---------------
-    # Optimizer
-    # ----------------
-
-    def configure_optimizers(self):
-        """Configures optimizers for training (pytorch lightning)."""
-        return Adam(self.parameters(), **self.optimizer_params)
-
-    # ---------------
-    # Training
-    # ----------------
-
-    def training_step(self, batch, batch_idx):
-        """Training step (pytorch lightning)."""
-        images, background = batch["images"], batch["background"]
-        recon_mean, pq_z = self.forward(images, background)
-        loss = self.get_loss(images, recon_mean, pq_z)
-        self.log("train/loss", loss)
-        return loss
-
-    # ----------------
-    # Validation
-    # ----------------
-
-    def validation_step(self, batch, batch_idx):
-        """Validation step (pytorch lightning)."""
-        images, background = batch["images"], batch["background"]
-        recon_mean, pq_z = self.forward(images, background)
-        residuals = (images - recon_mean) / torch.sqrt(recon_mean)
-        loss = self.get_loss(images, recon_mean, pq_z)
-
-        # metrics
-        self.log("val/loss", loss)
-        self.log("val/max_residual", residuals.abs().max())
-
-        d = {"images": images, "recon_mean": recon_mean, "residuals": residuals}
-        self.validation_step_outputs.append(d)
-        return loss
-
-    def on_validation_epoch_end(self):
-        """Validation epoch end (pytorch lightning)."""
-
-        outputs = self.validation_step_outputs
-        # combine all images and recon_mean's into a single tensor
-        images = torch.cat([output["images"] for output in outputs])
-        recon_mean = torch.cat([output["recon_mean"] for output in outputs])
-        residuals = torch.cat([output["residuals"] for output in outputs])
-
-        fig_random = self.plot_reconstruction(images, recon_mean, residuals, mode="random")
-        fig_worst = self.plot_reconstruction(images, recon_mean, residuals, mode="worst")
-        if self.logger:
-            self.logger.experiment.add_figure(f"Random Images {self.current_epoch}", fig_random)
-            self.logger.experiment.add_figure(f"Worst Images {self.current_epoch}", fig_worst)
-
-        self.validation_step_outputs.clear()
-
-    def plot_reconstruction(
-        self, images, recon_mean, residuals, n_examples=10, mode="random", width=10, pad=6.0
-    ):
-        # only plot i band if available, otherwise the highest band given.
-        assert images.size(0) >= n_examples
-        assert images.shape[1] == recon_mean.shape[1] == residuals.shape[1] == 1, "1 band only."
-        figsize = (width, width * n_examples / 3)
-        fig, axes = plt.subplots(nrows=n_examples, ncols=3, figsize=figsize)
-
-        if mode == "random":
-            indices = torch.randint(0, len(images), size=(n_examples,))
-        elif mode == "worst":
-            # get indices where absolute residual is the largest.
-            absolute_residual = residuals.abs().sum(axis=(1, 2, 3))
-            indices = absolute_residual.argsort()[-n_examples:]
-        else:
-            raise NotImplementedError(f"Specified mode '{mode}' has not been implemented.")
-
-        # pick standard ranges for residuals
-        vmin_res = residuals[indices].min().item()
-        vmax_res = residuals[indices].max().item()
-
-        for i in range(n_examples):
-            idx = indices[i]
-
-            ax_true = axes[i, 0]
-            ax_recon = axes[i, 1]
-            ax_res = axes[i, 2]
-
-            # only add titles to the first axes.
-            if i == 0:
-                ax_true.set_title("Images $x$", pad=pad)
-                ax_recon.set_title(r"Reconstruction $\tilde{x}$", pad=pad)
-                ax_res.set_title(
-                    r"Residual $\left(x - \tilde{x}\right) / \sqrt{\tilde{x}}$", pad=pad
-                )
-
-            # standarize ranges of true and reconstruction
-            image = images[idx, 0].detach().cpu().numpy()
-            recon = recon_mean[idx, 0].detach().cpu().numpy()
-            residual = residuals[idx, 0].detach().cpu().numpy()
-            vmin = min(image.min().item(), recon.min().item())
-            vmax = max(image.max().item(), recon.max().item())
-
-            # plot images
-            plot_image(fig, ax_true, image, vrange=(vmin, vmax))
-            plot_image(fig, ax_recon, recon, vrange=(vmin, vmax))
-            plot_image(fig, ax_res, residual, vrange=(vmin_res, vmax_res))
-
-        plt.tight_layout()
-
-        return fig
-
-    def test_step(self, batch, batch_idx):
-        """Testing step (pytorch lightning)."""
-        images, background = batch["images"], batch["background"]
-        recon_mean, _ = self.forward(images, background)
-        residuals = (images - recon_mean) / torch.sqrt(images)
-        self.log("max_residual", residuals.abs().max())
 
 
 class CenteredGalaxyEncoder(nn.Module):
@@ -269,3 +135,61 @@ def _conv2d_out_dim(x: int) -> int:
 
 def _conv2d_inv_dim(x: int) -> int:
     return (x - 1) * 3 + 5
+
+
+def plot_reconstruction(
+    images: np.ndarray,
+    recon_mean: np.ndarray,
+    residuals: np.ndarray,
+    n_examples: int = 10,
+    mode: str = "random",
+    width: float = 10,
+    pad: float = 6.0,
+):
+    # only plot i band if available, otherwise the highest band given.
+    assert images.shape[0] >= n_examples
+    assert images.shape[1] == recon_mean.shape[1] == residuals.shape[1] == 1, "1 band only."
+    figsize = (width, width * n_examples / 3)
+    fig, axes = plt.subplots(nrows=n_examples, ncols=3, figsize=figsize)
+
+    if mode == "random":
+        indices = torch.randint(0, len(images), size=(n_examples,))
+    elif mode == "worst":
+        # get indices where absolute residual is the largest.
+        absolute_residual = residuals.abs().sum(axis=(1, 2, 3))
+        indices = absolute_residual.argsort()[-n_examples:]
+    else:
+        raise NotImplementedError(f"Specified mode '{mode}' has not been implemented.")
+
+    # pick standard ranges for residuals
+    vmin_res = residuals[indices].min().item()
+    vmax_res = residuals[indices].max().item()
+
+    for i in range(n_examples):
+        idx = indices[i]
+
+        ax_true = axes[i, 0]
+        ax_recon = axes[i, 1]
+        ax_res = axes[i, 2]
+
+        # only add titles to the first axes.
+        if i == 0:
+            ax_true.set_title("Images $x$", pad=pad)
+            ax_recon.set_title(r"Reconstruction $\tilde{x}$", pad=pad)
+            ax_res.set_title(r"Residual $\left(x - \tilde{x}\right) / \sqrt{\tilde{x}}$", pad=pad)
+
+        # standarize ranges of true and reconstruction
+        image = images[idx, 0].detach().cpu().numpy()
+        recon = recon_mean[idx, 0].detach().cpu().numpy()
+        residual = residuals[idx, 0].detach().cpu().numpy()
+        vmin = min(image.min().item(), recon.min().item())
+        vmax = max(image.max().item(), recon.max().item())
+
+        # plot images
+        plot_image(fig, ax_true, image, vrange=(vmin, vmax))
+        plot_image(fig, ax_recon, recon, vrange=(vmin, vmax))
+        plot_image(fig, ax_res, residual, vrange=(vmin_res, vmax_res))
+
+    plt.tight_layout()
+
+    return fig
