768bf4b6bf8699459969742c400c71801f8e7396
Declan McNamara <declan@umich.edu>
2024-03-12 15:37:21 -0400

    new beta prior, empty redshift scraper

--- case_studies/redshift_estimation/prior.py
+++ case_studies/redshift_estimation/prior.py
@@ -1,10 +1,10 @@
-from torch.distributions import Uniform
+from torch.distributions import Gamma, Uniform
 
 from bliss.simulator.prior import CatalogPrior
 from case_studies.redshift_estimation.catalog import RedshiftTileCatalog
 
 
-class RedshiftPrior(CatalogPrior):
+class RedshiftUniformPrior(CatalogPrior):
     """Prior distribution of objects in an astronomical image.
 
     Inherits from CatalogPrior, adding redshift. Temporarily,
@@ -27,6 +27,60 @@ class RedshiftPrior(CatalogPrior):
     def _sample_redshifts(self):
         latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 1)
         return Uniform(self.redshift_min, self.redshift_max).sample(latent_dims)
+
+    def sample(self) -> RedshiftTileCatalog:
+        """Overrides this method from CatalogPrior to include redshift samples from prior.
+
+        Returns:
+            A dictionary of tensors. Each tensor is a particular per-tile quantity; i.e.
+            the first three dimensions of each tensor are
+            `(batch_size, self.n_tiles_h, self.n_tiles_w)`.
+            The remaining dimensions are variable-specific.
+        """
+        locs = self._sample_locs()
+        galaxy_fluxes, galaxy_params = self._sample_galaxy_prior()
+        star_fluxes = self._sample_star_fluxes()
+
+        n_sources = self._sample_n_sources()
+        source_type = self._sample_source_type()
+        redshifts = self._sample_redshifts()
+
+        catalog_params = {
+            "n_sources": n_sources,
+            "source_type": source_type,
+            "locs": locs,
+            "galaxy_fluxes": galaxy_fluxes,
+            "galaxy_params": galaxy_params,
+            "star_fluxes": star_fluxes,
+            "redshifts": redshifts,
+        }
+
+        return RedshiftTileCatalog(self.tile_slen, catalog_params)
+
+
+class RedshiftBetaPrior(CatalogPrior):
+    """Prior distribution of objects in an astronomical image.
+
+    Inherits from CatalogPrior, adding redshift. Temporarily,
+    redshift is drawn from a tight uniform distribution Unif(0.99,1.01)
+    for validation.
+    """
+
+    def __init__(
+        self,
+        *args,
+        redshift_alpha: float = 0.0,
+        redshift_beta: float = 1.0,
+        **kwargs,
+    ):
+        """Initializes CatalogPrior."""
+        super().__init__(*args, **kwargs)
+        self.redshift_alpha = redshift_alpha
+        self.redshift_beta = redshift_beta
+
+    def _sample_redshifts(self):
+        latent_dims = (self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources, 1)
+        return Gamma(self.redshift_alpha, self.redshift_beta).sample(latent_dims)
 
     def sample(self) -> RedshiftTileCatalog:
         """Overrides this method from CatalogPrior to include redshift samples from prior.
