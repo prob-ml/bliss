0119fa77e267f54dfe1bf1a29c858cce40d50fa7
Ismael Mendoza <imendoza@umich.edu>
2024-03-21 13:53:55 -0400

    we use this throughout

--- bliss/datasets/background.py
+++ bliss/datasets/background.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import torch
 from einops import rearrange
+from torch import Tensor
 
 
 def get_constant_background(value: float, shape: Tuple[int, ...]):
@@ -9,3 +10,9 @@ def get_constant_background(value: float, shape: Tuple[int, ...]):
     bg = torch.tensor(value)
     bg = rearrange(bg, "c -> 1 c 1 1")
     return bg.expand(b, c, h, w)
+
+
+def add_noise_and_background(image: Tensor, background: Tensor) -> Tensor:
+    image_with_background = image + background
+    noise = image_with_background.sqrt() * torch.randn_like(image_with_background)
+    return image_with_background + noise
