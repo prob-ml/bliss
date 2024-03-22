7de71a48d5f649cba7d93b85e9b16564a1725db1
Ismael Mendoza <imendoza@umich.edu>
2024-03-21 20:25:03 -0400

    fix

--- bliss/datasets/background.py
+++ bliss/datasets/background.py
@@ -7,8 +7,8 @@ from torch import Tensor
 
 def get_constant_background(value: float, shape: Tuple[int, ...]):
     b, c, h, w = shape
-    bg = torch.tensor(value)
-    bg = rearrange(bg, "c -> 1 c 1 1")
+    bg = torch.tensor([value])
+    bg = rearrange(bg, "1 -> 1 1 1 1")
     return bg.expand(b, c, h, w)
 
 
