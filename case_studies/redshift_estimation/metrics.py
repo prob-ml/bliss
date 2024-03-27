a6c5f421616f222f5aa68778329c8c655f608cca
Yolanda Zhou <yolandz@deeplearning-01.stat.lsa.umich.edu>
2024-03-27 00:07:01 -0400

    Add MSE for redshift estimation

--- case_studies/redshift_estimation/metrics.py
+++ case_studies/redshift_estimation/metrics.py
@@ -15,41 +15,15 @@ class RedshiftMeanSquaredError(Metric):
             tcat_matches, ecat_matches = matching[i]
             self.total += tcat_matches.size(0)
 
-            true_red = true_cat["redshift"].loc[i][tcat_matches]
-            est_red = est_cat["redshift"].loc[i][ecat_matches]
+            true_red = true_cat["redshifts"][i][tcat_matches]
+            est_red = est_cat["redshifts"][i][ecat_matches]
             red_err = ((true_red - est_red).abs()** 2).sum()
             
-            self.sum_squared_err += red_err
+            self.sum_squared_error += red_err
 
     def compute(self):
-        mse = self.sum_squared_err / self.total
+        mse = self.sum_squared_error / self.total
         return {"Mean squared error": mse.item()}
-
-
-
-class RedshiftCatastrophicErrorRate(Metric):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        self.add_state("catastrophic_errors", default=torch.zeros(1), dist_reduce_fx="sum")
-        self.add_state("total", default=torch.zeros(1), dist_reduce_fx="sum")
-
-    def update(self, true_cat, est_cat, matching):
-        for i in range(true_cat.batch_size):
-            tcat_matches, ecat_matches = matching[i]
-            self.total += tcat_matches.size(0)
-
-            est_miu = est_cat["redshift"].loc[i][ecat_matches]
-            est_sigma = est_cat["redshift"].scale[i][ecat_matches]
-            true_miu = true_cat["redshift"].loc[i][tcat_matches]
-            temp = abs(true_miu - est_miu ) > 5 * est_sigma
-            self.catastrophic_errors += temp.sum()
-        
-            
-    def compute(self):
-        csr = self.catastrophic_errors.float() / self.total
-        return {"Catastrophic error rate": csr.item()}
-    
 
 
 class RedshiftNLL(Metric):
@@ -62,23 +36,50 @@ class RedshiftNLL(Metric):
         self.add_state("negative_loglikelihood", default=torch.zeros(1), dist_reduce_fx="sum")
         self.add_state("total", default=torch.zeros(1), dist_reduce_fx="sum")
 
-    def update(self, true_cat, est_cat, matching):
-        for i in range(true_cat.batch_size):
-            tcat_matches, ecat_matches = matching[i]
-            self.total += tcat_matches.size(0)
-
-            est_miu = est_cat["redshift"].loc[i][ecat_matches]
-            est_sigma = est_cat["redshift"].scale[i][ecat_matches]
-            true_miu = true_cat["redshift"].loc[i][tcat_matches]
+    def update(self, true_cat, marginal_dist):
+        
+        for i in range(true_cat.shape[0]):
+            #tcat_matches, ecat_matches = matching[i]
+            #self.total += tcat_matches.size(0)
+            self.total += true_cat.shape[0]
+            est_miu = marginal_dist.factors["redshift"].loc[i]
+            est_sigma = marginal_dist.factors["redshift"].scale[i]
+            true_miu = true_cat[i, :, :] 
             distribution = torch.distributions.Normal(est_miu, est_sigma)
             nll = -distribution.log_prob(true_miu)
-            self.negative_loglikelihood += sum(nll)
+            self.negative_loglikelihood += nll.sum()
             
     
     def compute(self):
 
-        nll = self.negative_loglikelihood / self.total
+        nll = self.negative_loglikelihood / self.total 
         return {"negative loglikelihood": nll.item()}
+
+class RedshiftCatastrophicErrorRate(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.add_state("catastrophic_errors", default=torch.zeros(1), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.zeros(1), dist_reduce_fx="sum")
+
+    def update(self, true_cat, marginal_dist):
+        for i in range(true_cat.batch_size):
+            tcat_matches, ecat_matches = matching[i]
+            self.total += tcat_matches.size(0)
+
+            est_miu = marginal_dist["redshifts"].loc[i][ecat_matches]
+            est_sigma = marginal_dist["redshifts"].scale[i][ecat_matches]
+            true_miu = true_cat["redshifts"].loc[i][tcat_matches]
+            temp = abs(true_miu - est_miu ) > 5 * est_sigma
+            self.catastrophic_errors += temp.sum()
+        
+            
+    def compute(self):
+        csr = self.catastrophic_errors.float() / self.total
+        return {"Catastrophic error rate": csr.item()}
+    
+
+
     
     
 # preds = torch.tensor([0.9924, 0.9927, 0.9909,  0.9837, 0.9853, 0.9877])
