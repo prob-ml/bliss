256c88d785711f47e915ac1062dc8ff2dfae1ccb
Ismael Mendoza <imendoza@umich.edu>
2024-04-05 12:02:26 -0400

    refactor into simple functions in `training_Functions.py` for training models

---
+++ case_studies/galsim_galaxies/run/run_detection_train.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from bliss.encoders.detection import DetectionEncoder
+from bliss.encoders.layers import ConcatBackgroundTransform
+from case_studies.galsim_galaxies.run.training_functions import (
+    create_dataset,
+    setup_training_objects,
+)
+
+OVERWRITE = True
+N_SAMPLES = 1028 * 20
+SPLIT = N_SAMPLES * 15 // 20
+BATCH_SIZE = 32
+NUM_WORKERS = 0
+N_EPOCHS = 100
+ONLY_BRIGHT = False
+VERSION = "5"  # for dataset
+TRAIN_DS_FILE = f"train_ds_{VERSION}.pt"
+VAL_DS_FILE = f"val_ds_{VERSION}.pt"
+VALIDATE_EVERY_N_EPOCH = 1
+VAL_CHECK_INTERVAL = 32
+
+
+# setup model to train
+input_transform = ConcatBackgroundTransform()
+detection_encoder = DetectionEncoder(input_transform)
+
+
+if OVERWRITE:
+    create_dataset(
+        "../../../data/OneDegSq.fits",
+        "../../../data/stars_med_june2018.fits",
+        N_SAMPLES,
+        SPLIT,
+        TRAIN_DS_FILE,
+        VAL_DS_FILE,
+        only_bright=ONLY_BRIGHT,
+    )
+
+if not OVERWRITE and not Path(TRAIN_DS_FILE).exists():
+    raise IOError
+
+train_dl, val_dl, trainer, logger, ccb = setup_training_objects(  # noqa: WPS236
+    TRAIN_DS_FILE,
+    VAL_DS_FILE,
+    N_SAMPLES,
+    SPLIT,
+    BATCH_SIZE,
+    NUM_WORKERS,
+    N_EPOCHS,
+    VALIDATE_EVERY_N_EPOCH,
+    VAL_CHECK_INTERVAL,
+)
+
+trainer.fit(model=detection_encoder, train_dataloaders=train_dl, val_dataloaders=val_dl)
---
+++ case_studies/galsim_galaxies/run/training_functions.py
@@ -0,0 +1,100 @@
+import pytorch_lightning as L
+import torch
+from astropy.table import Table
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.data import DataLoader
+
+from bliss.datasets.galsim_blends import SavedGalsimBlends, generate_dataset
+from bliss.datasets.lsst import get_default_lsst_psf
+from bliss.datasets.table_utils import column_to_tensor
+
+
+def create_dataset(
+    catsim_file: str,
+    stars_mag_file: str,
+    n_samples: int,
+    train_val_split: int,
+    train_ds_file: str,
+    val_ds_file: str,
+    only_bright=False,
+):
+    print("INFO: Overwriting dataset...")
+    # prepare bigger dataset
+    catsim_table = Table.read(catsim_file)
+    all_star_mags = column_to_tensor(Table.read(stars_mag_file), "i_ab")
+    psf = get_default_lsst_psf()
+
+    if only_bright:
+        bright_mask = catsim_table["i_ab"] < 23
+        new_table = catsim_table[bright_mask]
+        print("INFO: Smaller catalog with only bright sources of length:", len(new_table))
+
+    else:
+        mask = catsim_table["i_ab"] < 27.3
+        new_table = catsim_table[mask]
+        print("INFO: Complete catalog with only i < 27.3 magnitude of length:", len(new_table))
+
+    dataset = generate_dataset(
+        n_samples,
+        new_table,
+        all_star_mags,
+        psf=psf,
+        max_n_sources=15,  # https://www.wolframalpha.com/input?i=Poisson+distribution+with+mean+4
+        slen=40,
+        bp=24,
+        max_shift=0.5,
+    )
+
+    # train, test split
+    train_ds = {p: q[:train_val_split] for p, q in dataset.items()}
+    val_ds = {p: q[train_val_split:] for p, q in dataset.items()}
+
+    # now save  data
+    torch.save(train_ds, train_ds_file)
+    torch.save(val_ds, val_ds_file)
+
+
+def setup_training_objects(
+    train_ds_file: str,
+    val_ds_file: str,
+    n_samples: int,
+    train_val_split: int,
+    batch_size: int,
+    num_workers: int,
+    n_epochs: int,
+    validate_every_n_epoch: int,
+    val_check_interval: float,
+):
+    train_dataset = SavedGalsimBlends(train_ds_file, train_val_split)
+    validation_dataset = SavedGalsimBlends(val_ds_file, n_samples - train_val_split)
+    train_dl = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
+    )
+    val_dl = DataLoader(validation_dataset, batch_size=batch_size, num_workers=num_workers)
+
+    ccb = ModelCheckpoint(
+        filename="epoch={epoch}-val_loss={val/loss:.3f}",
+        save_top_k=5,
+        verbose=True,
+        monitor="val/loss",
+        mode="min",
+        save_on_train_epoch_end=False,
+        auto_insert_metric_name=False,
+    )
+
+    logger = TensorBoardLogger(save_dir="out", name="detection", default_hp_metric=False)
+
+    trainer = L.Trainer(
+        limit_train_batches=1.0,
+        max_epochs=n_epochs,
+        logger=logger,
+        callbacks=[ccb],
+        accelerator="gpu",
+        devices=1,
+        log_every_n_steps=16,
+        check_val_every_n_epoch=validate_every_n_epoch,
+        val_check_interval=val_check_interval,
+    )
+
+    return train_dl, val_dl, trainer, logger, ccb
