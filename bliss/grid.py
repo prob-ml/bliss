a4b8e27140ee848c936fa5070baf18292af50cb9
Ismael Mendoza <imendoza@umich.edu>
2024-04-08 16:23:08 -0400

    combining functions related to grid_sample into a single file

--- bliss/grid.py
+++ bliss/grid.py
@@ -1,5 +1,8 @@
+"""Utility functions for working with `torch.nn.functional.grid_sample`."""
+
 import torch
-from einops import pack
+from einops import pack, unpack
+from torch import Tensor
 
 
 def get_mgrid(slen: int, device: torch.device):
@@ -11,3 +14,10 @@ def get_mgrid(slen: int, device: torch.device):
     # normalize to (-1, 1) and scale slightly because of the way f.grid_sample
     # parameterizes the edges: (0, 0) is center of edge pixel
     return (mgrid_not_normalized / offset).float() * (slen - 1) / slen
+
+
+def swap_locs_columns(locs: Tensor) -> Tensor:
+    """Swap the columns of locs to invert 'x' and 'y' with einops!"""
+    assert locs.ndim == 2 and locs.shape[1] == 2
+    x, y = unpack(locs, [[1], [1]], "b *")
+    return pack([y, x], "b *")[0]
