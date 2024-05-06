06e3e09249af1762603132a4395afa311c5b8ca6
Aakash Patel <aakashdp@umich.edu>
2024-05-05 22:00:29 -0400

    Removed deconvolution (#970)
    fix style
    
    fix test_simulate

--- case_studies/dc2/train_dc2.sh
+++ case_studies/dc2/train_dc2.sh
@@ -10,15 +10,13 @@
 bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=base_model \
-    encoder.image_normalizer.use_deconv_channel=False \
     encoder.image_normalizer.concat_psf_params=False \
     encoder.do_data_augmentation=False
 
-# train both PSF params and deconvolution channel model
+# train PSF params model
 bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
-    train.trainer.logger.version=dc2_psf_deconv \
-    encoder.image_normalizer.use_deconv_channel=True \
+    train.trainer.logger.version=dc2_psf \
     encoder.image_normalizer.concat_psf_params=True \
     encoder.do_data_augmentation=False
 
@@ -26,14 +24,12 @@ bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
 bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
     train.trainer.logger.version=dc2_data_aug \
-    encoder.image_normalizer.use_deconv_channel=False \
     encoder.image_normalizer.concat_psf_params=False \
     encoder.do_data_augmentation=True
 
-# train with data augmentation and psf, deconvolution
+# train with data augmentation and psf
 bliss -cp ~/bliss/case_studies/dc2 -cn config mode=train \
     train.trainer.logger.name=DC2_MODELS \
-    train.trainer.logger.version=dc2_psf_deconv_da \
-    encoder.image_normalizer.use_deconv_channel=True \
+    train.trainer.logger.version=dc2_psf_da \
     encoder.image_normalizer.concat_psf_params=True \
     encoder.do_data_augmentation=True
