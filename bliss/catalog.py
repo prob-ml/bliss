Update catalog.py and add asinh normalization (#985) * fix the problem that to_tile_catalog throws error during data augmentation
* use torch.allclose to check whether catalogs are equal rather than using torch.equal

* add asinh normalization

* modify DC2 class to make sure the loaded background images are positive

* improve the training config and its command line entry

---------

bliss/catalog.py
@@ -61,10 +61,13 @@ class TileCatalog(UserDict):
         self.batch_size, self.n_tiles_h, self.n_tiles_w, self.max_sources = self.locs.shape[:-1]
         super().__init__(**d)
 
+    def _test_tensor_all_close(self, left, right):
+        return torch.allclose(left, right, atol=1e-4)
+
     def _test_data_equal(self, other_data):
         is_equal = True
         for k, v in self.data.items():
-            cur_test_equal = torch.equal(other_data[k], v)
+            cur_test_equal = self._test_tensor_all_close(other_data[k], v)
             if not cur_test_equal:
                 print(f"{k} are different")
             is_equal &= cur_test_equal
@@ -81,10 +84,10 @@ class TileCatalog(UserDict):
                     left: {self.tile_slen}; right: {other.tile_slen}
                   """)
 
-        if not torch.equal(self.locs, other.locs):
+        if not self._test_tensor_all_close(self.locs, other.locs):
             print("locs are different")
 
-        if not torch.equal(self.n_sources, other.n_sources):
+        if not self._test_tensor_all_close(self.n_sources, other.n_sources):
             print("n_sources are different")
 
         if self.batch_size != other.batch_size:
@@ -114,8 +117,8 @@ class TileCatalog(UserDict):
         self._test_data_equal(other.data)
 
         return self.tile_slen == other.tile_slen and \
-               torch.equal(self.locs, other.locs) and \
-               torch.equal(self.n_sources, other.n_sources) and \
+               self._test_tensor_all_close(self.locs, other.locs) and \
+               self._test_tensor_all_close(self.n_sources, other.n_sources) and \
                self.batch_size == other.batch_size and \
                self.n_tiles_h == other.n_tiles_h and \
                self.n_tiles_w == other.n_tiles_w and \
@@ -481,10 +484,13 @@ class FullCatalog(UserDict):
         assert self.n_sources.shape == (self.batch_size,)
         super().__init__(**d)
 
+    def _test_tensor_all_close(self, left, right):
+        return torch.allclose(left, right, atol=1e-4)
+
     def _test_data_equal(self, other_data):
         is_equal = True
         for k, v in self.data.items():
-            cur_test_equal = torch.equal(other_data[k], v)
+            cur_test_equal = self._test_tensor_all_close(other_data[k], v)
             if not cur_test_equal:
                 print(f"{k} are different")
             is_equal &= cur_test_equal
@@ -507,10 +513,10 @@ class FullCatalog(UserDict):
                     left: {self.width}; right: {other.width}
                   """)
 
-        if not torch.equal(self.plocs, other.plocs):
+        if not self._test_tensor_all_close(self.plocs, other.plocs):
             print("plocs are different")
 
-        if not torch.equal(self.n_sources, other.n_sources):
+        if not self._test_tensor_all_close(self.n_sources, other.n_sources):
             print("n_sources are different")
 
         if self.batch_size != other.batch_size:
@@ -529,8 +535,8 @@ class FullCatalog(UserDict):
 
         return self.height == other.height and \
                self.width == other.width and \
-               torch.equal(self.plocs, other.plocs) and \
-               torch.equal(self.n_sources, other.n_sources) and \
+               self._test_tensor_all_close(self.plocs, other.plocs) and \
+               self._test_tensor_all_close(self.n_sources, other.n_sources) and \
                self.batch_size == other.batch_size and \
                self.max_sources == other.max_sources and \
                self._test_data_equal(other.data)
@@ -648,7 +654,7 @@ class FullCatalog(UserDict):
         return type(self)(self.height, self.width, d_new)
 
     def _get_tile_to_source_mapping(self, mapping_matrix: Tensor) -> Tensor:
-        return (mapping_matrix > 0).nonzero()
+        return mapping_matrix.nonzero()
 
     def to_tile_catalog(
         self,
@@ -706,10 +712,13 @@ class FullCatalog(UserDict):
                 filter_sources = x_mask.sum()
                 source_tile_coords = source_tile_coords[x_mask]
 
+            if filter_sources == 0:
+                continue
+
             source_indices = source_tile_coords[:, 0] * n_tiles_w + source_tile_coords[:, 1].unsqueeze(0)
             tile_indices = torch.arange(n_tiles_h * n_tiles_w, device=self.device).unsqueeze(1)
 
-            tile_to_source_mapping = self._get_tile_to_source_mapping((source_indices == tile_indices).long())
+            tile_to_source_mapping = self._get_tile_to_source_mapping(source_indices == tile_indices)
             tile_source_count: Tuple[Tensor, Tensor] = tile_to_source_mapping[:, 0].unique(sorted=True, return_counts=True) # first element is tile index; second element is source count
             if tile_source_count[1].max() > max_sources_per_tile:
                 if not ignore_extra_sources:
@@ -740,7 +749,8 @@ class FullCatalog(UserDict):
         tile_params.update({"n_sources": tile_n_sources})
         return TileCatalog(tile_slen, tile_params)
 
-    def old_to_tile_catalog(
+    # from commit 48b8288
+    def previous_to_tile_catalog(
         self,
         tile_slen: int,
         max_sources_per_tile: int,
@@ -840,6 +850,62 @@ class FullCatalog(UserDict):
             # modify tile location
             tile_params["locs"][ii] = (tile_params["locs"][ii] % tile_slen) / tile_slen
         tile_params.update({"n_sources": tile_n_sources})
+        return TileCatalog(tile_slen, tile_params)
+
+    # from commit b65e24c
+    def ori_to_tile_params(
+        self, tile_slen: int, max_sources_per_tile: int, ignore_extra_sources=False
+    ) -> TileCatalog:
+        """Returns the TileCatalog corresponding to this FullCatalog.
+
+        Args:
+            tile_slen: The side length of the tiles.
+            max_sources_per_tile: The maximum number of sources in one tile.
+            ignore_extra_sources: If False (default), raises an error if the number of sources
+                in one tile exceeds the `max_sources_per_tile`. If True, only adds the tile
+                parameters of the first `max_sources_per_tile` sources to the new TileCatalog.
+
+        Returns:
+            TileCatalog correspond to the each source in the FullCatalog.
+
+        Raises:
+            ValueError: If the number of sources in one tile exceeds `max_sources_per_tile` and
+                `ignore_extra_sources` is False.
+        """
+        tile_coords = torch.div(self.plocs, tile_slen, rounding_mode="trunc").to(torch.int)
+        n_tiles_h = math.ceil(self.height / tile_slen)
+        n_tiles_w = math.ceil(self.width / tile_slen)
+
+        # prepare tiled tensors
+        tile_cat_shape = (self.batch_size, n_tiles_h, n_tiles_w, max_sources_per_tile)
+        tile_locs = torch.zeros((*tile_cat_shape, 2), device=self.device)
+        tile_n_sources = torch.zeros(tile_cat_shape[:3], dtype=torch.int64, device=self.device)
+        tile_params: Dict[str, Tensor] = {}
+        for k, v in self.items():
+            dtype = torch.int64 if k == "objid" else torch.float
+            size = (self.batch_size, n_tiles_h, n_tiles_w, max_sources_per_tile, v.shape[-1])
+            tile_params[k] = torch.zeros(size, dtype=dtype, device=self.device)
+
+        for ii in range(self.batch_size):
+            n_sources = int(self.n_sources[ii].item())
+            for idx, coords in enumerate(tile_coords[ii][:n_sources]):
+                if coords[0] >= tile_n_sources.shape[1] or coords[1] >= tile_n_sources.shape[2]:
+                    continue
+                # ignore sources outside of the image (usually caused by data augmentation - shift)
+
+                source_idx = tile_n_sources[ii, coords[0], coords[1]].item()
+                if source_idx >= max_sources_per_tile:
+                    if not ignore_extra_sources:
+                        raise ValueError(  # noqa: WPS220
+                            "# of sources per tile exceeds `max_sources_per_tile`."
+                        )
+                    continue  # ignore extra sources in this tile.
+                tile_loc = (self.plocs[ii, idx] - coords * tile_slen) / tile_slen
+                tile_locs[ii, coords[0], coords[1], source_idx] = tile_loc
+                for k, v in tile_params.items():
+                    v[ii, coords[0], coords[1], source_idx] = self[k][ii, idx]
+                tile_n_sources[ii, coords[0], coords[1]] = source_idx + 1
+        tile_params.update({"locs": tile_locs, "n_sources": tile_n_sources})
         return TileCatalog(tile_slen, tile_params)
 
     def to_astropy_table(self, encoder_survey_bands: Tuple[str]) -> Table:

bliss/encoder/image_normalizer.py
@@ -1,4 +1,5 @@
 import warnings
+from typing import Dict
 
 import torch
 
@@ -13,6 +14,7 @@ class ImageNormalizer(torch.nn.Module):
         log_transform_stdevs: list,
         use_clahe: bool,
         clahe_min_stdev: float,
+        asinh_params: Dict[str, float]
     ):
         """Initializes DetectionEncoder.
 
@@ -34,9 +36,10 @@ class ImageNormalizer(torch.nn.Module):
         self.log_transform_stdevs = log_transform_stdevs
         self.use_clahe = use_clahe
         self.clahe_min_stdev = clahe_min_stdev
+        self.asinh_params = asinh_params
 
-        if not (log_transform_stdevs or use_clahe):
-            warnings.warn("Either log transform or clahe should be enabled.")
+        if not (log_transform_stdevs or use_clahe or asinh_params):
+            warnings.warn("Normalization should be enabled (you could use log/clahe/asinh).")
 
     def num_channels_per_band(self):
         """Determine number of input channels for model based on desired input transforms."""
@@ -49,6 +52,8 @@ class ImageNormalizer(torch.nn.Module):
             nch += len(self.log_transform_stdevs)
         if self.use_clahe:
             nch += 1
+        if self.asinh_params:
+            nch += len(self.asinh_params["thresholds"])
         return nch
 
     def get_input_tensor(self, batch):
@@ -65,6 +70,9 @@ class ImageNormalizer(torch.nn.Module):
         assert batch["images"].size(3) % 16 == 0, "image dims must be multiples of 16"
 
         if self.log_transform_stdevs:
+            assert batch["background"].min() > 1e-6, "background must be positive"
+
+        if self.asinh_params:
             assert batch["background"].min() > 1e-6, "background must be positive"
 
         input_bands = batch["images"].shape[1]
@@ -101,6 +109,10 @@ class ImageNormalizer(torch.nn.Module):
             renormalized_img = self.clahe(raw_images, self.clahe_min_stdev)
             inputs.append(renormalized_img)
             inputs[0] = self.clahe(backgrounds, self.clahe_min_stdev)
+
+        if self.asinh_params:
+            for threshold in self.asinh_params["thresholds"]:
+                inputs.append(torch.asinh(((raw_images - backgrounds) / backgrounds.sqrt() - threshold) * self.asinh_params["scale"]))
 
         return torch.cat(inputs, dim=2)
 

bliss/surveys/dc2_new.py
@@ -5,6 +5,7 @@ import multiprocessing
 import pathlib
 import pickle
 import random
+import time
 
 import numpy as np
 import pandas as pd
@@ -43,12 +44,28 @@ def generate_split_file(image_index, object_attributes):
     full_cat, psf_params = Dc2FullCatalog.from_file(
         object_attributes["cat_path"], wcs, height, width, object_attributes["bands"]
     )
+    current_to_tile_start_time = time.time()
     tile_cat = full_cat.to_tile_catalog(4, 5).get_brightest_sources_per_tile()
+    current_to_tile_end_time = time.time()
 
     # for debug
-    # tile_cat_old = full_cat.old_to_tile_catalog(4, 5).get_brightest_sources_per_tile()
+    # previous_to_tile_start_time = time.time()
+    # tile_cat_previous = full_cat.previous_to_tile_catalog(4, 5).get_brightest_sources_per_tile()
+    # previous_to_tile_end_time = time.time()
+
+    # ori_to_tile_start_time = time.time()
+    # tile_cat_ori = full_cat.ori_to_tile_params(4, 5).get_brightest_sources_per_tile()
+    # ori_to_tile_end_time = time.time()
+
     # full_tile_cat = tile_cat.to_full_catalog()
-    # full_tile_cat_old = tile_cat_old.to_full_catalog()
+    # full_tile_cat_previous = tile_cat_previous.to_full_catalog()
+    # full_tile_cat_ori = tile_cat_ori.to_full_catalog()
+    # assert tile_cat == tile_cat_previous, "tile_cat are different between current implementation and previous one"
+    # assert tile_cat == tile_cat_ori, "tile_cat are different between current implementation and original one"
+    # assert tile_cat_previous == tile_cat_ori, "tile_cat are different between previous implementation and original one"
+    # assert full_tile_cat == full_tile_cat_previous, "full_cat are different between current implementation and previous one"
+    # assert full_tile_cat == full_tile_cat_ori, "full_cat are different between current implementation and original one"
+    # assert full_tile_cat_ori == full_tile_cat_previous, "full_cat are different between previous implementation and original one"
 
     tile_dict = tile_cat.to_dict()
 
@@ -131,6 +148,7 @@ class DC2(Survey):
     BANDS = ("g", "i", "r", "u", "y", "z")
 
     def __init__(self, data_dir, cat_path, batch_size, n_split, image_lim, num_workers,
+                 split_result_folder,
                  for_plotting=False):
         super().__init__()
         self.data_dir = data_dir
@@ -146,7 +164,7 @@ class DC2(Survey):
         self.image_lim = image_lim
         self.num_workers = num_workers
         self.for_plotting = for_plotting
-        self.split_file_path = pathlib.Path(data_dir) / "split_results/"
+        self.split_file_path = pathlib.Path(data_dir) / split_result_folder
 
         self.image_files = None
         self.bg_files = None
@@ -362,6 +380,10 @@ def read_frame_for_band(image_files, bg_files, n, n_bands, image_lim):
 
         image = torch.nan_to_num(torch.from_numpy(image_data)[: image_lim[0], : image_lim[1]])
         bg = torch.from_numpy(bg_data.astype(np.float32)).expand(image_lim[0], image_lim[1])
+
+        # to make sure the background are positive
+        bg = bg.clone()
+        bg[bg <= 1e-6] = 1e-5
 
         image += bg
         image_list.append(image)