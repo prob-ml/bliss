bf1e8ce7a75fd96764c9917fd2891f93f0d4e128
Tim White <twhit@umich.edu>
2024-05-14 20:43:40 -0400

    Presave weak lensing correlation function to remove dependency on pyccl in prior

--- case_studies/weak_lensing/lensing_prior.py
+++ case_studies/weak_lensing/lensing_prior.py
@@ -1,15 +1,13 @@
+import os
+
 import galsim
 import numpy as np
-
-try:
-    import pyccl as ccl
-except ModuleNotFoundError as err:
-    raise ModuleNotFoundError("Please install pyccl using pip: pip install pyccl") from err
 import torch
 from torch.distributions import Beta, Normal, Uniform
 
 from bliss.catalog import TileCatalog
 from bliss.simulator.prior import CatalogPrior
+from case_studies.weak_lensing import generate_angular_cl
 
 
 class LensingPrior(CatalogPrior):
@@ -28,10 +26,6 @@ class LensingPrior(CatalogPrior):
         super().__init__(*args, **kwargs)
         # validate that tiles height and width is the same, used as ngrid later
         self.arcsec_per_pixel = arcsec_per_pixel
-        try:
-            self.cosmology = ccl.cosmology.CosmologyVanillaLCDM()
-        except NameError as err:
-            raise NameError("Please install pyccl using pip: pip install pyccl") from err
 
         self.sample_method = sample_method
         self.shear_mean = shear_mean
@@ -40,39 +34,26 @@ class LensingPrior(CatalogPrior):
         self.convergence_mean = convergence_mean
         self.convergence_std = convergence_std
 
-    def sample_shear_and_convergence_cosmology(self):
-        ngrid = self.n_tiles_w
-        # ngrid is tiles, convert tiles to pixels, then to arcsecs, and then degrees
-        grid_size = (ngrid * self.tile_slen * self.arcsec_per_pixel) / 3600
+        if self.sample_method == "cosmology":
+            self.grid_size = (self.n_tiles_w * self.tile_slen * self.arcsec_per_pixel) / 3600
 
-        # redshift taken from https://github.com/LSSTDESC/CCLX/blob/master/CellsCorrelations.ipynb
-        z = np.linspace(0.0, 3.0, 512)
-        i_lim = 26.0  # Limiting i-band magnitude
-        z0 = 0.0417 * i_lim - 0.744
+            if os.path.exists("angular_cl.npy"):
+                angular_cl = np.load("angular_cl.npy")
+            else:
+                generate_angular_cl.main()
+                angular_cl = np.load("angular_cl.npy")
 
-        ngal = 46.0 * 100.31 * (i_lim - 25.0)  # Normalisation, galaxies/arcmin^2
-        pz = 1.0 / (2.0 * z0) * (z / z0) ** 2.0 * np.exp(-z / z0)  # Redshift distribution, p(z)
-        dndz = ngal * pz  # Number density distribution
-        try:
-            lensing_tracer = ccl.WeakLensingTracer(self.cosmology, dndz=(z, dndz))
-        except NameError as err:
-            raise NameError("Please install pyccl using pip: pip install pyccl") from err
+            angular_cl_table = galsim.LookupTable(x=angular_cl[0], f=angular_cl[1])
+            self.power_spectrum = galsim.PowerSpectrum(angular_cl_table, units=galsim.degrees)
 
-        # range of values required by buildGrid
-        ell = np.arange(1, 100000)
-
+    def _sample_shear_and_convergence(self):
         shear_map = torch.zeros((self.batch_size, self.n_tiles_h, self.n_tiles_w, 2))
         convergence_map = torch.zeros((self.batch_size, self.n_tiles_h, self.n_tiles_w, 1))
+
         for i in range(self.batch_size):
-            try:
-                angular_cl = ccl.angular_cl(self.cosmology, lensing_tracer, lensing_tracer, ell)
-            except NameError as err:
-                raise NameError("Please install pyccl using pip: pip install pyccl") from err
-            table = galsim.LookupTable(x=ell, f=angular_cl)
-            my_ps = galsim.PowerSpectrum(table, units=galsim.degrees)
-            g1, g2, kappa = my_ps.buildGrid(
-                grid_spacing=grid_size / ngrid,
-                ngrid=ngrid,
+            g1, g2, kappa = self.power_spectrum.buildGrid(
+                grid_spacing=self.grid_size / self.n_tiles_w,
+                ngrid=self.n_tiles_w,
                 get_convergence=True,
                 units=galsim.degrees,
             )
@@ -161,7 +142,7 @@ class LensingPrior(CatalogPrior):
             shear = self._sample_shear()
             convergence = self._sample_convergence()
         elif self.sample_method == "cosmology":
-            shear, convergence = self.sample_shear_and_convergence_cosmology()
+            shear, convergence = self._sample_shear_and_convergence()
 
         locs = self._sample_locs()
         galaxy_fluxes, galaxy_params = self._sample_galaxy_prior()
