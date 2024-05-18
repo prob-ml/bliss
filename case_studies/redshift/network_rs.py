c4394815a799d524f4ee38e5480542131569240f
Qiaozhi Huang <qiaozhih@deeplearning-01.stat.lsa.umich.edu>
2024-05-18 16:05:41 -0400

    lightning

--- case_studies/redshift/network_rs.py
+++ case_studies/redshift/network_rs.py
@@ -1,34 +1,99 @@
 # Author: Qiaozhi Huang
 # Class for network to predict redshift prediction
 import numpy as np
+import pytorch_lightning as pl
 import torch
+import torch.nn as nn
 from tqdm import tqdm
-    
-class Regressor(torch.nn.Module):
+
+
+class Regressor(nn.Module):
     def __init__(self,
         in_dim,                             # Number of input features.
         hidden_dim,                         # Number of input features.
         out_dim,                            # Number of output features.
-        device,
+        dropout_rate=0.5,
     ):
         super().__init__()
         self.in_dim = in_dim
         self.hidden_dim = hidden_dim
         self.out_dim = out_dim
-        self.device = device
-        # self.loss_fcn = loss_fcn
 
-        self.net = torch.nn.Sequential(
-            torch.nn.Linear(in_dim, hidden_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden_dim, hidden_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden_dim, hidden_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden_dim, out_dim),
+        self.net = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            # nn.BatchNorm1d(hidden_dim),
+            # nn.LayerNorm(hidden_dim),
+            nn.ReLU(),
+            # nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, hidden_dim),
+            # nn.BatchNorm1d(hidden_dim),
+            # nn.LayerNorm(hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, hidden_dim),
+            # nn.BatchNorm1d(hidden_dim),
+            # nn.LayerNorm(hidden_dim),
+            nn.ReLU(),
+            # nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, hidden_dim),
+            # nn.BatchNorm1d(hidden_dim),
+            # nn.LayerNorm(hidden_dim),
+            nn.ReLU(),
+            # nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, out_dim),
         )
 
     def forward(self, x):
         return self.net(x)
-    
-        
+
+
+class LitRegressor(pl.LightningModule):
+    def __init__(self,
+        in_dim,                             # Number of input features.
+        hidden_dim,                         # Number of input features.
+        out_dim,                            # Number of output features.
+        dropout_rate,
+        learning_rate,
+        loss_fcn,
+    ):
+        super().__init__()
+        self.model = Regressor(in_dim, hidden_dim, out_dim, dropout_rate)
+        self.learning_rate = learning_rate
+        self.loss_fcn = loss_fcn
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(-1, x.shape[-1])
+        y = y.view(-1, y.shape[-1])
+        y_hat = self.model(x)
+        if self.model.out_dim != 1:
+            y = y.long()
+        loss = self.loss_fcn(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(-1, x.shape[-1])
+        y = y.view(-1, y.shape[-1])
+        y_hat = self.model(x)
+        if self.model.out_dim != 1:
+            y = y.long()
+        loss = self.loss_fcn(y_hat, y)
+        self.log('val_loss', loss, prog_bar=True, sync_dist=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(-1, x.shape[-1])
+        y = y.view(-1, y.shape[-1])
+        y_hat = self.model(x)
+        if self.model.out_dim != 1:
+            y = y.long()
+        loss = self.loss_fcn(y_hat, y)
+        self.log('test_loss', loss, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
--- case_studies/redshift/train_rs.py
+++ case_studies/redshift/train_rs.py
@@ -1,10 +1,10 @@
 # Author: Qiaozhi Huang
 # train to predict redshift using network
-# python bliss/encoder/train_rs.py --resume-model=/home/qiaozhih/bliss/case_studies/redshift/training_runs/00003-run/010300_model.pt --resume-opt=/home/qiaozhih/bliss/case_studies/redshift/training_runs/00003-run/010300_opt.pt
+# python case_studies/redshift/train_rs.py --resume-model=/home/qiaozhih/bliss/case_studies/redshift/training_runs/00017-runreg/000000_model.pt --resume-opt=/home/qiaozhih/bliss/case_studies/redshift/training_runs/00017-runreg/000000_opt.pt --nick=reg
 import pandas as pd
 import numpy as np
 import torch
-from network_rs import Regressor
+from network_rs import Regressor, LitRegressor
 from torch.utils.data import TensorDataset, DataLoader
 from tqdm import tqdm
 import os
@@ -12,56 +12,78 @@ import re
 import matplotlib.pyplot as plt
 import json
 import click
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn as nn
 
 @click.command()
 # Optinal features
 @click.option('--resume-model', help='Resume from given network pt', metavar='[PATH|URL]',  type=str)
 @click.option('--resume-opt',   help='Resume from given optimizer pt', metavar='[PATH|URL]',  type=str)
+@click.option('--nick',   help='Nickname for remember', type=str)
 
-def main(resume_model, resume_opt):
+def main(resume_model, resume_opt, nick):
     ###### Prepare Training set
     print("start reading dataset!")
     dataset_name = 'desc_dc2_run2.2i_dr6_truth_nona_train'
+    # dataset_name = 'desc_dc2_run2.2i_dr6_truth_nona_train_small'
     # dataset_name = 'test'
     path = f'/home/qiaozhih/bliss/data/redshift/dc2/{dataset_name}.pkl'
     photo_z = pd.read_pickle(path)
     print("finish reading dataset!")
-    device = 'cuda'
-    batch_size = 2048
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = 512
     seed = 42
+    num_bins = 1
+    group_size = 128
     
     print("start tensor dataset preparation!")
     x = photo_z.values[:,:-1].astype(float)
     y = photo_z.values[:, -1].astype(float)
-    x_train = np.array(x)
-    y_train = np.array(y)
-    tensor_x = torch.Tensor(x_train)
-    tensor_y = torch.Tensor(y_train)
+    n_samples, n_features_x = x.shape
+    n_features_y = 1
+    n_samples = n_samples // group_size * group_size
+
+    x_train = np.array(x[:n_samples])
+    y_train = np.array(y[:n_samples])
+    # y_train = bin_target(y_train, num_bins) # bin
+    tensor_x = torch.Tensor(x_train).view(-1, group_size, n_features_x)
+    tensor_y = torch.Tensor(y_train).view(-1, group_size, n_features_y)
+    # tensor_x = torch.Tensor(x_train)
+    # tensor_y = torch.Tensor(y_train)
     custom_dataset = TensorDataset(tensor_x, tensor_y)     
-    dataloader = DataLoader(custom_dataset, batch_size=batch_size)
+    dataloader = DataLoader(custom_dataset, batch_size=batch_size, num_workers=4, pin_memory=True)
     print("finish tensor dataset preparation!")
     
     
     ###### Construct Network
     options = {
-        'hidden_dim': 256,
-        'out_dim': 1,
-        'n_epochs': 100001,
+        'hidden_dim': 512,
+        'out_dim': num_bins,
+        'n_epochs': 50001,
         'outdir': '/home/qiaozhih/bliss/case_studies/redshift/training_runs/',
-        'snap': 100,                                        # how many epoches to save one model once
+        'snap': 1,                                        # how many epoches to save one model once
         'loss_fcn': torch.nn.MSELoss(),                     # loss func
+        # 'loss_fcn': torch.nn.CrossEntropyLoss(),                     # loss func
+        'dropout_rate': 0.5,
+        'learning_rate': 1e-3,
+        'group_size': group_size,
     }
     in_dim = x.shape[1]
     
-    reg = Regressor(in_dim, options['hidden_dim'], options['out_dim'], device)
+    reg = Regressor(in_dim, options['hidden_dim'], options['out_dim'], options['dropout_rate'])
+    # reg = LitRegressor(in_dim, options['hidden_dim'], options['out_dim'], options['dropout_rate'], options['learning_rate'], options['loss_fcn'])
+    optimizer = torch.optim.Adam(reg.parameters(), lr=options['learning_rate'])
     reg = reg.to(device)
-    optimizer = torch.optim.Adam(reg.parameters(), lr=1e-3)
 
     if resume_model is not None and resume_opt is not None:
         print(resume_model)
         reg, optimizer = load_model(reg, optimizer, resume_model, resume_opt)
+        print('resume successfully!')
     
-    
+    reg = nn.DataParallel(reg)
+    reg = reg.to(device)
     ###### Start Train
     # Pick output directory.
     outdir = options['outdir']
@@ -72,41 +94,66 @@ def main(resume_model, resume_opt):
     prev_run_ids = [re.match(r'^\d+', x) for x in prev_run_dirs]
     prev_run_ids = [int(x.group()) for x in prev_run_ids if x is not None]
     cur_run_id = max(prev_run_ids, default=-1) + 1
-    run_dir = os.path.join(outdir, f'{cur_run_id:05d}-run')
+    nickname = ''
+    if nick != None:
+        nickname = nick
+    run_dir = os.path.join(outdir, f'{cur_run_id:05d}-run{nickname}')
     os.makedirs(run_dir)
     print("created running directory!")
+    writer = SummaryWriter(log_dir=run_dir)
+    logger = TensorBoardLogger(save_dir=run_dir, name='tensorboard_logs')
 
     # train
     print("start training")
-    train(reg, optimizer, dataloader, options, run_dir)
+    train(reg, optimizer, dataloader, options, run_dir, writer)
+
+    # trainer = pl.Trainer(max_epochs=options['n_epochs'], default_root_dir=run_dir, logger=logger)
+    # trainer.fit(model=reg, train_dataloaders=dataloader)
+    writer.close()
     print('finish training!')
 
-def train_one_epoch(model, optimizer, dataloader, loss_fcn = torch.nn.MSELoss(), device='cuda'):
+def train_one_epoch(model, optimizer, dataloader, options, device='cuda'):
     losses = []
-    for idx, (x, y) in tqdm(enumerate(dataloader), unit='batch'):
+    loss_fcn = options['loss_fcn']
+    total_batches = len(dataloader)
+    for idx, (x, y) in tqdm(enumerate(dataloader), total=total_batches, unit='batch'):
+    # for idx, (x, y) in enumerate(dataloader):
+        x = x.view(-1, x.shape[-1])
+        y = y.view(-1, y.shape[-1])
+        # print(f'x shape:{x.shape}')
+        # print(f'y shape:{y.shape}')
         x = x.to(device)
-        y = y.to(device)
+        if options['out_dim'] != 1:
+            y = y.long().to(device)
+        else:
+            y = y.to(device)
+        N, _ = x.shape
         optimizer.zero_grad()
-        loss = loss_fcn(model(x).view(-1), y)
+        if options['out_dim'] != 1:
+            loss = loss_fcn(model(x).view(N, -1), y.view(N))
+        else:
+            loss = loss_fcn(model(x).view(-1), y.view(N))
         loss.backward()
         optimizer.step()
         losses.append(loss.item())
             
-        return torch.tensor(losses).mean()
+    return torch.tensor(losses).mean()
     
-def train(model, optimizer, dataloader, options, run_dir='.'):
+def train(model, optimizer, dataloader, options, run_dir='.', writer=SummaryWriter()):
     n_epochs = options['n_epochs']
     stats_jsonal = None
     stats_jsonl = open(os.path.join(run_dir, 'stats.jsonl'), 'wt')
     fields = []
     for i in range(n_epochs):
-        this_epoch_average_loss = train_one_epoch(model, optimizer, dataloader, loss_fcn=options['loss_fcn'])
-        print('Epoch {}: Avg. Loss {}'.format(i, this_epoch_average_loss))
+        this_epoch_average_loss = train_one_epoch(model, optimizer, dataloader, options)
+        writer.add_scalar("Loss/train", this_epoch_average_loss, i)
+        writer.flush()
 
-        fields = ['Epoch {}: Avg. Loss {}'.format(i, this_epoch_average_loss)]
-        stats_jsonl.write(json.dumps(fields) + '\n')
-        stats_jsonl.flush()
         if i % options['snap'] == 0:
+            # print('Epoch {}: Avg. Loss {}'.format(i, this_epoch_average_loss))
+            fields = ['Epoch {}: Avg. Loss {}'.format(i, this_epoch_average_loss)]
+            stats_jsonl.write(json.dumps(fields) + '\n')
+            stats_jsonl.flush()
             path = os.path.join(run_dir, f'{i:06d}')
             save_checkpoint(model, optimizer, path)
     
@@ -125,6 +172,18 @@ def load_model(model, optimizer, model_path, optimizer_path):
     optimizer.load_state_dict(torch.load(optimizer_path))
     model.eval()
     return model, optimizer
+
+def bin_target(y, num_bins=30):
+    """
+    Params:
+    y: ndarray
+    ---
+    Returns:
+    ndarry representing the respective bin(indices) for each value of y
+    """
+    bin_edges = np.linspace(y.min(), y.max(), num_bins + 1)
+    y_binned = pd.cut(y, bins=bin_edges, labels=False, include_lowest=True)
+    return y_binned
 
 if __name__ == "__main__":
     main()
