bf560501d2de466f0d2795a47cbbc00ef6c12063
gapatron <gapatron@umich.edu>
2024-05-22 01:48:25 -0400

    Define variational distribution for galaxy clusters. Temporarily, only defined for galxy membership to cluster. Initial commit.

---
+++ case_studies/galaxy_clustering/variational_dist.py
@@ -0,0 +1,52 @@
+from bliss.catalog import TileCatalog
+from bliss.encoder.variational_dist import VariationalDistSpec, VariationalDist
+from bliss.encoder.unconstrained_dists import UnconstrainedBernoulli
+
+
+
+
+class GalaxyClusterVariationlDistSpec(VariationalDistSpec):
+    def __init__(self, survey_bands, tile_slen):
+        super().__init__(survey_bands, tile_slen)
+
+        self.factor_specs["member"] = UnconstrainedBernoulli()
+
+    def make_dist(self, x_cat):
+        factors = self._parse_factors(x_cat)
+        return GalaxyClusterVariationalDist(factors, survey_bands=self.survey_bands, tile_slen=self.tile_slen)
+    
+
+
+
+class GalaxyClusterVariationalDist(VariationalDist):
+    def __init__(self, factors, survey_bands, tile_slen):
+        super().__init__(factors, survey_bands, tile_slen)
+
+    def sample(self, use_mode=False) -> TileCatalog:
+        """Sample the variational distribution.
+
+        Args:
+            use_mode: whether to use the mode of the distribution instead of random sampling
+
+        Returns:
+            TileCatalog: Sampled catalog
+        """
+        q = self.factors
+
+        est_cat = {}
+        est_cat["MEM"] = q["mem_prob"].mode if use_mode else q["mem_prob"].sample()
+
+        return TileCatalog(self.tile_slen, est_cat)
+
+    def compute_nll(self, true_tile_cat: TileCatalog):
+        q = self.factors
+
+        # We just need counter loss for now
+        counter_loss = -q["mem_prob"].log_prob(true_tile_cat["MEM"])
+        loss = counter_loss
+
+        # TODO:
+        ## redshift loss: can define ourselves, or get it from redshift estimation (Declan's project)
+        ## fluxes loss: can be found in super().compute_nll() definition
+
+        return loss
