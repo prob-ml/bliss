686c0d1b6b30a121c3b0ae3648910ec76dc16cbf
Ishan Kapnadak <kapnadak@deeplearning-01.stat.lsa.umich.edu>
2024-06-05 17:23:14 -0400

    Create downsampling network for larger tile sizes and subclass Encoder.

---
+++ case_studies/galaxy_clustering/encoder/encoder.py
@@ -0,0 +1,111 @@
+from typing import Optional
+
+import torch
+from torchmetrics import MetricCollection
+
+from bliss.encoder.convnet import CatalogNet, ContextNet
+from bliss.encoder.encoder import Encoder
+from bliss.encoder.image_normalizer import ImageNormalizer
+from bliss.encoder.metrics import CatalogMatcher
+from bliss.encoder.variational_dist import VariationalDistSpec
+from case_studies.galaxy_clustering.encoder.convnet import GalaxyClusterFeaturesNet
+
+
+class GalaxyClusterEncoder(Encoder):
+    def __init__(
+        self,
+        survey_bands: list,
+        tile_slen: int,
+        tiles_to_crop: int,
+        image_normalizer: ImageNormalizer,
+        vd_spec: VariationalDistSpec,
+        metrics: MetricCollection,
+        sample_image_renders: MetricCollection,
+        matcher: CatalogMatcher,
+        min_flux_threshold: float = 0,
+        min_flux_threshold_during_test: float = 0,
+        optimizer_params: Optional[dict] = None,
+        scheduler_params: Optional[dict] = None,
+        do_data_augmentation: bool = False,
+        compile_model: bool = False,
+        double_detect: bool = False,
+        use_checkerboard: bool = True,
+        reference_band: int = 2,
+        downsample_at_front: bool = True,
+    ):
+        """Initializes Encoder.
+
+        Args:
+            survey_bands: all band-pass filters available for this survey
+            tile_slen: dimension in pixels of a square tile
+            tiles_to_crop: margin of tiles not to use for computing loss
+            image_normalizer: object that applies input transforms to images
+            vd_spec: object that makes a variational distribution from raw convnet output
+            sample_image_renders: for plotting relevant images (overlays, shear maps)
+            metrics: for scoring predicted catalogs during training
+            matcher: for matching predicted catalogs to ground truth catalogs
+            min_flux_threshold: Sources with a lower flux will not be considered when computing loss
+            min_flux_threshold_during_test: filter sources by flux during test
+            optimizer_params: arguments passed to the Adam optimizer
+            scheduler_params: arguments passed to the learning rate scheduler
+            do_data_augmentation: used for determining whether or not do data augmentation
+            compile_model: compile model for potential performance improvements
+            double_detect: whether to make up to two detections per tile rather than one
+            use_checkerboard: whether to use dependent tiling
+            reference_band: band to use for filtering sources
+            downsample_at_front: whether to downsample at front of network
+        """
+        super().__init__()
+
+        self.survey_bands = survey_bands
+        self.tile_slen = tile_slen
+        self.tiles_to_crop = tiles_to_crop
+        self.image_normalizer = image_normalizer
+        self.vd_spec = vd_spec
+        self.mode_metrics = metrics.clone()
+        self.sample_metrics = metrics.clone()
+        self.sample_image_renders = sample_image_renders
+        self.matcher = matcher
+        self.min_flux_threshold = min_flux_threshold
+        self.min_flux_threshold_during_test = min_flux_threshold_during_test
+        assert self.min_flux_threshold <= self.min_flux_threshold_during_test, "invalid threshold"
+        self.optimizer_params = optimizer_params
+        self.scheduler_params = scheduler_params if scheduler_params else {"milestones": []}
+        self.do_data_augmentation = do_data_augmentation
+        self.compile_model = compile_model
+        self.double_detect = double_detect
+        self.use_checkerboard = use_checkerboard
+        self.reference_band = reference_band
+        self.downsample_at_front = downsample_at_front
+
+        self.initialize_networks()
+
+    def initialize_networks(self):
+        """Load the convolutional neural networks that map normalized images to catalog parameters.
+        This method can be overridden to use different network architectures.
+        `checkerboard_net` and `second_net` can be left as None if not needed.
+        """
+        power_of_two = (self.tile_slen != 0) & (self.tile_slen & (self.tile_slen - 1) == 0)
+        assert power_of_two, "tile_slen must be a power of two"
+        ch_per_band = self.image_normalizer.num_channels_per_band()
+        num_features = 256
+        self.features_net = GalaxyClusterFeaturesNet(
+            len(self.image_normalizer.bands),
+            ch_per_band,
+            num_features,
+            tile_slen=self.tile_slen,
+            downsample_at_front=self.downsample_at_front,
+        )
+        n_params_per_source = self.vd_spec.n_params_per_source
+        self.marginal_net = CatalogNet(num_features, n_params_per_source)
+        self.checkerboard_net = ContextNet(num_features, n_params_per_source)
+        if self.double_detect:
+            self.second_net = CatalogNet(num_features, n_params_per_source)
+
+        if self.compile_model:
+            self.features_net = torch.compile(self.features_net)
+            self.marginal_net = torch.compile(self.marginal_net)
+            if self.use_checkerboard:
+                self.checkerboard_net = torch.compile(self.checkerboard_net)
+            if self.double_detect:
+                self.second_net = torch.compile(self.second_net)
