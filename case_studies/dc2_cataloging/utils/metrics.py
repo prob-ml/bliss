7f5905d61c61a8199da64ace71c74da83d798bd5
Yicun Duan <144750316+YicunDuanUMich@users.noreply.github.com>
2024-06-21 13:50:38 -0400

    Yd/add plot (#1020)
    * rename notebooks and add flux error plot
    
    * add a notebook to report n_sources count; update scripts for great lake
    
    * update run_dc2_exp.sh
    
    * update run_dc2_exp.sh
    
    * update the path in notebook; add a new notebook match_selector
    
    * update match_selector
    
    * fix the bug in catalog; modify dc2 to include multiple sources per tile; add a new metric to test multiple sources detection; update others
    
    * rename convnet to dynamic_asinh_convnet
    
    * simplify metric names
    
    * update scripts
    
    * write a script to directly run on great lake
    
    * refactor notebooks; update README
    
    * update plots
    
    * update path name
    
    ---------
    
    Co-authored-by: Yicun Duan <pduan@deeplearning-01.stat.lsa.umich.edu>
    Co-authored-by: Jeffrey Regier <jeff-regier@users.noreply.github.com>

--- case_studies/dc2_cataloging/utils/metrics.py
+++ case_studies/dc2_cataloging/utils/metrics.py
@@ -71,3 +71,32 @@ class OutBoundaryFilter(CatFilter):
 
     def get_cur_postfix(self):
         return "out_boundary"
+
+
+class SourceCountFilter(CatFilter):
+    def __init__(self, filter_source_count: str) -> None:
+        super().__init__()
+        self.filter_source_count = filter_source_count
+        assert filter_source_count in {
+            "1m",
+            "2m",
+            "2plus",
+        }, "invalid filter_source_count"
+
+    def get_cur_filter_bools(self, true_cat, est_cat):
+        if self.filter_source_count == "1m":
+            true_filter_bools = true_cat["one_source_mask"].squeeze(2)
+            est_filter_bools = est_cat["one_source_mask"].squeeze(2)
+        elif self.filter_source_count == "2m":
+            true_filter_bools = true_cat["two_sources_mask"].squeeze(2)
+            est_filter_bools = est_cat["two_sources_mask"].squeeze(2)
+        elif self.filter_source_count == "2plus":
+            true_filter_bools = true_cat["more_than_two_sources_mask"].squeeze(2)
+            est_filter_bools = est_cat["more_than_two_sources_mask"].squeeze(2)
+        else:
+            raise NotImplementedError()
+
+        return true_filter_bools, est_filter_bools
+
+    def get_cur_postfix(self):
+        return self.filter_source_count
