c6123cb73cc502fa3772406e052b527e4935b413
Jeffrey Regier <regier@umich.edu>
2024-06-22 12:17:18 -0400

    jun22_always_grouupnorm

--- bliss/encoder/convnet.py
+++ bliss/encoder/convnet.py
@@ -7,7 +7,7 @@ from torch import nn
 
 class ConvBlock(nn.Module):
     def __init__(
-        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, use_group_norm=False
+        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, use_group_norm=True
     ):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
