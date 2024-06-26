a659706bf4f8dfbbe3dc0427f8ceb5812e98589d
Eshan Jain <eshanj@deeplearning-01.stat.lsa.umich.edu>
2024-06-26 15:58:46 -0400

    more styling fixes

--- case_studies/strong_lensing/bliss_lens.py
+++ case_studies/strong_lensing/bliss_lens.py
@@ -9,8 +9,8 @@ def sie_deflection(x_coord, y_coord, lens_params):
     """Calculate the deflection angles for a Singular Isothermal Ellipsoid (SIE) lens model.
 
     Args:
-        x (vector): Vector or image of x coordinates.
-        y (vector): Vector or image of y coordinates.
+        x_coord (vector): Vector or image of x coordinates.
+        y_coord (vector): Vector or image of y coordinates.
         lens_params (vector): Vector of parameters with 1 to 5 elements, defined as follows:
             par[0]: Lens strength, or 'Einstein radius'.
             par[1]: (optional) x-center.
@@ -25,18 +25,21 @@ def sie_deflection(x_coord, y_coord, lens_params):
     """
     b_rad, center_x, center_y, e_1, e_2 = lens_params.cpu().numpy()
     q_val = (1 - (np.sqrt(e_1**2 + e_2**2))) / (1 + (np.sqrt(e_1**2 + e_2**2)))
-    phirad = np.arctan(e_2 / e_1)
 
     # Go into shifted coordinats of the potential:
-    xsie = (x_coord - center_x) * np.cos(phirad) + (y_coord - center_y) * np.sin(phirad)
-    ysie = (y_coord - center_y) * np.cos(phirad) - (x_coord - center_x) * np.sin(phirad)
+    xsie = (x_coord - center_x) * np.cos(np.arctan(e_2 / e_1)) + (y_coord - center_y) * np.sin(
+        np.arctan(e_2 / e_1)
+    )
+
+    ysie = (y_coord - center_y) * np.cos(np.arctan(e_2 / e_1)) - (x_coord - center_x) * np.sin(
+        np.arctan(e_2 / e_1)
+    )
 
     # Compute potential gradient in the transformed system:
     r_ell = np.sqrt(q_val * xsie**2 + ysie**2 / q_val)
     qfact = np.sqrt(1.0 / q_val - q_val)
 
     # (r_ell == 0) terms prevent divide-by-zero problems
-    # eps = 0.001
     if qfact >= 0.001:
         xtg = (b_rad / qfact) * np.arctan(qfact * xsie / (r_ell + (r_ell == 0)))
         ytg = (b_rad / qfact) * np.arctanh(qfact * ysie / (r_ell + (r_ell == 0)))
@@ -46,12 +49,28 @@ def sie_deflection(x_coord, y_coord, lens_params):
 
     # Transform back to un-rotated system:
     return (
-        xtg * np.cos(phirad) - ytg * np.sin(phirad),
-        ytg * np.cos(phirad) + xtg * np.sin(phirad),
+        xtg * np.cos(np.arctan(e_2 / e_1)) - ytg * np.sin(np.arctan(e_2 / e_1)),
+        ytg * np.cos(np.arctan(e_2 / e_1)) + xtg * np.sin(np.arctan(e_2 / e_1)),
     )
 
 
 def bilinear_interpolate_numpy(i_m, x_coord, y_coord):
+    """Perform bilinear interpolation on a 2D numpy array.
+
+    Args:
+    i_m (numpy.ndarray): Input 2D array (image) to interpolate.
+    x_coord (float): X-coordinate for interpolation.
+    y_coord (float): Y-coordinate for interpolation.
+
+    Returns:
+    numpy.ndarray: Interpolated value at (x_coord, y_coord) using bilinear interpolation.
+
+    Notes:
+    - Uses numpy's floor, clip, and array indexing operations for efficient interpolation.
+    - Assumes x_coord and y_coord are within the bounds of i_m.shape.
+    - Bilinear interpolation computes a weighted average of the four nearest
+    pixels around (x_coord, y_coord).
+    """
     x_0 = np.floor(x_coord).astype(int)
     x_1 = x_0 + 1
     y_0 = np.floor(y_coord).astype(int)
@@ -76,6 +95,28 @@ def bilinear_interpolate_numpy(i_m, x_coord, y_coord):
 
 
 def lens_galsim(unlensed_image, lens_params):
+    """Apply gravitational lensing to input image using Singular Isothermal Ellipsoid (SIE) model.
+
+    Args:
+    unlensed_image (numpy.ndarray): Input 2D array representing the unlensed image.
+    lens_params (numpy.ndarray): Parameters for the SIE lens model.
+        lens_params should contain:
+            - lens strength (Einstein radius)
+            - x-center (optional)
+            - y-center (optional)
+            - e1 ellipticity (optional)
+            - e2 ellipticity (optional)
+
+    Returns:
+    numpy.ndarray: Lensed image after applying gravitational lensing.
+        Has the same dtype as the input `unlensed_image`.
+
+    Notes:
+    - Uses numpy operations to generate coordinates and perform deflection.
+    - Calls `sie_deflection` to compute deflection angles for gravitational lensing.
+    - Uses `bilinear_interpolate_numpy` for bilinear interpolation of pixel values.
+    - Assumes the input image `unlensed_image` is centered at the origin.
+    """
     n_x, n_y = unlensed_image.shape
     x_range = [-n_x // 2, n_x // 2]
     y_range = [-n_y // 2, n_y // 2]
@@ -109,9 +150,9 @@ params = torch.tensor([theta_e, x_center, y_center, e1, e2])  # pylint: disable=
 lensed_img = lens_galsim(image, params)
 
 
-output_dir = sys.argv[2]
+OUTPUT_DIR = sys.argv[2]
 lensed_img = galsim.ImageF(lensed_img)
-lensed_img.write(output_dir + "/galsim.fits")
+lensed_img.write(OUTPUT_DIR + "/galsim.fits")
 
 params = [theta_e, x_center, y_center, e1, e2]
 print(" ".join(map(str, params)))  # noqa: WPS421
--- case_studies/strong_lensing/open_fits.py
+++ case_studies/strong_lensing/open_fits.py
@@ -4,10 +4,10 @@ from astropy.io import fits
 from matplotlib import pyplot as plt
 
 i = sys.argv[1]
-output_dir = sys.argv[2]
+OUTPUT_DIR = sys.argv[2]
 
-image_file = f"{output_dir}/image{i}.fits"
+image_file = f"{OUTPUT_DIR}/image{i}.fits"
 image_data = fits.getdata(image_file)
 c = plt.imshow(image_data)
 plt.colorbar(c)
-plt.savefig(f"{output_dir}/image{i}.png")
+plt.savefig(f"{OUTPUT_DIR}/image{i}.png")
