180fafd9afaa70ec83e59bd96aa496a13938c2d1
Jeffrey Regier <jeff-regier@users.noreply.github.com>
2024-06-26 17:12:55 -0400

    torchdata (#1026)
    * IterDataPipe with tests passing
    
    * distributed reading service
    
    * flags to enable/disable various data augmentation strategies

--- bliss/cached_dataset.py
+++ bliss/cached_dataset.py
@@ -1,19 +1,21 @@
-import logging
-import os
-import pathlib
 import random
-import re
 import warnings
-from typing import List, TypedDict
+from pathlib import Path
+from typing import TypedDict
 
 import pytorch_lightning as pl
 import torch
-from torch import distributed as dist
-from torch.utils.data import DataLoader, Dataset, DistributedSampler, Sampler
-from torchvision import transforms
+from torchdata.dataloader2 import (
+    DataLoader2,
+    DistributedReadingService,
+    InProcessReadingService,
+    MultiProcessingReadingService,
+    SequentialReadingService,
+)
+from torchdata.datapipes import functional_datapipe
+from torchdata.datapipes.iter import FileLister, IterableWrapper, IterDataPipe
 
 from bliss.catalog import FullCatalog, TileCatalog
-from bliss.global_settings import GlobalSettings
 
 # prevent pytorch_lightning warning for num_workers = 2 in dataloaders with IterableDataset
 warnings.filterwarnings(
@@ -34,219 +36,206 @@ FileDatum = TypedDict(
 )
 
 
-class FullCatalogToTileTransform(torch.nn.Module):
-    def __init__(self, tile_slen, max_sources):
+@functional_datapipe("rotate_and_flip")
+class RotateAndFlip(IterDataPipe):
+    def __init__(self, datapipe):
         super().__init__()
-        self.tile_slen = tile_slen
-        self.max_sources = max_sources
+        self.datapipe = datapipe
 
-    def __call__(self, datum_in):
-        datum_out = {k: v for k, v in datum_in.items() if k != "full_catalog"}
+    def do_rotate_flip(self, datum_in, rotate_id, do_flip):
+        # problematic if the psf isn't rotationally invariant
+        datum_out = {"psf_params": datum_in["psf_params"]}
 
-        h_pixels, w_pixels = datum_in["images"].shape[1:]
-        full_cat = FullCatalog(h_pixels, w_pixels, datum_in["full_catalog"])
-        tile_cat = full_cat.to_tile_catalog(self.tile_slen, self.max_sources).data
-        d = {k: v.squeeze(0) for k, v in tile_cat.items()}
-        datum_out["tile_catalog"] = d
+        # apply rotation
+        datum_out["images"] = datum_in["images"].rot90(rotate_id, [1, 2])
+        datum_out["background"] = datum_in["background"].rot90(rotate_id, [1, 2])
+        d = datum_in["tile_catalog"]
+        datum_out["tile_catalog"] = {k: v.rot90(rotate_id, [0, 1]) for k, v in d.items()}
+
+        # apply flip
+        if do_flip:
+            datum_out["images"] = datum_out["images"].flip([1])
+            datum_out["background"] = datum_out["background"].flip([1])
+            for k, v in datum_out["tile_catalog"].items():
+                datum_out["tile_catalog"][k] = v.flip([0])
+
+        # locations require special logic
+        if "locs" in datum_in["tile_catalog"]:
+            locs = datum_out["tile_catalog"]["locs"]
+            for _ in range(rotate_id):
+                # Rotate 90 degrees clockwise (in pixel coordinates)
+                locs = torch.stack((1 - locs[..., 1], locs[..., 0]), dim=3)
+            if do_flip:
+                locs = torch.stack((1 - locs[..., 0], locs[..., 1]), dim=3)
+            datum_out["tile_catalog"]["locs"] = locs
 
         return datum_out
 
-
-class ChunkingSampler(Sampler):
-    def __init__(self, dataset: Dataset) -> None:
-        assert isinstance(dataset, ChunkingDataset), "dataset should be MyDataset"
-        self.dataset = dataset
-
-    def __len__(self):
-        return len(self.dataset)
-
     def __iter__(self):
-        return iter(self.dataset.get_chunked_indices())
+        for datum_in in self.datapipe:
+            rotate_id = random.randint(0, 4)
+            do_flip = random.choice([True, False])
+            yield self.do_rotate_flip(datum_in, rotate_id, do_flip)
 
 
-# note that for this DistributedSampler, we don't need to call `set_epoch()`
-class DistributedChunkingSampler(DistributedSampler):
-    def __init__(
-        self,
-        dataset: Dataset,
-        num_replicas: int | None = None,
-        rank: int | None = None,
-        shuffle: bool = False,
-        seed: int = 0,
-        drop_last: bool = False,
-    ) -> None:
-        assert isinstance(dataset, ChunkingDataset), "dataset should be MyDataset"
-        assert not shuffle, "you should not use shuffle"
-        super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
-
-    def __iter__(self):
-        pre_indices = list(super().__iter__())
-        chunked_indices = self.dataset.get_chunked_indices()
-
-        return iter([chunked_indices[i] for i in pre_indices])
-
-
-class ChunkingDataset(Dataset):
-    def __init__(self, file_paths, shuffle=False, transform=None) -> None:
+@functional_datapipe("random_shift")
+class RandomShift(IterDataPipe):
+    def __init__(self, datapipe, tile_slen, max_sources_per_tile):
         super().__init__()
-        self.file_paths = file_paths
+        self.datapipe = datapipe
+        assert tile_slen % 2 == 0 and tile_slen > 1
+        self.tile_slen = tile_slen
+        self.max_sources_per_tile = max_sources_per_tile
+
+    def do_shift(self, datum_in, vertical_shift, horizontal_shift):
+        datum_out = {"psf_params": datum_in["psf_params"]}
+
+        for k in ("images", "background"):
+            img = datum_in[k]
+            img = torch.roll(img, shifts=vertical_shift, dims=1)
+            img = torch.roll(img, shifts=horizontal_shift, dims=2)
+            datum_out[k] = img
+
+        d = {k: v.unsqueeze(0) for k, v in datum_in["tile_catalog"].items()}
+        tile_cat = TileCatalog(self.tile_slen, d)
+        full_cat = tile_cat.to_full_catalog()
+
+        full_cat["plocs"][:, :, 0] += vertical_shift
+        full_cat["plocs"][:, :, 1] += horizontal_shift
+
+        aug_tile = full_cat.to_tile_catalog(
+            self.tile_slen, self.max_sources_per_tile, filter_oob=True
+        )
+        d_out = {k: v.squeeze(0) for k, v in aug_tile.items()}
+        datum_out["tile_catalog"] = d_out
+
+        return datum_out
+
+    def __iter__(self):
+        for datum_in in self.datapipe:
+            shift_ub = self.tile_slen // 2
+            shift_lb = -(shift_ub - 1)
+            vertical_shift = random.randint(shift_lb, shift_ub)
+            horizontal_shift = random.randint(shift_lb, shift_ub)
+            yield self.do_shift(datum_in, vertical_shift, horizontal_shift)
+
+
+@functional_datapipe("full_catalog_to_tile")
+class FullCatalogToTile(IterDataPipe):
+    def __init__(self, datapipe, tile_slen, max_sources):
+        super().__init__()
+
+        self.datapipe = datapipe
+        self.tile_slen = tile_slen
+        self.max_sources = max_sources
+
+    def __iter__(self):
+        for datum_in in self.datapipe:
+            datum_out = {k: v for k, v in datum_in.items() if k != "full_catalog"}
+
+            h_pixels, w_pixels = datum_in["images"].shape[1:]
+            full_cat = FullCatalog(h_pixels, w_pixels, datum_in["full_catalog"])
+            tile_cat = full_cat.to_tile_catalog(self.tile_slen, self.max_sources).data
+            d = {k: v.squeeze(0) for k, v in tile_cat.items()}
+            datum_out["tile_catalog"] = d
+
+            yield datum_out
+
+
+@functional_datapipe("parse_pt")
+class ReadTorchPt(IterDataPipe):
+    def __init__(self, datapipe, shuffle=False) -> None:
+        self.datapipe = datapipe
         self.shuffle = shuffle
-        self.transform = transform
 
-        self.accumulated_file_sizes = torch.zeros(len(self.file_paths), dtype=torch.int64)
-        for i, file_path in enumerate(self.file_paths):
-            file_size_match = re.search(r"size_(\d+)", file_path)
-            if file_size_match:
-                cached_data_len = int(file_size_match.group(1))
-            else:
-                if i == 0:
-                    logger = logging.getLogger("MyDataset")
-                    warning_msg = (
-                        "WARNING: add postfix '_size_XXXX' to file name; "
-                        "otherwise it'll be very slow\n"
-                    )
-                    logger.warning(warning_msg)
-                with open(file_path, "rb") as f:
-                    cached_data_len = len(torch.load(f))
-
-            if i == 0:
-                self.accumulated_file_sizes[i] = cached_data_len
-            else:
-                self.accumulated_file_sizes[i] = (
-                    self.accumulated_file_sizes[i - 1] + cached_data_len
-                )
-
-        self.buffered_file_index = None
-        self.buffered_data = None
-
-    def __len__(self):
-        return self.accumulated_file_sizes[-1].item()
-
-    def __getitem__(self, index):
-        converted_index = (self.accumulated_file_sizes <= index).sum().item()
-        converted_sub_index = (index - self.accumulated_file_sizes[converted_index]).item()
-        if self.buffered_file_index != converted_index:
-            self.buffered_file_index = converted_index
-            with open(self.file_paths[converted_index], "rb") as f:
-                self.buffered_data = torch.load(f)
-        output_data = self.buffered_data[converted_sub_index]
-        return self.transform(output_data)
-
-    def get_chunked_indices(self):
-        accumulated_file_sizes_list = self.accumulated_file_sizes.tolist()
-
-        output_list = []
-        if self.shuffle:
-            epoch_seed = GlobalSettings.seed_in_this_program + GlobalSettings.current_encoder_epoch
-            logger = logging.getLogger("ChunkingDataset")
-            logger.info(
-                "INFO: seed is %d; current epoch is %d; epoch_seed is set to %d",
-                GlobalSettings.seed_in_this_program,
-                GlobalSettings.current_encoder_epoch,
-                epoch_seed,
-            )
-            right_shift_list = [0, *accumulated_file_sizes_list[:-1]]
-            for start, end in zip(right_shift_list, accumulated_file_sizes_list):
-                output_list.append(random.Random(epoch_seed).sample(range(start, end), end - start))
-            random.Random(epoch_seed).shuffle(output_list)
-            # flatten the list
-            return sum(output_list, [])
-
-        return list(range(0, len(self)))
+    def __iter__(self):
+        for filename in self.datapipe:
+            file_datums = torch.load(filename)
+            if self.shuffle:
+                file_datums = IterableWrapper(file_datums).shuffle()
+            yield from file_datums
 
 
 class CachedSimulatedDataModule(pl.LightningDataModule):
     def __init__(
         self,
+        data_dir,
         splits: str,
-        batch_size: int,
-        num_workers: int,
-        cached_data_path: str,
-        train_transforms: List,
-        nontrain_transforms: List,
+        split_seed: int = 0,
+        batch_size: int = 32,
+        num_workers: int = 0,
+        convert_full_cat=False,
+        use_rotate_and_flip=True,
+        use_random_shift=False,
+        tile_slen=None,
+        max_sources=None,
     ):
         super().__init__()
-
+        self.data_dir = Path(data_dir)
         self.splits = splits
+        self.split_seed = split_seed
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.cached_data_path = pathlib.Path(cached_data_path)
-        self.train_transforms = train_transforms
-        self.nontrain_transforms = nontrain_transforms
+        self.convert_full_cat = convert_full_cat
+        self.use_rotate_and_flip = use_rotate_and_flip
+        self.use_random_shift = use_random_shift
+        self.tile_slen = tile_slen
+        self.max_sources_per_tile = max_sources
 
-        self.file_paths = None
-        self.slices = None
-        self.train_dataset = None
-        self.val_dataset = None
-        self.test_dataset = None
-        self.predict_dataset = None
+        self.all_pipe = None
+        self.train_pipe = None
+        self.val_pipe = None
+        self.test_pipe = None
 
-    def setup(self, stage: str) -> None:  # noqa: WPS324
-        if stage == "fit":
-            file_names = [f for f in os.listdir(str(self.cached_data_path)) if f.endswith(".pt")]
-            self.file_paths = [os.path.join(str(self.cached_data_path), f) for f in file_names]
+        if self.num_workers == 0:
+            mp_rs = InProcessReadingService()
+        else:
+            mp_rs = MultiProcessingReadingService(num_workers=self.num_workers)
 
-            # parse slices from percentages to indices
-            self.slices = self.parse_slices(self.splits, len(self.file_paths))
+        if torch.cuda.device_count() > 1:
+            dist_rs = DistributedReadingService()
+            self.reading_service = SequentialReadingService(dist_rs, mp_rs)
+        else:
+            self.reading_service = mp_rs
 
-            self.train_dataset = self._get_dataset(
-                self.file_paths[self.slices[0]], self.train_transforms, shuffle=True
-            )
+    def setup(self, stage=None):
+        self.all_pipe = FileLister([self.data_dir]).filter(filter_fn=lambda f: f.endswith(".pt"))
 
-            self.val_dataset = self._get_dataset(
-                self.file_paths[self.slices[1]], self.nontrain_transforms
-            )
-            return None
+        num_files = len(list(self.all_pipe))
+        ds_names = ["train", "val", "test"]
+        split_props = {k: float(v) for k, v in zip(ds_names, self.splits.split("/"))}
 
-        if stage == "validate":
-            return None
-
-        if stage == "test":
-            self.test_dataset = self._get_dataset(
-                self.file_paths[self.slices[2]], self.nontrain_transforms
-            )
-            return None
-
-        if stage == "predict":
-            self.predict_dataset = self._get_dataset(self.file_paths, self.nontrain_transforms)
-            return None
-
-        raise RuntimeError(f"setup skips stage {stage}")
-
-    def _percent_to_idx(self, x, length):
-        """Converts string in percent to an integer index."""
-        return int(float(x.strip()) / 100 * length) if x.strip() else None
-
-    def parse_slices(self, splits: str, length: int):
-        slices = [slice(0, 0) for _ in range(3)]  # default to empty slice for each split
-        for i, data_split in enumerate(splits.split("/")):
-            # map "start_percent:stop_percent" to slice(start_idx, stop_idx)
-            slices[i] = slice(*(self._percent_to_idx(val, length) for val in data_split.split(":")))
-        return slices
-
-    def _get_dataset(self, sub_file_paths, defined_transforms, shuffle: bool = False):
-        assert sub_file_paths, "No cached data found"
-        transform = transforms.Compose(defined_transforms)
-        return ChunkingDataset(sub_file_paths, shuffle=shuffle, transform=transform)
-
-    def _get_dataloader(self, my_dataset):
-        distributed_is_used = dist.is_available() and dist.is_initialized()
-        sampler_type = DistributedChunkingSampler if distributed_is_used else ChunkingSampler
-        return DataLoader(
-            my_dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            sampler=sampler_type(my_dataset),
+        self.train_pipe, self.val_pipe, self.test_pipe = self.all_pipe.random_split(
+            total_length=num_files, weights=split_props, seed=self.split_seed
         )
 
-    def train_dataloader(self):
-        return self._get_dataloader(self.train_dataset)
+    def train_dataloader(self) -> DataLoader2:
+        pipe = self.train_pipe.shuffle().sharding_filter().parse_pt(shuffle=True)
 
-    def val_dataloader(self):
-        return self._get_dataloader(self.val_dataset)
+        if self.convert_full_cat:
+            pipe = pipe.full_catalog_to_tile(self.tile_slen, self.max_sources_per_tile)
 
-    def test_dataloader(self):
-        return self._get_dataloader(self.test_dataset)
+        if self.use_rotate_and_flip:
+            pipe = pipe.rotate_and_flip()
 
-    def predict_dataloader(self):
-        return self._get_dataloader(self.predict_dataset)
+        if self.use_random_shift:
+            pipe = pipe.random_shift(self.tile_slen, self.max_sources_per_tile)
+
+        pipe = pipe.batch(self.batch_size).collate()
+        return DataLoader2(pipe, reading_service=self.reading_service)
+
+    def nontrain_dataloader(self, base_pipe) -> DataLoader2:
+        pipe = base_pipe.sharding_filter().parse_pt()
+        if self.convert_full_cat:
+            pipe = pipe.full_catalog_to_tile(self.tile_slen, self.max_sources_per_tile)
+        pipe = pipe.batch(self.batch_size).collate()
+        return DataLoader2(pipe)
+
+    def val_dataloader(self) -> DataLoader2:
+        return self.nontrain_dataloader(self.val_pipe)
+
+    def test_dataloader(self) -> DataLoader2:
+        return self.nontrain_dataloader(self.test_pipe)
+
+    def predict_dataloader(self) -> DataLoader2:
+        return self.nontrain_dataloader(self.all_pipe)
