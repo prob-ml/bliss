e848dc4cb03971d96358a0829fc3c1ee8fa291d1
Jeffrey Regier <regier@umich.edu>
2024-06-25 22:18:27 -0400

    jun25_restore_sample_metrics

--- bliss/encoder/encoder.py
+++ bliss/encoder/encoder.py
@@ -68,7 +68,8 @@ class Encoder(pl.LightningModule):
         self.tiles_to_crop = tiles_to_crop
         self.image_normalizer = image_normalizer
         self.var_dist = var_dist
-        self.metrics = metrics
+        self.mode_metrics = metrics.clone()
+        self.sample_metrics = metrics.clone()
         self.sample_image_renders = sample_image_renders
         self.matcher = matcher
         self.min_flux_for_loss = min_flux_for_loss
@@ -239,19 +240,27 @@ class Encoder(pl.LightningModule):
         )
         target_cat = target_tile_cat.symmetric_crop(self.tiles_to_crop).to_full_catalog()
 
-        mode_tile_cat = self.sample(batch, use_mode=True).filter_by_flux(
+        sample_tile_cat = self.sample(batch, use_mode=True).filter_by_flux(
             min_flux=self.min_flux_for_metrics,
             band=self.reference_band,
         )
-        mode_cat = mode_tile_cat.to_full_catalog()
-        matching = self.matcher.match_catalogs(target_cat, mode_cat)
-        self.metrics.update(target_cat, mode_cat, matching)
+        sample_cat = sample_tile_cat.to_full_catalog()
+        matching = self.matcher.match_catalogs(target_cat, sample_cat)
+        self.mode_metrics.update(target_cat, sample_cat, matching)
+
+        sample_tile_cat = self.sample(batch, use_mode=False).filter_by_flux(
+            min_flux=self.min_flux_for_metrics,
+            band=self.reference_band,
+        )
+        sample_cat = sample_tile_cat.to_full_catalog()
+        smatching = self.matcher.match_catalogs(target_cat, sample_cat)
+        self.sample_metrics.update(target_cat, sample_cat, smatching)
 
         self.sample_image_renders.update(
             batch,
             target_cat,
-            mode_tile_cat,
-            mode_cat,
+            sample_tile_cat,
+            sample_cat,
             self.current_epoch,
             batch_idx,
         )
@@ -277,7 +286,8 @@ class Encoder(pl.LightningModule):
         metrics.reset()
 
     def on_validation_epoch_end(self):
-        self.report_metrics(self.metrics, "val/mode", show_epoch=True)
+        self.report_metrics(self.mode_metrics, "val/mode", show_epoch=True)
+        self.report_metrics(self.sample_metrics, "val/mode", show_epoch=True)
         self.report_metrics(self.sample_image_renders, "val/image_renders", show_epoch=True)
 
     def test_step(self, batch, batch_idx):
@@ -286,7 +296,8 @@ class Encoder(pl.LightningModule):
         self.update_metrics(batch, batch_idx)
 
     def on_test_epoch_end(self):
-        self.report_metrics(self.metrics, "test/mode", show_epoch=False)
+        self.report_metrics(self.mode_metrics, "test/mode", show_epoch=False)
+        self.report_metrics(self.sample_metrics, "test/mode", show_epoch=False)
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         """Pytorch lightning method."""
