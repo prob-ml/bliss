abdd6634e04d5b98cbfab0a336d37fe35ae27cf4
Ishan Kapnadak <kapnadak@deeplearning-01.stat.lsa.umich.edu>
2024-06-27 11:44:45 -0400

    Modify encoder to be compatible with new set-up.

--- case_studies/galaxy_clustering/encoder/encoder.py
+++ case_studies/galaxy_clustering/encoder/encoder.py
@@ -2,12 +2,9 @@ import torch
 from torch.nn.functional import pad
 
 from bliss.catalog import BaseTileCatalog, TileCatalog
+from bliss.encoder.convnets import CatalogNet
 from bliss.encoder.encoder import Encoder
-from case_studies.galaxy_clustering.encoder.convnet import (
-    GalaxyClusterCatalogNet,
-    GalaxyClusterContextNet,
-    GalaxyClusterFeaturesNet,
-)
+from case_studies.galaxy_clustering.encoder.convnet import GalaxyClusterFeaturesNet
 
 
 class GalaxyClusterEncoder(Encoder):
@@ -32,43 +29,22 @@ class GalaxyClusterEncoder(Encoder):
             downsample_at_front=self.downsample_at_front,
         )
         n_params_per_source = self.var_dist.n_params_per_source
-        self.marginal_net = GalaxyClusterCatalogNet(num_features, n_params_per_source)
-        self.checkerboard_net = GalaxyClusterContextNet(num_features, n_params_per_source)
-        if self.use_double_detect:
-            self.second_net = GalaxyClusterCatalogNet(num_features, n_params_per_source)
+        self.catalog_net = CatalogNet(num_features, n_params_per_source)
 
     def get_features_and_parameters(self, batch):
+        batch_size, _n_bands, h, w = batch["images"].shape[0:4]
+        ht, wt = h // self.tile_slen, w // self.tile_slen
+
         x = self.image_normalizer.get_input_tensor(batch)
         x_features = self.features_net(x)
-        x_cat_marginal = self.marginal_net(x_features)
+        mask = torch.zeros([batch_size, ht, wt])
+        context = self.make_context(None, mask).cuda()
+        x_cat_marginal = self.catalog_net(x_features, context)
         return x_features, x_cat_marginal
 
     def sample(self, batch, use_mode=True):
-        batch_size, _n_bands, h, w = batch["images"].shape[0:4]
-
-        x_features, x_cat_marginal = self.get_features_and_parameters(batch)
-
-        marginal_cat = self.var_dist.sample(x_cat_marginal, use_mode=use_mode)
-
-        if not self.use_checkerboard:
-            est_cat = marginal_cat
-        else:
-            ht, wt = h // self.tile_slen, w // self.tile_slen
-            tile_cb = self._get_checkerboard(ht, wt).squeeze(1)
-            white_history_mask = tile_cb.expand([batch_size, -1, -1])
-
-            white_context = self.make_context(marginal_cat, white_history_mask)
-            x_cat_white = self.checkerboard_net(x_features, white_context)
-            white_cat = self.var_dist.sample(x_cat_white, use_mode=use_mode)
-            est_cat = self.interleave_catalogs(marginal_cat, white_cat, white_history_mask)
-
-        if self.use_double_detect:
-            x_cat_second = self.second_net(x_features)
-            second_cat = self.var_dist.sample(x_cat_second, use_mode=use_mode)
-            # our loss function implies that the second detection is ignored for a tile
-            # if the first detection is empty for that tile
-            second_cat["n_sources"] *= est_cat["n_sources"]
-            est_cat = est_cat.union(second_cat)
+        _, x_cat_marginal = self.get_features_and_parameters(batch)
+        est_cat = self.var_dist.sample(x_cat_marginal, use_mode=use_mode)
 
         return est_cat.symmetric_crop(self.tiles_to_crop)
 
@@ -98,6 +74,7 @@ class GalaxyClusterEncoder(Encoder):
 
     def _compute_loss(self, batch, logging_name):
         batch_size, _n_bands, h, w = batch["images"].shape[0:4]
+        ht, wt = h // self.tile_slen, w // self.tile_slen
 
         target_cat = TileCatalog(self.tile_slen, batch["tile_catalog"])
 
@@ -115,28 +92,12 @@ class GalaxyClusterEncoder(Encoder):
 
         x = self.image_normalizer.get_input_tensor(batch)
         x_features = self.features_net(x)
-
-        pred["x_cat_marginal"] = self.marginal_net(x_features)
+        mask = torch.zeros([batch_size, ht, wt])
+        context = self.make_context(None, mask).cuda()
+        pred["x_cat_marginal"] = self.catalog_net(x_features, context)
         x_features = x_features.detach()  # is this helpful? doing it here to match old code
 
-        if self.use_checkerboard:
-            ht, wt = h // self.tile_slen, w // self.tile_slen
-            tile_cb = self._get_checkerboard(ht, wt).squeeze(1)
-            white_history_mask = tile_cb.expand([batch_size, -1, -1])
-            pred["white_history_mask"] = white_history_mask
-
-            white_context = self.make_context(target_cat1, white_history_mask)
-            pred["x_cat_white"] = self.checkerboard_net(x_features, white_context)
-
-            black_context = self.make_context(target_cat1, 1 - white_history_mask)
-            pred["x_cat_black"] = self.checkerboard_net(x_features, black_context)
-
-        # compute loss
-        if not self.use_double_detect:
-            loss = self._single_detection_nll(target_cat1, pred)
-        else:
-            pred["x_cat_second"] = self.second_net(x_features)
-            loss = self._double_detection_nll(target_cat1, target_cat, pred)
+        loss = self.var_dist.compute_nll(pred["x_cat_marginal"], target_cat1)
 
         # exclude border tiles and report average per-tile loss
         ttc = self.tiles_to_crop
@@ -145,20 +106,3 @@ class GalaxyClusterEncoder(Encoder):
         self.log(f"{logging_name}/_loss", loss, batch_size=batch_size, sync_dist=True)
 
         return loss
-
-    def _single_detection_nll(self, target_cat, pred):
-        marginal_loss = self.var_dist.compute_nll(pred["x_cat_marginal"], target_cat)
-
-        if not self.use_checkerboard:
-            return marginal_loss
-
-        white_loss = self.var_dist.compute_nll(pred["x_cat_white"], target_cat)
-        white_loss_mask = 1 - pred["white_history_mask"]
-        white_loss *= white_loss_mask
-
-        black_loss = self.var_dist.compute_nll(pred["x_cat_black"], target_cat)
-        black_loss_mask = pred["white_history_mask"]
-        black_loss *= black_loss_mask
-
-        # we divide by two because we score two predictions for each tile
-        return (marginal_loss + white_loss + black_loss) / 2
