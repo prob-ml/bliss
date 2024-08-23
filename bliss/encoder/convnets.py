d44461de4fcfd386271d1b70b83566919dc5ffa6
Jeffrey Regier <regier@umich.edu>
2024-08-23 14:38:48 -0400

    earlier first skip connection

--- bliss/encoder/convnets.py
+++ bliss/encoder/convnets.py
@@ -7,40 +7,38 @@ from bliss.encoder.convnet_layers import C3, ConvBlock
 class FeaturesNet(nn.Module):
     def __init__(self, n_bands, ch_per_band, num_features, double_downsample=False):
         super().__init__()
-
-        nch_hidden = 64
         self.preprocess3d = nn.Sequential(
-            nn.Conv3d(n_bands, nch_hidden, [ch_per_band, 5, 5], padding=[0, 2, 2]),
-            nn.BatchNorm3d(nch_hidden),
+            nn.Conv3d(n_bands, 64, [ch_per_band, 5, 5], padding=[0, 2, 2]),
+            nn.BatchNorm3d(64),
             nn.SiLU(),
         )
-        self.backbone = nn.Sequential(
-            ConvBlock(nch_hidden, 64, kernel_size=5),
-            nn.Sequential(*[ConvBlock(64, 64, kernel_size=5) for _ in range(3)]),
-            ConvBlock(64, 128, stride=2),
-            nn.Sequential(*[ConvBlock(128, 128) for _ in range(5)]),
-            ConvBlock(128, 256, stride=(2 if double_downsample else 1)),  # 4
-        )
         u_net_layers = [
-            C3(256, 256, n=4),  # 0
+            ConvBlock(64, 64, kernel_size=5),
+            nn.Sequential(*[ConvBlock(64, 64, kernel_size=5) for _ in range(3)]),
+            ConvBlock(64, 128, stride=2),  # 2
+            C3(128, 128, n=3),
+            ConvBlock(128, 256, stride=2),  # 4
+            C3(256, 256, n=3),
             ConvBlock(256, 512, stride=2),
-            C3(512, 512, n=3, shortcut=False),
-            ConvBlock(512, 256, kernel_size=1),
-            nn.Upsample(scale_factor=2, mode="nearest"),  # 4
-            C3(768, num_features, n=3, shortcut=False),
+            C3(512, 256, n=3, shortcut=False),
+            nn.Upsample(scale_factor=2, mode="nearest"),  # 8
+            C3(512, 256, n=3, shortcut=False),
+            nn.Upsample(scale_factor=2, mode="nearest"),  # 10
+            C3(384, num_features, n=3, shortcut=False),
         ]
         self.u_net = nn.ModuleList(u_net_layers)
 
     def forward(self, x):
         x = self.preprocess3d(x).squeeze(2)
-        x = self.backbone(x)
 
-        save_lst = [x]
+        save_lst = []
         for i, m in enumerate(self.u_net):
             x = m(x)
-            if i in {0, 4}:
+            if i in {2, 5}:
                 save_lst.append(x)
-            if i == 4:
-                x = torch.cat(save_lst, dim=1)
+            if i == 8:
+                x = torch.cat((x, save_lst[1]), dim=1)
+            if i == 10:
+                x = torch.cat((x, save_lst[0]), dim=1)
 
         return x
