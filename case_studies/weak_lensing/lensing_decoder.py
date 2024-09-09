941f26121f195248d791c18b29e57b845a061b29
Tim White <twhit@umich.edu>
2024-09-09 13:33:22 -0400

    Get rid of super().render_galaxy() in lensing decoder to avoid double convolution of galaxy

--- case_studies/weak_lensing/lensing_decoder.py
+++ case_studies/weak_lensing/lensing_decoder.py
@@ -1,4 +1,5 @@
 import galsim
+import numpy as np
 
 from bliss.simulator.decoder import Decoder
 
@@ -16,7 +17,25 @@ class LensingDecoder(Decoder):
         Returns:
             GSObject: a galsim representation of the rendered galaxy convolved with the PSF
         """
-        galaxy = super().render_galaxy(psf, band, source_params)
+        disk_flux = source_params["galaxy_fluxes"][band] * source_params["galaxy_disk_frac"]
+        bulge_frac = 1 - source_params["galaxy_disk_frac"]
+        bulge_flux = source_params["galaxy_fluxes"][band] * bulge_frac
+        beta = source_params["galaxy_beta_radians"] * galsim.radians
+
+        components = []
+        if disk_flux > 0:
+            b_d = source_params["galaxy_a_d"] * source_params["galaxy_disk_q"]
+            disk_hlr_arcsecs = np.sqrt(source_params["galaxy_a_d"] * b_d)
+            disk = galsim.Exponential(flux=disk_flux, half_light_radius=disk_hlr_arcsecs)
+            sheared_disk = disk.shear(q=source_params["galaxy_disk_q"].item(), beta=beta)
+            components.append(sheared_disk)
+        if bulge_flux > 0:
+            b_b = source_params["galaxy_a_b"] * source_params["galaxy_bulge_q"]
+            bulge_hlr_arcsecs = np.sqrt(source_params["galaxy_a_b"] * b_b)
+            bulge = galsim.DeVaucouleurs(flux=bulge_flux, half_light_radius=bulge_hlr_arcsecs)
+            sheared_bulge = bulge.shear(q=source_params["galaxy_bulge_q"].item(), beta=beta)
+            components.append(sheared_bulge)
+        galaxy = galsim.Add(components)
 
         shear = source_params["shear"]
         shear1, shear2 = shear
