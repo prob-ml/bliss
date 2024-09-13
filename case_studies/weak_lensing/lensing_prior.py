f783435c1f89a4c97cf7b3cc855239c1a92bd011
Tim White <twhit@umich.edu>
2024-09-12 23:26:21 -0400

    Revert "Apply shear and convergence in prior, get rid of LensingDecoder"
    This reverts commit 98eda8334ec2bc51d03f9e4df689d35d03807552.

--- case_studies/weak_lensing/lensing_prior.py
+++ case_studies/weak_lensing/lensing_prior.py
@@ -26,26 +26,6 @@ class LensingPrior(CatalogPrior):
 
         return shear, convergence
 
-    def _apply_shear_and_convergence(self, shear, convergence, q, beta, a):
-        complex_shear = shear[..., 0].unsqueeze(-1) + shear[..., 1].unsqueeze(-1) * 1j
-        reduced_shear = complex_shear / (1.0 - convergence)
-        magnification = 1 / (
-            (1 - convergence) ** 2
-            - shear[..., 0].unsqueeze(-1) ** 2
-            - shear[..., 1].unsqueeze(-1) ** 2
-        )
-
-        e = (1 - q) / (1 + q)
-        e1 = e * torch.cos(2 * beta)
-        e2 = e * torch.sin(2 * beta)
-        complex_e = e1 + e2 * 1j
-        complex_e_lensed = (complex_e + reduced_shear) / (1.0 + reduced_shear.conj() * complex_e)
-        galaxy_bulge_e_lensed = complex_e_lensed.absolute()
-        q_lensed = (1 - galaxy_bulge_e_lensed) / (1 + galaxy_bulge_e_lensed)
-        a_lensed = a * (q * magnification / q_lensed).sqrt()
-
-        return q_lensed, a_lensed
-
     def sample(self) -> TileCatalog:
         """Samples latent variables from the prior of an astronomical image.
 
@@ -61,20 +41,5 @@ class LensingPrior(CatalogPrior):
         shear, convergence = self._sample_shear_and_convergence()
         d["shear"] = shear
         d["convergence"] = convergence
-
-        d["galaxy_disk_q"], d["galaxy_a_d"] = self._apply_shear_and_convergence(
-            d["shear"],
-            d["convergence"],
-            d["galaxy_disk_q"],
-            d["galaxy_beta_radians"],
-            d["galaxy_a_d"],
-        )
-        d["galaxy_bulge_q"], d["galaxy_a_b"] = self._apply_shear_and_convergence(
-            d["shear"],
-            d["convergence"],
-            d["galaxy_bulge_q"],
-            d["galaxy_beta_radians"],
-            d["galaxy_a_b"],
-        )
 
         return TileCatalog(d)
