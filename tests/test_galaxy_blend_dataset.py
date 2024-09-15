4c2fa27243b5cc4c050b36dfdbefdd6528e69746
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2024-09-15 17:42:48 -0500

    add more testing on tiled catalog (#43)

--- tests/test_galaxy_blend_dataset.py
+++ tests/test_galaxy_blend_dataset.py
@@ -42,6 +42,25 @@ def test_galaxy_blend_catalogs(home_dir: Path):
 
     max_n_sources = full_cat.max_n_sources
 
+    # checks on tile catalogs
+    for ii in range(100):
+        for jj in range(n_tiles):
+            for kk in range(n_tiles):
+                n = tile_cat.n_sources[ii, jj, kk].item()
+                l = tile_cat.locs[ii, jj, kk]
+                gb = tile_cat["galaxy_bools"][ii, jj, kk].item()
+                sb = tile_cat["star_bools"][ii, jj, kk].item()
+                if n > 0:
+                    assert n == 1  # at most 1 source per tile
+                    assert (l > 0).all()
+                    assert l[0] <= 1 and l[1] <= 1
+                    assert gb == 1 or sb == 1
+                    assert gb == 0 or sb == 0
+                else:
+                    assert n == 0
+                    assert l[0] == 0 and l[1] == 0
+                    assert gb == 0 and sb == 0
+
     # checks on full catalog
     n_sources = full_cat.n_sources
     plocs = full_cat.plocs
@@ -88,6 +107,10 @@ def test_galaxy_blend_catalogs(home_dir: Path):
     # check consistency of tile catalog vs full catalog
     assert tile_cat.n_sources.sum() == full_cat_from_tiles.n_sources.sum()
     assert full_cat.n_sources.sum() >= tile_cat.n_sources.sum()
+    assert full_cat["galaxy_bools"].sum() >= tile_cat["galaxy_bools"].sum()
+    assert full_cat["star_bools"].sum() >= tile_cat["star_bools"].sum()
+    assert full_cat_from_tiles["galaxy_bools"].sum() == tile_cat["galaxy_bools"].sum()
+    assert full_cat_from_tiles["star_bools"].sum() == tile_cat["star_bools"].sum()
 
     total_n_stars = full_cat["star_bools"].sum()
     assert total_n_stars >= 1
