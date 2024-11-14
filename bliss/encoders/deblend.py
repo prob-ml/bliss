9198131e54b20f0da749856ce7b9e37307cdb553
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2024-11-14 13:37:22 -0600

    Update python poetry (#77)
    * no changes
    
    * update poetry
    
    * flake8
    
    * litn

--- bliss/encoders/deblend.py
+++ bliss/encoders/deblend.py
@@ -49,7 +49,7 @@ class GalaxyEncoder(pl.LightningModule):
 
         # decoder
         ae = OneCenteredGalaxyAE(decoder_slen, latent_dim, hidden, n_bands)
-        ae.load_state_dict(torch.load(ae_state_dict_path, weights_only=True))
+        ae.load_state_dict(torch.load(ae_state_dict_path, weights_only=True))  # noqa: S614
         self._dec = deepcopy(ae.dec)
         self._dec.requires_grad_(False)
         self._dec.eval()
