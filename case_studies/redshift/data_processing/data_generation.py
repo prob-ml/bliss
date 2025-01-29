74bb21e85d5470c4a72bcb3f2c9e801fd9f027bd
Declan McNamara <declan@umich.edu>
2025-01-29 10:18:08 -0500

    tweaks

--- case_studies/redshift/data_processing/data_generation.py
+++ case_studies/redshift/data_processing/data_generation.py
@@ -1,23 +1,22 @@
-import os
-os.environ['OMP_NUM_THREADS'] = '8'
-os.environ['MKL_NUM_THREADS'] = '8'
-os.environ['NUMEXPR_NUM_THREADS'] = '8'
-
-from os import environ
-from pathlib import Path
 import logging
+import os
+import pickle
+
+os.environ["OMP_NUM_THREADS"] = "8"
+os.environ["MKL_NUM_THREADS"] = "8"
+os.environ["NUMEXPR_NUM_THREADS"] = "8"
+
+
+from pathlib import Path
 
 import GCRCatalogs
-import pandas as pd
-import torch
-from hydra import compose, initialize
-from hydra.utils import instantiate
-import pickle
-import numpy as np
-from bliss.catalog import SourceType
-from omegaconf import DictConfig
-from bliss.surveys.dc2 import DC2DataModule, wcs_from_wcs_header_str
 import hydra
+import numpy as np
+import pandas as pd
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+
+from bliss.surveys.dc2 import DC2DataModule
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,9 +24,9 @@ logging.basicConfig(level=logging.INFO)
 def create_rail_artifacts(rail_cfg: DictConfig):
     """Create DataFrames of ugrizy magnitudes and errors for RAIL training.
     """
-    logging.info("Creating RAIL artifacts at %s", rail_cfg.paths.output_dir)
+    logging.info("Creating RAIL artifacts at %s", rail_cfg.paths.processed_data_dir)
     log_dir = Path(rail_cfg.paths.processed_data_dir)
-    
+
     # Create output directory if it does not exist, or skip if artifacts already exist
     if not log_dir.exists():
         log_dir.mkdir(parents=True, exist_ok=True)
@@ -39,7 +38,7 @@ def create_rail_artifacts(rail_cfg: DictConfig):
     lsst_root_dir = rail_cfg.pipeline.lsst_root_dir
     GCRCatalogs.set_root_dir(lsst_root_dir)
     lsst_catalog_gcr = GCRCatalogs.load_catalog(rail_cfg.pipeline.truth_match_catalog)
-    lsst_catalog_subset = lsst_catalog_gcr.get_quantities(rail_cfg.pipeline.quantities)
+    lsst_catalog_subset = lsst_catalog_gcr.get_quantities(list(rail_cfg.pipeline.quantities))
     lsst_catalog_df = pd.DataFrame(lsst_catalog_subset)
 
     # Drop rows with inf or NaN
@@ -65,11 +64,11 @@ def create_rail_artifacts(rail_cfg: DictConfig):
 
     lsst_catalog_df_nona_newname = lsst_catalog_df_nona.rename(new_name, axis=1)
 
-    # Save pickle for RAIL-based training. 
+    # Save pickle for RAIL-based training.
     train_nrow = rail_cfg.pipeline.train_size
     val_nrow = rail_cfg.pipeline.val_size
     lsst_catalog_df_nona_newname[:train_nrow].to_pickle(log_dir / "lsst_train_nona_200k.pkl")
-    lsst_catalog_df_nona_newname[-1-val_nrow -1].to_pickle(log_dir / "lsst_val_nona_100k.pkl")
+    lsst_catalog_df_nona_newname[-1-val_nrow -1:].to_pickle(log_dir / "lsst_val_nona_100k.pkl")
     return
 
 
@@ -78,7 +77,7 @@ def create_rail_artifacts(rail_cfg: DictConfig):
 def create_bliss_artifacts(bliss_cfg: DictConfig):
     """CONSTRUCT BATCHES (.pt files) FOR DATA LOADING
     """
-    logging.info("Creating BLISS artifacts at %s", bliss_cfg.paths.processed_output_dir)
+    logging.info("Creating BLISS artifacts at %s", bliss_cfg.paths.processed_data_dir)
     dc2: DC2DataModule = instantiate(bliss_cfg.surveys.dc2)
     dc2.prepare_data()
     return
@@ -88,7 +87,12 @@ def create_bliss_artifacts(bliss_cfg: DictConfig):
 @hydra.main(config_path=".", config_name="data")
 def main(cfg: DictConfig) -> None:
     logging.info("Starting data generation")
-    logging.info(cfg.pretty())
+    logging.info(OmegaConf.to_yaml(cfg))
+
+    # print(cfg.rail.pipeline.quantities)
+    # print(type(cfg.rail.pipeline.quantities))
+    # print(type(cfg.rail.pipeline.quantities[0]))
+    # return
 
     # Create RAIL artifacts
     create_rail_artifacts(cfg.rail)
@@ -100,4 +104,3 @@ def main(cfg: DictConfig) -> None:
 
 if __name__ == "__main__":
     main()
-
