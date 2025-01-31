bbdc12356f7ee52bf00aa085802177ce0e00f47c
Declan McNamara <declan@umich.edu>
2025-01-31 13:55:07 -0500

    stash

--- case_studies/redshift/runner.sh
+++ case_studies/redshift/runner.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export OUT_DIR="/data/scratch/declan/redshift/dc2"
+export OMP_NUM_THREADS="16"
+export MKL_NUM_THREADS="16"
+export NUMEXPR_NUM_THREADS="16"
 
 # Produce data artifacts 
 echo "producing data artifacts for BLISS and RAIL from DC2"
@@ -16,9 +19,11 @@ else
   echo "BLISS training logs/checkpoints will be saved to $OUT_DIR/$DIRNAME"
 fi
 
-nohup bliss -cp ~/bliss/case_studies/redshift/redshift_from_img -cn full_train_config_redshift > "$OUT_DIR/$DIRNAME/output.out" 2>&1 &
+nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn continuous > "$OUT_DIR/$DIRNAME/output.out" 2>&1 &
 
 # Run BLISS (continuous variational distribution)
+DIRNAME="BLISS_DC2_redshift_continuous_results"
+
 if [ ! -d "$OUT_DIR/$DIRNAME" ]; then
   mkdir -p "$OUT_DIR/$DIRNAME"
   echo "BLISS training logs/checkpoints will be saved to $OUT_DIR/$DIRNAME"
@@ -26,7 +31,7 @@ else
   echo "BLISS training logs/checkpoints will be saved to $OUT_DIR/$DIRNAME"
 fi
 
-nohup bliss -cp ~/bliss/case_studies/redshift/redshift_from_img -cn full_train_config_redshift > "$OUT_DIR/$DIRNAME/output.out" 2>&1 &
+nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn discrete > "$OUT_DIR/$DIRNAME/output.out" 2>&1 &
 
 # Run RAIL
 # TODO
