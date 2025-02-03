a4df3aed1c96dd06092180138b7f18b4b5594d82
Declan McNamara <declan@umich.edu>
2025-02-03 12:49:45 -0500

    linting

--- case_studies/weak_lensing/lensing_dc2.py
+++ case_studies/weak_lensing/lensing_dc2.py
@@ -156,7 +156,7 @@ class LensingDC2DataModule(DC2DataModule):
 
     # override generate_cached_data
     def generate_cached_data(self, naming_info):
-        image_index, patch_name = naming_info
+        image_index, _ = naming_info
         result_dict = self.load_image_and_catalog(image_index)
 
         image = result_dict["inputs"]["image"]
