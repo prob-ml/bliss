427154e7096f84df657b1b826dd7e1341b6d310a
Declan McNamara <declan@umich.edu>
2025-02-20 13:24:57 -0500

    stashing here b/c this was a good set of checkpoints for BLISS + Discrete + Low Risk bin; we let it run with the learning rate 1e-2 until OOM at 42 epochs due to memory leak. For reference, cts val MSE was 0.130 or so, disc was 0.175 or so

--- case_studies/redshift/runner.sh
+++ case_studies/redshift/runner.sh
@@ -5,39 +5,39 @@ export OMP_NUM_THREADS="16"
 export MKL_NUM_THREADS="16"
 export NUMEXPR_NUM_THREADS="16"
 
-# Produce data artifacts
-echo "producing data artifacts for BLISS and RAIL from DC2"
-python artifacts/data_generation.py
+# # Produce data artifacts
+# echo "producing data artifacts for BLISS and RAIL from DC2"
+# python artifacts/data_generation.py
 
 # Run BLISS (discrete variational distribution)
-DIRNAME="$OUT_DIR/discrete"
+# DIRNAME="$OUT_DIR/discrete"
 
-if [ ! -d "$DIRNAME" ]; then
-  mkdir -p "$DIRNAME"
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-else
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-fi
+# if [ ! -d "$DIRNAME" ]; then
+#   mkdir -p "$DIRNAME"
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# else
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# fi
 
-nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn discrete > "$DIRNAME/output.out" 2>&1 &
+# nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn discrete > "$DIRNAME/output.out" 2>&1 &
 
 # Run BLISS (continuous variational distribution)
-DIRNAME="$OUT_DIR/continuous"
+# DIRNAME="$OUT_DIR/continuous"
 
-if [ ! -d "$DIRNAME" ]; then
-  mkdir -p "$DIRNAME"
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-else
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-fi
+# if [ ! -d "$DIRNAME" ]; then
+#   mkdir -p "$DIRNAME"
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# else
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# fi
 
-nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn continuous > "$DIRNAME/output.out" 2>&1 &
+# nohup python bliss/main.py -cp ~/bliss/case_studies/redshift/redshift_from_img -cn continuous > "$DIRNAME/output.out" 2>&1 &
 
-# # Run RAIL
-# # TODO
+# # # Run RAIL
+# # # TODO
 
-# # Create plots
-echo "creating plots for BLISS and RAIL from DC2"
-python evaluation/evaluate_cts.py
+# # # Create plots
+# echo "creating plots for BLISS and RAIL from DC2"
+#python evaluation/evaluate_cts.py
 python evaluation/evaluate_discrete.py
-python evaluation/plots_bliss.py
+# python evaluation/plots_bliss.py
