5b417cafa3d08798c0bdd9a58192894ef4f05fd6
Declan McNamara <declan@umich.edu>
2025-03-14 12:14:45 -0400

    tweak

--- case_studies/redshift/runner.sh
+++ case_studies/redshift/runner.sh
@@ -6,20 +6,20 @@ export MKL_NUM_THREADS="16"
 export NUMEXPR_NUM_THREADS="16"
 
 # # Produce data artifacts
-# echo "producing data artifacts for BLISS and RAIL from DC2"
-# python artifacts/data_generation.py
+echo "producing data artifacts for BLISS and RAIL from DC2"
+python artifacts/data_generation.py
 
 # Run BLISS (discrete variational distribution)
-# DIRNAME="$OUT_DIR/discrete"
+DIRNAME="$OUT_DIR/discrete"
 
-# if [ ! -d "$DIRNAME" ]; then
-#   mkdir -p "$DIRNAME"
-#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-# else
-#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-# fi
+if [ ! -d "$DIRNAME" ]; then
+  mkdir -p "$DIRNAME"
+  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+else
+  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+fi
 
-# nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_discrete > "$DIRNAME/output.out" 2>&1 &
+nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_discrete > "$DIRNAME/output.out" 2>&1 &
 
 # Run BLISS (continuous variational distribution)
 DIRNAME="$OUT_DIR/continuous"
@@ -37,7 +37,7 @@ nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_contin
 # # # TODO
 
 # # # Create plots
-# echo "creating plots for BLISS and RAIL from DC2"
-#python evaluation/evaluate_cts.py
-# python evaluation/evaluate_discrete.py
-# python evaluation/plots_bliss.py
+echo "creating plots for BLISS and RAIL from DC2"
+python evaluation/evaluate_cts.py
+python evaluation/evaluate_discrete.py
+python evaluation/plots_bliss.py
