6bc4883a9e15fc77d10474d83794851130c7c7a7
Jackson Loper <jacksonloper@users.noreply.github.com>
2025-03-19 13:52:30 -0400

    Jl/redshiftrail (#1083)
    * Code for training flexzboost
    
    * augmented script with "cells" (# %% notation)
    
    * Code for efficiently evaluating grid-search-losses with flexzboost predictions
    
    * A few more docstrings
    
    * code for testing rail
    
    * fixed some bugs, removed flexzboost data creation from data_generation.py
    
    * added data generation for flexzboost
    
    * address WPS473 (which wasn't found locally?)
    
    * updated pylint
    
    * raise exception if match_gating isn't specified correctly
    
    * black fix
    
    ---------
    
    Co-authored-by: Jackson Loper <jaloper@umich.edu>

---
+++ case_studies/redshift/redshift_flexzboost/train_rail.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import numpy as np
+import rail  # pylint: disable=import-error
+from hydra import compose, initialize
+from rail.core.stage import RailStage  # pylint: disable=import-error
+from rail.estimation.algos.flexzboost import FlexZBoostInformer  # pylint: disable=import-error
+
+RailStage.data_store.__class__.allow_overwrite = True
+
+# %% configs
+with initialize(config_path="../", version_base=None):
+    notebook_cfg = compose("redshift_flexzboost")
+rail_dir = Path(notebook_cfg.paths["processed_data_dir_rail"])
+out_model_fn = rail_dir / "flexzboost_model_results.pkl"
+
+# parameters foroptimization
+fz_dict = {
+    "zmin": 0.0,
+    "zmax": 3.0,
+    "nzbins": 301,
+    "trainfrac": 0.75,
+    "bumpmin": 0.02,
+    "bumpmax": 0.35,
+    "nbump": 20,
+    "sharpmin": 0.7,
+    "sharpmax": 2.1,
+    "nsharp": 15,
+    "max_basis": 35,
+    "basis_system": "cosine",
+    "hdf5_groupname": None,
+    "nondetect_val": np.nan,
+    "regression_params": {"max_depth": 8, "objective": "reg:squarederror"},
+}
+
+# %% load training data
+training_data = RailStage.data_store.read_file(
+    "training_data", rail.core.data.Hdf5Handle, rail_dir / "train_df.hdf5"
+)
+
+# %% do the training
+inform_pzflex = FlexZBoostInformer.make_stage(name="inform_fzboost", model=out_model_fn, **fz_dict)
+inform_pzflex.inform(training_data)
