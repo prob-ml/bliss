3f42b88134e2cee39e17156c1757e4260ed22df9
Yicun Duan <pduan@deeplearning-01.stat.lsa.umich.edu>
2025-04-14 14:01:22 -0400

    update config and scripts

--- case_studies/dc2_mdt/run_dc2_mdt.sh
+++ case_studies/dc2_mdt/run_dc2_mdt.sh
@@ -4,3 +4,5 @@ bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_net_train_config
 bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_net_speed_train_config
 bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_ar_net_train_config
 bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_cond_true_net_train_config
+# run inference
+nohup jupyter nbconvert --execute --to html case_studies/dc2_mdt/inference.ipynb > infer_notebook.out 2>&1 &
