51b4d89c08fc0ebbda2ca6c9882135119d9dd407
Yicun Duan <pduan@deeplearning-01.stat.lsa.umich.edu>
2025-04-14 21:00:01 -0400

    add multi gpus infer

--- case_studies/dc2_mdt/run_dc2_mdt.sh
+++ case_studies/dc2_mdt/run_dc2_mdt.sh
@@ -6,3 +6,13 @@ bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_ar_net_train_config
 bliss -cp ~/bliss/case_studies/dc2_mdt/mdt_config -cn simple_cond_true_net_train_config
 # run inference
 nohup jupyter nbconvert --execute --to html case_studies/dc2_mdt/inference.ipynb > infer_notebook.out 2>&1 &
+
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=2 \
+                                multi_gpus_infer.py \
+                                --model-tag-name="mdt" \
+                                --exp-name="exp_04-12-1" \
+                                --exp-check-point-name="encoder_101.ckpt" \
+                                --cfg-name="mdt_train_config" \
+                                --cached-data-path="/data/scratch/pduan/posterior_cached_files" \
+                                --infer-batch-size=400 \
+                                --infer-total-iters=500
