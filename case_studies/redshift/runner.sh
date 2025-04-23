15051bff7489d645b40ebc795bf38ca19bf4531c
Declan McNamara <declan@umich.edu>
2025-04-22 23:35:17 -0400

    again, weird naming with the mse metric threw off the runs. Running bspline and mdn again

--- case_studies/redshift/runner.sh
+++ case_studies/redshift/runner.sh
@@ -4,7 +4,7 @@ export OUT_DIR="/data/scratch/declan/redshift/dc2/training_logs"
 export OMP_NUM_THREADS="16"
 export MKL_NUM_THREADS="16"
 export NUMEXPR_NUM_THREADS="16"
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=3
 
 timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
 
@@ -13,16 +13,16 @@ timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
 # python artifacts/data_generation.py
 
 # Run BLISS (discrete variational distribution)
-DIRNAME="$OUT_DIR/discrete_$timestamp"
+# DIRNAME="$OUT_DIR/discrete_$timestamp"
 
-if [ ! -d "$DIRNAME" ]; then
-  mkdir -p "$DIRNAME"
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-else
-  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-fi
+# if [ ! -d "$DIRNAME" ]; then
+#   mkdir -p "$DIRNAME"
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# else
+#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+# fi
 
-nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_discrete timestamp=$timestamp > "$DIRNAME/output.out" 2>&1 &
+# nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_discrete timestamp=$timestamp > "$DIRNAME/output.out" 2>&1 &
 
 # Run BLISS (continuous variational distribution)
 # DIRNAME="$OUT_DIR/continuous_$timestamp"
@@ -36,16 +36,16 @@ nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_discre
 
 # nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_continuous timestamp=$timestamp > "$DIRNAME/output.out" 2>&1 &
 
-# DIRNAME="$OUT_DIR/bspline_$timestamp"
+DIRNAME="$OUT_DIR/bspline_$timestamp"
 
-# if [ ! -d "$DIRNAME" ]; then
-#   mkdir -p "$DIRNAME"
-#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-# else
-#   echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
-# fi
+if [ ! -d "$DIRNAME" ]; then
+  mkdir -p "$DIRNAME"
+  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+else
+  echo "BLISS training logs/checkpoints will be saved to $DIRNAME"
+fi
 
-# nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_bspline > "$DIRNAME/output.out" 2>&1 &
+nohup python bliss/main.py -cp ~/bliss/case_studies/redshift -cn redshift_bspline > "$DIRNAME/output.out" 2>&1 &
 
 # DIRNAME="$OUT_DIR/mdn_$timestamp"
 
