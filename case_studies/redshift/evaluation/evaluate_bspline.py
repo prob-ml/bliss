9dd854c259e21bbf233373dc94e07d7607b685a3
Declan McNamara <declan@umich.edu>
2025-04-25 11:36:58 -0400

    worked for evaluation; seems bpsline better than mdn on brighter objects; going to trim metrics to redshift 0-1 or simliar

---
+++ case_studies/redshift/evaluation/evaluate_bspline.py
@@ -0,0 +1,68 @@
+# pylint: disable=R0801
+import pickle
+from pathlib import Path
+
+import hydra
+import torch
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+from tqdm import tqdm
+from hydra import compose, initialize
+
+
+def get_kth_best_ckpt(ckpt_dir: str, k: int = 0):
+    ckpt_dir = Path(ckpt_dir)
+    ckpt_files = list(ckpt_dir.glob("*.ckpt"))
+    sorted_files = sorted(ckpt_files, key=lambda f: float(f.stem.split("_")[1]))
+    if sorted_files:
+        return sorted_files[k]
+
+    raise FileExistsError("No ckpt files found in the directory")
+
+
+@hydra.main(config_path=".", config_name="bspline_eval")
+def main(cfg: DictConfig):
+    # with initialize(config_path=".", job_name="bspline_eval"):
+    #     cfg = compose(config_name="bspline_eval")
+    k = 0
+    output_dir = cfg.paths.plot_dir
+    ckpt_dir = cfg.paths.ckpt_dir
+    run_name = ckpt_dir.split("/")[-2]
+
+    output_dir = Path(output_dir) / run_name
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    ckpt_path = get_kth_best_ckpt(ckpt_dir, k)
+    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+
+    # set up testing dataset
+    dataset = instantiate(cfg.train.data_source)
+    dataset.setup("test")
+
+    # load bliss trained model - continuous version
+    bliss_encoder = instantiate(cfg.encoder).to(device=device)
+    pretrained_weights = torch.load(ckpt_path, device)["state_dict"]
+    bliss_encoder.load_state_dict(pretrained_weights)
+    bliss_encoder.eval()
+
+    # load bliss trained model - continuous version
+    bliss_output_path = output_dir / "bspline_mode_metrics_{}thbest_new_bins.pkl".format(k)
+
+    # compute metrics -- continuous version
+    if not bliss_output_path.exists() or True:
+        test_loader = dataset.test_dataloader()
+        for batch_idx, batch in tqdm(enumerate(test_loader), total=len(test_loader)):
+            batch["images"] = batch["images"].to(device)
+            batch["tile_catalog"] = {
+                key: value.to(device) for key, value in batch["tile_catalog"].items()
+            }
+            batch["psf_params"] = batch["psf_params"].to(device)
+            bliss_encoder.update_metrics(batch, batch_idx)
+        bliss_out_dict = bliss_encoder.mode_metrics.compute()
+
+        with open(bliss_output_path, "wb") as outp:  # Overwrites any existing file.
+            pickle.dump(bliss_out_dict, outp, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    main()
--- case_studies/redshift/evaluation/plots_bliss.py
+++ case_studies/redshift/evaluation/plots_bliss.py
@@ -21,6 +21,14 @@ def main(cfg: DictConfig):
     bliss_discrete_output_path = output_dir / disc_run_name / "discrete_mode_metrics_0thbest.pkl"
     bliss_discrete_grid_output_path = output_dir / disc_run_name / "discrete_grid_metrics_0thbest.pkl"
 
+    # Get bspline 
+    bspline_run_name = "bspline_split_0_2025-04-22-23-17-28"
+    bliss_bspline_output_path = output_dir / bspline_run_name / "bspline_mode_metrics_0thbest_new_bins.pkl"
+
+    # Get mdn
+    mdn_run_name = "mdn_split_0_2025-04-22-23-17-15"
+    bliss_mdn_output_path = output_dir / mdn_run_name / "mdn_mode_metrics_0thbest_new_bins.pkl"
+
     # Get discrete metrics
     with open(bliss_discrete_output_path, "rb") as inputp:
         bliss_mode_out_dict = pickle.load(inputp)
@@ -30,6 +38,14 @@ def main(cfg: DictConfig):
     # Do continuous metrics
     with open(bliss_output_path, "rb") as inputp:
         bliss_out_dict = pickle.load(inputp)
+
+    # Do bspline metrics
+    with open(bliss_bspline_output_path, "rb") as inputp:
+        bliss_bspline_out_dict = pickle.load(inputp)
+    # Do mdn metrics
+    with open(bliss_mdn_output_path, "rb") as inputp:
+        bliss_mdn_out_dict = pickle.load(inputp)
+
 
     metrics = ["outlier_fraction_cata", "outlier_fraction", "l1", "mse"]
     metric_labels = [
@@ -49,10 +65,11 @@ def main(cfg: DictConfig):
 
     rail_results = pd.read_csv("/data/scratch/declan/redshift/dc2/checkpoints/rail/results.csv")
 
-
+    save_dir = output_dir / run_name / "mdnb"
+    save_dir.mkdir(parents=True, exist_ok=True)
     for this_metric in metric_names:
         save_as = this_metric.split("/")[0]
-        save_name = output_dir / run_name/ f"{save_as}_{run_name}_log.pdf"
+        save_name = save_dir / f"{save_as}_{run_name}_log.pdf"
 
         # Get loss type mapping to rail
         if "L1" in this_metric:
@@ -81,18 +98,23 @@ def main(cfg: DictConfig):
         bliss_discrete_grid = [
             bliss_discrete_out_dict[f"{this_metric}_{i}"] for i in range(num_values)
         ]
+        bliss_bspline = [bliss_bspline_out_dict[f"{this_metric}_{i}"] for i in range(num_values)]
+        bliss_mdn = [bliss_mdn_out_dict[f"{this_metric}_{i}"] for i in range(num_values)]
 
         plt.figure(figsize=(6, 6))
-        plt.plot(bin_labels, bliss_values, label="BLISS+Normal", marker="o", c="blue")
-        plt.plot(bin_labels, bliss_discrete, label="BLISS+Discrete Bin", marker="o", c="green")
-        plt.plot(
-            bin_labels,
-            bliss_discrete_grid,
-            label="BLISS+Discrete Bin w/ Grid Search",
-            marker="o",
-            c="orange",
-        )
-        plt.plot(bin_labels, rail_nums["loss"].values, label="FlexZBoost", marker="o", c="red")
+        # plt.plot(bin_labels, bliss_values, label="BLISS+Normal", marker="o", c="blue")
+        # plt.plot(bin_labels, bliss_discrete, label="BLISS+Discrete Bin", marker="o", c="green")
+        # plt.plot(
+        #     bin_labels,
+        #     bliss_discrete_grid,
+        #     label="BLISS+Discrete Bin w/ Grid Search",
+        #     marker="o",
+        #     c="orange",
+        # )
+        # plt.plot(bin_labels, rail_nums["loss"].values, label="FlexZBoost", marker="o", c="red")
+        plt.plot(bin_labels, bliss_bspline, label="BLISS+BSpline", marker="o", c="aqua")
+        plt.plot(bin_labels, bliss_mdn, label="BLISS+MDN", marker="o", c="fuchsia")
+
         xlabel = "Redshift Bin" if "rs" in this_metric else "Magnitude Bin"
         ylabel = None
         for i, metric in enumerate(metrics):
