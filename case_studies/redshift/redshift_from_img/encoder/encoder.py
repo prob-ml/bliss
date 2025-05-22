f96129ca1eefb761f9354c7457accddd799112d3
Declan McNamara <declan@umich.edu>
2025-05-22 10:23:06 -0400

    moved evaluation code to encoder; this should let us entirely remove evaluation scripts. Going to debug on bspline first

--- case_studies/redshift/redshift_from_img/encoder/encoder.py
+++ case_studies/redshift/redshift_from_img/encoder/encoder.py
@@ -13,6 +13,11 @@ import pyarrow as pa
 import pandas as pd
 import pyarrow.parquet as pq
 import einops
+import logging
+import pyarrow as pa
+import pandas as pd
+import pyarrow.parquet as pq
+logging.basicConfig(level=logging.INFO)
 
 
 def get_best_ckpt(ckpt_dir: str):
@@ -27,14 +32,22 @@ def get_best_ckpt(ckpt_dir: str):
 class RedshiftsEncoder(Encoder):
     def __init__(
         self,
+        checkpoint_dir: str,
+        plot_dir: str,
+        eval_from_checkpoint: bool,
         discrete_metrics,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.discrete_metrics = discrete_metrics
+        self.run_name = checkpoint_dir.split("/")[-2]
+        self.ckpt_dir = checkpoint_dir if eval_from_checkpoint else None
+        self.plot_dir = plot_dir
+        self.writer = None # writer to save predictions to parquet
 
     def save_preds(self, batch, batch_idx, use_mode=True, writer=None):
+        """Called within a test loop to save predictions to disk."""
         if writer is None:
             raise ValueError("Provide a writer to save predictions")
         if not use_mode:
@@ -82,27 +95,25 @@ class RedshiftsEncoder(Encoder):
 
 
     def update_metrics(self, batch, batch_idx):
+        # Checkpointing metrics must be computable with target/mode cat, matching, and loss.
+
+        # Get target and mode catalogs and matching
         target_cat = TileCatalog(batch["tile_catalog"]).get_brightest_sources_per_tile()
-
-        # for risk_type in self.discrete_metrics.keys():
-        #     mode_cat = self.discrete_sample(batch, use_mode=True, risk_type=risk_type)
-        #     matching = self.matcher.match_catalogs(target_cat, mode_cat)
-        #     self.discrete_metrics[risk_type].update(target_cat, mode_cat, matching)
-
         mode_cat = self.sample(batch, use_mode=True)
+        # sample_cat = self.sample(batch, use_mode=False) # unused right now
         matching = self.matcher.match_catalogs(target_cat, mode_cat)
+
+        
+        # Get NLL - TODO: deduplicate from Encoder._compute_loss
         patterns_to_use = torch.randperm(15)[:4] if self.use_checkerboard else (0,)
         history_mask_patterns = self.mask_patterns[patterns_to_use, ...]
-
         loss_mask_patterns = 1 - history_mask_patterns
-
         loss = self.compute_masked_nll(batch, history_mask_patterns, loss_mask_patterns) # b x n_tile x n_tile
         loss = einops.rearrange(loss, "b l w -> b l w 1 1") 
+
+        # Update metrics
         self.mode_metrics.update(target_cat, mode_cat, matching, loss)
 
-        # sample_cat = self.sample(batch, use_mode=False)
-        # matching = self.matcher.match_catalogs(target_cat, sample_cat)
-        # self.sample_metrics.update(target_cat, sample_cat, matching)
 
     def on_validation_epoch_end(self):
         # https://github.com/Lightning-AI/pytorch-lightning/discussions/2529
@@ -117,6 +128,71 @@ class RedshiftsEncoder(Encoder):
             self.sample_metrics.reset()
         if self.discrete_metrics:  # TODO: should be same as above, not empty dict
             self.discrete_metrics.reset()
+
+    def on_test_epoch_start(self):
+        # Optionally load the best checkpoint, else use current weights
+        if self.ckpt_dir is not None:
+            ckpt_dir = Path(self.ckpt_dir)
+            ckpt_files = list(ckpt_dir.glob("*.ckpt"))
+            sorted_files = sorted(ckpt_files, key=lambda f: float(f.stem.split("_")[1]))
+            if sorted_files:
+                logging.info(f"Loading checkpoint {sorted_files[0]}")
+                ckpt_to_load = sorted_files[0]
+            else:
+                logging.info(f"No checkpoint found in {ckpt_dir}")
+                raise FileExistsError("No ckpt files found in the directory")
+            
+            if ckpt_to_load.exists():
+                checkpoint = torch.load(ckpt_to_load, map_location=self.device)
+                self.load_state_dict(checkpoint["state_dict"])
+                self.eval()
+            else:
+                logging.info(f"Checkpoint {ckpt_to_load} does not exist")
+                raise FileExistsError("Specified checkpoint does not exist")
+            
+        else:
+            logging.info("No checkpoint directory specified, using current weights")
+
+        # Create parquet file for saving predictions
+        output_dir = Path(self.plot_dir) / self.run_name
+        bliss_output_path = output_dir / "predictions.parquet"
+        file_path = Path(bliss_output_path)
+        if not file_path.exists():
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        columns = [
+            "z_true",
+            "z_pred",
+            "nll_true",
+            "u_mag",
+            "g_mag",
+            "r_mag",
+            "i_mag",
+            "z_mag",
+            "y_mag",
+        ]
+        empty_df = pd.DataFrame(columns=columns, dtype='float32')
+        dummy_table = pa.Table.from_pandas(empty_df, preserve_index=False)
+        self.writer = pq.ParquetWriter(bliss_output_path, dummy_table.schema, use_dictionary=True)
+
+
+    def test_step(self, batch, batch_idx):
+        """Pytorch lightning method."""
+        self._compute_loss(batch, "test")
+        self.update_metrics(batch, batch_idx)
+        if self.writer is None:
+            logging.warning("Writer not initialized, cannot save predictions")
+            return
+        else:
+            self.save_preds(batch, batch_idx, use_mode=True, writer=self.writer)
+
+    def on_test_epoch_end(self):
+        # note: metrics are not reset here, to give notebooks access to them
+        self.report_metrics(self.mode_metrics, "test/mode", show_epoch=False)
+        if self.sample_metrics is not None:
+            self.report_metrics(self.sample_metrics, "test/sample", show_epoch=False)
+        if self.writer is not None:
+            self.writer.close()
 
     def get_features_and_parameters(self, batch):
         batch = (
