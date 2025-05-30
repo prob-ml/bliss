a867bdeeb8b8aa436b30bed3e5f549c96705a1cc
Declan McNamara <73667873+declanmcnamara@users.noreply.github.com>
2025-05-30 17:36:30 -0400

    Cleaned BLISS redshift functionality with 4 different variational families + logging.  (#1088)
    * merging into master
    
    * restore what should be untouched
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * linting
    
    * fixed a small typo

---
+++ case_studies/redshift/splinex/test.py
@@ -0,0 +1,25 @@
+import math
+
+import torch
+
+from case_studies.redshift.splinex.bspline import DeclanBSpline
+
+
+min_val = 0.0
+max_val = 2 * math.pi
+nknots = 40
+degree = 3
+n_grid_points = 100
+my_spline = DeclanBSpline(
+    min_val=min_val,
+    max_val=max_val,
+    nknots=nknots,
+    n_grid_points=n_grid_points,
+    degree=degree,
+)
+
+
+coeffs = torch.zeros((5, nknots), dtype=torch.float32)
+coeffs[0, 3] = 1.0
+
+spline_curve_vals, _, _ = my_spline(coeffs.T)
