69e436d8b34ba19bbc4a53aed03f416711c88637
Ismael Mendoza <imendoza@umich.edu>
2025-07-17 10:57:25 -0400

    keep homedir

--- bliss/__init__.py
+++ bliss/__init__.py
@@ -1,5 +1,3 @@
 from pathlib import Path
 
 HOME_DIR = Path(__file__).parent.parent
-DATASETS_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/datasets/")
-CACHE_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/cache/")
