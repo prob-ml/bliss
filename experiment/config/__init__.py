e82e7c79ed9af951406390519c61778022956963
Ismael Mendoza <imendoza@umich.edu>
2025-07-17 10:58:54 -0400

    cache cache

---
+++ experiment/config/__init__.py
@@ -0,0 +1,9 @@
+"""Replace the following paths with where you want to save figures, datasets, etc."""
+
+from pathlib import Path
+
+DATASETS_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/datasets/")
+CACHE_DIR = Path("/nfs/turbo/lsa-regier/scratch/ismael/cache/")
+FIGURE_DIR = Path("/home/imendoza/bliss/experiment/figures")
+MODELS_DIR = Path("/home/imendoza/bliss/experiment/models")
+SEED = 52
