db76b7600d0529b51c49d253735f95294f2653b3
Tim White <twhit@umich.edu>
2025-09-23 12:03:28 -0400

    Add final layer flag to WeakLensingNet

--- case_studies/weak_lensing/convnet.py
+++ case_studies/weak_lensing/convnet.py
@@ -19,6 +19,7 @@ class WeakLensingNet(nn.Module):
         initial_downsample,
         more_up_layers,
         num_bottleneck_layers,
+        map_to_var_params=True,
     ):
         super().__init__()
 
@@ -46,7 +47,10 @@ class WeakLensingNet(nn.Module):
 
         self.resnet_blocks = nn.ModuleList(layers)
 
-        self.final_layer = Map(ch_final, n_var_params)
+        if map_to_var_params:
+            self.final_layer = Map(ch_final, n_var_params)
+        else:
+            self.final_layer = None
 
     def _make_layers(
         self,
@@ -111,6 +115,7 @@ class WeakLensingNet(nn.Module):
         for block in self.resnet_blocks:
             x = block(x)
 
-        x = self.final_layer(x)
+        if self.final_layer is not None:
+            x = self.final_layer(x)
 
         return x
